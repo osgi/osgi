897e979ba93625dbaaa5d6512ee2ce04624a6d63
Peter Kriens <peter.kriens@aqute.se>
2004-07-05 16:19:45 +0000

    *** empty log message ***

---
+++ org.osgi.test.cases.device/src/org/osgi/test/cases/device/DeviceTestActivator.java
@@ -0,0 +1,15 @@
+package org.osgi.test.cases.device;
+
+import org.osgi.test.cases.util.*;
+
+/**
+ * The activator of the device access test case
+ *
+ * @author ProSyst
+ * @version 1.0
+ */
+public class DeviceTestActivator extends DefaultTestCase{
+    public String getDescription()  { return "Device Access tests";  }
+    public String getName()         { return "test.cases.device"; }
+}
+
