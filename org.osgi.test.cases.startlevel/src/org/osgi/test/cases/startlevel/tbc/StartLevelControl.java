e03947cf36caee9043e5d8f11fb4612e39959295
Peter Kriens <peter.kriens@aqute.se>
2004-07-07 11:30:14 +0000

    *** empty log message ***

---
+++ org.osgi.test.cases.startlevel/src/org/osgi/test/cases/startlevel/tbc/StartLevelControl.java
@@ -0,0 +1,324 @@
+/*
+ * Copyright (c) The Open Services Gateway Initiative 2002.
+ * All Rights Reserved.
+ *
+ * Implementation of certain elements of the Open Services Gateway Initiative
+ * (OSGI) Specification may be subject to third party intellectual property
+ * rights, including without limitation, patent rights (such a third party may
+ * or may not be a member of OSGi). OSGi is not responsible and shall not be
+ * held responsible in any manner for identifying or failing to identify any or
+ * all such third party intellectual property rights.
+ *
+ * This document and the information contained herein are provided on an "AS
+ * IS" basis and OSGI DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION HEREIN WILL
+ * NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL OSGI BE LIABLE FOR ANY
+ * LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF USE OF DATA, INTERRUPTION OF
+ * BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR EXEMPLARY, INCIDENTIAL,
+ * PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN CONNECTION WITH THIS
+ * DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ *
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
+
+package org.osgi.test.cases.startlevel.tbc;
+
+import org.osgi.framework.*;
+import org.osgi.service.startlevel.*;
+import org.osgi.test.cases.util.*;
+
+public class StartLevelControl extends DefaultTestBundleControl implements FrameworkListener
+{
+
+  private StartLevel sl;
+  private BundleContext bc;
+  private boolean logStartLevelChanged = false;
+
+	String methods[] = {
+		"testInitialBundleStartLevel",
+		"testStartOrder",
+		"testSetStartLevel",
+		"testSetBundleStartLevel",
+		"testPersistentlyStarted",
+		"testEvents",
+		"testSystemBundle",
+		"testExceptionInActivator",
+		"testActivator",
+	};
+	
+	public String [] getMethods() { return methods; }
+	
+  public boolean checkPrerequisites()
+  {
+    return true;
+  }
+
+  public void prepare() throws Exception
+  {
+    sl = (StartLevel)getService(StartLevel.class);
+    getContext().addFrameworkListener(this);
+  }
+
+  public void unprepare() throws Exception {}
+  public void setState() throws Exception {}
+  public void clearState() throws Exception {}
+
+  public void testInitialBundleStartLevel() throws Exception
+  {
+    sl.setInitialBundleStartLevel(20);
+    assertEquals("getInitialBundleStartLevel", 20,  sl.getInitialBundleStartLevel());
+
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+
+    Bundle tb1 = getContext().installBundle(getWebServer() + "tb1.jar");
+    tb1.start();
+    assertEquals("bundle.getState", Bundle.RESOLVED, tb1.getState());
+
+    sl.setInitialBundleStartLevel(10);
+    Bundle tb2 = getContext().installBundle(getWebServer() + "tb2.jar");
+    tb2.start();
+    assertEquals("bundle.getState", Bundle.ACTIVE, tb2.getState());
+
+    tb1.uninstall();
+    tb2.stop();
+    tb2.uninstall();
+  }
+
+
+  public void testStartOrder() throws Exception
+  {
+    sl.setInitialBundleStartLevel(20);
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    Bundle tb1 = getContext().installBundle(getWebServer() + "tb1.jar");
+    tb1.start();
+    Bundle tb2 = getContext().installBundle(getWebServer() + "tb2.jar");
+    tb2.start();
+
+    //start tb1 and tb2
+    sl.setStartLevel(20);
+    Thread.sleep(1000);
+
+    //stop tb2 and tb1
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+
+    //reverse the start order
+    sl.setBundleStartLevel(tb2, 15);
+    Thread.sleep(1000);
+
+    //start tb2 and tb1
+    sl.setStartLevel(20);
+    Thread.sleep(1000);
+
+    //stop tb1 and tb2
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+
+    tb1.uninstall();
+    tb2.uninstall();
+  }
+
+  public void testSetStartLevel() throws Exception
+  {
+    sl.setInitialBundleStartLevel(15);
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    Bundle tb1 = getContext().installBundle(getWebServer() + "tb1.jar");
+
+    sl.setStartLevel(20);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", false, isActive(tb1));
+
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", false, isActive(tb1));
+
+    tb1.start();
+    assertEquals("bundle.getState", false, isActive(tb1));
+
+    sl.setStartLevel(20);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", true, isActive(tb1));
+
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", false, isActive(tb1));
+
+    tb1.stop();
+    sl.setStartLevel(20);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", false, isActive(tb1));
+
+    tb1.uninstall();
+  }
+
+  public void testSetBundleStartLevel() throws Exception
+  {
+    sl.setInitialBundleStartLevel(15);
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    Bundle tb1 = getContext().installBundle(getWebServer() + "tb1.jar");
+
+    sl.setBundleStartLevel(tb1, 5);
+    Thread.sleep(2000);
+    assertEquals("Startlevel 10/5 stop", false, isActive(tb1));
+
+    sl.setBundleStartLevel(tb1, 15);
+    Thread.sleep(2000);
+    assertEquals("StartLevel 10/15 stop", false, isActive(tb1));
+
+    tb1.start();
+    assertEquals("StartLevel 10/15 start", false, isActive(tb1));
+
+    sl.setBundleStartLevel(tb1, 5);
+    Thread.sleep(2000);
+    assertEquals("StartLevel 10/5 start", true, isActive(tb1));
+
+    sl.setBundleStartLevel(tb1, 15);
+    Thread.sleep(2000);
+    assertEquals("StartLevel 10/15 start", false, isActive(tb1));
+
+    sl.setBundleStartLevel(tb1, 5);
+    Thread.sleep(2000);
+    assertEquals("StartLevel 10/5 start", true, isActive(tb1));
+
+    tb1.stop();
+    tb1.uninstall();
+  }
+
+  public void testPersistentlyStarted() throws Exception
+  {
+    sl.setInitialBundleStartLevel(15);
+    assertEquals("setInitialBundleStartLevel", 15,  sl.getInitialBundleStartLevel());
+
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+
+    Bundle tb1 = getContext().installBundle(getWebServer() + "tb1.jar");
+    assertEquals("isBundlePersistentlyStarted", false, sl.isBundlePersistentlyStarted(tb1));
+
+    tb1.start();
+    assertEquals("isBundlePersistentlyStarted", true, sl.isBundlePersistentlyStarted(tb1));
+
+    sl.setStartLevel(20);
+    Thread.sleep(1000);
+    assertEquals("isBundlePersistentlyStarted", true, sl.isBundlePersistentlyStarted(tb1));
+
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    assertEquals("isBundlePersistentlyStarted", true, sl.isBundlePersistentlyStarted(tb1));
+
+    sl.setBundleStartLevel(tb1, 5);
+    Thread.sleep(2000);
+    assertEquals("isBundlePersistentlyStarted", true, sl.isBundlePersistentlyStarted(tb1));
+
+    sl.setBundleStartLevel(tb1, 15);
+    Thread.sleep(2000);
+    assertEquals("isBundlePersistentlyStarted", true, sl.isBundlePersistentlyStarted(tb1));
+
+    tb1.uninstall();
+  }
+
+  public void testEvents() throws Exception
+  {
+    logStartLevelChanged = true;
+    sl.setStartLevel(10);
+    Thread.sleep(2000);
+    sl.setStartLevel(20);
+    Thread.sleep(2000);
+    logStartLevelChanged = false;
+  }
+
+  public void testSystemBundle() throws Exception
+  {
+    Bundle systemBundle = getContext().getBundle(0);
+    assertEquals("getBundleStartLevel", 0,  sl.getBundleStartLevel(systemBundle));
+    try {
+      sl.setBundleStartLevel(systemBundle, 42);
+      log("got no IllegalArgumentException");
+    } catch (IllegalArgumentException iae) {
+    }
+  }
+
+
+  public void testExceptionInActivator() throws Exception
+  {
+    sl.setInitialBundleStartLevel(5);
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    Bundle tb5 = getContext().installBundle(getWebServer() + "tb5.jar");
+
+    try {
+      tb5.start();
+      assertEquals("bundle.getState", true, isActive(tb5));
+
+    } catch (Exception e) {
+    }
+
+    //FrameworkEvent.ERROR due to active startlevel change
+    sl.setStartLevel(4);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", false, isActive(tb5));
+    
+    //no FrameworkEvent.ERROR
+    sl.setStartLevel(5);
+    Thread.sleep(1000);
+    assertEquals("bundle.getState", true, isActive(tb5));
+    
+    
+    //FrameworkEvent.ERROR due to bundle startlevel change
+    sl.setBundleStartLevel(tb5, 6);
+    Thread.sleep(2000);
+    assertEquals("bundle.getState", false, isActive(tb5));
+    
+    tb5.uninstall();
+  }
+
+  public void testActivator() throws Exception
+  {
+    sl.setInitialBundleStartLevel(5);
+    sl.setStartLevel(10);
+    Thread.sleep(1000);
+    Bundle tb3 = getContext().installBundle(getWebServer() + "tb3.jar");
+    tb3.start();
+    Thread.sleep(2000);
+    assertEquals("getBundleStartLevel", 15,  sl.getBundleStartLevel(tb3));
+    tb3.uninstall();
+
+    Bundle tb4 = getContext().installBundle(getWebServer() + "tb4.jar");
+    tb4.start();
+    Thread.sleep(1000);
+    assertEquals("getStartLevel", 15,  sl.getStartLevel());
+
+    tb4.stop();
+    Thread.sleep(1000);
+    assertEquals("getStartLevel", 10,  sl.getStartLevel());
+
+    tb4.uninstall();
+  }
+
+  public void frameworkEvent(FrameworkEvent event)
+  {
+    switch (event.getType()) {
+    case FrameworkEvent.ERROR:
+      log("got framework event " + event.getType() + ": " + event.getThrowable().getClass().getName());
+      break;
+    case FrameworkEvent.STARTLEVEL_CHANGED:
+      if (logStartLevelChanged)
+        log("got framework event " + event.getType());
+      break;
+    }
+  }
+	
+	boolean isActive( Bundle b ) {
+		return (b.getState() & Bundle.ACTIVE)!=0;
+	}
+
+
+}
+
