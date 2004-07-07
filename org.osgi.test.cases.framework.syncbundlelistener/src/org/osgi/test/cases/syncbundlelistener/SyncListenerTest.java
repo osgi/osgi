ab13056e5ee5518d6deccfec505fa0ff1679646d
Peter Kriens <peter.kriens@aqute.se>
2004-07-07 11:40:39 +0000

    *** empty log message ***

---
+++ org.osgi.test.cases.framework.syncbundlelistener/src/org/osgi/test/cases/syncbundlelistener/SyncListenerTest.java
@@ -0,0 +1,24 @@
+/** 
+ * OSGi Test Suite Implementation. OSGi Confidential.
+ * (C) Copyright Ericsson Telecom AB. 2001.
+ * This source code is owned by Ericsson Telecom AB, and is being distributed to OSGi 
+ * MEMBERS as MEMBER LICENSED MATERIALS under the terms of section 3.2 of the OSGi MEMBER AGREEMENT. 
+ */
+
+package org.osgi.test.cases.syncbundlelistener;
+
+import org.osgi.test.cases.util.*;
+
+/**
+   A TestCase testing diverse issues.
+
+   @author Ericsson Telecom AB
+*/
+
+public class SyncListenerTest 
+	extends DefaultTestCase 
+{
+	public String getDescription() { return "Tests the synchronous bundle listener"; }
+	public String getName() { return "test.cases.syncbundlelistener"; }
+}
+
