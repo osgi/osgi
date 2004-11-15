e343fa2737f27d363da40823a11b3ca84b1e0c84
Peter Kriens <peter.kriens@aqute.se>
2004-11-15 09:40:55 +0000

    Cleanup

--- org.osgi.service.monitor/src/org/osgi/service/monitor/KPI.java
+++ org.osgi.service.monitor/src/org/osgi/service/monitor/KPI.java
@@ -1,150 +1,283 @@
 package org.osgi.service.monitor;
 
 import java.util.Date;
-import java.lang.UnsupportedOperationException;
 
 /**
- * A KPI object represents the value of a status variable taken with a certain
- * collection method at a certain point of time. The type of the KPI can be
- * integer, float or String. Additionally Object type is supported which can
- * hold any type supported by the OSGi Configuration Admin service. KPI objects
- * are immutable.
+ * A KPI object represents the value of a status variable taken with a
+ * certain collection method at a certain point of time. The type of
+ * the KPI can be integer, float or String.  Additionally Object type
+ * is supported which can hold any type supported by the OSGi
+ * Configuration Admin service. KPI objects are immutable.
  */
 public class KPI {
-	public static final int	TYPE_INTEGER	= 0;
-	public static final int	TYPE_FLOAT		= 1;
-	public static final int	TYPE_STRING		= 2;
-	public static final int	TYPE_OBJECT		= 3;
-	public static final int	CM_CC			= 0;
-	public static final int	CM_DER			= 1;
-	public static final int	CM_GAUGE		= 2;
-	public static final int	CM_SI			= 3;
+    //----- Public constants -----//
 
-	/**
-	 * Constructor for a KPI of integer type
-	 * 
-	 * @param cm Collection method, should be one of the CM_ constants
-	 * @param data The integer value of the KPI
-	 */
-	public KPI(int cm, int data) {
-	}
+    public static final int TYPE_INTEGER = 0;
+    public static final int TYPE_FLOAT   = 1;
+    public static final int TYPE_STRING  = 2;
+    public static final int TYPE_OBJECT  = 3;
+        
+    public static final int CM_CC    = 0;
+    public static final int CM_DER   = 1;
+    public static final int CM_GAUGE = 2;
+    public static final int CM_SI    = 3;
 
-	/**
-	 * Constructor for a KPI of float type
-	 * 
-	 * @param cm Collection method, should be one of the CM_ constants
-	 * @param data The float value of the KPI
-	 */
-	public KPI(int cm, float data) {
-	}
 
-	/**
-	 * Constructor for a KPI of String type
-	 * 
-	 * @param cm Collection method, should be one of the CM_ constants
-	 * @param data The string value of the KPI
-	 */
-	public KPI(int cm, String data) {
-	}
+    //----- Private fields -----//
 
-	/**
-	 * Constructor for a KPI of Object type
-	 * 
-	 * @param cm Collection method, should be one of the CM_ constants
-	 * @param data The value of the KPI. The type of the object should be
-	 *        supported by the Configuration Admin
-	 */
-	public KPI(int cm, Object data) {
-	}
+    private String id;
+    private String description;
+    private Date timeStamp;
+    private int cm;
+    private int type;
 
-	/**
-	 * Returns the name of this KPI. A KPI name is unique within the scope of a
-	 * Monitorable. A KPI names must not contain a '/' character.
-	 * 
-	 * @return the name of this KPI
-	 */
-	public String getID() {
-		return null;
-	}
+    private int intData;
+    private float floatData;
+    private String stringData;
+    private Object objectData;
 
-	/**
-	 * Returns a human readable description of this KPI. This can be used by
-	 * management systems on their GUI. Null return value is allowed.
-	 * 
-	 * @return the human readable description of this KPI or null if it is not
-	 *         set
-	 */
-	public String getDescription() {
-		return null;
-	}
 
-	/**
-	 * Returns information on the data type of this KPI
-	 * 
-	 * @return one value of the set of type constants
-	 */
-	public int getType() {
-		return 0;
-	}
+    //----- Constructors -----//
 
-	/**
-	 * Returns the time when the KPI value was queried. The KPI's value is set
-	 * when the getKpi() or getKpis() methods are called on the Monitorable
-	 * object.
-	 * 
-	 * @return the time when the KPI value was queried
-	 */
-	public Date getTimeStamp() {
-		return null;
-	}
+    /**
+     * Constructor for a KPI of integer type.
+     * @param id The identifier of the KPI
+     * @param description The human-readable description of the KPI
+     * @param cm Collection method, should be one of the CM_ constants
+     * @param data The integer value of the KPI
+     * @throws IllegalArgumentException if <code>id</code> contains
+     * invalid characters or if <code>cm</code> is not one of the
+     * collection method constants
+     * @throws NullPointerException if <code>id</code> is <code>null</code>
+     */
+    public KPI(String id, String description, int cm, int data) {
+        setCommon(id, description, cm);
+        type = TYPE_INTEGER;
+        intData = data;
+    }
+        
+    /**
+     * Constructor for a KPI of float type.
+     * @param id The identifier of the KPI
+     * @param description The human-readable description of the KPI
+     * @param cm Collection method, should be one of the CM_ constants
+     * @param data The float value of the KPI
+     * @throws IllegalArgumentException if <code>id</code> contains
+     * invalid characters or if <code>cm</code> is not one of the
+     * collection method constants
+     * @throws NullPointerException if <code>id</code> is <code>null</code>
+     */
+    public KPI(String id, String description, int cm, float data) {
+        setCommon(id, description, cm);
+        type = TYPE_FLOAT;
+        floatData = data;
+    }
+        
+    /**
+     * Constructor for a KPI of String type.
+     * @param id The identifier of the KPI
+     * @param description The human-readable description of the KPI
+     * @param cm Collection method, should be one of the CM_ constants
+     * @param data The string value of the KPI
+     * @throws IllegalArgumentException if <code>id</code> contains
+     * invalid characters or if <code>cm</code> is not one of the
+     * collection method constants
+     * @throws NullPointerException if <code>id</code> is <code>null</code>
+     */
+    public KPI(String id, String description, int cm, String data) {
+        setCommon(id, description, cm);
+        type = TYPE_STRING;
+        stringData = data;
+    }
+        
+    /**
+     * Constructor for a KPI of Object type.
+     * @param id The identifier of the KPI
+     * @param description The human-readable description of the KPI
+     * @param cm Collection method, should be one of the CM_ constants
+     * @param data The value of the KPI. The type of the object should
+     * be supported by the Configuration Admin
+     * @throws IllegalArgumentException if <code>id</code> contains
+     * invalid characters or if <code>cm</code> is not one of the
+     * collection method constants
+     * @throws NullPointerException if <code>id</code> is <code>null</code>
+     */
+    public KPI(String id, String description, int cm, Object data) {
+        setCommon(id, description, cm);
+        type = TYPE_OBJECT;
+        objectData = data;
 
-	/**
-	 * Returns the KPI value if it's type is string.
-	 * 
-	 * @return the KPI value as a string
-	 * @throws java.lang.UnsupportedOperationException if the type of KPI is not
-	 *         string
-	 */
-	public String getString() throws UnsupportedOperationException {
-		return null;
-	}
+        // TODO check that 'data' is really of a supported type
+    }
 
-	/**
-	 * @return the KPI value as an integer
-	 * @throws java.lang.UnsupportedOperationException
-	 */
-	public int getInteger() throws UnsupportedOperationException {
-		return 0;
-	}
 
-	/**
-	 * @return the KPI value as a float
-	 * @throws java.lang.UnsupportedOperationException
-	 */
-	public float getFloat() throws UnsupportedOperationException {
-		return 0;
-	}
+    //----- Public methods -----//
 
-	/**
-	 * Returns the KPI value if it's type is Object. All types supported by the
-	 * OSGi Configuration Admin service can be used. Exact type information can
-	 * be queried using getClass().getName()
-	 * 
-	 * @return the KPI value as an Object
-	 * @throws java.lang.UnsupportedOperationException if the type of KPI is not
-	 *         Object
-	 */
-	public Object getObject() throws UnsupportedOperationException {
-		return null;
-	}
+    /**
+     * Returns the name of this KPI.  A KPI name is unique within the
+     * scope of a Monitorable.  A KPI names must not contain a '/'
+     * character.
+     * @return the name of this KPI
+     */
+    public String getID() {
+        return id;
+    }
+        
+    /**
+     * Returns a human readable description of this KPI.  This can be
+     * used by management systems on their GUI. Null return value is
+     * allowed.
+     * @return the human readable description of this KPI or null if
+     * it is not set
+     */
+    public String getDescription() {
+        return description;
+    }
 
-	/**
-	 * Returns the collection method of this KPI See section 3.3 b) in [ETSI TS
-	 * 132 403]
-	 * 
-	 * @return one of the CM_ constants
-	 */
-	public int getCollectionMethod() {
-		return 0;
-	}
+    /**
+     * Returns information on the data type of this KPI.
+     * @return one value of the set of type constants
+     */
+    public int getType() {
+        return type;
+    }
+        
+    /**
+     * Returns the time when the KPI value was queried.  The KPI's
+     * value is set when the getKpi() or getKpis() methods are called
+     * on the Monitorable object.
+     * @return the time when the KPI value was queried
+     */
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+        
+    /**
+     * Returns the KPI value if its type is String.
+     * @return the KPI value as a string
+     * @throws java.lang.IllegalStateException if the type of the KPI
+     * is not String
+     */
+    public String getString() throws IllegalStateException {
+        if(type != TYPE_STRING)
+            throw new IllegalStateException(
+                "This KPI does not contain a String value.");
+
+        return stringData;
+    }
+        
+    /**
+     * Returns the KPI value if its type is integer.
+     * @return the KPI value as an integer
+     * @throws java.lang.IllegalStateException if the type of this KPI
+     * is not integer
+     */
+    public int getInteger() throws IllegalStateException {
+        if(type != TYPE_INTEGER)
+            throw new IllegalStateException(
+                "This KPI does not contain an integer value.");
+
+        return intData;
+    }
+        
+    /**
+     * Returns the KPI value if its type is float.
+     * @return the KPI value as a float
+     * @throws java.lang.IllegalStateException if the type of this KPI
+     * is not float
+     */
+    public float getFloat() throws IllegalStateException {
+        if(type != TYPE_FLOAT)
+            throw new IllegalStateException(
+                "This KPI does not contain a float value.");
+
+        return floatData;
+    }
+        
+    /**
+     * Returns the KPI value if its type is Object.  All types
+     * supported by the OSGi Configuration Admin service can be used.
+     * Exact type information can be queried using
+     * <code>getClass().getName()</code>.
+     * @return the KPI value as an Object
+     * @throws java.lang.IllegalStateException if the type of this KPI
+     * is not Object
+     */
+    public Object getObject() throws IllegalStateException {
+        if(type != TYPE_OBJECT)
+            throw new IllegalStateException(
+                "This KPI does not contain an Object type value.");
+
+        return objectData;
+    }
+        
+    /**
+     * Returns the collection method of this KPI.  See section 3.3 b)
+     * in [ETSI TS 132 403]
+     * @return one of the CM_ constants
+     */ 
+    public int getCollectionMethod() {
+        return cm;
+    }
+
+    public boolean equals(Object obj) {
+        if(!(obj instanceof KPI))
+            return false;
+
+        KPI other = (KPI) obj;
+
+        if(!equals(id, other.id) || !equals(description, other.description) ||
+           !equals(timeStamp, other.timeStamp) || cm != other.cm || 
+           type != other.type)
+            return false;
+
+        switch(type) {
+        case TYPE_INTEGER: return intData == other.intData;
+        case TYPE_FLOAT:   return floatData == other.floatData;
+        case TYPE_STRING:  return equals(stringData, other.stringData);
+        case TYPE_OBJECT:  return equals(objectData, other.objectData);
+        }
+
+        return false;           // never reached
+    }
+
+    public int hashCode() {
+        int hash = 
+            hashCode(id) + hashCode(description) + hashCode(timeStamp) + cm;
+
+        switch(type) {
+        case TYPE_INTEGER: return hash + hashCode(new Integer(intData));
+        case TYPE_FLOAT:   return hash + hashCode(new Float(floatData));
+        case TYPE_STRING:  return hash + hashCode(stringData);
+        case TYPE_OBJECT:  return hash + hashCode(objectData);
+        }
+        
+        return 0;               // never reached
+    }
+
+    //----- Private methods -----//
+
+    private void setCommon(String id, String description, int cm) {
+        // TODO check that ID is a valid URI element (that it does not cause problems in the DM tree)
+        if(id.indexOf('/') != -1)
+            throw new IllegalArgumentException("Invalid character '/' in KPI ID.");
+        
+        if(cm != CM_CC && cm != CM_DER && cm != CM_GAUGE && cm != CM_SI)
+            throw new IllegalArgumentException(
+                "Unknown data collection method constant '" + cm + "'.");
+
+        this.id = id;
+        this.description = description;
+        this.cm = cm;
+        
+        timeStamp = new Date();
+    }
+
+    private boolean equals(Object o1, Object o2) {
+        return o1 == null ? o2 == null : o1.equals(o2);
+    }
+
+    private int hashCode(Object o) {
+        return o == null ? 0 : o.hashCode();
+    }
 }
--- org.osgi.service.monitor/src/org/osgi/service/monitor/MonitorAdmin.java
+++ org.osgi.service.monitor/src/org/osgi/service/monitor/MonitorAdmin.java
@@ -1,48 +1,48 @@
 package org.osgi.service.monitor;
 
 /**
- * A MonitorAdmin implementation handles KPI query requests and measurement job
- * control requests.
- *  
+ * A MonitorAdmin implementation handles KPI query requests and measurement 
+ * job control requests.
+ * 
  */
 public interface MonitorAdmin {
-	/**
-	 * Returns a KPI addressed by its ID in [Monitorable_ID]/[KPI_ID] format.
-	 * This is a convenience feature for the cases when the full path of the KPI
-	 * is known. KPIs can also be obtained by querying the list of Monitorable
-	 * services from the service registry and then querying the list of KPI
-	 * names from the Monitorable services.
-	 * 
-	 * @param path the full path of the KPI in [Monitorable_ID]/[KPI_ID] format
-	 * @return the KPI object
-	 * @throws IllegalArgumentException if the path is invalid or points to a
-	 *         non existing KPI
-	 */
-	public KPI getKPI(String path) throws IllegalArgumentException;
 
-	/**
-	 * Starts a Monitoring Job with the parameters provided. In case of
-	 * successful starting of a job the MonitorAdmin generates the appropriate
-	 * event. If the list of KPI names contains a non existing KPI or the
-	 * schedule or count parameters are invalid then IllegalArgumentException is
-	 * thrown.
-	 * 
-	 * @param kpis list of KPIs to be monitored. The KPI names must be given in
-	 *        [Monitorable_PID]/[KPI_ID] format
-	 * @param schedule the time in minutes between two measurements. The value 0
-	 *        is not allowed
-	 * @param count the number of measurements to be taken or 0 if the
-	 *        measurement must run infinitely
-	 * @return the successfully started job object
-	 * @throws IllegalArgumentException
-	 */
-	public MonitoringJob startJob(String[] kpis, int schedule, int count)
-			throws IllegalArgumentException;
+    /**
+     * Returns a KPI addressed by its ID in [Monitorable_ID]/[KPI_ID] format.
+     * This is a convenience feature for the cases when the full path
+     * of the KPI is known. KPIs can also be obtained by querying the
+     * list of Monitorable services from the service registry and then querying
+     * the list of KPI names from the Monitorable services.
+     * @param path the full path of the KPI in [Monitorable_ID]/[KPI_ID] format
+     * @return the KPI object
+     * @throws IllegalArgumentException if the path is invalid or points to a 
+     * non existing KPI
+     */
+    public KPI getKPI(String path) throws IllegalArgumentException;
 
-	/**
-	 * Returns the list of currently running Monitoring Jobs.
-	 * 
-	 * @return the list of running jobs
-	 */
-	public MonitoringJob[] getRunningJobs();
+    /**
+     * Starts a Monitoring Job with the parameters provided.
+     * In case of successful starting of a job the MonitorAdmin  
+     * generates the appropriate event. If the list of KPI names contains
+     * a non existing KPI or the schedule or count parameters are invalid then
+     * IllegalArgumentException is thrown.
+     * @param kpis list of KPIs to be monitored. The KPI names must be 
+     * given in [Monitorable_PID]/[KPI_ID] format
+     * @param schedule the time in minutes between two measurements. The value 0
+     * is not allowed
+     * @param count the number of measurements to be taken or 0 if the 
+     * measurement must run infinitely
+     * @return the successfully started job object
+     * @throws IllegalArgumentException
+     */
+    public MonitoringJob startJob(String[] kpis, 
+                                  int schedule, int count) 
+        throws IllegalArgumentException;
+
+    /**
+     * Returns the list of currently running Monitoring Jobs.
+     * @return the list of running jobs
+     */
+    public MonitoringJob[] getRunningJobs();
+
 }
