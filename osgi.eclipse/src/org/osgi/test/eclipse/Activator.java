5a594dccb1ba31705f36cf05d2dac62c21da17cd
Peter Kriens <peter.kriens@aqute.se>
2004-11-17 11:16:05 +0000

    *** empty log message ***

---
+++ osgi.eclipse/src/org/osgi/test/eclipse/Activator.java
@@ -0,0 +1,57 @@
+package org.osgi.test.eclipse;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.impl.service.http.HttpServiceController;
+import org.osgi.test.director.*;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class Activator extends AbstractUIPlugin {
+	static Handler					handler;
+	static Director					director	= new Director();
+	static BundleContext			context;
+	static HttpServiceController	http		= new HttpServiceController();
+
+	/**
+	 * The constructor.
+	 */
+	public Activator() {
+		super();
+	}
+
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		try {
+			Activator.context = context;
+			super.start(context);
+			http.start(context);
+			handler = director.initialize(context);
+		}
+		catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		director.stop(context);
+		http.stop(context);
+		handler = null;
+	}
+
+
+	/**
+	 * @return
+	 */
+	public static BundleContext getContext() {
+		return context;
+	}
+
+}
