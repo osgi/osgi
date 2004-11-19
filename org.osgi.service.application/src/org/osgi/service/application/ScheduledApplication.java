b9bf4dd899f7f01afea6c89f97026450181d2ae9
Peter Kriens <peter.kriens@aqute.se>
2004-11-19 09:36:18 +0000

    Copyrights e.d.

--- org.osgi.service.application/src/org/osgi/service/application/ScheduledApplication.java
+++ org.osgi.service.application/src/org/osgi/service/application/ScheduledApplication.java
@@ -1,4 +1,31 @@
+/*
+ * Copyright (c) The Open Services Gateway Initiative (2004).
+ * All Rights Reserved.
+ *
+ * Implementation of certain elements of the Open Services Gateway Initiative
+ * (OSGI) Specification may be subject to third party intellectual property
+ * rights, including without limitation, patent rights (such a third party may
+ * or may not be a member of OSGi). OSGi is not responsible and shall not be
+ * held responsible in any manner for identifying or failing to identify any or
+ * all such third party intellectual property rights.
+ *
+ * This document and the information contained herein are provided on an "AS
+ * IS" basis and OSGI DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION HEREIN WILL
+ * NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL OSGI BE LIABLE FOR ANY
+ * LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF USE OF DATA, INTERRUPTION OF
+ * BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR EXEMPLARY, INCIDENTIAL,
+ * PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN CONNECTION WITH THIS
+ * DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ *
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
 package org.osgi.service.application;
+
+import java.util.Date;
 
 /**
  * A scheduled application contains the information of future registered
@@ -8,7 +35,7 @@ package org.osgi.service.application;
 public interface ScheduledApplication {
 
 	/** @modelguid {A505D4C7-198B-4D7F-BB35-B72F70B97561} */
-    public String getEventType();
+    public Date getDate();
 
 	/** @modelguid {55C3C652-E899-48F2-BDBE-6F75EBB6518F} */
     public ApplicationDescriptor getApplicationDescriptor();
