be8682bbdc6220677c6e5cb030525292bbbb314d
Peter Kriens <peter.kriens@aqute.se>
2004-11-19 09:56:24 +0000

    Prototype 1.1, formatted and copyrights

--- archive/org.osgi.impl.service.basiccontainer/src/org/osgi/impl/service/basiccontainer/BasicContainerDescriptor.java
+++ archive/org.osgi.impl.service.basiccontainer/src/org/osgi/impl/service/basiccontainer/BasicContainerDescriptor.java
@@ -1,94 +1,96 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors. 
+ * All rights are reserved.
+ * 
+ * These materials have been contributed  to the Open Services Gateway 
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
+ * to the terms of, the OSGi Member Agreement specifically including, but not 
+ * limited to, the license rights and warranty disclaimers as set forth in 
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
+ * All company, brand and product names contained within this document may be 
+ * trademarks that are the sole property of the respective owners.  
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
 package org.osgi.impl.service.basiccontainer;
 
-import org.osgi.service.application.*;
+import java.io.InputStream;
 import java.util.*;
-import java.io.*;
+import org.osgi.service.application.ApplicationDescriptor;
 
-public class BasicContainerDescriptor implements ApplicationDescriptor
-{
-  private String appName;
-  private String appCategory;
-  private String appVersion;
-  private String contID;
-  private String id;
-  private String mainClass;
+public class BasicContainerDescriptor implements ApplicationDescriptor {
+	private String	appName;
+	private String	appCategory;
+	private String	appVersion;
+	private String	contID;
+	private String	id;
+	private String	mainClass;
 
-  public BasicContainerDescriptor( String name, String category, String version, String cId, 
-                                  String appId, String mainCls )
-  {
-    appName = name;
-    appCategory = category;
-    appVersion = version;
-    contID = cId;
-    id = appId;
-    mainClass = mainCls;
-  }
+	public BasicContainerDescriptor(String name, String category,
+			String version, String cId, String appId, String mainCls) {
+		appName = name;
+		appCategory = category;
+		appVersion = version;
+		contID = cId;
+		id = appId;
+		mainClass = mainCls;
+	}
 
-  public String getCategory()
-  {
-    return appCategory;
-  }
-  
-  public void setCategory(String cat)
-  {
-    appCategory = cat;
-  }
+	public String getCategory() {
+		return appCategory;
+	}
 
-  public String getContainerID()
-  {
-    return contID;
-  }
+	public void setCategory(String cat) {
+		appCategory = cat;
+	}
 
-  public Map getContainerProperties(String locale)
-  {
-    /* TODO TODO TODO TODO TODO */ 
-    return null;
-  }
+	public String getContainerID() {
+		return contID;
+	}
 
-  public String getName()
-  {
-    return appName;
-  }
-  
-  public void setName(String name)
-  {
-    appName = name;
-  }
+	public Map getContainerProperties(String locale) {
+		/* TODO */
+		return null;
+	}
 
-  public Map getProperties(String locale)
-  {
-    /* TODO TODO TODO TODO TODO */ 
-    return null;
-  }
-  
-  public String getUniqueID()
-  {
-    return contID + "-" + appCategory + "-" + appName + "-" + appVersion;
-  }
-  
-  public String getVersion()
-  {
-    return appVersion;
-  }
-  
-  public void setVersion(String ver)
-  {
-    appVersion = ver;
-  }
-  
-  public InputStream getIcon(Locale aLocale, int pixNum)
-  {
-    /* TODO TODO TODO TODO TODO */ 
-    return null;
-  }
-  
-  public String getId()
-  {
-    return id;
-  }
-  
-  public String getMainClass()
-  {
-    return mainClass;
-  }  
+	public String getName() {
+		return appName;
+	}
+
+	public void setName(String name) {
+		appName = name;
+	}
+
+	public Map getProperties(String locale) {
+		/* TODO  */
+		return null;
+	}
+
+	public String getUniqueID() {
+		return contID + "-" + appCategory + "-" + appName + "-" + appVersion;
+	}
+
+	public String getVersion() {
+		return appVersion;
+	}
+
+	public void setVersion(String ver) {
+		appVersion = ver;
+	}
+
+	public InputStream getIcon(Locale aLocale, int pixNum) {
+		/* TODO  */
+		return null;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getMainClass() {
+		return mainClass;
+	}
 }
