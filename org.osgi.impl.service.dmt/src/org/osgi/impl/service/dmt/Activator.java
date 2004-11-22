d59ebe9a7b53a73fd5344e60b13383a3aa36cc39
Peter Kriens <peter.kriens@aqute.se>
2004-11-22 11:12:13 +0000

    Merged the bundles so this starst the DMT admin, the log and configuration plugins

--- org.osgi.impl.service.dmt/src/org/osgi/impl/service/dmt/Activator.java
+++ org.osgi.impl.service.dmt/src/org/osgi/impl/service/dmt/Activator.java
@@ -1,72 +1,54 @@
 /*
- * ============================================================================
- * (c) Copyright 2004 Nokia
- * This material, including documentation and any related computer programs,
- * is protected by copyright controlled by Nokia and its licensors. 
- * All rights are reserved.
+ * $Header$
  * 
- * These materials have been contributed  to the Open Services Gateway 
- * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
- * to the terms of, the OSGi Member Agreement specifically including, but not 
- * limited to, the license rights and warranty disclaimers as set forth in 
- * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
- * All company, brand and product names contained within this document may be 
- * trademarks that are the sole property of the respective owners.  
- * The above notice must be included on all copies of this document.
- * ============================================================================
+ * Copyright (c) The OSGi Alliance (2004). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
  */
 package org.osgi.impl.service.dmt;
 
 import org.osgi.framework.*;
-import org.osgi.impl.service.dmt.api.RemoteAlertSender;
-import org.osgi.service.dmt.*;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.framework.BundleActivator;
 
-// TODO cleanup service registrations properly in case of error
-// TODO check that the service is not registered already?
-// TODO when stop() is called, notify the impl. to release all refs to other
-// services
-// (these should be done in all activators!)
+/**
+ * 
+ * Merges DMT Admin, login and configuration plugin
+ * 
+ * @version $Revision$
+ */
 public class Activator implements BundleActivator {
-	private ServiceRegistration	sessionReg;
-	private ServiceRegistration	alertReg;
-	private ServiceTracker		remoteAdapterTracker;
-	private ServiceTracker		pluginTracker;
-
-	public void start(BundleContext bc) throws BundleException {
-		try {
-			DmtPlugInDispatcher dispatcher = new DmtPlugInDispatcher(bc);
-			//tracker = new ServiceTracker(bc, DmtDataPlugIn.class.getName(),
-			// dispatcher);
-			String filter = "(|(objectClass=org.osgi.service.dmt.DmtDataPlugIn)"
-					+ "(objectClass=org.osgi.service.dmt.DmtExecPlugIn))";
-			pluginTracker = new ServiceTracker(bc, bc.createFilter(filter),
-					dispatcher);
-			pluginTracker.open();
-			// creating the services
-			DmtFactoryImpl dmtFactory = new DmtFactoryImpl(dispatcher);
-			DmtAlertSenderImpl dmtAlertSender = new DmtAlertSenderImpl(bc);
-			remoteAdapterTracker = new ServiceTracker(bc,
-					RemoteAlertSender.class.getName(), dmtAlertSender);
-			remoteAdapterTracker.open();
-			// registering the services
-			sessionReg = bc.registerService(DmtFactory.class.getName(),
-					dmtFactory, null);
-			alertReg = bc.registerService(DmtAlertSender.class.getName(),
-					dmtAlertSender, null);
-		}
-		catch (Exception e) {
-			System.out.println("Exception:" + e.getMessage());
-			throw new BundleException("Failure in start() method.", e);
-		}
+	DmtAdminActivator dmtAdminActivator = new DmtAdminActivator();
+	LogPluginActivator logPluginActivator = new LogPluginActivator();
+	ConfigurationPluginActivator configurationPluginActivator = new ConfigurationPluginActivator();
+	
+	public void start(BundleContext context) throws Exception {
+		dmtAdminActivator.start(context);
+		logPluginActivator.start(context);
+		configurationPluginActivator.start(context);
 	}
 
-	public void stop(BundleContext bc) throws BundleException {
-		// stopping service trackers
-		pluginTracker.close();
-		remoteAdapterTracker.close();
-		// unregistering the service
-		sessionReg.unregister();
-		alertReg.unregister();
+	public void stop(BundleContext context) throws Exception {
+		configurationPluginActivator.stop(context);
+		logPluginActivator.stop(context);
+		dmtAdminActivator.stop(context);
 	}
 }
---
+++ org.osgi.impl.service.dmt/src/org/osgi/impl/service/dmt/DmtAdminActivator.java
@@ -0,0 +1,72 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors. 
+ * All rights are reserved.
+ * 
+ * These materials have been contributed  to the Open Services Gateway 
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
+ * to the terms of, the OSGi Member Agreement specifically including, but not 
+ * limited to, the license rights and warranty disclaimers as set forth in 
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
+ * All company, brand and product names contained within this document may be 
+ * trademarks that are the sole property of the respective owners.  
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
+package org.osgi.impl.service.dmt;
+
+import org.osgi.framework.*;
+import org.osgi.impl.service.dmt.api.RemoteAlertSender;
+import org.osgi.service.dmt.*;
+import org.osgi.util.tracker.ServiceTracker;
+
+// TODO cleanup service registrations properly in case of error
+// TODO check that the service is not registered already?
+// TODO when stop() is called, notify the impl. to release all refs to other
+// services
+// (these should be done in all activators!)
+public class DmtAdminActivator implements BundleActivator {
+	private ServiceRegistration	sessionReg;
+	private ServiceRegistration	alertReg;
+	private ServiceTracker		remoteAdapterTracker;
+	private ServiceTracker		pluginTracker;
+
+	public void start(BundleContext bc) throws BundleException {
+		try {
+			DmtPlugInDispatcher dispatcher = new DmtPlugInDispatcher(bc);
+			//tracker = new ServiceTracker(bc, DmtDataPlugIn.class.getName(),
+			// dispatcher);
+			String filter = "(|(objectClass=org.osgi.service.dmt.DmtDataPlugIn)"
+					+ "(objectClass=org.osgi.service.dmt.DmtExecPlugIn))";
+			pluginTracker = new ServiceTracker(bc, bc.createFilter(filter),
+					dispatcher);
+			pluginTracker.open();
+			// creating the services
+			DmtFactoryImpl dmtFactory = new DmtFactoryImpl(dispatcher);
+			DmtAlertSenderImpl dmtAlertSender = new DmtAlertSenderImpl(bc);
+			remoteAdapterTracker = new ServiceTracker(bc,
+					RemoteAlertSender.class.getName(), dmtAlertSender);
+			remoteAdapterTracker.open();
+			// registering the services
+			sessionReg = bc.registerService(DmtFactory.class.getName(),
+					dmtFactory, null);
+			alertReg = bc.registerService(DmtAlertSender.class.getName(),
+					dmtAlertSender, null);
+		}
+		catch (Exception e) {
+			System.out.println("Exception:" + e.getMessage());
+			throw new BundleException("Failure in start() method.", e);
+		}
+	}
+
+	public void stop(BundleContext bc) throws BundleException {
+		// stopping service trackers
+		pluginTracker.close();
+		remoteAdapterTracker.close();
+		// unregistering the service
+		sessionReg.unregister();
+		alertReg.unregister();
+	}
+}
