b322c9a34c0903e681fa38c4c162e2c258b30123
BJ Hargrave <hargrave@us.ibm.com>
2004-12-03 19:30:27 +0000

    Updated for RFC 60

--- org.osgi.service.prefs/src/org/osgi/service/prefs/Preferences.java
+++ org.osgi.service.prefs/src/org/osgi/service/prefs/Preferences.java
@@ -1,17 +1,30 @@
 /*
- * @(#)Preferences.java 1.7 01/07/18
  * $Header$
- *
-
- *
- * (C) Copyright 1996-2001 Sun Microsystems, Inc.
- *
- * This source code is licensed to OSGi as MEMBER LICENSED MATERIALS
- * under the terms of Section 3.2 of the OSGi MEMBER AGREEMENT.
- *
+ * 
+ * Copyright (c) OSGi Alliance (2001, 2004). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
  */
 package org.osgi.service.prefs;
-
 
 /**
  * A node in a hierarchical collection of preference data.
@@ -618,12 +631,10 @@ public interface Preferences {
 	 * 
 	 * <p>
 	 * The removal is not guaranteed to be persistent until the <tt>flush</tt>
-	 * method is called on the parent of this node. (It is illegal to remove the
-	 * root node.)
+	 * method is called on the parent of this node.
 	 * 
 	 * @throws IllegalStateException if this node (or an ancestor) has already
 	 *         been removed with the {@link #removeNode()}method.
-	 * @throws RuntimeException if this is a root node.
 	 * @throws BackingStoreException if this operation cannot be completed due
 	 *         to a failure in the backing store, or inability to communicate
 	 *         with it.
