4462bbce5e5ff1fedc0029c97a6868cedcc163e2
Tamas Szeredi <tamas.szeredi@nokia.com>
2004-12-10 14:09:10 +0000

    Replacing copyright notice with standard OSGi text. Formatting files according to OSGi source style.

--- org.osgi.service.condpermadmin/src/org/osgi/service/condpermadmin/BundleLocationCondition.java
+++ org.osgi.service.condpermadmin/src/org/osgi/service/condpermadmin/BundleLocationCondition.java
@@ -1,6 +1,27 @@
 /*
- * Copyright (c) 2004, Nokia
- * COMPANY CONFIDENTAL - for internal use only
+ * Copyright (c) The Open Services Gateway Initiative (2004).
+ * All Rights Reserved.
+ *
+ * Implementation of certain elements of the Open Services Gateway Initiative
+ * (OSGI) Specification may be subject to third party intellectual property
+ * rights, including without limitation, patent rights (such a third party may
+ * or may not be a member of OSGi). OSGi is not responsible and shall not be
+ * held responsible in any manner for identifying or failing to identify any or
+ * all such third party intellectual property rights.
+ *
+ * This document and the information contained herein are provided on an "AS
+ * IS" basis and OSGI DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION HEREIN WILL
+ * NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL OSGI BE LIABLE FOR ANY
+ * LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF USE OF DATA, INTERRUPTION OF
+ * BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR EXEMPLARY, INCIDENTIAL,
+ * PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN CONNECTION WITH THIS
+ * DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ *
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
  */
 package org.osgi.service.condpermadmin;
 
@@ -10,18 +31,20 @@ import java.util.Dictionary;
 
 import org.osgi.framework.Bundle;
 
-/**
- * @author Peter Nagy 
- */
 public class BundleLocationCondition implements Condition {
-
 	boolean match;
-	public BundleLocationCondition(final Bundle b,String location) {
-		if ((location == null) || (b==null)) { match=false; return; }
-		String l = (String) AccessController.doPrivileged(new PrivilegedAction() {
-			public Object run() {
-				return b.getLocation();
-			}});
+
+    public BundleLocationCondition(final Bundle b, String location) {
+		if ((location == null) || (b == null)) {
+			match = false;
+			return;
+		}
+		String l = (String) AccessController
+				.doPrivileged(new PrivilegedAction() {
+					public Object run() {
+						return b.getLocation();
+					}
+				});
 		match = l.equals(location);
 	}
 
@@ -38,10 +61,10 @@ public class BundleLocationCondition implements Condition {
 	}
 
 	public boolean isSatisfied(Condition[] conds, Dictionary oneShotData) {
-		for(int i=0;i<conds.length;i++) {
-			if (!((BundleLocationCondition)conds[i]).match) return false;
+		for (int i = 0; i < conds.length; i++) {
+			if (!((BundleLocationCondition) conds[i]).match)
+				return false;
 		}
 		return true;
 	}
-
 }
--- org.osgi.service.condpermadmin/src/org/osgi/service/condpermadmin/ConditionInfo.java
+++ org.osgi.service.condpermadmin/src/org/osgi/service/condpermadmin/ConditionInfo.java
@@ -1,23 +1,41 @@
 /*
- * Copyright (c) 2004, Nokia
- * COMPANY CONFIDENTAL - for internal use only
+ * Copyright (c) The Open Services Gateway Initiative (2004).
+ * All Rights Reserved.
+ *
+ * Implementation of certain elements of the Open Services Gateway Initiative
+ * (OSGI) Specification may be subject to third party intellectual property
+ * rights, including without limitation, patent rights (such a third party may
+ * or may not be a member of OSGi). OSGi is not responsible and shall not be
+ * held responsible in any manner for identifying or failing to identify any or
+ * all such third party intellectual property rights.
+ *
+ * This document and the information contained herein are provided on an "AS
+ * IS" basis and OSGI DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION HEREIN WILL
+ * NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL OSGI BE LIABLE FOR ANY
+ * LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF USE OF DATA, INTERRUPTION OF
+ * BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR EXEMPLARY, INCIDENTIAL,
+ * PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN CONNECTION WITH THIS
+ * DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ *
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
  */
 package org.osgi.service.condpermadmin;
 
-/**
- * @author Peter Nagy
- */
 public final class ConditionInfo {
-	final String type;
-	final String[] args;
-	final int hashCode;
-	final String encoded;
+	final String	type;
+	final String[]	args;
+	final int		hashCode;
+	final String	encoded;
 
 	private String encode() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("[");
 		sb.append(type);
-		for(int i=0;i<args.length;i++) {
+		for (int i = 0; i < args.length; i++) {
 			sb.append(" \"");
 			sb.append(args[i]); // TODO: needs escaping
 			sb.append("\"");
@@ -29,7 +47,8 @@ public final class ConditionInfo {
 	public ConditionInfo(String encodedCondition) {
 		throw new IllegalArgumentException("not implemented yet!");
 	}
-	public ConditionInfo(String type,String[] args) {
+    
+	public ConditionInfo(String type, String[] args) {
 		this.type = type;
 		this.args = (String[]) args.clone();
 		encoded = encode();
@@ -37,15 +56,30 @@ public final class ConditionInfo {
 	}
 	
 	public boolean equals(Object obj) {
-		if (obj==null) return false;
-		if (! (obj instanceof ConditionInfo)) return false;
-		return ((ConditionInfo)obj).encoded.equals(this.encoded);
+		if (obj == null)
+			return false;
+		if (!(obj instanceof ConditionInfo))
+			return false;
+		return ((ConditionInfo) obj).encoded.equals(this.encoded);
 	}
 	
-	public String[] getArgs() { return (String[]) args.clone(); }
-	public String getEncoded() { return encoded; }
-	public String getType() { return type; }
-	public int hashCode() { return hashCode; }
-	public String toString() { return "[ConditionInfo "+getEncoded()+"]";}
-	
+	public String[] getArgs() {
+		return (String[]) args.clone();
+	}
+
+	public String getEncoded() {
+		return encoded;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int hashCode() {
+		return hashCode;
+	}
+
+	public String toString() {
+		return "[ConditionInfo " + getEncoded() + "]";
+	}
 }
