406b30a7696d62301e9d3e2f63da5744c6c90ba6
Tamas Szeredi <tamas.szeredi@nokia.com>
2004-12-17 12:46:45 +0000

    Updated with changes in the RFC (merged with new javadoc, added methods to query monitorables)

--- org.osgi.service.monitor/src/org/osgi/service/monitor/MonitorAdmin.java
+++ org.osgi.service.monitor/src/org/osgi/service/monitor/MonitorAdmin.java
@@ -28,33 +28,78 @@ package org.osgi.service.monitor;
 /**
  * A MonitorAdmin implementation handles KPI query requests and measurement job
  * control requests.
- *  
+ * <p>
+ * Note that an alternative but not recommended way of obtaining KPIs is by
+ * querying the list of Monitorable services from the service registry and then
+ * querying the list of KPI names from the Monitorable services. This way all
+ * services which publish KPIs will be returned regardless of whether they do or
+ * do not hold the necessary <code>KpiPermission</code> for publishing KPIs.
+ * By using the MonitorAdmin to obtain the Monitorables it is guaranteed that
+ * only those services will be accessed who are authorized to publish KPIs. It
+ * is the responsibility of the MonitorAdmin implementation to check the
+ * required permissions and show only those services which pass this check.
  */
 public interface MonitorAdmin {
-	/**
+
+    /**
+	 * Returns all the Monitorable services that are currently registered. For
+	 * security reasons this method should be used instead of querying the
+	 * monitorable services from the service registry.
+	 * 
+	 * @return the array of Monitorables or <code>null</code> if none are
+	 *         registered
+	 */
+    public Monitorable[] getMonitorables();
+
+    /**
+	 * Returns the Monitorable service addressed by its PID. For security
+	 * reasons this method should be used instead of querying the monitorable
+	 * services from the service registry.
+	 * 
+	 * @param id the persistent identity of the monitorable service
+	 * @return the Monitorable object
+	 * @throws IllegalArgumentException if the ID is invalid or points to a non
+	 *         existing monitorable service
+	 */
+    public Monitorable getMonitorable(String id)
+            throws IllegalArgumentException;
+
+    /**
 	 * Returns a KPI addressed by its ID in [Monitorable_ID]/[KPI_ID] format.
 	 * This is a convenience feature for the cases when the full path of the KPI
-	 * is known. KPIs can also be obtained by querying the list of Monitorable
-	 * services from the service registry and then querying the list of KPI
-	 * names from the Monitorable services.
+	 * is known.
+	 * <p>
+	 * The entity which queries a KPI needs to hold <code>KpiPermission</code>
+	 * for the given target with the <code>read</code> action present.
 	 * 
 	 * @param path the full path of the KPI in [Monitorable_ID]/[KPI_ID] format
 	 * @return the KPI object
 	 * @throws IllegalArgumentException if the path is invalid or points to a
 	 *         non existing KPI
+	 * @throws SecurityException if the caller does not hold a
+	 *         <code>KpiPermission</code> for the KPI specified by
+	 *         <code>path</code> with the <code>read</code> action present
 	 */
-	public KPI getKPI(String path) throws IllegalArgumentException;
+    public KPI getKPI(String path) throws IllegalArgumentException;
 
-	/**
+    /**
 	 * Starts a Monitoring Job with the parameters provided. If the list of KPI
 	 * names contains a non existing KPI or the schedule or count parameters are
-	 * invalid then IllegalArgumentException is thrown. The initiator string is
-	 * used in the <code>listener.id</code> field of all events triggered by
-	 * the job, to allow filtering the events based on the initiator.
+	 * invalid then <code>IllegalArgumentException</code> is thrown. The
+	 * initiator string is used in the <code>listener.id</code> field of all
+	 * events triggered by the job, to allow filtering the events based on the
+	 * initiator.
+	 * <p>
+	 * The entity which initiates a Monitoring Job needs to hold
+	 * <code>KpiPermission</code> for all the specified target KPIs with the
+	 * <code>startjob</code> action present. In case of time based jobs, if
+	 * the permission's action string specifies a minimal sampling interval then
+	 * the <code>schedule</code> parameter should be at least as great as the
+	 * value in the action string.
 	 * 
 	 * @param initiator the identifier of the entity that initiated the job
-	 * @param kpis list of KPIs to be monitored. The KPI names must be given in
-	 *        [Monitorable_PID]/[KPI_ID] format
+	 * @param kpis List of KPIs to be monitored. The KPI names must be given in
+	 *        [Monitorable_PID]/[KPI_ID] format.
 	 * @param schedule The time in seconds between two measurements. The value 0
 	 *        means that instant notification on KPI updates is requested. For
 	 *        values greater than 0 the first measurement will be taken when the
@@ -62,18 +107,23 @@ public interface MonitorAdmin {
 	 * @param count For time based monitoring jobs, this should be the number of
 	 *        measurements to be taken, or 0 if the measurement must run
 	 *        infinitely. For change based monitoring (if schedule is 0), this
-	 *        must be a positive interger that specifies the number of changes
-	 *        that must happen before a new notification is sent.
+	 *        must be a positive interger specifying the number of changes that
+	 *        must happen before a new notification is sent.
 	 * @return the successfully started job object
 	 * @throws IllegalArgumentException
+	 * @throws SecurityException if the caller does not hold
+	 *         <code>KpiPermission</code> for all the specified KPIs, with the
+	 *         <code>startjob</code> action present, or if the permission does
+	 *         not allow starting the job with the given frequency
 	 */
-	public MonitoringJob startJob(String initiator, String[] kpis,
-			int schedule, int count) throws IllegalArgumentException;
+    public MonitoringJob startJob(String initiator, String[] kpis,
+                                  int schedule, int count)
+        throws IllegalArgumentException;
 
-	/**
+    /**
 	 * Returns the list of currently running Monitoring Jobs.
 	 * 
 	 * @return the list of running jobs
 	 */
-	public MonitoringJob[] getRunningJobs();
+    public MonitoringJob[] getRunningJobs();
 }
--- org.osgi.service.monitor/src/org/osgi/service/monitor/Monitorable.java
+++ org.osgi.service.monitor/src/org/osgi/service/monitor/Monitorable.java
@@ -28,59 +28,88 @@ package org.osgi.service.monitor;
 /**
  * A Monitorable can provide information about itself in the form of KPIs.
  * Instances of this interface should register themselves at the OSGi Service
- * registry. The MonitorAdmin listens to the registration of Monitorable
+ * Registry. The MonitorAdmin listens to the registration of Monitorable
  * services, and makes the information they provide available also through the
- * Device Management Tree (DMT). The monitorable service is identified by its
- * PID string which must not contain the Reserved characters described in 2.2 of
- * RFC-2396 (URI Generic Syntax).
+ * Device Management Tree (DMT).
+ * <p>
+ * The monitorable service is identified by its PID string which must not
+ * contain the Reserved characters described in 2.2 of RFC-2396 small as
+ * possible. The PID will be used as a node name in the DMT (URI Generic
+ * Syntax). Also the length of the PID should be kept as and certain DMT
+ * implementations may have limits on node name length. The length limit is not
+ * specified in any standard, it is recommended not to use names longer than 20
+ * characters.
  * <p>
  * A Monitorable may optionally support sending notifications when the status of
  * its KPIs change.
+ * <p>
+ * Publishing KPIs requires the presence of the <code>KpiPermission</code>
+ * with the <code>publish</code> action string. This permission, however, is
+ * not checked during registration of the Monitorable service. Instead, the
+ * MonitorAdmin implemenatation must make sure that when a KPI is queried, it is
+ * shown only if the Monitorable is authorized to publish the given KPI.
  */
 public interface Monitorable {
-	/**
+    /**
 	 * Returns the list of KPI identifiers published by this Monitorable. A KPI
 	 * name is unique within the scope of a Monitorable. The array contains the
 	 * elements in no particular order.
 	 * 
 	 * @return the name of KPIs published by this object
 	 */
-	public String[] getKpiNames();
+    public String[] getKpiNames();
 
-	/**
+    /**
 	 * Returns the list of long KPI names published by this Monitorable. This
 	 * name is the combination of the ID of the Monitorable and the name of the
 	 * KPI in the following format: [Monitorable_ID]/[KPI_name], for example
 	 * MyApp/QueueSize. This name is guaranteed to be unique on the service
-	 * platform and it is used in a Monitoring's list of observed KPIs. The
+	 * platform and it is used in a MonitoringJob's list of observed KPIs. The
 	 * array contains the elements in no particular order.
 	 * 
 	 * @return the 'fully qualified' name of KPIs published by this object
 	 */
-	public String[] getKpiPaths();
+    public String[] getKpiPaths();
 
-	/**
+    /**
 	 * Returns all the KPI objects published by this Monitorable instance. The
 	 * KPIs will hold the values taken at the time of this method call. The
 	 * array contains the elements in no particular order.
+	 * <p>
+	 * The entity which queries the KPI list needs to hold
+	 * <code>KpiPermission</code> with the <code>read</code> action present.
+	 * The target field of the permission must match all the KPIs published by
+	 * this Monitorable.
 	 * 
 	 * @return the KPI objects published by this Monitorable instance
+	 * @throws SecurityException if the caller does not hold
+	 *         <code>KpiPermission</code> with the <code>read</code> action
+	 *         or if there is any KPI published by the Monitorable which is not
+	 *         allowed to be read as per the target field of the permission
 	 */
-	public KPI[] getKpis();
+    public KPI[] getKpis();
 
-	/**
+    /**
 	 * Returns the KPI object addressed by its identifier. The KPI will hold the
 	 * value taken at the time of this method call.
+	 * <p>
+	 * The entity which queries the KPI needs to hold <code>KpiPermission</code>
+	 * with the <code>read</code> action present. The target field of the
+	 * permission must match the KPI to be read.
 	 * 
 	 * @param id the identifier of the KPI. The method returns the same KPI
 	 *        regardless of whether the short or long ID is used.
 	 * @return the KPI object
 	 * @throws IllegalArgumentException if the path is invalid or points to a
 	 *         non existing KPI
+	 * @throws SecurityException if the caller does not hold
+	 *         <code>KpiPermission</code> with the <code>read</code> action
+	 *         or if the specified KPI is not allowed to be read as per the
+	 *         target field of the permission
 	 */
-	public KPI getKpi(String id) throws IllegalArgumentException;
+    public KPI getKpi(String id) throws IllegalArgumentException;
 
-	/**
+    /**
 	 * Tells whether the KPI provider is able to send instant notifications when
 	 * the given KPI changes. If the Monitorable supports sending change updates
 	 * it must notify the UpdateListener when the value of the KPI changes. The
@@ -90,18 +119,23 @@ public interface Monitorable {
 	 * @param id the identifier of the KPI. The method works the same way
 	 *        regardless of whether the short or long ID is used.
 	 * @return <code>true</code> if the Monitorable can send notification when
-	 *         the given KPI chages, <code>false</code> otherwise.
+	 *         the given KPI chages, <code>false</code> otherwise
 	 * @throws IllegalArgumentException if the path is invalid or points to a
 	 *         non existing KPI
 	 */
-	public boolean notifiesOnChange(String id) throws IllegalArgumentException;
+    public boolean notifiesOnChange(String id) throws IllegalArgumentException;
 
-	/**
+    /**
 	 * Issues a request to reset a given KPI. Depending on the semantics of the
 	 * KPI this call may or may not succeed: it makes sense to reset a counter
 	 * to its starting value, but e.g. a KPI of type String might not have a
 	 * meaningful default value. Note that for numeric KPIs the starting value
 	 * may not necessarily be 0. Resetting a KPI triggers a monitor event.
+	 * <p>
+	 * The entity that wants to reset the KPI needs to hold
+	 * <code>KpiPermission</code> with the <code>reset</code> action
+	 * present. The target field of the permission must match the KPI to be
+	 * reset.
 	 * 
 	 * @param id the identifier of the KPI. The method works the same way
 	 *        regardless of whether the short or long ID is used.
@@ -109,6 +143,10 @@ public interface Monitorable {
 	 *         the given KPI, <code>false</code> otherwise
 	 * @throws IllegalArgumentException if the path is invalid or points to a
 	 *         non existing KPI
+	 * @throws SecurityException if the caller does not hold
+	 *         <code>KpiPermission</code> with the <code>reset</code> action
+	 *         or if the specified KPI is not allowed to be reset as per the
+	 *         target field of the permission
 	 */
-	public boolean resetKpi(String id) throws IllegalArgumentException;
+    public boolean resetKpi(String id) throws IllegalArgumentException;
 }
