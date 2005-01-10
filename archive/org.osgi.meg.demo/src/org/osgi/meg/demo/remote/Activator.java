922ab8ac3519ed0d0dc5ab60edda086944afa76c
Ivan Zahoranszky <izahoransky@nokia.com>
2005-01-10 16:05:38 +0000

    *** empty log message ***

--- archive/org.osgi.meg.demo/src/org/osgi/meg/demo/remote/Activator.java
+++ archive/org.osgi.meg.demo/src/org/osgi/meg/demo/remote/Activator.java
@@ -18,37 +18,51 @@
 package org.osgi.meg.demo.remote;
 
 import java.io.*;
+import java.util.Dictionary;
 import org.osgi.framework.*;
 import org.osgi.impl.service.dmt.api.RemoteAlertSender;
 import org.osgi.service.dmt.DmtFactory;
 
 public class Activator implements BundleActivator {
-	private static final int	PORT	= 7777;
+	private static final int	PORT	 = 7777;
+	private static final int	PINGTIME = 10;
 	private ServiceReference	serviceRef;
 	private ServiceRegistration	remoteAlertSenderReg;
 	private ClientAdaptor		clientAdaptor;
 
 	public void start(BundleContext bc) throws BundleException {
+		
+		Bundle bundle = bc.getBundle();
+		Dictionary headers = bundle.getHeaders();
+		String host = (String) headers.get("Host-name");
+		int pingTime = PINGTIME;
+		String pingTimeStr = (String) headers.get("Ping-time");
+		if (null != pingTimeStr)
+			pingTime = Integer.parseInt((String) headers.get("Ping-time"));
+		
 		System.out.println("Remote adapter activated.");
 		try {
 			serviceRef = bc.getServiceReference(DmtFactory.class.getName());
 			DmtFactory factory = (DmtFactory) bc.getService(serviceRef);
 			// TODO get the parameters from configuration
-			String host = null;
-			System.out
-					.println("Enter host name (press 'enter' for localhost): ");
-			try {
-				BufferedReader reader = new BufferedReader(
-						new InputStreamReader(System.in));
-				host = reader.readLine();
-			}
-			catch (IOException e) {
-				throw new BundleException("I/O Error reading host name.", e);
+			//String host = null;
+			if (null == host) {
+				System.out
+						.println("Enter host name (press 'enter' for localhost): ");
+				try {
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(System.in));
+					host = reader.readLine();
+				}
+				catch (IOException e) {
+					throw new BundleException("I/O Error reading host name.", e);
+				}
 			}
 			if (host == null || "".equals(host))
 				host = "localhost";
+			System.out.println("Remote adapter connects to server: " + host + ":" + PORT);
 			//creating the adaptor
-			clientAdaptor = new ClientAdaptor(factory, host, PORT);
+			clientAdaptor = new ClientAdaptor(factory, host, PORT, pingTime);
 			System.out.println("Remote adapter connected to server " + host
 					+ ":" + PORT);
 			//registering the remote alert sender service
