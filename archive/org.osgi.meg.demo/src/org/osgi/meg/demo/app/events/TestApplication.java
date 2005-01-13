6827a8d4ea4866ec727a58c716cff37bcd6d90e3
Tamas Szeredi <tamas.szeredi@nokia.com>
2005-01-13 09:39:54 +0000

    Making TestApplication configurable and one of the configured parameters monitorable.

--- archive/org.osgi.meg.demo/src/org/osgi/meg/demo/app/events/TestApplication.java
+++ archive/org.osgi.meg.demo/src/org/osgi/meg/demo/app/events/TestApplication.java
@@ -17,56 +17,139 @@
  */
 package org.osgi.meg.demo.app.events;
 
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.application.*;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.osgi.service.event.*;
+import org.osgi.service.monitor.KPI;
+import org.osgi.service.monitor.Monitorable;
+import org.osgi.service.monitor.UpdateListener;
 import java.io.*;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
-public class TestApplication extends MEGApplication
-{
-  String fileName = null;
-
-  public TestApplication(MEGApplicationContext context)
-  {
-    super( context );
+public class TestApplication 
+        extends MEGApplication 
+		implements ManagedService, Monitorable {
     
-    if( context.getLaunchArgs() != null )
-      fileName = (String)context.getLaunchArgs().get( "TestResult" );
-  }
-  
-  public void startApplication() throws Exception
-  {
-    writeResult( "START" );
-  }
-
-  public void stopApplication() throws Exception
-  {
-    writeResult( "STOP" );
-  }
-
-  public void suspendApplication() throws Exception
-  {
-    writeResult( "SUSPEND" );
-  }
-
-  public void resumeApplication() throws Exception
-  {
-    writeResult( "RESUME" );
-  }
-  
-  public void channelEvent(ChannelEvent event)
-  {
-  }
-  
-  public void writeResult( String result )
-  {
-    try {
-      if( fileName == null )
-        return;
-      File file = new File( fileName );
-      FileOutputStream stream = new FileOutputStream( file );
+    private static final String	PARAMETER_NAME = "TestParameter";
+	static final String SERVICE_PID = 
+        "org.osgi.meg.demo.app.events.TestApplication";
     
-      stream.write( result.getBytes() );
-      stream.close();      
-    }catch( IOException e ) {}
-  }
+    private MEGApplicationContext context;
+	private String fileName;
+    private String testParameter;
+    
+    private ServiceRegistration reg = null;
+	private UpdateListener updateListener = null;
+
+	public TestApplication(MEGApplicationContext context) {
+		super(context);
+        
+        this.context = context;
+        testParameter = "<unset>";
+        
+        if (context.getLaunchArgs() != null)
+            fileName = (String) context.getLaunchArgs().get("TestResult");
+        else
+            fileName = null;
+	}
+
+	public void startApplication() throws Exception {
+		writeResult("START");
+
+        Hashtable config = new Hashtable();
+        config.put("service.pid", SERVICE_PID);
+        reg = Activator.context.registerService(
+                new String[] { ManagedService.class.getName(),
+                		       Monitorable.class.getName() }, this, config);
+        
+        updateListener = (UpdateListener) 
+		    context.getServiceObject(UpdateListener.class.getName(), null);
+	}
+
+	public void stopApplication() throws Exception {
+		writeResult("STOP");
+        
+		reg.unregister();
+        context.ungetServiceObject(updateListener);
+	}
+
+	public void suspendApplication() throws Exception {
+		writeResult("SUSPEND");
+	}
+
+	public void resumeApplication() throws Exception {
+		writeResult("RESUME");
+	}
+
+	public void channelEvent(ChannelEvent event) {
+	}
+
+	public void writeResult(String result) {
+		try {
+			if (fileName == null)
+				return;
+			File file = new File(fileName);
+			FileOutputStream stream = new FileOutputStream(file);
+			stream.write(result.getBytes());
+			stream.close();
+		} catch (IOException e) {
+		}
+	}
+
+	public void updated(Dictionary properties) throws ConfigurationException {
+        if(properties == null)
+        	return;
+        
+        String testParameterOld = testParameter;
+        
+        Object param = properties.get(PARAMETER_NAME);
+        if(!(param instanceof String))
+            testParameter = "<unset>";
+        else
+            testParameter = (String) param;
+        
+        if(testParameter != testParameterOld)
+            updateListener.updated(getTestParameterKpi());
+	}
+
+	public String[] getKpiNames() {
+		return new String[] { PARAMETER_NAME };
+	}
+
+	public String[] getKpiPaths() {
+		return new String[] { SERVICE_PID + "/" + PARAMETER_NAME };
+	}
+
+	public KPI[] getKpis() {
+		return new KPI[] { getTestParameterKpi() };
+	}
+
+	public KPI getKpi(String id) throws IllegalArgumentException {
+        if(matchingId(id, PARAMETER_NAME))
+			return getTestParameterKpi();
+        
+        throw new IllegalArgumentException("KPI '" + id + 
+                "' not found in Monitorable '" + SERVICE_PID + "'.");
+	}
+
+	public boolean notifiesOnChange(String id) throws IllegalArgumentException {
+		return true;
+	}
+
+	public boolean resetKpi(String id) throws IllegalArgumentException {
+		return false;
+	}
+
+    private KPI getTestParameterKpi() {
+        return new KPI(SERVICE_PID, PARAMETER_NAME, 
+                "Value of the configuration item of the same name.", 
+                KPI.CM_SI, testParameter);
+    }
+
+    private boolean matchingId(String id, String name) {
+        return name.equals(id) || (SERVICE_PID + '/' + name).equals(id);
+    }
 }
