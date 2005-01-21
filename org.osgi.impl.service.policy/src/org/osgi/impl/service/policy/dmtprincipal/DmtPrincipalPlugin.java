f7a4bd07be237944cfa6d52b406ed170717e376a
Peter Nagy <peter.nagy@nokia.com>
2005-01-21 10:59:49 +0000

    skeleton functionality

---
+++ org.osgi.impl.service.policy/src/org/osgi/impl/service/policy/dmtprincipal/DmtPrincipalPlugin.java
@@ -0,0 +1,233 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors. 
+ * All rights are reserved.
+ * 
+ * These materials have been contributed  to the Open Services Gateway 
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
+ * to the terms of, the OSGi Member Agreement specifically including, but not 
+ * limited to, the license rights and warranty disclaimers as set forth in 
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
+ * All company, brand and product names contained within this document may be 
+ * trademarks that are the sole property of the respective owners.  
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
+
+package org.osgi.impl.service.policy.dmtprincipal;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+import org.osgi.impl.service.dmt.api.DmtPrincipalPermissionAdmin;
+import org.osgi.impl.service.policy.util.HashCalculator;
+import org.osgi.impl.service.policy.util.Splitter;
+import org.osgi.service.dmt.DmtData;
+import org.osgi.service.dmt.DmtDataPlugin;
+import org.osgi.service.dmt.DmtException;
+import org.osgi.service.dmt.DmtMetaNode;
+import org.osgi.service.dmt.DmtSession;
+import org.osgi.service.permissionadmin.PermissionInfo;
+
+/**
+ *
+ * dmt plugin for exposing dmt principal data.
+ * 
+ * @version $Revision$
+ */
+public class DmtPrincipalPlugin implements DmtDataPlugin {
+	
+	private static final String	PERMISSIONINFO	= "PermissionInfo";
+	private static final String	PRINCIPAL	= "Principal";
+	/**
+	 * This object is the interface to the DMT Admin. Through this, we can
+	 * modify the different Principals PermissionInfo.
+	 */
+	private final DmtPrincipalPermissionAdmin dmtPrincipalPermissionAdmin;
+	
+	/**
+	 * The current state of the tree. It is a map from hashes to PrincipalPermissions.
+	 * The hashes are the first level node names of the tree.
+	 */
+	private Map currentState;
+	
+	/**
+	 * Info about a principal and its associated permissions.
+	 */
+	private class PrincipalPermission {
+		String principal;
+		PermissionInfo permissionInfo[];
+		public PrincipalPermission(String principal,PermissionInfo[] permissionInfo) {
+			this.principal = principal;
+			this.permissionInfo = permissionInfo;
+		}
+	}
+
+	/**
+	 * true, if changes are made from the system state
+	 */
+	private boolean dirty;
+
+	/**
+	 * The point where this plugin should go in the DMT.
+	 */
+	public static final String dataRootURI = "./OSGi/Policies/Java/DmtPrincipal";
+	
+	private static final DmtMetaNode rootMetaNode = new RootMetaNode();
+	private final HashCalculator hashCalculator;
+
+	public DmtPrincipalPlugin(DmtPrincipalPermissionAdmin dmtPrincipalPermissionAdmin) throws NoSuchAlgorithmException {
+		this.dmtPrincipalPermissionAdmin = dmtPrincipalPermissionAdmin;
+		this.currentState = null; // open() fills it
+		hashCalculator = new HashCalculator();
+	}
+
+	public void open(String subtreeUri, int lockMode, DmtSession session)
+			throws DmtException {
+		currentState = new HashMap();
+		Map systemState = dmtPrincipalPermissionAdmin.getPrincipalPermissions();
+
+		// copy the permission structure to our tree structure
+		for (Iterator iter = systemState.entrySet().iterator(); iter.hasNext();) {
+			Map.Entry e = (Entry) iter.next();
+			String p =  (String) e.getKey();
+			PermissionInfo[] pi = (PermissionInfo[]) e.getValue();
+			pi = (PermissionInfo[]) pi.clone();
+			currentState.put(hashCalculator.getHash(p),new PrincipalPermission(p,pi));
+		}
+		
+		dirty = false;
+	}
+
+	public DmtMetaNode getMetaNode(String nodeUri)
+			throws DmtException {
+		String[] path = getPath(nodeUri);
+		if (path.length==0) return rootMetaNode;
+
+		// TODO the rest
+		throw new IllegalStateException();
+	}
+
+	public boolean supportsAtomic() {
+		return true;
+	}
+
+	public void rollback() throws DmtException {
+		throw new DmtException(dataRootURI,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void setNodeTitle(String nodeUri, String title) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void setNodeValue(String nodeUri, DmtData data) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void setNodeType(String nodeUri, String type) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void deleteNode(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void createInteriorNode(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void createInteriorNode(String nodeUri, String type)
+			throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void createLeafNode(String nodeUri, DmtData value)
+			throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void copy(String nodeUri, String newNodeUri, boolean recursive)
+			throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void renameNode(String nodeUri, String newName) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public void close() throws DmtException {
+		throw new DmtException("",DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public boolean isNodeUri(String nodeUri) {
+		String path[] = getPath(nodeUri);
+		if (path.length==0) return true;
+		PrincipalPermission pp = null;
+		pp = (PrincipalPermission) currentState.get(path[0]);
+		if (pp==null) return false;
+		if (path.length==1) return true;
+		if (path.length>2) return false;
+		if (path[1].equals(PRINCIPAL)) return true;
+		if (path[2].equals(PERMISSIONINFO)) return true;
+		return false;
+	}
+
+	public DmtData getNodeValue(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public String getNodeTitle(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public String getNodeType(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public int getNodeVersion(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public Date getNodeTimestamp(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public int getNodeSize(String nodeUri) throws DmtException {
+		throw new DmtException(nodeUri,DmtException.FEATURE_NOT_SUPPORTED,"");
+	}
+
+	public String[] getChildNodeNames(String nodeUri) throws DmtException {
+		String[] path = getPath(nodeUri);
+		// note: nodeUri is already checked
+		if (path.length==0) {
+			Set ks = currentState.keySet();
+			String[] ss = new String[ks.size()];
+			return (String[]) ks.toArray(ss);
+		}
+		if (path.length==1) {
+			return new String[] { PRINCIPAL, PERMISSIONINFO };
+		}
+		
+		// since nodeUri is checked, we CANNOT get her
+		throw new IllegalStateException();
+	}
+
+	/**
+	 * return the path elements, from our base
+	 * @param nodeUri
+	 * @return an array of nodenames
+	 */
+	private String[] getPath(String nodeUri) {
+		if (!nodeUri.startsWith(dataRootURI)) 
+			throw new IllegalStateException("Dmt should not give me URIs that are not mine");
+		if (nodeUri.length()==dataRootURI.length()) return new String[] {};
+		return Splitter.split(nodeUri.substring(dataRootURI.length()+1),'/',0);
+	}
+
+}
---
+++ org.osgi.impl.service.policy/src/unittests/DmtPrincipalPluginTest.java
@@ -0,0 +1,102 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors. 
+ * All rights are reserved.
+ * 
+ * These materials have been contributed  to the Open Services Gateway 
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
+ * to the terms of, the OSGi Member Agreement specifically including, but not 
+ * limited to, the license rights and warranty disclaimers as set forth in 
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
+ * All company, brand and product names contained within this document may be 
+ * trademarks that are the sole property of the respective owners.  
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
+package unittests;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.osgi.impl.service.dmt.api.DmtPrincipalPermissionAdmin;
+import org.osgi.impl.service.policy.dmtprincipal.DmtPrincipalPlugin;
+import org.osgi.service.dmt.DmtException;
+import org.osgi.service.dmt.DmtSession;
+import org.osgi.service.permissionadmin.PermissionInfo;
+import unittests.util.DmtPluginTestCase;
+
+/**
+ *
+ * TODO Add Javadoc comment for this type.
+ * 
+ * @version $Revision$
+ */
+public class DmtPrincipalPluginTest extends DmtPluginTestCase implements DmtPrincipalPermissionAdmin {
+	public static final String PRINCIPAL1 = "principal1";
+	public static final String PRINCIPAL1_HASH = "zDcCo9K+A67rtQI3TQEDg6_LEIw";
+	public static final String PRINCIPAL2 = "principal2";
+	public static final String PRINCIPAL2_HASH = "aIUZn8KSmU04HKUyaecKhd0jWsY";
+	
+	/**
+	 * the plugin currently tested
+	 */
+	public DmtPrincipalPlugin plugin;
+
+	/**
+	 * an opened DMT session to our subtree
+	 */
+	public DmtSession dmtSession;
+
+	/**
+	 * permission info table simulation for DmtPrincipal.
+	 * It is String -> PermissionInfo[]
+	 */
+	Map principalPermissions;
+	public Map getPrincipalPermissions() { return principalPermissions; }
+	public void setPrincipalPermissions(Map permissions) { principalPermissions = permissions; }
+
+	public void setUp() throws Exception {
+		super.setUp();
+		principalPermissions = new HashMap();
+		plugin = new DmtPrincipalPlugin(this);
+		addDataPlugin(DmtPrincipalPlugin.dataRootURI,plugin);
+	}
+
+	public void tearDown() throws Exception {
+		plugin = null;
+		principalPermissions = null;
+		dmtSession = null;
+		super.tearDown();
+	}
+
+	public void newSession() throws DmtException {
+		dmtSession = dmtFactory.getSession(DmtPrincipalPlugin.dataRootURI);
+		assertNotNull(dmtSession);
+	}
+
+	public void newAtomicSession() throws DmtException {
+		dmtSession = dmtFactory.getSession(DmtPrincipalPlugin.dataRootURI,DmtSession.LOCK_TYPE_ATOMIC);
+		assertNotNull(dmtSession);
+	}
+
+	public void testEmptyTable() throws Exception {
+		newSession();
+		String[] children = dmtSession.getChildNodeNames(DmtPrincipalPlugin.dataRootURI);
+		assertEquals(0,children.length);
+	}
+
+	public void testFilledTable() throws Exception {
+		principalPermissions.put(PRINCIPAL1,new PermissionInfo[]{});
+		principalPermissions.put(PRINCIPAL2,new PermissionInfo[]{});
+		newSession();
+		String[] children = dmtSession.getChildNodeNames(DmtPrincipalPlugin.dataRootURI);
+		assertEquals(2,children.length);
+		Arrays.sort(children);
+		assertEquals(PRINCIPAL2_HASH,children[0]);
+		assertEquals(PRINCIPAL1_HASH,children[1]);
+		
+		
+	}
+}
