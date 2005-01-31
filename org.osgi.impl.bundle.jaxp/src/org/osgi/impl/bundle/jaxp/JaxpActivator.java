55a4b42cadc2caa15b50b13d85301c7018a3f2bc
Peter Kriens <peter.kriens@aqute.se>
2005-01-31 11:19:31 +0000

    *** empty log message ***

--- org.osgi.impl.bundle.jaxp/src/org/osgi/impl/bundle/jaxp/JaxpActivator.java
+++ org.osgi.impl.bundle.jaxp/src/org/osgi/impl/bundle/jaxp/JaxpActivator.java
@@ -11,27 +11,43 @@ public class JaxpActivator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
 		DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
-		if ( f != null ) {
-			f.setNamespaceAware(true);
-			f.setValidating(true);
-			Hashtable properties = new Hashtable();
-			properties.put( XMLParserActivator.PARSER_NAMESPACEAWARE, ""+f.isNamespaceAware());
-			properties.put( XMLParserActivator.PARSER_VALIDATING, ""+f.isValidating());
-			context.registerService(DocumentBuilderFactory.class.getName(), f, properties);
+		try {
+			if (f != null) {
+				f.setNamespaceAware(true);
+				f.setValidating(true);
+				Hashtable properties = new Hashtable();
+				properties.put(XMLParserActivator.PARSER_NAMESPACEAWARE, ""
+						+ f.isNamespaceAware());
+				properties.put(XMLParserActivator.PARSER_VALIDATING, ""
+						+ f.isValidating());
+				context.registerService(DocumentBuilderFactory.class.getName(),
+						f, properties);
+			}
+			else
+				System.err.println("No DocumentBuilderFactory");
 		}
-		else
-			System.err.println("No DocumentBuilderFactory");
-		SAXParserFactory s = SAXParserFactory.newInstance();
-		if ( s != null ) {
-			s.setNamespaceAware(true);
-			s.setValidating(true);
-			Hashtable properties = new Hashtable();
-			properties.put( XMLParserActivator.PARSER_NAMESPACEAWARE, ""+s.isNamespaceAware());
-			properties.put( XMLParserActivator.PARSER_VALIDATING, ""+s.isValidating());
-			context.registerService(SAXParserFactory.class.getName(), s, properties);
+		catch (Exception e) {
+			e.printStackTrace();
 		}
-		else
-			System.err.println("No SAXParserFactory");
+		try {
+			SAXParserFactory s = SAXParserFactory.newInstance();
+			if (s != null) {
+				s.setNamespaceAware(true);
+				s.setValidating(true);
+				Hashtable properties = new Hashtable();
+				properties.put(XMLParserActivator.PARSER_NAMESPACEAWARE, ""
+						+ s.isNamespaceAware());
+				properties.put(XMLParserActivator.PARSER_VALIDATING, ""
+						+ s.isValidating());
+				context.registerService(SAXParserFactory.class.getName(), s,
+						properties);
+			}
+			else
+				System.err.println("No SAXParserFactory");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void stop(BundleContext context) throws Exception {
