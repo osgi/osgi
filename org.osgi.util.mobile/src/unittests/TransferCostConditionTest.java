19bf415f63d311ac234d003c509a6550021a744e
Peter Nagy <peter.nagy@nokia.com>
2005-02-04 08:30:06 +0000

    *** empty log message ***

---
+++ org.osgi.util.mobile/src/unittests/TransferCostConditionTest.java
@@ -0,0 +1,71 @@
+/*
+ * $Header$
+ * 
+ * Copyright (c) OSGi Alliance (2005). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
+
+package unittests;
+
+import org.osgi.service.condpermadmin.Condition;
+import org.osgi.util.mobile.TransferCostCondition;
+import junit.framework.TestCase;
+
+public class TransferCostConditionTest extends TestCase {
+	public void testHighLimit() throws Exception {
+		Condition c = TransferCostCondition.getInstance(null,"HIGH");
+		TransferCostCondition.setTransferCost(null);
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("LOW");
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("MEDIUM");
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("HIGH");
+		assertTrue(c.isSatisfied());
+	}
+	
+	public void testMediumLimit() throws Exception {
+		Condition c = TransferCostCondition.getInstance(null,"MEDIUM");
+		TransferCostCondition.setTransferCost(null);
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("LOW");
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("MEDIUM");
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("HIGH");
+		assertFalse(c.isSatisfied());
+	}
+
+	public void testLowLimit() throws Exception {
+		Condition c = TransferCostCondition.getInstance(null,"LOW");
+		TransferCostCondition.setTransferCost(null);
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("LOW");
+		assertTrue(c.isSatisfied());
+		TransferCostCondition.setTransferCost("MEDIUM");
+		assertFalse(c.isSatisfied());
+		TransferCostCondition.setTransferCost("HIGH");
+		assertFalse(c.isSatisfied());
+	}
+
+}
