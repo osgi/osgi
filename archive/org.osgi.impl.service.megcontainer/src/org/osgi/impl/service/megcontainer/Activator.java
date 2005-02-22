6e8217676a61c8cfd39195b848ad7703300221cf
ckarai <cvsadmin@www.osgi.org>
2005-02-22 14:25:42 +0000

    meglet initial

--- archive/org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/megcontainer/Activator.java
+++ archive/org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/megcontainer/Activator.java
@@ -2,26 +2,23 @@
  * ============================================================================
  * (c) Copyright 2004 Nokia
  * This material, including documentation and any related computer programs,
- * is protected by copyright controlled by Nokia and its licensors. 
+ * is protected by copyright controlled by Nokia and its licensors.
  * All rights are reserved.
- * 
- * These materials have been contributed  to the Open Services Gateway 
- * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
- * to the terms of, the OSGi Member Agreement specifically including, but not 
- * limited to, the license rights and warranty disclaimers as set forth in 
- * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
- * All company, brand and product names contained within this document may be 
- * trademarks that are the sole property of the respective owners.  
+ *
+ * These materials have been contributed  to the Open Services Gateway
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject
+ * to the terms of, the OSGi Member Agreement specifically including, but not
+ * limited to, the license rights and warranty disclaimers as set forth in
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work.
+ * All company, brand and product names contained within this document may be
+ * trademarks that are the sole property of the respective owners.
  * The above notice must be included on all copies of this document.
  * ============================================================================
  */
 package org.osgi.impl.service.megcontainer;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
+import java.util.*;
 import org.osgi.framework.*;
-import org.osgi.service.event.EventConstants;
 
 /**
  * The Bundle Activator of the MEG container
@@ -29,9 +26,8 @@ import org.osgi.service.event.EventConstants;
 public class Activator extends Object implements
 		BundleActivator {
 	private BundleContext		bc;
-	private ServiceRegistration	serviceReg;
 	private ServiceRegistration	serviceListener;
-	private MEGContainerImpl	megContainerImpl;
+	private MegletContainer		megletContainer;
 
 	public Activator() {
 		super();
@@ -39,28 +35,19 @@ public class Activator extends Object implements
 
 	public void start(BundleContext bc) throws Exception {
 		this.bc = bc;
-		megContainerImpl = new MEGContainerImpl(bc, "MEG");
-		Dictionary properties = new Hashtable();
-		properties.put("application_type", "MEG");
-		properties.put("bundle_id", new Long(bc.getBundle().getBundleId())
-				.toString());
-		//registering the services
-		serviceReg = bc.registerService(
-				"org.osgi.service.application.ApplicationContainer",
-				megContainerImpl, properties);
+		megletContainer = new MegletContainer(bc);
 		Hashtable serviceListenerProps = new Hashtable();
-		serviceListenerProps.put(EventConstants.EVENT_TOPIC, new String[] {"*"});
+		serviceListenerProps.put("topic", new String[] { "*" } );
 		serviceListener = bc.registerService(
-				"org.osgi.service.event.EventHandler", megContainerImpl,
+				"org.osgi.service.event.EventHandler", megletContainer,
 				serviceListenerProps);
 		System.out.println("MEG container started successfully!");
 	}
 
 	public void stop(BundleContext bc) throws Exception {
 		//unregistering the service
-		megContainerImpl.unregisterAllApplications();
+		megletContainer.unregisterAllApplications();
 		serviceListener.unregister();
-		serviceReg.unregister();
 		this.bc = null;
 		System.out.println("MEG container stopped successfully!");
 	}
---
+++ archive/org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/megcontainer/MegletHandle.java
@@ -0,0 +1,207 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors.
+ * All rights are reserved.
+ *
+ * These materials have been contributed  to the Open Services Gateway
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject
+ * to the terms of, the OSGi Member Agreement specifically including, but not
+ * limited to, the license rights and warranty disclaimers as set forth in
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work.
+ * All company, brand and product names contained within this document may be
+ * trademarks that are the sole property of the respective owners.
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
+package org.osgi.impl.service.megcontainer;
+
+import java.util.*;
+import java.security.*;
+import org.osgi.framework.*;
+import org.osgi.service.application.*;
+import org.osgi.service.event.*;
+
+/**
+ * This class realizes the Application Handle
+ */
+public class MegletHandle implements ApplicationHandle {
+	private int						status;
+	private ApplicationDescriptor	appDesc;
+	private Meglet					meglet;
+	private ServiceRegistration		serviceReg;
+	private BundleContext			bc;
+	private Long					appHndServiceID;
+	private Long					appDescServiceID;
+
+	public MegletHandle(Meglet meglet, ApplicationDescriptor desc, BundleContext bc)
+			throws Exception {
+		appDesc = desc;
+		status = ApplicationHandle.NONEXISTENT;
+		this.bc = bc;
+		this.meglet = meglet;
+		appHndServiceID = appDescServiceID = null;
+		setStatus( status );
+	}
+
+	public int getAppStatus() {
+		return status;
+	}
+
+	void setStatus( int status ) {
+		int lastStatus = this.status;
+		this.status = status;
+
+		Hashtable props = new Hashtable();
+		props.put( "name", (String)appDesc.getProperties("").get(
+			ApplicationDescriptor.APPLICATION_NAME ) );
+		if( appHndServiceID != null )
+			props.put( "application_instance.id", new Long( appHndServiceID.longValue() ) );
+		if( appDescServiceID != null )
+			props.put( "application_descriptor.id", new Long( appDescServiceID.longValue() ) );
+
+		String topic = "org.osgi.application.";
+
+		switch( status ) {
+		case ApplicationHandle.RUNNING:
+			if( lastStatus == ApplicationHandle.RESUMING )
+				topic += "resumed";
+			else if( lastStatus == ApplicationHandle.NONEXISTENT )
+				topic += "started";
+			break;
+		case ApplicationHandle.SUSPENDING:
+			topic += "suspending";
+			break;
+		case ApplicationHandle.SUSPENDED:
+			topic += "suspended";
+			break;
+		case ApplicationHandle.RESUMING:
+			topic += "resuming";
+			break;
+		case ApplicationHandle.STOPPING:
+			topic += "stopping";
+			break;
+		case ApplicationHandle.NONEXISTENT:
+			if( lastStatus == ApplicationHandle.STOPPING )
+				topic += "stopped";
+			else if( lastStatus == ApplicationHandle.NONEXISTENT )
+				topic += "starting";
+			break;
+		}
+
+		sendEvent( new Event( topic, props ), false );
+	}
+
+	boolean sendEvent( Event event, boolean asynchron ) {
+		ServiceReference eventRef = bc.getServiceReference( "org.osgi.service.event.EventAdmin" );
+		if (eventRef != null) {
+			EventAdmin eventAdmin = (EventAdmin) bc.getService( eventRef );
+			if (eventAdmin != null) {
+				try {
+					if( asynchron )
+						eventAdmin.postEvent( event );
+					else
+						eventAdmin.sendEvent( event );
+					return true;
+				} finally {
+					bc.ungetService( eventRef );
+				}
+			}
+		}
+		return false;
+	}
+
+	public ApplicationDescriptor getAppDescriptor() {
+		return appDesc;
+	}
+
+
+	public void startHandle( Map args ) throws Exception {
+		AccessController.checkPermission( new ApplicationAdminPermission(
+			appDesc.getApplicationPID(), ApplicationAdminPermission.LAUNCH ) );
+
+        if (status != ApplicationHandle.NONEXISTENT )
+			throw new Exception("Invalid State");
+
+		if (meglet != null) {
+			meglet.startApplication( args );
+			setStatus ( ApplicationHandle.RUNNING );
+			registerAppHandle();
+		}
+		else
+			throw new Exception("Invalid meglet handle!");
+	}
+
+	public void destroyApplication() throws Exception {
+
+		AccessController.checkPermission( new ApplicationAdminPermission(
+			appDesc.getApplicationPID(), ApplicationAdminPermission.MANIPULATE ) );
+
+		if (status == ApplicationHandle.NONEXISTENT
+				|| status == ApplicationHandle.STOPPING)
+			throw new Exception("Invalid State");
+		if (meglet != null) {
+			setStatus ( ApplicationHandle.STOPPING );
+			meglet.stopApplication();
+			setStatus ( ApplicationHandle.NONEXISTENT );
+			unregisterAppHandle();
+		}
+		else
+			throw new Exception("Invalid meglet handle!");
+	}
+
+	public void suspendApplication() throws Exception {
+
+		AccessController.checkPermission( new ApplicationAdminPermission(
+			appDesc.getApplicationPID(), ApplicationAdminPermission.MANIPULATE ) );
+
+		if (status != ApplicationHandle.RUNNING)
+			throw new Exception("Invalid State");
+		if (meglet != null) {
+			setStatus ( ApplicationHandle.SUSPENDING );
+			meglet.suspendApplication();
+			setStatus ( ApplicationHandle.SUSPENDED );
+		}
+		else
+			throw new Exception("Invalid meglet handle!");
+	}
+
+	public void resumeApplication() throws Exception {
+
+		AccessController.checkPermission( new ApplicationAdminPermission(
+			appDesc.getApplicationPID(), ApplicationAdminPermission.MANIPULATE ) );
+
+		if (status != ApplicationHandle.SUSPENDED)
+			throw new Exception("Invalid State");
+		if (meglet != null) {
+			setStatus ( ApplicationHandle.RESUMING );
+			meglet.resumeApplication();
+			setStatus ( ApplicationHandle.RUNNING );
+		}
+		else
+			throw new Exception("Invalid meglet handle!");
+	}
+
+	void registerAppHandle() throws Exception {
+		//registering the ApplicationHandle
+		Hashtable props = new Hashtable();
+		props.put( Constants.SERVICE_PID , appDesc.getApplicationPID());
+		serviceReg = bc.registerService(
+				"org.osgi.service.application.ApplicationHandle", this, props);
+		appHndServiceID = (Long)serviceReg.getReference().getProperty( Constants.SERVICE_ID );
+		ServiceReference[] appDescRefs = bc.getServiceReferences(
+				"org.osgi.service.application.ApplicationDescriptor", "(" +
+				Constants.SERVICE_PID + "=" + appDesc.getApplicationPID() + ")" );
+		if( appDescRefs != null && appDescRefs.length != 0 )
+			appDescServiceID = (Long)appDescRefs[ 0 ].getProperty( Constants.SERVICE_ID );
+	}
+
+	void unregisterAppHandle() {
+		if (serviceReg != null) {
+			//unregistering the ApplicationHandle
+			serviceReg.unregister();
+			serviceReg = null;
+		}
+	}
+}
