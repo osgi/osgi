948cf41449de017457ad73c20072564e9e59b28d
ckarai <cvsadmin@www.osgi.org>
2005-02-22 14:09:27 +0000

    scheduler

---
+++ archive/org.osgi.impl.service.scheduler/src/org/osgi/impl/service/scheduler/ScheduledApplicationImpl.java
@@ -0,0 +1,137 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors.
+ * All rights are reserved.
+ *
+ * These materials have been contributed  to the Open Services Gateway
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject
+ * to the terms of, the OSGi Member Agreement specifically including, but not
+ * limited to, the license rights and warranty disclaimers as set forth in
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work.
+ * All company, brand and product names contained within this document may be
+ * trademarks that are the sole property of the respective owners.
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
+package org.osgi.impl.service.scheduler;
+
+import java.io.*;
+import java.util.*;
+import org.osgi.framework.*;
+import org.osgi.service.application.*;
+import org.osgi.service.application.Scheduler;
+import org.osgi.service.log.LogService;
+
+/**
+ * The realization of the ScheduledApplication interface
+ */
+public class ScheduledApplicationImpl implements ScheduledApplication,
+		Comparable, Serializable {
+	private ApplicationDescriptor	appDesc;
+	private Date					schedDate;
+	private Hashtable				args;
+	private BundleContext			bc;
+	private String					appUID;
+	private boolean					launchOnOverdue;
+	private SchedulerImpl			scheduler;
+
+	public ScheduledApplicationImpl(Scheduler scheduler, BundleContext bc,
+			ApplicationDescriptor appDesc, Map args, Date schedDate, boolean launchOnOverdue ) {
+		this.scheduler = (SchedulerImpl)scheduler;
+		this.bc = bc;
+		this.appDesc = appDesc;
+		this.schedDate = schedDate;
+		this.args = new Hashtable(args);
+		this.appUID = appDesc.getApplicationPID();
+		this.launchOnOverdue = launchOnOverdue;
+	}
+
+	public void validate(Scheduler scheduler, BundleContext bc)
+			throws Exception {
+		this.bc = bc;
+		this.scheduler = (SchedulerImpl)scheduler;
+
+		try {
+			ServiceReference[] references = bc.getServiceReferences(
+					"org.osgi.service.application.ApplicationDescriptor", null);
+			if (references == null || references.length == 0)
+				return;
+
+			for (int i = 0; i != references.length; i++) {
+				try {
+					ApplicationDescriptor appDesc = (ApplicationDescriptor) bc
+						.getService(references[i]);
+					if( appDesc.getApplicationPID().equals( appUID ) ) {
+						this.appDesc = appDesc;
+						break;
+					}
+					bc.ungetService(references[i]);
+				}catch( SecurityException e ) {}
+			}
+		}
+		catch (Exception e) {
+			SchedulerImpl.log( bc, LogService.LOG_ERROR,
+				"Exception occurred at searching the application descriptors!", e);
+		}
+	}
+
+	public Date getDate() {
+		return (Date) schedDate.clone();
+	}
+
+	public boolean launchOnOverdue() {
+		return launchOnOverdue;
+	}
+
+	public Map getArguments() {
+		return new Hashtable( args );
+	}
+
+	public ApplicationDescriptor getApplicationDescriptor() {
+		return appDesc;
+	}
+
+	public void remove() {
+		try {
+			scheduler.removeScheduledApplication( this );
+		}
+		catch (Exception e) {
+			SchedulerImpl.log( bc, LogService.LOG_ERROR,
+				"Exception occurred at removing a scheduled application!", e);
+		}
+	}
+
+	public int compareTo(Object schedObj) {
+		ScheduledApplicationImpl schedApp = (ScheduledApplicationImpl) schedObj;
+		int compResult = getDate().compareTo(schedApp.getDate());
+		if (compResult != 0)
+			return compResult;
+		if (getArguments().size() < schedApp.getArguments().size())
+			return -1;
+		if (getArguments().size() > schedApp.getArguments().size())
+			return 1;
+		if (schedApp.equals(this))
+			return 0;
+		if (hashCode() < schedApp.hashCode())
+			return -1;
+		else
+			return 1;
+	}
+
+	private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+		out.writeObject(appUID);
+		out.writeObject(schedDate);
+		out.writeObject(args);
+	}
+
+	private void readObject(java.io.ObjectInputStream in) throws IOException,
+			ClassNotFoundException {
+		appDesc = null;
+		bc = null;
+		appUID = (String) in.readObject();
+		schedDate = (Date) in.readObject();
+		args = (Hashtable) in.readObject();
+	}
+}
---
+++ archive/org.osgi.impl.service.scheduler/src/org/osgi/impl/service/scheduler/SchedulerImpl.java
@@ -0,0 +1,168 @@
+/*
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors.
+ * All rights are reserved.
+ *
+ * These materials have been contributed  to the Open Services Gateway
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject
+ * to the terms of, the OSGi Member Agreement specifically including, but not
+ * limited to, the license rights and warranty disclaimers as set forth in
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work.
+ * All company, brand and product names contained within this document may be
+ * trademarks that are the sole property of the respective owners.
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
+ */
+
+package org.osgi.impl.service.scheduler;
+
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import org.osgi.framework.*;
+import org.osgi.service.application.*;
+import org.osgi.service.log.LogService;
+import org.osgi.service.application.Scheduler;
+
+public class SchedulerImpl implements Scheduler, Runnable {
+    private BundleContext   bc;
+    private TreeSet         scheduledApps;
+    private boolean         stopped;
+    private Thread          schedulerThread;
+
+    public SchedulerImpl( BundleContext bc ) {
+        this.bc = bc;
+        scheduledApps = new TreeSet();
+        loadScheduledApplications();
+        stopped = false;
+        schedulerThread = new Thread(this);
+        schedulerThread.start();
+    }
+
+    public void stop() {
+        stopped = true;
+        schedulerThread.interrupt();
+    }
+
+    public synchronized ScheduledApplication addScheduledApplication(
+            ApplicationDescriptor appDescriptor, Map arguments, Date date, boolean launchOnOverdue ) {
+
+        AccessController.checkPermission( new ApplicationAdminPermission(
+            appDescriptor.getApplicationPID(), ApplicationAdminPermission.SCHEDULE ) );
+
+        ScheduledApplication app = new ScheduledApplicationImpl(this, bc,
+                appDescriptor, arguments, date, launchOnOverdue);
+        scheduledApps.add(app);
+        saveScheduledApplications();
+        schedulerThread.interrupt();
+        return app;
+    }
+
+    public synchronized void removeScheduledApplication(
+            ScheduledApplication scheduledApplication) throws Exception {
+
+        AccessController.checkPermission( new ApplicationAdminPermission(
+            scheduledApplication.getApplicationDescriptor().getApplicationPID(),
+            ApplicationAdminPermission.SCHEDULE ) );
+
+        scheduledApps.remove(scheduledApplication);
+        saveScheduledApplications();
+    }
+
+    private synchronized void loadScheduledApplications() {
+        try {
+            File schedApps = bc.getDataFile("ScheduledApplications");
+            if( !schedApps.exists() )
+                return;
+            FileInputStream stream = new FileInputStream(schedApps);
+            ObjectInputStream is = new ObjectInputStream(stream);
+            scheduledApps = (TreeSet) is.readObject();
+            is.close();
+            Iterator it = scheduledApps.iterator();
+            while (it.hasNext()) {
+                ScheduledApplicationImpl schedApp = (ScheduledApplicationImpl) it
+                        .next();
+                schedApp.validate(this, bc);
+            }
+        }
+        catch (Exception e) {
+            log(bc, LogService.LOG_ERROR,
+                "Exception occurred at loading the scheduled applications!", e);
+        }
+    }
+
+    private synchronized void saveScheduledApplications() {
+        try {
+            File schedApps = bc.getDataFile("ScheduledApplications");
+            FileOutputStream stream = new FileOutputStream(schedApps);
+            ObjectOutputStream os = new ObjectOutputStream(stream);
+            os.writeObject(scheduledApps);
+            os.close();
+        }
+        catch (Exception e) {
+            log(bc, LogService.LOG_ERROR,
+                "Exception occurred at saving the scheduled applications!", e);
+        }
+    }
+
+    public void run() {
+        while (!stopped) {
+            long currentTime = System.currentTimeMillis();
+            long startTime;
+            do {
+                startTime = currentTime + 1000000000000l; /* means infinity */
+                try {
+                    ScheduledApplicationImpl schedApp = (ScheduledApplicationImpl) scheduledApps
+                            .first();
+                    startTime = schedApp.getDate().getTime();
+                    if (startTime <= currentTime) {
+                        long diffTime = currentTime - startTime;
+
+                        if( diffTime < 60000 || schedApp.launchOnOverdue() ) { /* after 1 min, it's overdue */
+                            try {
+                                schedApp.getApplicationDescriptor().launchApplication(
+                                        schedApp.getArguments());
+                            }
+                            catch (Exception e) {
+                                log(bc, LogService.LOG_ERROR,
+                                    "Exception occurred at launching a scheduled application!", e);
+                            }
+                        }
+                        schedApp.remove();
+                        saveScheduledApplications();
+                    }
+                }
+                catch (NoSuchElementException e) {
+                }
+            } while (startTime <= currentTime);
+            long sleepTime = startTime - currentTime;
+            try {
+                Thread.sleep(sleepTime);
+            }
+            catch (InterruptedException e) {
+            }
+        }
+    }
+
+    static boolean log( BundleContext bc, int severity, String message, Throwable throwable) {
+        System.out.println("Serverity:" + severity + " Message:" + message
+                + " Throwable:" + throwable);
+
+        ServiceReference serviceRef = bc.getServiceReference("org.osgi.service.log.LogService");
+        if (serviceRef != null) {
+            LogService logService = (LogService) bc.getService(serviceRef);
+            if (logService != null) {
+                try {
+                    logService.log(severity, message, throwable);
+                    return true;
+                }
+                finally {
+                    bc.ungetService(serviceRef);
+                }
+            }
+        }
+        return false;
+    }
+}
