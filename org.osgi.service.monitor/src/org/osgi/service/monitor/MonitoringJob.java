fb5cb7699b4b4866b8393b968adde4cad4c95eaa
Tamas Szeredi <tamas.szeredi@nokia.com>
2005-03-09 16:36:02 +0000

    renamed KPI to StatusVariable

--- org.osgi.service.monitor/src/org/osgi/service/monitor/MonitoringJob.java
+++ org.osgi.service.monitor/src/org/osgi/service/monitor/MonitoringJob.java
@@ -26,23 +26,26 @@
 package org.osgi.service.monitor;
 
 /**
- * A Monitoring Job is a scheduled update of a set of KPIs. The job is a data
- * structure that holds a non-empty list of KPI names, an identification of the
- * initiator of the job, and the sampling parameters. There are two kinds of
- * monitoring jobs: time based and change based. Time based jobs take samples of
- * all KPIs with a specified frequency. The number of samples to be taken before
- * the job finishes may be specified. Change based jobs are only interested in
- * the changes of the monitored KPIs. In this case, the number of changes that
- * must take place between two notifications can be specified.
+ * A Monitoring Job is a request for scheduled or event based notifications on
+ * update of a set of StatusVariables. The job is a data structure that holds a
+ * non-empty list of StatusVariable names, an identification of the initiator of
+ * the job, and the sampling parameters. There are two kinds of monitoring jobs:
+ * time based and change based. Time based jobs take samples of all
+ * StatusVariables with a specified frequency. The number of samples to be taken
+ * before the job finishes may be specified. Change based jobs are only
+ * interested in the changes of the monitored StatusVariables. In this case, the
+ * number of changes that must take place between two notifications can be
+ * specified.
  * <p>
  * The job can be started on the MonitorAdmin interface. Running the job
- * (querying the KPIs, listening to changes, and sending out notifications on
- * updates) is the task of the MonitorAdmin implementation.
+ * (querying the StatusVariables, listening to changes, and sending out
+ * notifications on updates) is the task of the MonitorAdmin implementation.
  * <p>
- * Whether a monitoring job keeps track dynamically of the KPIs it monitors is
- * not specified. This means that if we monitor a KPI of a Monitorable service
- * which disappears and later reappears then it is implementation specific
- * whether we still receive updates of the KPI changes or not.
+ * Whether a monitoring job keeps track dynamically of the StatusVariables it
+ * monitors is not specified. This means that if we monitor a StatusVariable of
+ * a Monitorable service which disappears and later reappears then it is
+ * implementation specific whether we still receive updates of the
+ * StatusVariable changes or not.
  */
 public interface MonitoringJob {
     /**
@@ -63,22 +66,23 @@ public interface MonitoringJob {
     public String getInitiator();
 
     /**
-	 * Returns the list of KPI names that are the targets of this measurement
-	 * job. For time based jobs, the MonitorAdmin will iterate through this list
-	 * and query all KPIs when its timer set by the job's frequency rate
-	 * expires.
-	 * 
-	 * @return the target list of the measurement job in
-	 *         [Monitorable_ID]/[KPI_ID] format
-	 */
-    public String[] getKpiNames();
+     * Returns the list of StatusVariable names that are the targets of this
+     * measurement job. For time based jobs, the MonitorAdmin will iterate
+     * through this list and query all StatusVariables when its timer set by the
+     * job's frequency rate expires.
+     * 
+     * @return the target list of the measurement job in
+     *         [Monitorable_ID]/[StatusVariable_ID] format
+     */
+    public String[] getStatusVariableNames();
 
     /**
      * Returns the delay (in seconds) between two samples. If this call returns
-     * N (greater than 0) then the MonitorAdmin queries each KPI that belongs to
-     * this job every N seconds. The value 0 means that instant notification on
-     * changes is requested (at every nth change of the value, as specified by
-     * the report count parameter).
+     * N (greater than 0) then the MonitorAdmin queries each StatusVariable that
+     * belongs to this job every N seconds. The value 0 means that the job is
+     * not scheduled but event based: in this case instant
+     * notification on changes is requested (at every nth change of the value,
+     * as specified by the report count parameter).
      * 
      * @return the delay (in seconds) between samples, or 0 for change based
      *         jobs
@@ -86,16 +90,16 @@ public interface MonitoringJob {
     public long getSchedule();
 
     /**
-	 * Returns the number of times MonitorAdmin will query the KPIs (for time
-	 * based jobs), or the number of changes of a KPI between notifications (for
-	 * change based jobs). Time based jobs with non-zero report count will take
-	 * getReportCount()*getSchedule() time to finish. Time based jobs with 0
-	 * report count and change based jobs do not stop automatically, but all
-	 * jobs can be stopped with the stop method.
-	 * 
-	 * @return the number of measurements to be taken, or the number of changes
-	 *         between notifications
-	 */
+     * Returns the number of times MonitorAdmin will query the StatusVariables
+     * (for time based jobs), or the number of changes of a StatusVariable
+     * between notifications (for change based jobs). Time based jobs with
+     * non-zero report count will take getReportCount()*getSchedule() time to
+     * finish. Time based jobs with 0 report count and change based jobs do not
+     * stop automatically, but all jobs can be stopped with the {@link #stop} method.
+     * 
+     * @return the number of measurements to be taken, or the number of changes
+     *         between notifications
+     */
     public int getReportCount();
 
     /**
