59006fc6989ec75477158cf133c3064b6a3740cd
BJ Hargrave <hargrave@us.ibm.com>
2005-03-20 16:54:07 +0000

    update javadoc for isAssignableTo

--- org.osgi.framework/src/org/osgi/framework/ServiceReference.java
+++ org.osgi.framework/src/org/osgi/framework/ServiceReference.java
@@ -37,16 +37,16 @@ import java.util.Dictionary;
  * <code>BundleContext.getServiceReference</code> and
  * <code>BundleContext.getServiceReferences</code> methods.
  * <p>
- * A <code>ServiceReference</code> may be shared between bundles and can be used
- * to examine the properties of the service and to get the service object.
+ * A <code>ServiceReference</code> may be shared between bundles and can be
+ * used to examine the properties of the service and to get the service object.
  * <p>
  * Every service registered in the Framework has a unique
  * <code>ServiceRegistration</code> object and may have multiple, distinct
  * <code>ServiceReference</code> objects referring to it.
  * <code>ServiceReference</code> objects associated with a
- * <code>ServiceRegistration</code> object have the same <code>hashCode</code> and
- * are considered equal (more specifically, their <code>equals()</code> method
- * will return <code>true</code> when compared).
+ * <code>ServiceRegistration</code> object have the same <code>hashCode</code>
+ * and are considered equal (more specifically, their <code>equals()</code>
+ * method will return <code>true</code> when compared).
  * <p>
  * If the same service object is registered multiple times,
  * <code>ServiceReference</code> objects associated with different
@@ -61,8 +61,8 @@ import java.util.Dictionary;
 public abstract interface ServiceReference {
 	/**
 	 * Returns the property value to which the specified property key is mapped
-	 * in the properties <code>Dictionary</code> object of the service referenced
-	 * by this <code>ServiceReference</code> object.
+	 * in the properties <code>Dictionary</code> object of the service
+	 * referenced by this <code>ServiceReference</code> object.
 	 * 
 	 * <p>
 	 * Property keys are case-insensitive.
@@ -70,12 +70,12 @@ public abstract interface ServiceReference {
 	 * <p>
 	 * This method must continue to return property values after the service has
 	 * been unregistered. This is so references to unregistered services (for
-	 * example, <code>ServiceReference</code> objects stored in the log) can still
-	 * be interrogated.
+	 * example, <code>ServiceReference</code> objects stored in the log) can
+	 * still be interrogated.
 	 * 
 	 * @param key The property key.
-	 * @return The property value to which the key is mapped; <code>null</code> if
-	 *         there is no property named after the key.
+	 * @return The property value to which the key is mapped; <code>null</code>
+	 *         if there is no property named after the key.
 	 */
 	public abstract Object getProperty(String key);
 
@@ -87,8 +87,8 @@ public abstract interface ServiceReference {
 	 * <p>
 	 * This method will continue to return the keys after the service has been
 	 * unregistered. This is so references to unregistered services (for
-	 * example, <code>ServiceReference</code> objects stored in the log) can still
-	 * be interrogated.
+	 * example, <code>ServiceReference</code> objects stored in the log) can
+	 * still be interrogated.
 	 * 
 	 * <p>
 	 * This method is <i>case-preserving </i>; this means that every key in the
@@ -106,50 +106,58 @@ public abstract interface ServiceReference {
 	 * <code>ServiceReference</code> object.
 	 * 
 	 * <p>
-	 * This method will always return <code>null</code> when the service has been
-	 * unregistered. This can be used to determine if the service has been
+	 * This method will always return <code>null</code> when the service has
+	 * been unregistered. This can be used to determine if the service has been
 	 * unregistered.
 	 * 
 	 * @return The bundle that registered the service referenced by this
-	 *         <code>ServiceReference</code> object; <code>null</code> if that
-	 *         service has already been unregistered.
+	 *         <code>ServiceReference</code> object; <code>null</code> if
+	 *         that service has already been unregistered.
 	 * @see BundleContext#registerService(String[],Object,Dictionary)
 	 */
 	public abstract Bundle getBundle();
 
 	/**
 	 * Returns the bundles that are using the service referenced by this
-	 * <code>ServiceReference</code> object. Specifically, this method returns the
-	 * bundles whose usage count for that service is greater than zero.
+	 * <code>ServiceReference</code> object. Specifically, this method returns
+	 * the bundles whose usage count for that service is greater than zero.
 	 * 
 	 * @return An array of bundles whose usage count for the service referenced
-	 *         by this <code>ServiceReference</code> object is greater than zero;
-	 *         <code>null</code> if no bundles are currently using that service.
+	 *         by this <code>ServiceReference</code> object is greater than
+	 *         zero; <code>null</code> if no bundles are currently using that
+	 *         service.
 	 * 
 	 * @since 1.1
 	 */
 	public abstract Bundle[] getUsingBundles();
 
 	/**
-	 * Returns true if the bundle which registered the service referenced by this
-	 * <tt>ServiceReference</tt> and the specified bundle use the same source for
-	 * the package of the specified class name.
+	 * Tests if the bundle which registered the service referenced by this
+	 * <tt>ServiceReference</tt> and the specified bundle use the same source
+	 * for the package of the specified class name.
 	 * <p>
-	 * This method performs the following checks: <br>
-	 * 1)  Get the package name from the className <br>
-	 * 2)  For the bundle that registered the service referenced by this
-	 * <tt>ServiceReference</tt> object (registrant bundle); find the source for the package.  
-	 * If no source is found then return true if the registrant bundle is equal to 
-	 * the specified bundle; otherwise return false; <br>
-	 * 3)  If the package source of the registrant bundle is equal to the package source of the 
-	 * specified bundle then return true; otherwise return false. <br>
-	 * @param bundle the <tt>Bundle</tt> object to check the package source for
-	 * @param className the class name to check the package source for
-	 * @return true if the bundle which registered the service referenced by this
-	 * <tt>ServiceReference</tt> and the specified bundle use the same source for
-	 * the package of the specified class name; otherwise false is returned.
+	 * This method performs the following checks:
+	 * <ol>
+	 * <li>Get the package name from the specified class name.</li>
+	 * <li>For the bundle that registered the service referenced by this
+	 * <tt>ServiceReference</tt> (registrant bundle); find the source for the
+	 * package. If no source is found then return <code>true</code> if the
+	 * registrant bundle is equal to the specified bundle; otherwise return
+	 * <code>false</code>.</li>
+	 * <li>If the package source of the registrant bundle is equal to the
+	 * package source of the specified bundle then return <code>true</code>;
+	 * otherwise return <code>false</code>.</li>
+	 * </ol>
+	 * 
+	 * @param bundle The <tt>Bundle</tt> object to check.
+	 * @param className The class name to check.
+	 * @return <code>true</code> if the bundle which registered the service
+	 *         referenced by this <tt>ServiceReference</tt> and the specified
+	 *         bundle use the same source for the package of the specified class
+	 *         name. Otherwise <code>false</code> is returned.
+	 * 
+	 * @since 1.3
 	 */
-	public abstract boolean isAssignableTo(Bundle bundle, String className); 
+	public abstract boolean isAssignableTo(Bundle bundle, String className);
 
 }
-
