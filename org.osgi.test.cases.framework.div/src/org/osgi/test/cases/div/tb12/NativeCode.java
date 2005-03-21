7fb2b216773d323613930f6a327c9403cb4586c3
jmascena <cvsadmin@www.osgi.org>
2005-03-21 19:12:11 +0000

    RFC0072

--- org.osgi.test.cases.framework.div/src/org/osgi/test/cases/div/tb12/NativeCode.java
+++ org.osgi.test.cases.framework.div/src/org/osgi/test/cases/div/tb12/NativeCode.java
@@ -1,38 +1,17 @@
-/*
- * $Header$
- * 
- * Copyright (c) OSGi Alliance (2005). All Rights Reserved.
- * 
- * Implementation of certain elements of the OSGi Specification may be subject
- * to third party intellectual property rights, including without limitation,
- * patent rights (such a third party may or may not be a member of the OSGi
- * Alliance). The OSGi Alliance is not responsible and shall not be held
- * responsible in any manner for identifying or failing to identify any or all
- * such third party intellectual property rights.
- * 
- * This document and the information contained herein are provided on an "AS IS"
- * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
- * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
- * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
- * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
- * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
- * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
- * 
- * All Company, brand and product names may be trademarks that are the sole
- * property of their respective owners. All rights reserved.
+/** 
+ * OSGi Test Suite Implementation. OSGi Confidential.
+ * (C) Copyright Ericsson Radio Systems AB. 2000.
+ * This source code is owned by Ericsson Radio Systems AB, and is being distributed to OSGi 
+ * MEMBERS as MEMBER LICENSED MATERIALS under the terms of section 3.2 of the OSGi MEMBER AGREEMENT. 
  */
-
-package org.osgi.test.cases.div.tb12; // kept same package from tb2 (native code test from R3)
+package org.osgi.test.cases.div.tb2; // could not rename this package without rebuilding native so/dll
 
 import org.osgi.framework.*;
 
 /**
- * Bundle for the NativeCode selection filter test.
+ * Bundle for the NativeCode test.
  * 
- * @author Jorge Mascena
+ * @author Ericsson Radio Systems AB
  */
 public class NativeCode implements BundleActivator {
 	static boolean	initOk	= false;
@@ -45,51 +24,28 @@ public class NativeCode implements BundleActivator {
 			initOk = true;
 		}
 		catch (UnsatisfiedLinkError ule) {
-			ule.printStackTrace();
 			// initOk will remain false.
 		}
 	}
 
 	/**
 	 * The native method.
-	 * 
-	 * @param i the number of iterations.
 	 */
 	public native void count(int i);
-	
-	/**
-	 * This java implementation will be used if no native code clause matches the
-	 * selection filter. 
-	 * 
-	 * @param c the number of iterations.
-	 */
-	private void countJavaImpl(int c) {
-		for(int i=0; i<=c;);
-		
-	}
 
 	/**
-	 * Starts the bundle. Uses the java implementation since no native code clause
-	 * matches the selection filter in this test.
-	 * 
-	 * @param bc the bundle context.
-	 * 
-	 * @throws BundleException if the native library was successfuly loaded during
-	 *         bundle startup
+	 * Starts the bundle. Excercises the native code.
 	 */
 	public void start(BundleContext bc) throws BundleException {
 		if (initOk) {
-			throw new BundleException("Selection filter should not match any native code clause");
+			count(10000000);
 		}
-		else {
-			countJavaImpl(10000000);
-		}
+		else
+			throw new BundleException("Native code not initialized.");
 	}
 
 	/**
 	 * Stops the bundle.
-	 * 
-	 * @param bc the bundle context.
 	 */
 	public void stop(BundleContext bc) {
 	}
