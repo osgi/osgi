5de61aa7996b2c8d8e38ccee0953eb8a3f8eb32c
ckarai <cvsadmin@www.osgi.org>
2005-03-25 17:18:31 +0000

    FIXED: adding meglet specific files

--- archive/org.osgi.meglet/src/org/osgi/meglet/Meglet.java
+++ archive/org.osgi.meglet/src/org/osgi/meglet/Meglet.java
@@ -1,186 +1,185 @@
+/*
+ * $Header$
+ *
+ * Copyright (c) OSGi Alliance (2004, 2005). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
+
 package org.osgi.meglet;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Dictionary;
-import java.util.Map;
-
-import org.osgi.service.component.ComponentContext;
-import org.osgi.service.component.ComponentInstance;
-import org.osgi.service.event.Event;
-import org.osgi.service.event.EventHandler;
-import org.osgi.service.event.EventAdmin;
-
+import java.util.*;
+import java.io.*;
+import org.osgi.framework.*;
 import org.osgi.service.log.LogService;
+import org.osgi.service.event.*;
 
 /**
- * Abstract base class for Meglets. Developers are expected to extend this class
- * to implement the behavior and event handling for their applications.
- * <p>
- * This class provides many convenience methods which cannot be overridden.
+ * A MEG Application. a.k.a Meglet
  */
-public abstract class Meglet implements EventHandler, ComponentInstance {
 
-	/**
-	 * Stores the component context received in the activate() method.
-	 */
-	private ComponentContext componentContext;
+public abstract class Meglet implements EventHandler {
 
-	/**
-	 * Returns the component context assigned to the Meglet instance.
-	 * 
-	 * @return the component context
-	 */
-	protected final ComponentContext getComponentContext() {
-		return componentContext;
-	}
+	private ServiceReference eventAdminServiceRef = null;
+	private EventAdmin eventAdmin = null;
+	private ServiceReference logServiceServiceRef = null;
+	private LogService logService = null;
 
-	/**
-	 * This method is called by the framework to start this application
-	 * instance. Must not be called directly.
-	 * 
-	 * @param args
-	 *            The startup parameters for the application instance as
-	 *            key-value pairs.
-	 * @param stateStorage
-	 *            the input stream from where the application can load its saved
-	 *            inner state. It is null if a brand new instance of the Meglet
-	 *            is started, so no previously saved state exist to be loaded.
-	 *            It is not mandated to close the stream.
-	 * 
-	 * @throws Exception
-	 *             if any error occures
-	 */
-	public void start(Map args, InputStream stateStorage) throws Exception {
-	}
+	private BundleContext bc = null;
+	private MegletHandle appHandle = null;
 
-	/**
-	 * This method is called by the framework to stop this application instance.
-	 * Must not be called directly. The method must free all the used resources.
-	 * 
-	 * @param stateStorage
-	 *            the output stream to where the application can save its inner
-	 *            state. If it is null then the Meglet instance is stopped
-	 *            permanently so saving its inner state is not expected. It is
-	 *            not mandated to close the stream.
-	 * 
-	 * @throws Exception
-	 *             if any error occures
-	 */
-	public void stop(OutputStream stateStorage) throws Exception {
-	}
+	private ServiceRegistration serviceListenerRegistration = null;
+	private Vector listenedTopics = new Vector();
 
-	/**
-	 * Receives events. By default it does nothing so the developer has to
-	 * override it to deal with events.
-	 * <p>
-	 * This method must not be called directly.
-	 * 
-	 * @param event
-	 *            the event to be handled, not null
-	 */
-	public void handleEvent(Event event) {
-		// default implementation does nothing
-	}
 
-	/**
-	 * The application can ask the environment for suspending. The environment
-	 * must call the stop() method at some point with non-null stateStorage
-	 * parameter.
-	 */
-	protected final void requestSuspend() {
-	}
+	public Meglet() {}
 
-	/**
-	 * The application can ask the environment for stopping. The environment
-	 * must call the stop() method at some point with null stateStorage
-	 * parameter.
-	 */
+	protected void start( Map args, InputStream stateStorage ) throws Exception {}
+	protected void stop( OutputStream stateStorage ) throws Exception {}
+	public void handleEvent(Event event) {}
+
 	protected final void requestStop() {
+    class StopperThread extends Thread {
+      public void run() {
+        
+        try {
+          appHandle.destroy();
+        }catch( Exception e ) {
+          e.printStackTrace();
+        }          
+      }
+    };
+    
+    StopperThread st = new StopperThread();
+    st.start();
 	}
 
-	/**
-	 * Retrieves the event channel service if available. Otherwise returns null.
-	 * 
-	 * @return the event channel service, or null if it is not available
-	 */
+  protected final void requestSuspend() {
+    class SuspenderThread extends Thread {
+      public void run() {
+        
+        try {
+          appHandle.suspend();
+        }catch( Exception e ) {
+          e.printStackTrace();
+        }          
+      }
+    };
+    
+    SuspenderThread st = new SuspenderThread();
+    st.start();
+  }
+
 	protected final EventAdmin getEventAdmin() {
+		if( eventAdmin != null )
+			return eventAdmin;
+
+		try {
+			eventAdminServiceRef = bc.getServiceReference( "org.osgi.service.event.EventAdmin" );
+			if (eventAdminServiceRef != null) {
+				eventAdmin = (EventAdmin) bc.getService( eventAdminServiceRef );
+				return eventAdmin;
+			}
+		} catch( Exception e ) {}
+
 		return null;
 	}
 
-	/**
-	 * Retrieves the log service if available. Otherwise returns null.
-	 * 
-	 * @return the retrieved log service, or null if it is not available
-	 */
 	protected final LogService getLogService() {
+		if( logService != null )
+			return logService;
+
+		try {
+			logServiceServiceRef = bc.getServiceReference( "org.osgi.service.log.LogService" );
+			if (logServiceServiceRef != null) {
+				logService = (LogService) bc.getService( logServiceServiceRef );
+				return logService;
+			}
+		} catch( Exception e ) {}
+
 		return null;
 	}
 
-	/**
-	 * Registers the Meglet to listen to events with the specified topic and
-	 * eventFilter.
-	 * 
-	 * @param topic
-	 *            the topic of the event the subscriber is interested to, it may
-	 *            contain a trailing asterisk as wildcard, the empty string is
-	 *            treated as "*", must not be null
-	 * @param eventFilter
-	 *            LDAP filter for the event as defined in Generic Event
-	 *            Mechanism RFC, may be null
-	 * @throws NullPointerException
-	 *             if topic is null
-	 */
-	protected final void registerForEvents(String topic, String eventFilter) {
+	private void changeServiceRegistration() {
+		if( bc == null )
+			return;
+		if( listenedTopics.size() == 0 ) {
+			if( serviceListenerRegistration != null ) {
+				serviceListenerRegistration.unregister();
+				serviceListenerRegistration = null;
+			}
+		} else {
+			String topic[] = new String [ listenedTopics.size() ];
+			for (int k = 0; k != listenedTopics.size(); k++)
+				topic[ k ] = (String) listenedTopics.get(k);
+
+			Hashtable props = new Hashtable();
+			props.put( EventConstants.EVENT_TOPIC, topic );
+			if ( serviceListenerRegistration == null )
+				serviceListenerRegistration = bc.registerService(
+					EventHandler.class.getName(), this, props);
+			else
+				serviceListenerRegistration.setProperties( props );
+		}
 	}
 
-	/**
-	 * Unregisters the Meglet not to listen to the specified event. It works
-	 * only if it was registered by the registerForEvents() method to listen to
-	 * event with exactly the same topic and eventFilter.
-	 * 
-	 * @param topic
-	 *            the topic of the event the subscriber is interested to, it may
-	 *            contain a trailing asterisk as wildcard, the empty string is
-	 *            treated as "*", must not be null
-	 * @param eventFilter
-	 *            LDAP filter for the event as defined in Generic Event
-	 *            Mechanism RFC, may be null
-	 * @throws NullPointerException
-	 *             if topic is null
-	 */
-	protected final void unregisterForEvents(String topic, String eventFilter) {
+	protected final void registerForEvents( String topicName ) {
+		listenedTopics.add( topicName );
+		changeServiceRegistration();
 	}
 
-	/**
-	 * Retrieves the service properties of the application descriptor of this
-	 * application instance.
-	 * 
-	 * @return the properties of the application descriptor
-	 */
-	protected final Dictionary getProperties() {
+	protected final void unregisterForEvents( String topicName ) {
+		listenedTopics.remove( topicName );
+		changeServiceRegistration();
+	}
+
+	public final void startApplication( Map args, InputStream stateStorage ) throws Exception {
+		start( args, stateStorage );
+	}
+
+	public final void stopApplication( OutputStream stateStorage ) throws Exception {
+		listenedTopics.clear();
+		changeServiceRegistration();
+
+		stop( stateStorage );
+
+		if( eventAdminServiceRef != null ) {
+			bc.ungetService( eventAdminServiceRef );
+			eventAdmin = null;
+		}
+
+		if( logServiceServiceRef != null ) {
+			bc.ungetService( logServiceServiceRef );
+			logService = null;
+		}
+	};
+
+	protected final Dictionary getApplicationProperties() {
 		return null;
 	}
 
-	/**
-	 * Called by the framework when this component is activated. Called before
-	 * the start().
-	 * 
-	 * @param context
-	 *            the component context received from the framework
-	 */
-	protected final void activate(ComponentContext context) {
-		componentContext = context;
-	}
-
-	/**
-	 * Called by the framework when this component is deactivated. Called after
-	 * the stop().
-	 * 
-	 * @param context
-	 *            the component context received from the framework
-	 */
-	protected final void deactivate(ComponentContext context) {
-		componentContext = null;
+	void init( MegletHandle appHandle, BundleContext bc ) {
+		this.bc = bc;
+		this.appHandle = appHandle;
+		changeServiceRegistration();
 	}
 }
