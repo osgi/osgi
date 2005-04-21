31415cf0c2f689874eaeb6891e29b08e20de96e5
ckarai <cvsadmin@www.osgi.org>
2005-04-21 08:53:54 +0000

    CHANGED: start( args ), stop(), suspend( stream ), resume( stream )

--- archive/org.osgi.impl.service.megcontainer/src/com/nokia/test/megletsample2/TestApplication2.java
+++ archive/org.osgi.impl.service.megcontainer/src/com/nokia/test/megletsample2/TestApplication2.java
@@ -30,29 +30,36 @@ public class TestApplication2 extends Meglet implements EventHandler {
 	public TestApplication2() {
 		super();
 	}
+  
+  public void start( Map args ) throws Exception
+  {
+    if( args != null )
+      fileName = (String)args.get( "TestResult" );
+    
+    writeResult( "START" );
+  }
 
-	public void start(Map args, InputStream stateStorage) throws Exception {
-		if (args != null)
-			fileName = (String) args.get("TestResult");
-		if (stateStorage != null) {
-			ObjectInputStream ois = new ObjectInputStream(stateStorage);
-			storedString = (String) ois.readObject();
-		}
-		if (storedString == null)
-			writeResult("START");
-		else
-			writeResult("RESUME:" + storedString);
+  public void stop() throws Exception
+  {
+    writeResult( "STOP" );
+  }
+
+  public void suspend( OutputStream stateStorage ) throws Exception
+	{
+    storedString = "StorageTestString";      
+    ObjectOutputStream ois = new ObjectOutputStream( stateStorage );
+    ois.writeObject( fileName );
+    ois.writeObject( storedString );
+    
+    writeResult( "SUSPEND:" + storedString );	
 	}
-
-	public void stop(OutputStream stateStorage) throws Exception {
-		if (stateStorage != null) {
-			storedString = "StorageTestString";
-			ObjectOutputStream ois = new ObjectOutputStream(stateStorage);
-			ois.writeObject(storedString);
-			writeResult("SUSPEND:" + storedString);
-		}
-		else
-			writeResult("STOP");
+  
+  public void resume( InputStream stateStorage ) throws Exception
+	{
+    ObjectInputStream ois = new ObjectInputStream( stateStorage );
+    fileName = (String)ois.readObject();
+    storedString = (String)ois.readObject();
+    writeResult( "RESUME:" + storedString );  	
 	}
 
 	public void handleEvent(Event event) {
@@ -98,24 +105,5 @@ public class TestApplication2 extends Meglet implements EventHandler {
 			stream.close();
 		}
 		catch (IOException e) {}
-	}
-
-
-	/**
-	 * 
-	 * @see org.osgi.service.component.ComponentInstance#dispose()
-	 */
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * @return
-	 * @see org.osgi.service.component.ComponentInstance#getInstance()
-	 */
-	public Object getInstance() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
