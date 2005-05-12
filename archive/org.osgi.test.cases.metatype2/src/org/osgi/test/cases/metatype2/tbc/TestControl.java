3d0814e602d37712b2f2f935e7d50b84ed7669ac
Olivier Pave <olivier.pave@siemens.com>
2005-05-12 12:39:33 +0000

    Creation of Metatype2 service Test Case

--- archive/org.osgi.test.cases.metatype2/src/org/osgi/test/cases/metatype2/tbc/TestControl.java
+++ archive/org.osgi.test.cases.metatype2/src/org/osgi/test/cases/metatype2/tbc/TestControl.java
@@ -26,8 +26,12 @@
  */
 package org.osgi.test.cases.metatype2.tbc;
 
+import java.util.Hashtable;
+
 import org.osgi.framework.*;
-import org.osgi.service.packageadmin.PackageAdmin;
+import org.osgi.service.metatype.AttributeDefinition;
+import org.osgi.service.metatype.ObjectClassDefinition;
+import org.osgi.service.metatype2.*;
 import org.osgi.test.cases.util.DefaultTestBundleControl;
 
 /**
@@ -41,7 +45,244 @@ import org.osgi.test.cases.util.DefaultTestBundleControl;
  * @version $Revision$
  */
 public class TestControl extends DefaultTestBundleControl {
+	private ServiceReference ref;
+	private MetaDataService MDService;
+	private Bundle tb1;
+	private Bundle tb2;
+
+	/**
+	 * List of test methods used in this test case.
+	 */
+	static String[]	methods	= new String[] {"testConstants",
+											"testBasicCommands",
+											"testExtendedOCD",
+											"testExtendedAD",
+											"testAction",
+											"testListeners"};
+
+	/**
+	 * 
+	 * @return
+	 * @see org.osgi.test.cases.util.DefaultTestBundleControl#getMethods()
+	 */
+	public void testConstants() {
+		log("hidden", ExtendedAttributeDefinition.HIDDEN);
+		log("maxValue", ExtendedAttributeDefinition.MAXIMUM);
+		log("minValue", ExtendedAttributeDefinition.MINIMUM);
+		log("readOnly", ExtendedAttributeDefinition.READ_ONLY);
+		log("stepValue", ExtendedAttributeDefinition.STEP);
+		log("org.osgi.metatype.filter", MetaDataListener.METATYPE_FILTER);
+		log("org.osgi.metatype.category", MetaDataService.METATYPE_CATEGORY);
+	}
 	
+	/**
+	 * 
+	 * @return
+	 * @see org.osgi.test.cases.util.DefaultTestBundleControl#getMethods()
+	 */
+	public void testBasicCommands() {
+		String[] cats;
+		String[] mts;
+		String[] locs;
+		String ver;
+
+		// Gets available categories
+		log("List of available categories");
+		cats = MDService.getAvailableCatagories();
+		if (cats != null)
+			listStringArray(cats);
+		
+		// Gets available metatype with null parameters
+		log("List of available metatype for category = null");
+		mts = MDService.getAvailableMetaTypes(null);
+		if (mts != null)
+			listStringArray(mts);
+
+		// Gets available locales with null parameters
+		try {
+			mts = MDService.getMetaTypeLocales(null, null);
+			log("List of available locales for category = null and metatype ID = null");
+		}
+		catch (NullPointerException e) {
+			assertException("List of available locales for category = null and metatype ID = null: ", e.getClass(), e);
+		}
+
+		// Gets available locales with null parameters
+		try {
+			ver = MDService.getMetaTypeVersion(null, null);
+			log("List of available verion for category = null and metatype ID = null");
+		}
+		catch (NullPointerException e) {
+			assertException("List of available verion for category = null and metatype ID = null: ", e.getClass(), e);
+		}
+
+		// Gets list of metatypes
+		if (cats != null) {
+			for (int i=0; i<cats.length; i++) {
+				log("List of available metatype for category = " + cats[i]);
+				mts = MDService.getAvailableMetaTypes(cats[i]);
+				if (mts != null)
+					listStringArray(mts);
+			
+				for (int j=0; j<mts.length; j++) {
+					log("List of locales for metatype ID = " + mts[j]);				
+					locs = MDService.getMetaTypeLocales(cats[i], mts[j]);
+					if (locs != null)
+						listStringArray(locs);				
+					log("List of version for metatype ID = " + mts[j]);
+					ver = MDService.getMetaTypeVersion(cats[i], mts[j]);
+					if (ver != null) log(ver);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @see org.osgi.test.cases.util.DefaultTestBundleControl#getMethods()
+	 */
+	public void testExtendedOCD() {
+		ExtendedObjectClassDefinition ocd;
+		
+		try {
+			ocd = (ExtendedObjectClassDefinition)MDService.getObjectClassDefinition(null, null, null, null);
+			log("Gets OCD with all parameters set to null");
+		}
+		catch (Exception e) {
+			assertException("Gets OCD with all parameters set to null: ", e.getClass(), e);
+		}
+		
+		try {
+			ocd = (ExtendedObjectClassDefinition)MDService.getObjectClassDefinition("ControlUnit", null, null, null);
+			log("Gets OCD with category = ControlUnit and other parameters set to null");
+		}
+		catch (Exception e) {
+			assertException("Gets OCD with category = ControlUnit and other parameters set to null: ", e.getClass(), e);
+		}
+
+		try {
+			ocd = (ExtendedObjectClassDefinition)MDService.getObjectClassDefinition("ControlUnit", "HipModule", null, null);
+			log("Gets OCD with category = ControlUnit, metatype ID = HipModule and other parameters set to null");
+		}
+		catch (Exception e) {
+			assertException("Gets OCD with category = ControlUnit, metatype ID = HipModule and other parameters set to null: ", e.getClass(), e);
+		}
+		
+		try {
+			ocd = (ExtendedObjectClassDefinition)MDService.getObjectClassDefinition("ControlUnit", "HipModule", "messageDigestService", null);
+			log("Gets OCD with category = ControlUnit, metatype ID = HipModule, ocd ID = messageDigestService, locale = null");
+			OCDtoString(ocd);
+		}
+		catch (Exception e) {
+			assertException("Gets OCD with category = ControlUnit, metatype ID = HipModule, ocd ID = messageDigestService, locale = null: ", e.getClass(), e);
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @see org.osgi.test.cases.util.DefaultTestBundleControl#getMethods()
+	 */
+	public void testExtendedAD() {
+		ExtendedObjectClassDefinition ocd;
+		AttributeDefinition ad;
+		AttributeDefinition[] ads;
+		
+		// Retrieve all attributes
+		ocd = (ExtendedObjectClassDefinition)MDService.getObjectClassDefinition("ControlUnit", "HipModule", "messageDigestService", null);
+		ads = ocd.getAttributeDefinitions(ObjectClassDefinition.ALL);
+		
+		log("Retrieve list of attributes of ocd ID = messageDigestService");
+		for (int i=0; i<ads.length; i++) {
+			ADtoString(ads[i]);
+		}		
+		
+		// retrieve only one attribute
+		ad = (AttributeDefinition)ocd.getAttributeDefinition("Att1");		
+		log("Retrieve only one attribute ID = Att1");
+		if (ad != null) 
+			ADtoString(ad);
+		
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @see org.osgi.test.cases.util.DefaultTestBundleControl#getMethods()
+	 */
+	public void testAction() {
+		ExtendedObjectClassDefinition ocd;
+		ActionDefinition[] acts;
+		ActionDefinition act;
+		
+		// Retrieve all actions
+		ocd = (ExtendedObjectClassDefinition)MDService.getObjectClassDefinition("ControlUnit", "HipModule", "messageDigestService", null);
+		acts = ocd.getActionDefinitions();
+		
+		log("Retrieve list of actions of ocd ID = messageDigestService");
+		for (int i=0; i<acts.length; i++) {
+			ActDtoString(acts[i]);
+		}		
+		
+		// retrieve only one attribute
+		act = (ActionDefinition)ocd.getActionDefinition("updateInstance");		
+		log("Retrieve only one action ID = updateInstance");
+		if (act != null) 
+			ActDtoString(act);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 * @see org.osgi.test.cases.util.DefaultTestBundleControl#getMethods()
+	 */
+	public void testListeners() {
+		Bundle tb3 = null;
+		
+		try {
+			tb3 = installBundle("tb3.jar");
+			uninstallBundle(tb2);
+			tb2 = installBundle("tb2.jar");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		// Uninstall bundle to avoid interference with other test methods
+		try {
+			if (tb3 != null)
+				uninstallBundle(tb3);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the list of test methods contained in this test case.
+	 * 
+	 * @return list of test methods
+	 */
+	public String[] getMethods() {
+		return methods;
+	}
+	
+	/**
+	 * Checks if some prerequisites are met like getting some services
+	 * 
+	 * @return true if the pre-requisites are met, otherwise false
+	 */
+	public boolean checkPrerequisites() {
+		ref = getContext().getServiceReference(MetaDataService.class.getName());
+		if (ref == null)
+			return false;
+		MDService = (MetaDataService) getContext().getService(ref);
+		if (MDService == null)
+			return false;
+		return true;
+	}
 	/**
 	 * <remove>Prepare for each run. It is important that a test run is properly
 	 * initialized and that each case can run standalone. To save a lot
@@ -52,73 +293,14 @@ public class TestControl extends DefaultTestBundleControl {
 	 */
 	public void prepare() {
 		log("#before each run");
-	}
-
-	/**
-	 * <remove>Prepare for each method. It is important that each method can
-	 * be executed independently of each other method. Do not keep
-	 * state between methods, if possible. This method can be used
-	 * to clean up any possible remaining state.</remove> 
-	 */
-	public void setState() {
-		log("#before each method");
-	}
-
-	/**
-	 * <remove>Test methods starts with "test" and are automatically
-	 * picked up by the base class. The order is in the order of declaration.
-	 * (It is possible to control this). Test methods should use the assert methods
-	 * to test.</remove>
-	 * <remove>The documentation of the test method is the test method
-	 * specification. Normal java tile and html rules apply.</remove>
-	 * 
-	 * TODO Fill in tags
-	 * @specification			<remove>Specification</remove>
-	 * @interface				<remove>Related interface, e.g. org.osgi.util.measurement</remove>
-	 * @specificationVersion	<remove>Version nr of the specification</remove>
-	 * @methods					<remove>Related method(s)</remove>
-	 */
-	public void testA() {
-		log("#test a");
-	}
-
-	/**
-	 * System bundle exports system services.
-	 * 
-	 * Verify that the System bundle exists and exports the
-	 * system services: PackageAdmin, PermissionAdmin.
-	 * @throws Exception
-	 *
-	 * @specification			org.osgi.framework
-	 * @specificationSection    system.bundle
-	 * @specificationVersion	3
-	 */
-	public void testExporter() throws Exception {
-		Bundle tb = installBundle("tb1.jar");
-		assertBundle(TBCService.class.getName(), tb );
 		
-		Bundle system = getContext().getBundle(0);
-		assertBundle(PackageAdmin.class.getName(), system);
-	}
-
-	/**
-	 * Verify that the service with name is exported by the bundle b.
-	 * 
-	 * @param name		fqn of the service, e.g. com.acme.foo.Foo
-	 */
-	private void assertBundle(String name, Bundle b) {
-		ServiceReference	ref = getContext().getServiceReference(name);
-		assertNotNull( name + "  service must be registered ", ref );
-		assertEquals("Invalid exporter for " + name, b, ref.getBundle());
-	}
-
-	
-	/**
-	 * Clean up after each method. Notice that during debugging
-	 * many times the unsetState is never reached.
-	 */
-	public void unsetState() {
-		log("#after each method");
+		try {
+			tb1 = installBundle("tb1.jar");
+			tb2 = installBundle("tb2.jar");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -127,5 +309,74 @@ public class TestControl extends DefaultTestBundleControl {
 	 */
 	public void unprepare() {
 		log("#after each run");
+		try {
+			uninstallAllBundles();
+		}
+		catch (Exception e) {
+		}
+	}	
+	
+	/**
+	 * Logs elements of an array of String.
+	 * @param elts
+	 */
+	private void listStringArray(String[] elts) {
+		if (elts != null) 
+			for (int i=0; i<elts.length; i++)
+				log(elts[i]);
+	}
+	
+	/**
+	 * 
+	 * @version $Revision$
+	 */
+	private void ADtoString(AttributeDefinition ad) {
+		log("AttID="+ad.getID()+", Name="+ad.getName()+", Descr="+ad.getDescription()+
+			", Type="+Integer.toString(ad.getType())+", Card="+Integer.toString(ad.getCardinality()));		
+	}
+	
+	/**
+	 * 
+	 * @version $Revision$
+	 */
+	private void ActDtoString(ActionDefinition ad) {
+		ExtendedAttributeDefinition[] ads;
+		
+		log("ActionID="+ad.getID()+", Name="+ad.getName()+", Descr="+ad.getDescription()+
+			", Type="+Integer.toString(ad.getType())+", Card="+Integer.toString(ad.getCardinality()));
+		
+		ads = ad.getInputArgumentDefinitions();
+		if (ads != null) {
+			log("with following input args:");
+			for (int i=0; i<ads.length; i++)
+				ADtoString(ads[i]);
+		}
+	}
+	
+	/**
+	 * 
+	 * @version $Revision$
+	 */
+	private void ExADtoString(ExtendedAttributeDefinition ad) {
+		ExtendedAttributeDefinition[] ads;
+		Hashtable props = null;
+		
+		ADtoString((AttributeDefinition)ad);
+		ads = ad.getAttributeDefinitions();
+		for (int i=0; i<ads.length; i++) {
+			ExADtoString(ads[i]);
+		}
+		
+		props = (Hashtable)ad.getProperties();		
+		if (props != null)
+			props.toString();
+	}
+	
+	/**
+	 * 
+	 * @version $Revision$
+	 */
+	private void OCDtoString(ObjectClassDefinition ocd) {
+		log("OCDID="+ocd.getID()+", Name="+ocd.getName()+", Descr="+ocd.getDescription());
 	}
 }
