172cbd75c123327a7d5b9e46dbceb71ccd7647d9
Tamas Szeredi <tamas.szeredi@nokia.com>
2005-06-08 11:52:43 +0000

    Added "correlator" parameter to execute()

--- archive/org.osgi.service.dmt/src/org/osgi/service/dmt/DmtExecPlugin.java
+++ archive/org.osgi.service.dmt/src/org/osgi/service/dmt/DmtExecPlugin.java
@@ -25,25 +25,31 @@ public interface DmtExecPlugin {
      * Execute the given node with the given data. The <code>execute()</code>
      * method of the <code>DmtSession</code> is forwarded to the appropriate
      * <code>DmtExecPlugin</code> which handles the request. This operation
-     * corresponds to the EXEC command in OMA DM. The semantics of an EXEC
-     * operation and the data parameters it takes depend on the definition of
-     * the managed object on which the command is issued. Session information is
-     * given as it is needed for sending alerts back from the plugin.
+     * corresponds to the EXEC command in OMA DM.
+     * <p>
+     * The semantics of an EXEC operation and the data parameters it takes
+     * depend on the definition of the managed object on which the command is
+     * issued. Session information is given as it is needed for sending alerts
+     * back from the plugin. If a correlation ID is specified, it should be used
+     * as the <code>correlator</code> parameter for alerts sent in response to
+     * this EXEC operation.
      * 
      * @param session a reference to the session in which the operation was
      *        issued, must not be <code>null</code>
      * @param nodeUri the node to be executed, must not be <code>null</code>
+     * @param correlator an identifier to associate this operation with any
+     *        alerts sent in response to it, can be <code>null</code>
      * @param data the data of the EXEC operation, can be <code>null</code>
      * @throws DmtException with the following possible error codes
      *         <li><code>NODE_NOT_FOUND</code> if the plugin does not allow
      *         executing unexisting nodes
-     *         <li><code>OTHER_ERROR</code> 
+     *         <li><code>OTHER_ERROR</code>
      *         <li><code>COMMAND_NOT_ALLOWED</code> if the node is
      *         non-executable
      *         <li><code>DATA_STORE_FAILURE</code>
      * @throws IllegalStateException if the session is invalidated because of
      *         timeout, or if the session is already closed.
      */
-    void execute(DmtSession session, String nodeUri, String data) 
-         throws DmtException;
+    void execute(DmtSession session, String nodeUri, String correlator,
+            String data) throws DmtException;
 }
