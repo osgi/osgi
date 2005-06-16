b8fbe3916441b971af1a26dfe28a77115fc7da49
Peter Kriens <peter.kriens@aqute.se>
2005-06-16 17:16:55 +0000

    Changed the test case to use reflection because otherwise it was a compiler test

--- org.osgi.test.cases.framework.div/src/org/osgi/test/cases/framework/div/tbc/Constants/ConstantsValues.java
+++ org.osgi.test.cases.framework.div/src/org/osgi/test/cases/framework/div/tbc/Constants/ConstantsValues.java
@@ -27,8 +27,9 @@
 
 package org.osgi.test.cases.framework.div.tbc.Constants;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
+import java.lang.reflect.Field;
+
+import org.osgi.framework.*;
 import org.osgi.test.service.TestCaseLink;
 
 /**
@@ -41,6 +42,75 @@ public class ConstantsValues {
 	private BundleContext	context;
 	private String			tcHome;
 	private TestCaseLink	link;
+	final static String [] constants = {
+		"SYSTEM_BUNDLE_LOCATION", "System Bundle",
+		"SYSTEM_BUNDLE_SYMBOLICNAME", "system.bundle",
+		"BUNDLE_CATEGORY", "Bundle-Category",
+		"BUNDLE_CLASSPATH", "Bundle-ClassPath",
+		"BUNDLE_COPYRIGHT", "Bundle-Copyright",
+		"BUNDLE_DESCRIPTION", "Bundle-Description",
+		"BUNDLE_NAME", "Bundle-Name",
+		"BUNDLE_NATIVECODE", "Bundle-NativeCode",
+		"EXPORT_PACKAGE", "Export-Package",
+		"EXPORT_SERVICE", "Export-Service",
+		"IMPORT_PACKAGE", "Import-Package",
+		"DYNAMICIMPORT_PACKAGE", "DynamicImport-Package",
+		"IMPORT_SERVICE", "Import-Service",
+		"BUNDLE_VENDOR", "Bundle-Vendor",
+		"BUNDLE_VERSION", "Bundle-Version",
+		"BUNDLE_DOCURL", "Bundle-DocURL",
+		"BUNDLE_CONTACTADDRESS", "Bundle-ContactAddress",
+		"BUNDLE_ACTIVATOR", "Bundle-Activator",
+		"BUNDLE_UPDATELOCATION", "Bundle-UpdateLocation",
+		"PACKAGE_SPECIFICATION_VERSION", "specification-version",
+		"BUNDLE_NATIVECODE_PROCESSOR", "processor",
+		"BUNDLE_NATIVECODE_OSNAME", "osname",
+		"BUNDLE_NATIVECODE_OSVERSION", "osversion",
+		"BUNDLE_NATIVECODE_LANGUAGE", "language",
+		"BUNDLE_REQUIREDEXECUTIONENVIRONMENT", "Bundle-RequiredExecutionEnvironment",
+		"FRAMEWORK_VERSION", "org.osgi.framework.version",
+		"FRAMEWORK_VENDOR", "org.osgi.framework.vendor",
+		"FRAMEWORK_LANGUAGE", "org.osgi.framework.language",
+		"FRAMEWORK_OS_NAME", "org.osgi.framework.os.name",
+		"FRAMEWORK_OS_VERSION", "org.osgi.framework.os.version",
+		"FRAMEWORK_PROCESSOR", "org.osgi.framework.processor",
+		"FRAMEWORK_EXECUTIONENVIRONMENT", "org.osgi.framework.executionenvironment",
+		"SUPPORTS_FRAMEWORK_EXTENSION", "org.osgi.supports.framework.extension",
+		"SUPPORTS_BOOTCLASSPATH_EXTENSION", "org.osgi.supports.bootclasspath.extension",
+		"OBJECTCLASS", "objectClass",
+		"SERVICE_ID", "service.id",
+		"SERVICE_PID", "service.pid",
+		"SERVICE_RANKING", "service.ranking",
+		"SERVICE_VENDOR", "service.vendor",
+		"SERVICE_DESCRIPTION", "service.description",
+		"BUNDLE_SYMBOLICNAME", "Bundle-SymbolicName",
+		"SINGLETON_DIRECTIVE", "singleton",
+		"FRAGMENT_ATTACHMENT_DIRECTIVE", "fragment-attachment",
+		"FRAGMENT_ATTACHMENT_ALWAYS", "always",
+		"FRAGMENT_ATTACHMENT_RESOLVETIME", "resolve-time",
+		"FRAGMENT_ATTACHMENT_NEVER", "never",
+		"BUNDLE_LOCALIZATION", "Bundle-Localization",
+		"BUNDLE_LOCALIZATION_DEFAULT_BASENAME", "OSGI-INF/l10n/bundle",
+		"REQUIRE_BUNDLE", "Require-Bundle",
+		"BUNDLE_VERSION_ATTRIBUTE", "bundle-version",
+		"FRAGMENT_HOST", "Fragment-Host",
+		"MULTIPLE_HOSTS_DIRECTIVE", "multiple-hosts",
+		"SELECTION_FILTER_ATTRIBUTE", "selection-filter",
+		"BUNDLE_MANIFESTVERSION", "Bundle-ManifestVersion",
+		"VERSION_ATTRIBUTE", "version",
+		"BUNDLE_SYMBOLICNAME_ATTRIBUTE", "bundle-symbolic-name",
+		"RESOLUTION_DIRECTIVE", "resolution",
+		"RESOLUTION_MANDATORY", "mandatory",
+		"RESOLUTION_OPTIONAL", "optional",
+		"USES_DIRECTIVE", "uses",
+		"INCLUDE_DIRECTIVE", "include",
+		"EXCLUDE_DIRECTIVE", "exclude",
+		"MANDATORY_DIRECTIVE", "mandatory",
+		"VISIBILITY_DIRECTIVE", "visibility",
+		"VISIBILITY_PRIVATE", "private",
+		"VISIBILITY_REEXPORT", "reexport",
+		"REEXPORT_PACKAGE", "Reexport-Package",
+	};
 
 	/**
 	 * Creates a new ConstantsValues
@@ -67,109 +137,19 @@ public class ConstantsValues {
 	 * Test the constant values
 	 */
 	public void testConstantValues0001() throws Exception {
-		if (!Constants.BUNDLE_SYMBOLICNAME.equals("Bundle-SymbolicName")) {
-			throw new ConstantsTestException("Testing constants");
+		for ( int i=0; i<constants.length; i+=2 ) {
+			String name = constants[i];
+			String value = constants[i+1];
+			testConstant(Constants.class, name, value );
 		}
-
-		if (!Constants.SINGLETON_DIRECTIVE.equals("singleton")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.FRAGMENT_ATTACHMENT_ALWAYS.equals("always")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.FRAGMENT_ATTACHMENT_RESOLVETIME.equals("resolve-time")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.FRAGMENT_ATTACHMENT_NEVER.equals("never")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.BUNDLE_LOCALIZATION.equals("Bundle-Localization")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.BUNDLE_LOCALIZATION_DEFAULT_BASENAME.equals("OSGI-INF/l10n/bundle")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.REQUIRE_BUNDLE.equals("Require-Bundle")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.BUNDLE_VERSION_ATTRIBUTE.equals("bundle-version")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.FRAGMENT_HOST.equals("Fragment-Host")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.MULTIPLE_HOSTS_DIRECTIVE.equals("multiple-hosts")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.SELECTION_FILTER_ATTRIBUTE.equals("selection-filter")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.BUNDLE_MANIFESTVERSION.equals("Bundle-ManifestVersion")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.VERSION_ATTRIBUTE.equals("version")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.BUNDLE_SYMBOLICNAME_ATTRIBUTE
-				.equals("bundle-symbolic-name")) { //xxx
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.RESOLUTION_DIRECTIVE.equals("resolution")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.RESOLUTION_MANDATORY.equals("mandatory")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.RESOLUTION_OPTIONAL.equals("optional")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.USES_DIRECTIVE.equals("uses")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.INCLUDE_DIRECTIVE.equals("include")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.EXCLUDE_DIRECTIVE.equals("exclude")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.MANDATORY_DIRECTIVE.equals("mandatory")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.VISIBILITY_DIRECTIVE.equals("visibility")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.VISIBILITY_PRIVATE.equals("private")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.VISIBILITY_REEXPORT.equals("reexport")) {
-			throw new ConstantsTestException("Testing constants");
-		}
-
-		if (!Constants.REEXPORT_PACKAGE.equals("Reexport-Package")) {
-			throw new ConstantsTestException("Testing constants");
-		}
+	}
+	
+	private void testConstant(Class clazz, String constant, String value) throws Exception {
+		Field f = clazz.getField(constant);
+		String actual = (String) f.get(null);
+		if ( actual == null )
+			throw new ConstantsTestException(constant +  " is null");
+		if ( ! actual.equals(value))
+			throw new ConstantsTestException(constant +  " is not equal to " + value );			
 	}
 }
