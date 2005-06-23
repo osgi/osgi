ef3663f3cdefdb84fb94653de26cef43dc1246d3
BJ Hargrave <hargrave@us.ibm.com>
2005-06-23 12:12:15 +0000

    update  notice

--- ee.foundation/src/java/util/jar/Attributes.java
+++ ee.foundation/src/java/util/jar/Attributes.java
@@ -2,7 +2,11 @@
  * $Header$
  *
  * (C) Copyright 2001 Sun Microsystems, Inc.
- * Copyright (c) OSGi Alliance (2001, 2004). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2001, 2005). All Rights Reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html.
  */
 
 package java.util.jar;
