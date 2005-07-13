c65d475e1bb8ad8cb3377a3ea0787bf61e8dbea9
d_dikova <cvsadmin@www.osgi.org>
2005-07-13 08:12:09 +0000

    *** empty log message ***

--- org.osgi.test.cases.permissionadmin.conditional/src/org/osgi/test/cases/permissionadmin/conditional/tbc/TestCondition.java
+++ org.osgi.test.cases.permissionadmin.conditional/src/org/osgi/test/cases/permissionadmin/conditional/tbc/TestCondition.java
@@ -4,40 +4,40 @@ package org.osgi.test.cases.permissionadmin.conditional.tbc;
 import java.util.Dictionary;
 
 import org.osgi.service.condpermadmin.Condition;
+import org.osgi.service.condpermadmin.ConditionInfo;
 import org.osgi.framework.Bundle;
 
 
 public class TestCondition implements Condition {
-
-	private boolean evaluated;
+  private static final String CONDITION_TYPE = "org.osgi.test.cases.permissionadmin.conditional.tbc.TestCondition";
+	  
+  private boolean postponed;
 	private boolean satisfied;
 	private boolean mutable;
 	
-	public TestCondition(Bundle bundle, String isEvaluated, String isSatisfied, String isMutable) {
-		evaluated = (new Boolean(isEvaluated)).booleanValue();
-		satisfied = (new Boolean(isSatisfied)).booleanValue();
-		mutable = (new Boolean(isMutable)).booleanValue();
-	}
-	
-//	public static Condition getInstance(Bundle bundle, String[] args) {
-//		return null;
-//	}
-	
+	public TestCondition(Bundle bundle, ConditionInfo info) {
+    if (!CONDITION_TYPE.equals(info.getType()))
+      throw new IllegalArgumentException("ConditionInfo must be of type \"" + CONDITION_TYPE + "\"");
+    String[] args = info.getArgs();
+    if (args.length != 3)
+      throw new IllegalArgumentException("Illegal number of args: " + args.length);
+    postponed = (new Boolean(args[0])).booleanValue();
+		satisfied = (new Boolean(args[1])).booleanValue();
+		mutable = (new Boolean(args[2])).booleanValue();
+	}	
 	
 	public boolean isPostponed() {
-		return !evaluated;
+		return postponed;
 	}
-
 
 	public boolean isSatisfied() {
+    if (postponed) System.out.println("#isSatisfied method cheched of postponed TestCondition");
 		return satisfied;
 	}
-
 
 	public boolean isMutable() {
 		return mutable;
 	}
-
 
 	public boolean isSatisfied(Condition[] conds, Dictionary context) {
 		for (int i = 0; i < conds.length; i++) {
