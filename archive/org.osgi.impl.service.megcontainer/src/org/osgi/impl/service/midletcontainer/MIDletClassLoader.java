ac5f14ec8c40b3ad6520c7c29f77e88806bd174f
ckarai <cvsadmin@www.osgi.org>
2005-08-23 10:06:20 +0000

    FIXES

--- archive/org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/midletcontainer/MIDletClassLoader.java
+++ archive/org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/midletcontainer/MIDletClassLoader.java
@@ -1,8 +1,3 @@
-// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.geocities.com/kpdus/jad.html
-// Decompiler options: packimports(3) 
-// Source File Name:   MidletContainer.java
-
 package org.osgi.impl.service.midletcontainer;
 
 import java.io.InputStream;
@@ -11,49 +6,44 @@ import java.net.URLConnection;
 import java.security.ProtectionDomain;
 import org.osgi.framework.Bundle;
 
-class MIDletClassLoader extends ClassLoader
-{
+class MIDletClassLoader extends ClassLoader {
+	private Bundle				bundle;
+	private ProtectionDomain	protectionDomain;
 
-    public MIDletClassLoader(ClassLoader parent, Bundle bundle, ProtectionDomain protectionDomain)
-    {
-        super(parent);
-        this.bundle = bundle;
-        this.protectionDomain = protectionDomain;
-    }
+	public MIDletClassLoader(ClassLoader parent, Bundle bundle,
+			ProtectionDomain protectionDomain) {
+		super(parent);
+		this.bundle = bundle;
+		this.protectionDomain = protectionDomain;
+	}
 
-    protected Class findClass(String name)
-        throws ClassNotFoundException
-    {
-        byte b[] = loadClassData(name);
-        return defineClass(name, b, 0, b.length, protectionDomain);
-    }
+	protected Class findClass(String name) throws ClassNotFoundException {
+		byte b[] = loadClassData(name);
+		return defineClass(name, b, 0, b.length, protectionDomain);
+	}
 
-    private byte[] loadClassData(String name)
-        throws ClassNotFoundException
-    {
-        byte data[];
-        String classFile = name.replace('.', '/') + ".class";
-        URL url = bundle.getResource(classFile);
-        if(url == null)
-            throw new ClassNotFoundException();
-        URLConnection connection = url.openConnection();
-        int length = connection.getContentLength();
-        data = new byte[length];
-        InputStream input = connection.getInputStream();
-        try
-        {
-            input.read(data);
-        }
-        finally
-        {
-            input.close();
-        }
-        return data;
-        Exception e;
-        e;
-        throw new ClassNotFoundException("Cannot load the required class!", e);
-    }
-
-    private Bundle bundle;
-    private ProtectionDomain protectionDomain;
+	private byte[] loadClassData(String name) throws ClassNotFoundException {
+		try {
+			byte data[];
+			String classFile = name.replace('.', '/') + ".class";
+			URL url = bundle.getResource(classFile);
+			if (url == null)
+				throw new ClassNotFoundException();
+			URLConnection connection = url.openConnection();
+			int length = connection.getContentLength();
+			data = new byte[length];
+			InputStream input = connection.getInputStream();
+			try {
+				input.read(data);
+			}
+			finally {
+				input.close();
+			}
+			return data;
+		}
+		catch (Exception e) {
+			throw new ClassNotFoundException("Cannot load the required class!",
+					e);
+		}
+	}
 }
