5a1507b901fa17fb322d5f355f2ecbcb70661d4f
ckarai <cvsadmin@www.osgi.org>
2005-08-24 09:24:55 +0000

    ADDED: OAT XML parser

--- org.osgi.impl.service.application/src/org/osgi/impl/service/application/OAT.java
+++ org.osgi.impl.service.application/src/org/osgi/impl/service/application/OAT.java
@@ -28,19 +28,29 @@
 package org.osgi.impl.service.application;
 
 import org.osgi.framework.*;
+import org.osgi.service.log.LogService;
+import org.w3c.dom.Document;
+
+import java.io.InputStream;
 import java.lang.reflect.*;
+import java.net.URL;
 import java.util.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class OAT implements OATContainerInterface {
 	
 	private Hashtable oatHashtable = null;
 	private ServiceRegistration containerService = null;
+	private BundleContext bc = null;
 
 	public OAT() {
 		oatHashtable = new Hashtable();
 	}
 	
 	public void start( BundleContext bc ) throws Exception {
+		this.bc = bc;
 		setApplicationContextHashRef();
 		
 		containerService = bc.registerService( OATContainerInterface.class.getName(), this, new Hashtable() );
@@ -49,14 +59,16 @@ public class OAT implements OATContainerInterface {
 	public void stop( BundleContext bc ) throws Exception {
 		containerService.unregister();
 		
-		oatHashtable.clear();
 		oatHashtable = null;
+		bc = null;
 	}
 	
 	public void createApplicationContext(Object mainClass, Map args, Bundle bundle )
 			throws Exception {
 		
-		OATApplicationContextImpl appCtx = new OATApplicationContextImpl( bundle, args );
+		parseOATXML( bundle );
+		OATApplicationContextImpl appCtx = new OATApplicationContextImpl( bundle, args );		
+		
 		oatHashtable.put( mainClass, appCtx );    
 	}
 
@@ -70,5 +82,41 @@ public class OAT implements OATContainerInterface {
 		Field field = appFrameworkClass.getDeclaredField( "appContextHash" );
 		field.setAccessible(true);
     field.set( null, oatHashtable);		
+	}
+	
+	private void parseOATXML( Bundle bundle ) throws Exception {		
+		URL url = bc.getBundle( bundle.getBundleId() ).getResource(	"OSGI-INF/app/apps.xml");
+		if( url == null ) {
+			Activator.log(bc, LogService.LOG_ERROR, "Cannot open the OAT XML file!", null );
+			throw new Exception( "Cannot open the OAT XML file!" );			
+		}
+		
+		InputStream in = url.openStream();
+		if ( in == null ) {
+  			Activator.log(bc, LogService.LOG_ERROR, "Cannot open the OAT XML file!", null );
+				throw new Exception( "Cannot open the OAT XML file!" );
+		}
+		
+		ServiceReference domParserReference = bc.getServiceReference( DocumentBuilderFactory.class.getName() );
+		if (domParserReference == null) {
+			Activator.log(bc, LogService.LOG_ERROR, "Cannot find the DOM parser factory!", null );
+			throw new Exception( "Cannot find the DOM parser factory!" );
+		}
+			
+		DocumentBuilderFactory domFactory = (DocumentBuilderFactory) bc.getService( domParserReference );
+    boolean validating = domFactory.isValidating();
+    domFactory.setValidating( false );
+	  DocumentBuilder domParser = domFactory.newDocumentBuilder();
+    domFactory.setValidating( validating );
+		
+		if( domParser == null ) {
+				Activator.log(bc, LogService.LOG_ERROR, "Cannot create DOM parser!", null );
+				bc.ungetService( domParserReference );
+				throw new Exception( "Cannot create DOM parser!" );
+		}
+
+		Document doc = domParser.parse( in );
+		
+		bc.ungetService( domParserReference );
 	}
 }
