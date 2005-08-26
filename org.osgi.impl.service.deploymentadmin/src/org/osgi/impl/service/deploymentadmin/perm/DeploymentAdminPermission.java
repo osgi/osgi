81d99307ae4a528157a8e4c527dcecf9e0b6de80
Ivan Zahoranszky <izahoransky@nokia.com>
2005-08-26 11:45:00 +0000

    Inserted Legal Disclaimer

--- org.osgi.impl.service.deploymentadmin/src/org/osgi/impl/service/deploymentadmin/perm/DeploymentAdminPermission.java
+++ org.osgi.impl.service.deploymentadmin/src/org/osgi/impl/service/deploymentadmin/perm/DeploymentAdminPermission.java
@@ -1,13 +1,20 @@
 /*
- * $Header$
+ * ============================================================================
+ * (c) Copyright 2004 Nokia
+ * This material, including documentation and any related computer programs,
+ * is protected by copyright controlled by Nokia and its licensors. 
+ * All rights are reserved.
  * 
- * Copyright (c) The OSGi Alliance (2005). All Rights Reserved.
- * 
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this 
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * These materials have been contributed  to the Open Services Gateway 
+ * Initiative (OSGi)as "MEMBER LICENSED MATERIALS" as defined in, and subject 
+ * to the terms of, the OSGi Member Agreement specifically including, but not 
+ * limited to, the license rights and warranty disclaimers as set forth in 
+ * Sections 3.2 and 12.1 thereof, and the applicable Statement of Work. 
+ * All company, brand and product names contained within this document may be 
+ * trademarks that are the sole property of the respective owners.  
+ * The above notice must be included on all copies of this document.
+ * ============================================================================
  */
-
 package org.osgi.impl.service.deploymentadmin.perm;
 
 import java.security.Permission;
