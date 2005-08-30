027960fd5d4299aed2db9cebc812d0eed6099a89
Peter Kriens <peter.kriens@aqute.se>
2005-08-30 14:57:11 +0000

    Replaced badly written service listener to a service tracker

--- org.osgi.test.cases.upnp/src/org/osgi/test/cases/upnp/tbc/device/discovery/ServicesListener.java
+++ org.osgi.test.cases.upnp/src/org/osgi/test/cases/upnp/tbc/device/discovery/ServicesListener.java
@@ -1,49 +1,47 @@
 package org.osgi.test.cases.upnp.tbc.device.discovery;
 
 import org.osgi.framework.*;
-import org.osgi.service.upnp.*;
+import org.osgi.service.upnp.UPnPDevice;
+import org.osgi.util.tracker.ServiceTracker;
 
 /**
- * 
+ * Seems to track the latest registration/modification/unregistration of a device. 
+ * It was using a service listener with all dangers associated. It also did not
+ * discriminate between the looked for devices and the existing or found
+ * devices on the network.
  * 
  */
-public class ServicesListener implements ServiceListener {
+public class ServicesListener extends ServiceTracker {
 	public boolean				listen	= false;
 	public int					count	= 0;
-	private BundleContext		bc;
 	private ServiceReference	sr;
 	private static UPnPDevice	device;
 
-	public ServicesListener(BundleContext bc) {
-		this.bc = bc;
+	public ServicesListener(BundleContext bc) throws InvalidSyntaxException {
+		super(bc,bc.createFilter("(&(objectclass=org.osgi.service.upnp.UPnPDevice)(UPnP.device.manufacturer=ProSyst))"), null);
 	}
 
-	public void serviceChanged(ServiceEvent event) {
-		if (event.getType() == ServiceEvent.REGISTERED) {
-			listen = true;
-			count++;
-			sr = event.getServiceReference();
-			device = (UPnPDevice) bc.getService(sr);
-			//      String udn = (String)
-			// device.getDescriptions(null).get(UPnPDevice.UDN);
-			//
-			//      String upc = (String)
-			// device.getDescriptions(null).get(UPnPDevice.UPC);
+	
+	public Object addingService(ServiceReference ref) {
+		listen = true;
+		count++;
+		synchronized(this) {
+			notifyAll();
 		}
-		else
-			if (event.getType() == ServiceEvent.UNREGISTERING) {
-				sr = event.getServiceReference();
-				device = (UPnPDevice) bc.getService(sr);
-				//      String udn = (String)
-				// device.getDescriptions(null).get(UPnPDevice.UDN);
-			}
-			else
-				if (event.getType() == ServiceEvent.MODIFIED) {
-					sr = event.getServiceReference();
-					device = (UPnPDevice) bc.getService(sr);
-					//      String udn = (String)
-					// device.getDescriptions(null).get(UPnPDevice.UDN);
-				}
+		sr = ref;
+		device = (UPnPDevice) super.addingService(ref);
+		return device;
+	}
+	
+	public void removedService( Object service, ServiceReference ref ) {
+		sr = ref;
+		device = (UPnPDevice) service;
+	}
+	
+	
+	public void modifiedService( Object service, ServiceReference ref ) {
+		sr = ref;
+		device = (UPnPDevice) service;
 	}
 
 	public ServiceReference getServiceRef() {
@@ -52,5 +50,18 @@ public class ServicesListener implements ServiceListener {
 
 	public static UPnPDevice getUPnPDevice() {
 		return device;
+	}
+
+
+	public synchronized void waitFor(int i) {
+		while ( count != i ) {
+			try {
+				wait();
+			}
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
