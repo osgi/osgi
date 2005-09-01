2245f53ba3a7043894a60e070e1416acaa228647
ckarai <cvsadmin@www.osgi.org>
2005-09-01 11:50:12 +0000

    FIXED: MIDlet class loader hack

--- org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/midletcontainer/MIDletClassLoader.java
+++ org.osgi.impl.service.megcontainer/src/org/osgi/impl/service/midletcontainer/MIDletClassLoader.java
@@ -9,28 +9,56 @@ import org.osgi.framework.Bundle;
 class MIDletClassLoader extends ClassLoader {
 	private Bundle				bundle;
 	private ProtectionDomain	protectionDomain;
+	private ClassLoader   parent;
+	private String        mainClassLocation;
 
 	public MIDletClassLoader(ClassLoader parent, Bundle bundle,
-			ProtectionDomain protectionDomain) {
+			ProtectionDomain protectionDomain, String mainClassLocation ) {
 		super(parent);
+		
 		this.bundle = bundle;
 		this.protectionDomain = protectionDomain;
+		this.parent = parent;
+		this.mainClassLocation = mainClassLocation;
 	}
 
 	protected Class findClass(String name) throws ClassNotFoundException {
-		return bundle.loadClass( name );
-//		byte b[] = loadClassData(name);
-//		return defineClass(name, b, 0, b.length, protectionDomain);
+		byte b[] = loadClassData(name);
+		return defineClass(name, b, 0, b.length, protectionDomain);
+	}
+	
+	public Class loadClass( String name, boolean resolve ) throws ClassNotFoundException {
+		// First, check if the class has already been loaded
+		Class c = findLoadedClass(name);
+		if (c == null) {
+		  try {
+		    c = findClass(name);
+		  } catch (ClassNotFoundException e) {
+		    // If still not found, then invoke findClass in order
+		    // to find the class.
+			  if (parent != null) {
+			    c = parent.loadClass(name);
+			  }
+		  }
+		}
+		if (resolve) {
+		    resolveClass(c);
+		}
+		return c;
 	}
 
-	private byte[] loadClassData(String name) throws ClassNotFoundException {
+	private byte[] loadClassData(String name) throws ClassNotFoundException {			
 		try {
 			byte data[];
 			String classFile = name.replace('.', '/') + ".class";
 			URL url = bundle.getResource(classFile);
 			if (url == null)
 				throw new ClassNotFoundException();
-			URLConnection connection = url.openConnection();
+			
+			if( !url.toString().startsWith( mainClassLocation ) )
+				throw new ClassNotFoundException();
+
+			URLConnection connection = url.openConnection();			
 			int length = connection.getContentLength();
 			data = new byte[length];
 			InputStream input = connection.getInputStream();
