9f57282052c904d3e71f4d1ed06cc4bc84c25092
Peter Kriens <peter.kriens@aqute.se>
2005-09-07 07:20:20 +0000

    This class just duplicated Icon class. It also opened a stream instead of openening when it is needed. This viloated the spec because you should be able to get the stream multiple times. This class is now a proxy to Icon and opens the stream when needed

--- org.osgi.impl.service.upnp.cp/src/org/osgi/impl/service/upnp/cp/basedriver/UPnPIconImpl.java
+++ org.osgi.impl.service.upnp.cp/src/org/osgi/impl/service/upnp/cp/basedriver/UPnPIconImpl.java
@@ -6,61 +6,47 @@ import java.io.IOException;
 import org.osgi.service.upnp.*;
 import org.osgi.impl.service.upnp.cp.description.*;
 
+import sun.awt.image.URLImageSource;
+
 public class UPnPIconImpl implements UPnPIcon {
 	private Icon		icon;
-	private int			width;
-	private int			height;
-	private int			size;
-	private int			depth;
-	private String		mimeType;
-	private InputStream	ins;
-	private URL			iconurl;
+	private String		baseURL;
 
 	// This constructor creates the UPnPIcon object based on the given Icon
 	// object.
 	UPnPIconImpl(Icon icon, String baseURL) {
 		this.icon = icon;
-		mimeType = icon.getMimeType();
-		width = icon.getWidth();
-		height = icon.getHeight();
-		depth = icon.getDepth();
-		try {
-			URL iconurl = new URL(new URL(baseURL), icon.getURL());
-			ins = iconurl.openStream();
-			size = ins.available();
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		this.baseURL = baseURL;
 	}
 
 	// This method returns the mimetype of the icon.
 	public String getMimeType() {
-		return mimeType;
+		return icon.getMimeType();
 	}
 
 	// This method returns the width of the icon.
 	public int getWidth() {
-		return width;
+		return icon.getWidth();
 	}
 
 	// This method returns the height of the icon.
 	public int getHeight() {
-		return height;
+		return icon.getHeight();
 	}
 
 	// This method returns the size of the icon.
 	public int getSize() {
-		return size;
+		return -1;
 	}
 
 	// This method returns the depth of the icon.
 	public int getDepth() {
-		return depth;
+		return icon.getDepth();
 	}
 
 	// This method returns the input stream of the icon.
 	public InputStream getInputStream() throws IOException {
-		return ins;
+		URL		url = new URL( new URL(baseURL), icon.getURL());
+		return url.openStream();
 	}
 }
