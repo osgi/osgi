f18612580997b1c88a7918861440f1429de7e633
ckarai <cvsadmin@www.osgi.org>
2005-10-06 10:51:51 +0000

    CHANGED: necessary updates

--- org.osgi.service.application/src/org/osgi/service/application/ApplicationAdminPermission.java
+++ org.osgi.service.application/src/org/osgi/service/application/ApplicationAdminPermission.java
@@ -10,7 +10,6 @@
 
 package org.osgi.service.application;
 
-import java.security.BasicPermission;
 import java.security.Permission;
 import java.util.StringTokenizer;
 import java.util.Vector;
@@ -27,7 +26,7 @@ import java.util.Vector;
  * The permission <code>schedule</code> implies the permission
  * <code>lifecycle</code>.
  */
-public class ApplicationAdminPermission extends BasicPermission {
+public class ApplicationAdminPermission extends Permission {
 	private static final long serialVersionUID = 1L;
   
 	/**
@@ -48,10 +47,21 @@ public class ApplicationAdminPermission extends BasicPermission {
 	 */
 	public static final String LOCK = "lock";
 
+	private final ApplicationDescriptor	applicationDescriptor;
+
 	/**
 	 * Constructs an ApplicationAdminPermission. The <code>filter</code>
-	 * specifies the target application. If the <code>filter</code> is
-	 * <code>null</code> then it matches <code>"*"</code>. If
+	 * specifies the target application. The <code>filter</code> is an
+	 * LDAP-style filter, the recognized properties are <code>signer</code>
+	 * and <code>pid</code>. The pattern specified in the <code>signer</code>
+	 * is matched with the Distinguished Name chain used to sign the application. 
+	 * Wildcards in a DN are not matched according to the filter string rules, 
+	 * but according to the rules defined for a DN chain. The attribute 
+	 * <code>pid</code> is matched with the PID of the application according to
+	 * the filter string rules. 
+	 * 
+	 * If the <code>filter</code> is <code>null</code> then it matches 
+	 * <code>"*"</code>. If
 	 * <code>actions</code> is <code>"*"</code> then it identifies all the
 	 * possible actions.
 	 * 
@@ -66,14 +76,38 @@ public class ApplicationAdminPermission extends BasicPermission {
 	 * 
 	 * @exception NullPointerException
 	 *                is thrown if the actions parameter is <code>null</code>
+	 * 
+	 * @see ApplicationDescriptor
+	 * @see org.osgi.framework.AdminPermission
 	 */
 	public ApplicationAdminPermission(String filter, String actions) {
 		super(filter == null ? "*" : filter);
 		
+		this.filter = (filter == null ? "*" : filter);
+		this.actions = actions;
+		
 		actionsVector = actionsVector( actions );
 
-		if (!ACTIONS.containsAll(actionsVector))
+		if ( actions.equals("*") )
+			actionsVector = actionsVector( LIFECYCLE + "," + SCHEDULE + "," + LOCK );
+		else if (!ACTIONS.containsAll(actionsVector))
       throw new IllegalArgumentException("Illegal action!");
+		
+		applicationID = null;
+		this.applicationDescriptor = null;
+	}
+	
+	/**
+	 * This contructor should be used when creating <code>ApplicationAdminPermission</code>
+	 * instance for <code>checkPermission</code> call. 
+	 * @param application the tareget of the operation, it must not be <code>null</code>
+	 * @param actions the required operation. it must not be <code>null</code>
+	 */
+	public ApplicationAdminPermission(ApplicationDescriptor application, String actions) {
+		super(application.getApplicationId());
+		this.filter = application.getApplicationId();
+		this.applicationDescriptor = application;
+		this.actions = actions;
 	}
 	
 	/**
@@ -86,7 +120,12 @@ public class ApplicationAdminPermission extends BasicPermission {
 	 * @return the permission updated with the ID of the current application
 	 */
 	public ApplicationAdminPermission setCurrentApplicationId(String applicationId) {
-		return this;
+		ApplicationAdminPermission newPerm = new ApplicationAdminPermission( this.applicationDescriptor, 
+				this.actions );
+		
+		newPerm.applicationID = applicationId;
+		
+		return newPerm;
 	}
 
   public boolean implies(Permission p) {
@@ -97,9 +136,18 @@ public class ApplicationAdminPermission extends BasicPermission {
           return false;
 
       ApplicationAdminPermission other = (ApplicationAdminPermission) p;
-      
-      if( ! super.implies(other) )
-      	return false;
+
+      /* TODO filter check */
+      if( !filter.equals("*") ) {
+      	if( filter.equals( "<<SELF>>") ) {
+      		if( other.applicationID == null )
+      			return false; /* it cannot be, this might be a bug */
+      		if( !other.applicationID.equals( other.filter ) )
+      			return false;
+      	}
+      	else if( !filter.equals( other.filter ) ) /* TODO LDAP match */
+      		return false;
+      }
       
       if( !actionsVector.containsAll( other.actionsVector ) )
       	return false;
@@ -107,8 +155,13 @@ public class ApplicationAdminPermission extends BasicPermission {
       return true;
   }
 
+  private String applicationID;
+
   private static final Vector ACTIONS = new Vector();
   private Vector actionsVector;
+  private final String filter;
+  private final String actions;
+  
   static {
       ACTIONS.add(LIFECYCLE);
       ACTIONS.add(SCHEDULE);
@@ -123,5 +176,29 @@ public class ApplicationAdminPermission extends BasicPermission {
           v.add(action.toLowerCase());
       }
       return v;
+  }
+
+  public boolean equals(Object with) {
+  	if( with == null || !(with instanceof ApplicationAdminPermission) )
+  		return false;
+  	
+  	ApplicationAdminPermission other = (ApplicationAdminPermission)with;  	
+  	
+  	if( other.actionsVector.size() != actionsVector.size() )
+  		return false;
+  	
+  	for( int i=0; i != actionsVector.size(); i++ )
+  		if( !other.actionsVector.contains( actionsVector.get( i ) ) )
+  			return false;
+  	
+  	return filter.equals( other.filter );
+  }
+
+  public int hashCode() {
+	  return actions.hashCode() + filter.hashCode();
+  }
+
+  public String getActions() {
+  	return actions;
   }
 }
