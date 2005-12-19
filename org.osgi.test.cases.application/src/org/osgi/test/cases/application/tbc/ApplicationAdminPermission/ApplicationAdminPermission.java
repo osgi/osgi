9d601d2478d46e1b3c2e46980ca308f1655b9361
ckarai <cvsadmin@www.osgi.org>
2005-12-19 15:51:55 +0000

    FIXED: ApplicationAdminPermission throws NullPointerException if the filter reference is null

--- org.osgi.test.cases.application/src/org/osgi/test/cases/application/tbc/ApplicationAdminPermission/ApplicationAdminPermission.java
+++ org.osgi.test.cases.application/src/org/osgi/test/cases/application/tbc/ApplicationAdminPermission/ApplicationAdminPermission.java
@@ -134,6 +134,7 @@ public class ApplicationAdminPermission {
 					ApplicationConstants.ACTIONS);
 			tbc.assertEquals("Asserting if the actions passed as parameter are the same returned.", ApplicationConstants.ACTIONS, ap.getActions());
 		} catch (Exception e) {
+			e.printStackTrace();
 			tbc.fail(MessagesConstants.UNEXPECTED_EXCEPTION + ": " + e.getClass().getName());
 		}
 	}
