9774dfe6bb6a014f4f54b3572a35ca26a01d8f17
Ivan Zahoranszky <izahoransky@nokia.com>
2006-01-05 11:14:11 +0000

    bugfix: testDeployedSubtree010

--- org.osgi.impl.service.deploymentadmin/src/org/osgi/impl/service/deploymentadmin/plugin/PluginDeployed.java
+++ org.osgi.impl.service.deploymentadmin/src/org/osgi/impl/service/deploymentadmin/plugin/PluginDeployed.java
@@ -529,10 +529,10 @@ public class PluginDeployed implements DataPlugin, ReadableDataSession,
             if (l == 9) {
                 if ("Signers".equals(nodeUriArr[7]))
                     return new Metanode(MetaNode.CMD_GET, Metanode.IS_LEAF, MetaNode.AUTOMATIC,
-                        "", 1, Metanode.ZERO_OCC, null, 0, 0, null, DmtData.FORMAT_STRING);
+                    		"", Integer.MAX_VALUE, !Metanode.ZERO_OCC, null, 0, 0, null, DmtData.FORMAT_STRING);
                 if ("Bundles".equals(nodeUriArr[7]))
                     return new Metanode(MetaNode.CMD_GET, !Metanode.IS_LEAF, MetaNode.AUTOMATIC,
-                        "", Integer.MAX_VALUE, !Metanode.ZERO_OCC, null, 0, 0, null, DmtData.FORMAT_NODE);
+                    		"", Integer.MAX_VALUE, !Metanode.ZERO_OCC, null, 0, 0, null, DmtData.FORMAT_NODE);
             }
             if (l == 10) {
                 if ("Signers".equals(nodeUriArr[9]))
@@ -551,7 +551,7 @@ public class PluginDeployed implements DataPlugin, ReadableDataSession,
             if (l == 11) {
                 if ("Signers".equals(nodeUriArr[9]))
                     return new Metanode(MetaNode.CMD_GET, Metanode.IS_LEAF, MetaNode.AUTOMATIC,
-                        "", 1, Metanode.ZERO_OCC, null, 0, 0, null, DmtData.FORMAT_NODE);        
+                    		"", 1, Metanode.ZERO_OCC, null, 0, 0, null, DmtData.FORMAT_NODE);        
             }
         }
        
