fb476f3974780f7188098f8dc6acbe501287b68d
Peter Kriens <peter.kriens@aqute.se>
2006-03-15 08:59:05 +0000

    Adding complex values to the DmtData (FORMAT_NODE)

--- org.osgi.service.dmt/src/org/osgi/service/dmt/DmtSession.java
+++ org.osgi.service.dmt/src/org/osgi/service/dmt/DmtSession.java
@@ -1054,14 +1054,19 @@ public interface DmtSession {
     void setDefaultNodeValue(String nodeUri) throws DmtException;
 
     /**
-     * Set the value of a leaf node. The format of the node is contained in the
-     * <code>DmtData</code> object. If the specified value is <code>null</code>,
+     * Set the value of a leaf or interior node. The format of the node is contained in the
+     * <code>DmtData</code> object. For an interior node, this format must be <code>FORMAT_NODE</code>
+     * and the value is a Java object. If the specified value is <code>null</code>,
      * the default value is taken. In this case, if the node does not have a 
      * default value, this method will throw a <code>DmtException</code> with 
-     * error code <code>METADATA_MISMATCH</code>.
+     * error code <code>METADATA_MISMATCH</code>. 
      * <p>
      * Nodes of <code>null</code> format can be set by using 
      * {@link DmtData#NULL_VALUE} as second argument.
+     * <p/>
+     * An Event of type REPLACE is sent out for a leaf node. A replaced interior
+     * node sends out events for each of its children in depth first order
+     * and node names sorted with Arrays.sort(String[]).
      * 
      * @param nodeUri the URI of the node
      * @param data the data to be set, can be <code>null</code>
@@ -1077,8 +1082,8 @@ public interface DmtSession {
      *         with a principal and the ACL of the node does not allow the
      *         <code>Replace</code> operation for the associated principal
      *         <li><code>COMMAND_NOT_ALLOWED</code> if the specified node is not
-     *         a leaf node, or in non-atomic sessions if the underlying plugin 
-     *         is read-only or does not support non-atomic writing 
+     *         a leaf node and does not support Java object values, or in non-atomic sessions if the underlying plugin 
+     *         is read-only or does not support non-atomic writing. 
      *         <li><code>METADATA_MISMATCH</code> if the node is permanent or
      *         cannot be modified according to the meta-data (does not have the 
      *         <code>MetaNode.CMD_REPLACE</code> access type), or if the given
@@ -1465,10 +1470,10 @@ public interface DmtSession {
     String getNodeType(String nodeUri) throws DmtException;
 
     /**
-     * Get the data contained in a leaf node.
+     * Get the data contained in a leaf or interior node.
      * 
      * @param nodeUri the URI of the node to retrieve
-     * @return the data of the leaf node, can not be <code>null</code>
+     * @return the data of the node, can not be <code>null</code>
      * @throws DmtException with the following possible error codes:
      *         <ul>
      *         <li><code>URI_TOO_LONG</code> if <code>nodeUri</code> or a
@@ -1481,7 +1486,8 @@ public interface DmtSession {
      *         with a principal and the ACL of the node does not allow the
      *         <code>Get</code> operation for the associated principal
      *         <li><code>COMMAND_NOT_ALLOWED</code> if the specified node is not
-     *         a leaf node
+     *         a leaf node or an interior node that does not support a 
+     *         complex value.
      *         <li><code>METADATA_MISMATCH</code> if the node value cannot be
      *         retrieved according to the meta-data (it does not have 
      *         <code>MetaNode.CMD_GET</code> access type)
