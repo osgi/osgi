645ecc63b756e717fc0d4b555c45fb1b2aaa53fe
Tamas Szeredi <tamas.szeredi@nokia.com>
2006-05-08 10:40:59 +0000

    Removing "VERY fishy" comment, the referenced line just checks that the parameter value is one of the allowed values.

--- info.dmtree/src/info/dmtree/Acl.java
+++ info.dmtree/src/info/dmtree/Acl.java
@@ -187,7 +187,6 @@ public final class Acl {
             if (!ALL_PRINCIPALS.equals(principals[i]))
                 checkPrincipal(principals[i]);
             checkPermissions(permissions[i]);
-            // ### That previous line looks VERY fishy
 
             Integer permInt = new Integer(permissions[i]);
             Object old = tempPrincipalPermissions.put(principals[i], permInt);
