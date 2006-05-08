4ab0c8baacc722e257fa895e645bb21b54fc2dc3
Stoyan Boshev <s.boshev@prosyst.com>
2006-05-08 07:38:59 +0000

    Fix failure when run on really slow devices/PCs

--- org.osgi.test.cases.wireadmin/src/org/osgi/test/cases/wireadmin/tbc/FilteredProducerImpl.java
+++ org.osgi.test.cases.wireadmin/src/org/osgi/test/cases/wireadmin/tbc/FilteredProducerImpl.java
@@ -19,16 +19,21 @@ public class FilteredProducerImpl implements Producer {
 			this.wire = wires[0];
 	}
 
-	public void updateWire(int delta, boolean wait) {
-		for (int i = 0; i < 10; i += delta) {
-			if (wait) {
-				try {
-					Thread.sleep(100 * i);
-				}
-				catch (Exception e) {
-				}
-			}
-			wire.update(new Integer(i));
-		}
-	}
+  public void updateWire(int delta, boolean wait) {
+    long processTime = 0;
+    for (int i = 0; i < 10; i += delta) {
+      if (wait) {
+        try {
+          if (100*i - processTime > 0) {
+            Thread.sleep((100 * i) - processTime);
+          }
+        }
+        catch (Exception e) {
+        }
+      }
+      processTime = System.currentTimeMillis();
+      wire.update(new Integer(i));
+      processTime = System.currentTimeMillis() - processTime;
+    }
+  }
 }
