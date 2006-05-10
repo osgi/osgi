be73f3cc2f8c7ffaadc49c621439ff568e0a1199
Tamas Szeredi <tamas.szeredi@nokia.com>
2006-05-10 18:03:59 +0000

    Updating to use the aligned DMT API from the info.dmtree package. Renaming package containing interfaces with other RI modules from org.osgi.impl.service.dmt.api to org.osgi.impl.service.dmt.export. Separated alert sending functionality into a separate NotificationService implementation class. Made DmtAdmin service accessible via an OSGi Service Factory, implemented by DmtServiceFactory.

--- org.osgi.impl.service.dmt/src/org/osgi/impl/service/dmt/DmtAdminActivator.java
+++ org.osgi.impl.service.dmt/src/org/osgi/impl/service/dmt/DmtAdminActivator.java
@@ -17,16 +17,21 @@
  */
 package org.osgi.impl.service.dmt;
 
+import info.dmtree.DmtAdmin;
+import info.dmtree.notification.NotificationService;
+import info.dmtree.registry.DmtServiceFactory;
+
+import java.lang.reflect.Field;
 import java.security.AllPermission;
 import java.util.Hashtable;
 import java.util.Map;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.impl.service.dmt.api.DmtPrincipalPermissionAdmin;
+import org.osgi.impl.service.dmt.export.DmtPrincipalPermissionAdmin;
 import org.osgi.service.cm.ManagedService;
-import org.osgi.service.dmt.DmtAdmin;
 import org.osgi.service.permissionadmin.PermissionInfo;
 
 public class DmtAdminActivator implements BundleActivator {
@@ -38,15 +43,27 @@ public class DmtAdminActivator implements BundleActivator {
     static final String DMT_PERMISSION_ADMIN_SERVICE_PID = 
         "org.osgi.impl.service.dmt.perms";
     
-    private ServiceRegistration	adminReg;
+    private ServiceRegistration	notificationReg;
+    private ServiceRegistration adminFactoryReg;
     private ServiceRegistration permissionReg;
+    
+    private Field factoryContext;
+    private DmtAdminFactory dmtAdminFactory;
     
     private Context context;
 
 	public void start(BundleContext bc) throws BundleException {
+        System.out.println("Dmt Admin activation started.");
+
 		try {
             System.setProperty(DMT_ROOT_PROPERTY, DMT_ROOT);
             
+            // TODO ensure that DmtAdmin is a singleton (otherwise the context field settings/resettings could get mixed up) 
+            factoryContext = DmtServiceFactory.class.getDeclaredField("context");
+            factoryContext.setAccessible(true);
+            factoryContext.set(null, bc);
+            referenceDmtServiceFactory();
+
             context = new Context(bc);
             
             DmtSessionImpl.init_acls();
@@ -54,13 +71,20 @@ public class DmtAdminActivator implements BundleActivator {
 			// creating the services
             DmtPrincipalPermissionAdmin dmtPermissionAdmin =
                 new DmtPrincipalPermissionAdminImpl(context);
-			DmtAdminImpl dmtAdmin = 
-                new DmtAdminImpl(dmtPermissionAdmin, context);
-			
+            DmtAdminCore dmtAdmin = 
+                new DmtAdminCore(dmtPermissionAdmin, context);
+            dmtAdminFactory = 
+                new DmtAdminFactory(context, dmtAdmin, dmtPermissionAdmin);
+            NotificationService notificationService =
+                new NotificationServiceImpl(context);
+
             // registering the services
-			adminReg = bc.registerService(DmtAdmin.class.getName(),
-					dmtAdmin, null);
+            notificationReg = bc.registerService(NotificationService.class
+                    .getName(), notificationService, null);
             
+            adminFactoryReg = bc.registerService(DmtAdmin.class.getName(), 
+                    dmtAdminFactory, null);
+                
             String[] services = new String[] {
                     DmtPrincipalPermissionAdmin.class.getName(),
                     ManagedService.class.getName()
@@ -80,13 +104,36 @@ public class DmtAdminActivator implements BundleActivator {
 			System.out.println("Exception:" + e.getMessage());
 			throw new BundleException("Failure in start() method.", e);
 		}
+        
+        System.out.println("Dmt Admin activation finished successfully.");
 	}
 
 	public void stop(BundleContext bc) throws BundleException {
-		// stopping everything in the context (e.g. service trackers)
-        context.close();
+        // stopping event delivery to listeners directly registered in DmtAdmin
+        dmtAdminFactory.stop();
+        
+	    // unsetting our context from the private variable in DmtServiceFactory
+        try {
+            factoryContext.set(null, null);
+        } catch(IllegalAccessException e) {
+            System.out.println("Exception:" + e.getMessage());
+            throw new BundleException("Failure in stop() method.", e);
+        }
 		// unregistering the services
-		adminReg.unregister();
+		notificationReg.unregister();
+        adminFactoryReg.unregister();
         permissionReg.unregister();
+        
+        // stopping everything in the context (e.g. service trackers)
+        context.close();
 	}
+    
+    // This dummy method is used to force btool to include the 
+    // info.dmtree.registry package in the imports in the manifest. 
+    // This package would otherwise not be imported, as only the 
+    // DmtServiceFactory.class field is used from the whole package,
+    // and that is not easily detected before Java 1.4.
+    private DmtServiceFactory referenceDmtServiceFactory() {
+        return null;
+    }
 }
