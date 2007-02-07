e3fa3159ece70bbde2fccc5a7d0b5f22035d4aa1
BJ Hargrave <hargrave@us.ibm.com>
2007-02-07 18:53:08 +0000

    update copyright year

--- org.osgi.service.useradmin/src/org/osgi/service/useradmin/Authorization.java
+++ org.osgi.service.useradmin/src/org/osgi/service/useradmin/Authorization.java
@@ -1,7 +1,7 @@
 /*
  * $Header$
  *
- * Copyright (c) OSGi Alliance (2001, 2006). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2001, 2007). All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
