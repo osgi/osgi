43c61f9f6ff7faae69bae2f5d6addcea1333aec5
BJ Hargrave <hargrave@us.ibm.com>
2007-08-03 02:00:27 +0000

    bug 410: add exception type support to BundleException as the first change to the framework package this release, the package version is incremented to 1.5

--- org.osgi.framework/src/org/osgi/framework/BundleException.java
+++ org.osgi.framework/src/org/osgi/framework/BundleException.java
@@ -26,6 +26,10 @@ package org.osgi.framework;
  * <code>BundleException</code> object is created by the Framework to denote
  * an exception condition in the lifecycle of a bundle.
  * <code>BundleException</code>s should not be created by bundle developers.
+ * A type code is used to identify the exception type for future extendability.
+ * 
+ * <p>
+ * OSGi Alliance reserves the right to extend the set of types.
  * 
  * <p>
  * This exception is updated to conform to the general purpose exception
@@ -40,29 +44,102 @@ public class BundleException extends Exception {
 	 * Nested exception.
 	 */
 	private final Throwable	cause;
+	
+	/**
+	 * Type of bundle exception.
+	 * @since 1.5
+	 */
+	private final int type;
+	
+	/**
+	 * No exception type is unspecified.
+	 * @since 1.5
+	 */
+	public static final int UNSPECIFIED = 0;
+	/**
+	 * The operation was unsupported.
+	 * @since 1.5
+	 */
+	public static final int UNSUPPORTED_OPERATION = 1;
+	/**
+	 * The operation was invalid.
+	 * @since 1.5
+	 */
+	public static final int INVALID_OPERATION = 2;
+	/**
+	 * The bundle manifest was in error.
+	 * @since 1.5
+	 */
+	public static final int MANIFEST_ERROR = 3;
+	/**
+	 * The bundle was not resolved.
+	 * @since 1.5
+	 */
+	public static final int RESOLVE_ERROR = 4;
+	/**
+	 * The bundle activator was in error.
+	 * @since 1.5
+	 */
+	public static final int ACTIVATOR_ERROR = 5;
+	/**
+	 * The operation failed due to insufficient permissions.
+	 * @since 1.5
+	 */
+	public static final int SECURITY_ERROR = 6;
+	/**
+	 * The operation failed to complete the requested lifecycle state change.
+	 * @since 1.5
+	 */
+	public static final int STATECHANGE_ERROR = 7;
 
 	/**
-	 * Creates a <code>BundleException</code> that wraps another exception.
+	 * Creates a <code>BundleException</code> 
+	 * with the specified message and exception cause.
 	 * 
 	 * @param msg The associated message.
 	 * @param cause The cause of this exception.
 	 */
 	public BundleException(String msg, Throwable cause) {
-		super(msg);
-		this.cause = cause;
+		this(msg, UNSPECIFIED, cause);
 	}
 
 	/**
-	 * Creates a <code>BundleException</code> object with the specified
+	 * Creates a <code>BundleException</code> with the specified
 	 * message.
 	 * 
 	 * @param msg The message.
 	 */
 	public BundleException(String msg) {
-		super(msg);
-		this.cause = null;
+		this(msg, UNSPECIFIED, null);
 	}
 
+	/**
+	 * Creates a <code>BundleException</code> 
+	 * with the specified message, type and exception cause.
+	 * 
+	 * @param msg The associated message.
+	 * @param type The type for this exception.
+	 * @param cause The cause of this exception.
+	 * @since 1.5
+	 */
+	public BundleException(String msg, int type, Throwable cause) {
+		super(msg);
+		this.type = type;
+		this.cause = cause;
+	}
+	
+	/**
+	 * Creates a <code>BundleException</code> with the specified
+	 * message and type.
+	 * 
+	 * @param msg The message.
+	 * @param type The type for this exception.
+	 * @since 1.5
+	 */
+	public BundleException(String msg, int type) {
+		this(msg, type, null);
+	}
+	
 	/**
 	 * Returns any nested exceptions included in this exception.
 	 * 
@@ -102,5 +179,16 @@ public class BundleException extends Exception {
 	 */
 	public Throwable initCause(Throwable cause) {
 		throw new IllegalStateException();
+	}
+
+	/**
+	 * Returns the type for this exception or <code>UNSPECIFIED</code>
+	 * if the type was unspecified or unknown.
+	 * 
+	 * @return The type of this exception.
+	 * @since 1.5
+	 */
+	public int getType() {
+		return type;
 	}
 }
