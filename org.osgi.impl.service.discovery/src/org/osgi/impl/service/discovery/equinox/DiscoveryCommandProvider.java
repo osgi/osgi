7ac3b2660b34491fb8481f02a6f096b0a7b5699f
Thomas Kiesslich <thomas.kiesslich@siemens.com>
2008-07-07 09:06:23 +0000

    added _unpublish(...); adaptations to Discovery impl changes

--- org.osgi.impl.service.discovery/src/org/osgi/impl/service/discovery/equinox/DiscoveryCommandProvider.java
+++ org.osgi.impl.service.discovery/src/org/osgi/impl/service/discovery/equinox/DiscoveryCommandProvider.java
@@ -1,10 +1,12 @@
 package org.osgi.impl.service.discovery.equinox;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Properties;
 
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.osgi.framework.Bundle;
@@ -16,14 +18,16 @@ import org.osgi.service.discovery.ServiceDescription;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
-public class DiscoveryCommandProvider implements org.eclipse.osgi.framework.console.CommandProvider {
+public class DiscoveryCommandProvider implements
+		org.eclipse.osgi.framework.console.CommandProvider {
 	Discovery discovery;
 	private BundleContext context;
-	
+
 	public DiscoveryCommandProvider(final BundleContext context) {
 		this.context = context;
-		
-		ServiceTracker tracker = new ServiceTracker(context, Discovery.class.getName(), new ServiceTrackerCustomizer() {
+
+		ServiceTracker tracker = new ServiceTracker(context, Discovery.class
+				.getName(), new ServiceTrackerCustomizer() {
 
 			public Object addingService(ServiceReference reference) {
 				discovery = (Discovery) context.getService(reference);
@@ -39,21 +43,22 @@ public class DiscoveryCommandProvider implements org.eclipse.osgi.framework.cons
 					Object service) {
 				discovery = null;
 			}
-			
+
 		});
 		tracker.open();
 	}
 
 	public String getHelp() {
-		return "\n---Discovery---\n\tlookup [<interface>] -- list interfaces\n\tpublish <bundle id> -- publish all interfaces of the given bundle\n";
+		return "\n---Discovery---\n\tlookup [<interface>] -- list interfaces\n\tpublish <bundle id> -- publish all interfaces of the given bundle\n\tunpublish <bundle id> -- unpublish all interfaces of the given bundle\n";
 	}
 
 	public void _lookup(CommandInterpreter ci) {
 		if (discovery != null) {
-			ServiceDescription serviceDescription = new ServiceDescriptionAdapter(ci.nextArgument());
-			
+			ServiceDescription serviceDescription = new ServiceDescriptionAdapter(
+					ci.nextArgument());
+
 			Collection services = discovery.findService(serviceDescription);
-			
+
 			ci.println("services found:");
 			for (Iterator i = services.iterator(); i.hasNext();) {
 				ci.println((ServiceDescription) i.next());
@@ -62,63 +67,33 @@ public class DiscoveryCommandProvider implements org.eclipse.osgi.framework.cons
 			ci.println("Discovery service not set");
 		}
 	}
-	
+
 	public void _publish(CommandInterpreter ci) {
 		if (discovery != null) {
 			String idstr = ci.nextArgument();
-			
 			if (idstr != null) {
 				try {
-					
 					Bundle bundle = context.getBundle(Long.parseLong(idstr));
 					ServiceReference[] refs = bundle.getRegisteredServices();
-					
-					
+					if (refs == null) {
+						ci.println("the bundle " + bundle.getSymbolicName()
+								+ " has not registered any service");
+					}
 					boolean result = false;
-					ServiceDescription serviceDescription;
-					
-					for (int i=0; i<refs.length; i++) {
-						final ServiceReference sr = refs[i];
-						
-						serviceDescription = new ServiceDescription() {
-
-							public String getInterfaceName() {
-								return ((String[])sr.getProperty(Constants.OBJECTCLASS))[0];
-							}
-
-							public Map getProperties() {
-								HashMap map = new HashMap();
-								
-								String[] keys = sr.getPropertyKeys();
-								for (int i=0; i<keys.length; i++) {
-									map.put(keys[i], sr.getProperty(keys[i]));
-								}
-								return map;
-							}
-
-							public Object getProperty(String key) {
-								return sr.getProperty(key);
-							}
-
-							public Collection keys() {
-								return Arrays.asList(sr.getPropertyKeys());
-							}
-
-							public int compare(Object obj, Object obj1) {
-								return 0;
-							}
-							
-						};
-						
-						result = discovery.publish(serviceDescription);
-						
+					ServiceDescriptionAdapter serviceDescription;
+					for (int i = 0; (refs != null) && (i < refs.length); i++) {
+						serviceDescription = new ServiceDescriptionAdapter(
+								refs[i]);
+						serviceDescription.setProperty("myKey", "myValue");
+						result = discovery.publishService(serviceDescription);
 						if (result) {
-							ci.println("Successfully published " + serviceDescription.getInterfaceName());
+							ci.println("Successfully published "
+									+ serviceDescription.getInterfaceName());
 						} else {
-							ci.println("Failed to publish " + serviceDescription.getInterfaceName());
+							ci.println("Failed to publish "
+									+ serviceDescription.getInterfaceName());
 						}
 					}
-					
 				} catch (Exception ex) {
 					ci.printStackTrace(ex);
 				}
@@ -129,40 +104,121 @@ public class DiscoveryCommandProvider implements org.eclipse.osgi.framework.cons
 			ci.println("Discovery service not set");
 		}
 	}
-	
+
+	public void _unpublish(CommandInterpreter ci) {
+		if (discovery != null) {
+			String idstr = ci.nextArgument();
+			if (idstr != null) {
+				try {
+					Bundle bundle = context.getBundle(Long.parseLong(idstr));
+					ServiceReference[] refs = bundle.getRegisteredServices();
+					if (refs == null) {
+						ci.println("the bundle " + bundle.getSymbolicName()
+								+ " has not registered any service");
+					}
+					for (int i = 0; (refs != null) && (i < refs.length); i++) {
+						discovery
+								.unpublishService(new ServiceDescriptionAdapter(
+										refs[i]));
+					}
+				} catch (Exception ex) {
+					ci.printStackTrace(ex);
+				}
+			} else {
+				ci.println("Must specify valid bundle id!");
+			}
+		} else {
+			ci.println("Discovery service not set");
+		}
+	}
+
 	private class ServiceDescriptionAdapter implements ServiceDescription {
-		private String intf;
-		
-		ServiceDescriptionAdapter(String interf) {
+		private String intf = null;
+		private ServiceReference serviceReference = null;
+
+		private Properties props = new Properties();
+
+		public ServiceDescriptionAdapter(ServiceReference sr) {
+			serviceReference = sr;
+		}
+
+		public ServiceDescriptionAdapter(String interf) {
 			intf = interf;
 		}
 
 		public String getInterfaceName() {
-			return intf;
+			if (intf != null) {
+				return intf;
+			}
+			if (serviceReference != null) {
+				return ((String[]) serviceReference
+						.getProperty(Constants.OBJECTCLASS))[0];
+			}
+			return null;
 		}
 
 		public Map getProperties() {
-			// TODO Auto-generated method stub
-			return null;
+			HashMap map = new HashMap();
+			if (serviceReference != null) {
+				String[] keys = serviceReference.getPropertyKeys();
+				for (int i = 0; i < keys.length; i++) {
+					map.put(keys[i], serviceReference.getProperty(keys[i]));
+				}
+			}
+			map.putAll(props);
+			return map;
 		}
 
 		public Object getProperty(String key) {
-			// TODO Auto-generated method stub
-			return null;
+			if (serviceReference != null) {
+				return serviceReference.getProperty(key);
+			} else {
+				return null;
+			}
 		}
 
 		public Collection keys() {
-			// TODO Auto-generated method stub
-			return null;
+			if (serviceReference != null) {
+				return Arrays.asList(serviceReference.getPropertyKeys());
+			} else {
+				return null;
+			}
 		}
 
 		public int compare(Object arg0, Object arg1) {
 			// TODO Auto-generated method stub
 			return 0;
 		}
-		
+
 		public String toString() {
 			return "ServiceDescription: interface=" + intf;
+		}
+
+		public URL getLocation() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public Collection getPropertyKeys() {
+			if (serviceReference != null) {
+				return Arrays.asList(serviceReference.getPropertyKeys());
+			} else {
+				return null;
+			}
+		}
+
+		public String getProtocolSpecificInterfaceName() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public String getVersion() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void setProperty(String key, String value) {
+			props.setProperty(key, value);
 		}
 	}
 }
