1aa5c7bad0a5e4a456ace96e14fda1512a9c8b5b
BJ Hargrave <hargrave@us.ibm.com>
2008-09-16 18:21:41 +0000

    NEW - bug 556: [concurrency]  org.osgi.util.gsm https://www.osgi.org/members/bugzilla/show_bug.cgi?id=556

--- org.osgi.util.gsm/src/org/osgi/util/gsm/IMEICondition.java
+++ org.osgi.util.gsm/src/org/osgi/util/gsm/IMEICondition.java
@@ -1,7 +1,7 @@
 /*
  * $Date$
  *
- * Copyright (c) OSGi Alliance (2004, 2007). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2004, 2008). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,10 +27,13 @@ import org.osgi.service.condpermadmin.ConditionInfo;
 /**
  * Class representing an IMEI condition. Instances of this class contain a
  * string value that is matched against the IMEI of the device.
+ * 
+ * @ThreadSafe
+ * @version $Revision$
  */
 public class IMEICondition {
 	private static final String ORG_OSGI_UTIL_GSM_IMEI = "org.osgi.util.gsm.imei";
-	private static final String imei ;
+	private static final String	imei;
 		
 	static {
 		imei = (String)
@@ -47,39 +50,45 @@ public class IMEICondition {
 	}
 
 	/**
-	 * Creates an IMEICondition object.
+	 * Creates an IMEI condition object.
 	 * 
-	 * @param bundle ignored, as the IMEI number is the property of the mobile device,
-	 * 					and thus the same for all bundles.
-	 * @param conditionInfo contains the IMEI value to match the device's IMEI against. Its
-	 * 		{@link ConditionInfo#getArgs()} method should return a String array with one value, the
-	 * 		IMEI string. The IMEI is 15 digits without hypens. Limited pattern matching is allowed,
-	 * 		then the string is 0 to 14 digits, followed by an asterisk(<code>*</code>).
-	 * @return An IMEICondition object, that can tell whether its IMEI number matches that of the device.
-	 * 			If the number contains an asterisk(<code>*</code>), then the beginning
-	 * 			of the imei is compared to the pattern.
-	 * @throws NullPointerException if one of the parameters is <code>null</code>.
-	 * @throws IllegalArgumentException if the IMEI is not a string of 15 digits, or 
-	 * 		0 to 14 digits with an <code>*</code> at the end.
+	 * @param bundle This parameter is ignored, as the IMEI number is a property
+	 *        of the mobile device and thus is the same for all bundles.
+	 * @param conditionInfo Contains the IMEI value against which to match the
+	 *        device's IMEI. Its {@link ConditionInfo#getArgs()} method should
+	 *        return a String array with one value: the IMEI string. The IMEI is
+	 *        15 digits without hyphens. Limited pattern matching is allowed:
+	 *        the string is 0 to 14 digits, followed by an asterisk (
+	 *        <code>*</code>).
+	 * @return A Condition object that indicates whether the specified IMEI
+	 *         number matches that of the device. If the number contains an
+	 *         asterisk ( <code>*</code>), then the beginning of the IMEI is
+	 *         compared to the pattern.
+	 * @throws IllegalArgumentException If the IMEI is not a string of 15
+	 *         digits, or 0 to 14 digits with an <code>*</code> at the end.
 	 */
 	public static Condition getCondition(Bundle bundle, ConditionInfo conditionInfo) {
-		if (bundle==null) throw new NullPointerException("bundle");
 		String imei = conditionInfo.getArgs()[0];
 		if (imei.length()>15) throw new IllegalArgumentException("imei too long: "+imei);
 		if (imei.endsWith("*")) {
 			imei = imei.substring(0,imei.length()-1);
 		} else {
-			if (imei.length()!=15) throw new IllegalArgumentException("not a valid imei: "+imei);
+			if (imei.length() != 15) {
+				throw new IllegalArgumentException("not a valid imei: " + imei);
+			}
 		}
-		for(int i=0;i<imei.length();i++) {
+		for (int i = 0; i < imei.length(); i++) {
 			int c = imei.charAt(i);
-			if (c<'0'||c>'9') throw new IllegalArgumentException("not a valid imei: "+imei);
+			if (c < '0' || c > '9') {
+				throw new IllegalArgumentException("not a valid imei: " + imei);
+			}
 		}
 		if (IMEICondition.imei==null) {
 			System.err.println("The OSGi Reference Implementation of org.osgi.util.gsm.IMEICondition ");
 			System.err.println("needs the system property "+ORG_OSGI_UTIL_GSM_IMEI+" set.");
 			return Condition.FALSE;
 		}
-		return IMEICondition.imei.startsWith(imei)?Condition.TRUE:Condition.FALSE;
+		return IMEICondition.imei.startsWith(imei) ? Condition.TRUE
+				: Condition.FALSE;
 	}
 }
--- org.osgi.util.gsm/src/org/osgi/util/gsm/IMSICondition.java
+++ org.osgi.util.gsm/src/org/osgi/util/gsm/IMSICondition.java
@@ -1,7 +1,7 @@
 /*
  * $Date$
  *
- * Copyright (c) OSGi Alliance (2004, 2007). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2004, 2008). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,9 @@ import org.osgi.service.condpermadmin.ConditionInfo;
 /**
  * Class representing an IMSI condition. Instances of this class contain a
  * string value that is matched against the IMSI of the subscriber.
+ * 
+ * @ThreadSafe
+ * @version $Revision$
  */
 public class IMSICondition {
 	private static final String ORG_OSGI_UTIL_GSM_IMSI = "org.osgi.util.gsm.imsi";
@@ -43,42 +46,49 @@ public class IMSICondition {
 				);
 	}
 
-	private IMSICondition() {}
+	private IMSICondition() {
+	}
 
 	/**
 	 * Creates an IMSI condition object.
 	 * 
-	 * @param bundle ignored, as the IMSI number is the same for all bundles.
-	 * @param conditionInfo contains the IMSI value to match the device's IMSI against. Its
-	 * 		{@link ConditionInfo#getArgs()} method should return a String array with one value, the
-	 * 		IMSI string. The IMSI is 15 digits without hypens. Limited pattern matching is allowed,
-	 * 		then the string is 0 to 14 digits, followed by an asterisk(<code>*</code>).
-	 * @return An IMSICondition object, that can tell whether its IMSI number matches that of the device.
-	 * 			If the number contains an asterisk(<code>*</code>), then the beginning
-	 * 			of the IMSI is compared to the pattern.
-	 * @throws NullPointerException if one of the parameters is <code>null</code>.
-	 * @throws IllegalArgumentException if the IMSI is not a string of 15 digits, or 
-	 * 		0 to 14 digits with an <code>*</code> at the end.
+	 * @param bundle This parameter is ignored, as the IMSI number is a property
+	 *        of the mobile subscriber and thus is the same for all bundles.
+	 * @param conditionInfo Contains the IMSI value against which to match the
+	 *        subscriber's IMSI. Its {@link ConditionInfo#getArgs()} method
+	 *        should return a String array with one value: the IMSI string. The
+	 *        IMSI is 15 digits without hyphens. Limited pattern matching is
+	 *        allowed: the string is 0 to 14 digits, followed by an asterisk (
+	 *        <code>*</code>).
+	 * @return A Condition object that indicates whether the specified IMSI
+	 *         number matches that of the subscriber. If the number contains an
+	 *         asterisk (<code>*</code>), then the beginning of the IMSI is
+	 *         compared to the pattern.
+	 * @throws IllegalArgumentException If the IMSI is not a string of 15
+	 *         digits, or 0 to 14 digits with an <code>*</code> at the end.
 	 */
 	public static Condition getCondition(Bundle bundle, ConditionInfo conditionInfo) {
-		if (bundle==null) throw new NullPointerException("bundle");
-		if (conditionInfo==null) throw new NullPointerException("conditionInfo");
 		String imsi = conditionInfo.getArgs()[0];
 		if (imsi.length()>15) throw new IllegalArgumentException("imsi too long: "+imsi);
 		if (imsi.endsWith("*")) {
 			imsi = imsi.substring(0,imsi.length()-1);
 		} else {
-			if (imsi.length()!=15) throw new IllegalArgumentException("not a valid imei: "+imsi);
+			if (imsi.length() != 15) {
+				throw new IllegalArgumentException("not a valid imsi: " + imsi);
+			}
 		}
-		for(int i=0;i<imsi.length();i++) {
+		for (int i = 0; i < imsi.length(); i++) {
 			int c = imsi.charAt(i);
-			if (c<'0'||c>'9') throw new IllegalArgumentException("not a valid imei: "+imsi);
+			if (c < '0' || c > '9') {
+				throw new IllegalArgumentException("not a valid imsi: " + imsi);
+			}
 		}
 		if (IMSICondition.imsi==null) {
 			System.err.println("The OSGi Reference Implementation of org.osgi.util.gsm.IMSICondition ");
 			System.err.println("needs the system property "+ORG_OSGI_UTIL_GSM_IMSI+" set.");
 			return Condition.FALSE;
 		}
-		return (IMSICondition.imsi.startsWith(imsi))?Condition.TRUE:Condition.FALSE;
+		return IMSICondition.imsi.startsWith(imsi) ? Condition.TRUE
+				: Condition.FALSE;
 	}
 }
