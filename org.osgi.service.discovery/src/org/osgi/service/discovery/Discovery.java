9e46c1faea8b2e719b7218328e6bdc99d96bdfab
Philipp Konradi <philipp.konradi@siemens.com>
2008-09-23 17:23:56 +0000

    [rfc 119] updated discovery service interface according F2F agreements (interfaceName & filter in findService(), no cache methods).

--- org.osgi.service.discovery/src/org/osgi/service/discovery/Discovery.java
+++ org.osgi.service.discovery/src/org/osgi/service/discovery/Discovery.java
@@ -20,178 +20,149 @@ package org.osgi.service.discovery;
 
 /**
  * 
- * TODO: How to update published ServiceDescriptions? How to identify ServiceDescriptions of the same service instance?
- * TODO: how about to rename auto-publish to push/pull?
+ * TODO: How to update published ServiceDescriptions? How to identify
+ * ServiceDescriptions of the same service instance? <br>
+ * TODO: how about to rename auto-publish to push/pull?<br>
+ * TODO: how to propagate exceptions in async findService calls?
  * 
  * @version $Revision$
  */
 public interface Discovery {
-    final String ORG_OSGI_DISCOVERY = "org.osgi.discovery";
+	final String ORG_OSGI_DISCOVERY = "org.osgi.discovery";
 
-    final String ORG_OSGI_DISCOVERY_NONE = "none";
+	final String ORG_OSGI_DISCOVERY_NONE = "none";
 
-    final String ORG_OSGI_DISCOVERY_AUTO_PUBLISH = "auto-publish";
+	final String ORG_OSGI_DISCOVERY_AUTO_PUBLISH = "auto-publish";
 
-    /**
-     * Add a ServiceListener for a particular service description.
-     * 
-     * @param serviceEndpointDescription
-     *            describes the service(s) to listen for. If serviceEndpointDescription is <code>null</code> then all
-     *            services are considered.
-     * @param listener
-     *            which is to call when discovery detects changes in availability or description of a service. The same
-     *            listener object may also be used to listen on multiple serviceEndpointDescription objects.
-     */
-    void addServiceListener(ServiceListener listener, ServiceEndpointDescription serviceEndpointDescription);
+	/**
+	 * Add a ServiceListener for a particular service description.
+	 * 
+	 * @param filter
+	 *            a filter to services to listen for. If filter is
+	 *            <code>null</code> then all services are considered.
+	 * @param listener
+	 *            which is to call when discovery detects changes in
+	 *            availability or description of a service. The same listener
+	 *            object may be used to listen on multiple service filters.
+	 * @throws IllegalArgumentException
+	 *             if listener is null or if filter is invalid
+	 */
+	void addServiceListener(ServiceListener listener, String filter);
 
-    /**
-     * Add a ServiceListener for a particular service description.
-     * 
-     * @param filter
-     *            an LDAP filter which the service has to satisfy.
-     * @param listener
-     *            which is to call when discovery detects changes in a service's availability or description. The same
-     *            listener object might also be used for multiple serviceEndpointDescription objects.
-     */
-    void addServiceListener(ServiceListener listener, String filter);
+	/**
+	 * This method is the same as calling
+	 * <code>Discovery.addServiceListener(ServiceListener listener, String filter)</code>
+	 * with <code>filter</code> set to <code>null</code>.
+	 * 
+	 * @param listener
+	 *            which is to call when discovery detects changes in
+	 *            availability or description of a service. The same listener
+	 *            object may be used to listen on multiple service filters.
+	 * @throws IllegalArgumentException
+	 *             if listener is null
+	 * @see #addServiceListener(ServiceListener, String)
+	 */
+	void addServiceListener(ServiceListener listener);
 
-    /**
-     * This method is the same as calling
-     * <code>Discovery.addServiceListener(ServiceListener listener, ServiceEndpointDescription serviceEndpointDescription)</code>
-     * with <code>serviceEndpointDescription</code> set to <code>null</code>.
-     * 
-     * @param listener
-     *            which is to call when discovery detects changes in availability or description of a service. The same
-     *            listener object may also be used to listen on multiple serviceEndpointDescription objects.
-     * @see #addServiceListener(ServiceListener, ServiceEndpointDescription)
-     */
-    void addServiceListener(ServiceListener listener);
+	/**
+	 * Removes a ServiceListener.
+	 * 
+	 * @param listener
+	 *            ServiceListener which should be removed. If that listener
+	 *            object was registered several times then all registrations
+	 *            will be removed. If that listener object haven't been added
+	 *            before, then the method returns without throwing exceptions.
+	 */
+	void removeServiceListener(ServiceListener listener);
 
-    /**
-     * Removes a ServiceListener.
-     * 
-     * @param listener
-     *            to remove. If that listener object haven't been added before, then the method returns without throwing
-     *            exceptions.
-     */
-    void removeServiceListener(ServiceListener listener);
+	/**
+	 * Searches for services matching the provided interface name and filter.
+	 * 
+	 * @param interfaceName
+	 *            name of the interface returned services have to provide. If
+	 *            name is null then all services are considered to match.
+	 * @param filter
+	 *            an LDAP filter which the service has to satisfy. Note that
+	 *            <code>ServiceEndpointDescription</code> defines some
+	 *            properties for service url, interface version etc.. If filter
+	 *            is null all services are considered to match.
+	 * @return Array of ServiceEndpointDescription objects matching the service
+	 *         that was found to satisfy the find criteria. The Collection may
+	 *         be empty if none was found.
+	 */
+	ServiceEndpointDescription[] findService(String interfaceName, String filter);
 
-    /**
-     * Looks for the given ServiceEndpointDescription in the cache. Cache might be local or global depending on the
-     * cache strategy of the Discovery.<b>
-     * 
-     * @param serviceEndpointDescription
-     *            to check for its existence in cache
-     * @return true if a service matching the given serviceEndpointDescription is found in the local cache
-     * @throws IllegalArgumentException
-     *             if serviceEndpointDescription is null or incomplete
-     */
-    boolean isCached(ServiceEndpointDescription serviceEndpointDescription);
+	/**
+	 * Asynchronous version of
+	 * <code>Discovery.findService(String interfaceName, String filter)</code>
+	 * method.
+	 * 
+	 * @param interfaceName
+	 *            name of the interface returned services have to provide. If
+	 *            name is null then all services are considered to match.
+	 * @param filter
+	 *            an LDAP filter which the service has to satisfy. Note that
+	 *            <code>ServiceEndpointDescription</code> defines some
+	 *            properties for service url, interface version etc.. If filter
+	 *            is null all services are considered to match.
+	 * @param callback
+	 *            to notify about the asynchronous response of the find
+	 *            operation
+	 * @throws IllegalArgumentException
+	 *             if callback is null
+	 * @see #findService(String, String)
+	 */
+	void findService(String interfaceName, String filter,
+			FindServiceCallback callback);
 
-    /**
-     * Returns an array of all ServiceEndpointDescription objects currently known to the Discovery implementation.
-     * 
-     * @return Array of ServiceEndpointDescription objects. An empty collection is returned if no service descriptions
-     *         were found.
-     */
-    ServiceEndpointDescription[] getCachedServiceEndpointDescriptions();
+	/**
+	 * Publish the provided service description. If the property
+	 * org.osgi.discovery = auto-publish, the Discovery implementation actively
+	 * pushes the information about the service to the network. Otherwise, it is
+	 * just available upon request from other Discovery implementations.
+	 * 
+	 * @param serviceEndpointDescription
+	 *            ServiceEndpointDescription of the service to publish
+	 * @return true if the service was successfully published.
+	 * @throws IllegalArgumentException
+	 *             if serviceEndpointDescription is null, incomplete or invalid
+	 *             (e.g. contains unknown property types)
+	 */
+	boolean publishService(ServiceEndpointDescription serviceEndpointDescription);
 
-    /**
-     * Searches for services matching the criteria provided with the service description. Matching means in this context
-     * a containment and not an equality relation though Discovery might choose to ignore some properties if it can't
-     * really compare them e.g. if a property of the provided service description is a File object (wsdl file for
-     * instance). For this reason its recommended to provide only simple properties with the service description object.
-     * Any other, more complex matching operations could be done in the next step and are caller's responsibility.
-     * 
-     * @param serviceEndpointDescription
-     *            ServiceEndpointDescription of the service to locate
-     * @return Array of ServiceEndpointDescription objects satisfying the find criteria. The Collection may be empty if
-     *         none was found
-     * @throws IllegalArgumentException
-     *             if serviceEndpointDescription is null.
-     */
-    ServiceEndpointDescription[] findService(ServiceEndpointDescription serviceEndpointDescription);
+	/**
+	 * Publish the provided service. The information is published by the
+	 * Discovery implementation.<b> If the parameter autopublish=true, the
+	 * Discovery implementation actively pushes the information about the
+	 * service to the network. Otherwise, it is just available upon request from
+	 * other Discovery implementations. The ServiceEndpointDescription is
+	 * matched using the Comparable interface.
+	 * 
+	 * @param serviceEndpointDescription
+	 *            ServiceEndpointDescription of the service to publish
+	 * @param autopublish
+	 *            if true, service information is actively pushed to the network
+	 *            for discovery
+	 * @return true if the service was successfully published.
+	 * @throws IllegalArgumentException
+	 *             if serviceEndpointDescription is null, incomplete or invalid
+	 *             (e.g. contains unknown property types)
+	 */
+	boolean publishService(
+			ServiceEndpointDescription serviceEndpointDescription,
+			boolean autopublish);
 
-    /**
-     * Find a service based on the provided LDAP filter.
-     * 
-     * @param filter
-     *            an LDAP filter which the service has to satisfy.
-     * @return Array of ServiceEndpointDescription objects matching the service that was found to satisfy the find
-     *         criteria. The Collection may be empty if none was found.
-     * @throws IllegalArgumentException
-     *             if serviceEndpointDescription is null or incomplete
-     * @throws UnsupportedOperationException
-     *             if method is not supported by the implementation
-     */
-    ServiceEndpointDescription[] findService(String filter);
-
-    /**
-     * Asynchronous version of <code>Discovery.findService(ServiceEndpointDescription serviceEndpointDescription)</code>
-     * method.
-     * 
-     * @param serviceEndpointDescription
-     *            ServiceEndpointDescription of the service to locate
-     * @param callback
-     *            to notify about the asynchronous response of the find operation
-     * @throws IllegalArgumentException
-     *             if the serviceEndpointDescription or callback is null
-     * @see #findService(ServiceEndpointDescription)
-     */
-    void findService(ServiceEndpointDescription serviceEndpointDescription, FindServiceCallback callback);
-
-    /**
-     * Asynchronous version of <code>Discovery.findService(String filter)</code> method.
-     * 
-     * @param filter
-     *            an LDAP filter which the service has to satisfy.
-     * @param callback
-     *            to notify about the asynchronous response of the find operation
-     * @throws IllegalArgumentException
-     *             if the serviceEndpointDescription is null or incomplete
-     * @throws UnsupportedOperationException
-     *             if method is not supported by the implementation
-     */
-    void findService(String filter, FindServiceCallback callback);
-
-    /**
-     * Publish the provided service description. If the property org.osgi.discovery = auto-publish, the Discovery
-     * implementation actively pushes the information about the service to the network. Otherwise, it is just available
-     * upon request from other Discovery implementations.
-     * 
-     * @param serviceEndpointDescription
-     *            ServiceEndpointDescription of the service to publish
-     * @return true if the service was successfully published.
-     * @throws IllegalArgumentException
-     *             if serviceEndpointDescription is null, incomplete or invalid (e.g. contains unknown property types)
-     */
-    boolean publishService(ServiceEndpointDescription serviceEndpointDescription);
-
-    /**
-     * Publish the provided service. The information is published by the Discovery implementation.<b> If the parameter
-     * autopublish=true, the Discovery implementation actively pushes the information about the service to the network.
-     * Otherwise, it is just available upon request from other Discovery implementations. The ServiceEndpointDescription
-     * is matched using the Comparable interface.
-     * 
-     * @param serviceEndpointDescription
-     *            ServiceEndpointDescription of the service to publish
-     * @param autopublish
-     *            if true, service information is actively pushed to the network for discovery
-     * @return true if the service was successfully published.
-     * @throws IllegalArgumentException
-     *             if serviceEndpointDescription is null, incomplete or invalid (e.g. contains unknown property types)
-     */
-    boolean publishService(ServiceEndpointDescription serviceEndpointDescription, boolean autopublish);
-
-    /**
-     * Make the given service un-discoverable. The previous publish request for a service is undone. The service
-     * information is also removed from the local or global cache if cached before.
-     * 
-     * @param serviceEndpointDescription
-     *            ServiceEndpointDescription of the service to unpublish. If this ServiceEndpointDescription haven't
-     *            been published before, then the method returns without throwing exceptions.
-     * @throws IllegalArgumentException
-     *             if serviceEndpointDescription is null or incomplete
-     */
-    void unpublishService(ServiceEndpointDescription serviceEndpointDescription);
+	/**
+	 * Make the given service un-discoverable. The previous publish request for
+	 * a service is undone. The service information is also removed from the
+	 * local or global cache if cached before.
+	 * 
+	 * @param serviceEndpointDescription
+	 *            ServiceEndpointDescription of the service to unpublish. If
+	 *            this ServiceEndpointDescription haven't been published before,
+	 *            then the method returns without throwing exceptions.
+	 * @throws IllegalArgumentException
+	 *             if serviceEndpointDescription is null or incomplete
+	 */
+	void unpublishService(ServiceEndpointDescription serviceEndpointDescription);
 }
