14a37765ea8813ab463a85120f7f9791d1739700
BJ Hargrave <hargrave@us.ibm.com>
2008-10-10 01:42:28 +0000

    major rewrite of the javadoc override the methods from Bundle which have special behavior for the system bundle

--- org.osgi.framework/src/org/osgi/framework/launch/SystemBundle.java
+++ org.osgi.framework/src/org/osgi/framework/launch/SystemBundle.java
@@ -13,10 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.osgi.framework.launch;
+
+import java.io.InputStream;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkEvent;
 
 /**
@@ -33,21 +37,23 @@ import org.osgi.framework.FrameworkEvent;
  * configuration argument provides this System Bundle with framework properties
  * to configure the framework instance. The framework instance must also examine
  * the system properties for framework properties which are not set in the
- * configuration argument.  Keys contained in the configuration argument with a  
- * <code>null</code> value must not be used to examine the system properties. 
- * <p> 
- * If framework properties are not provided by the
- * configuration argument or the system properties, this System Bundle must use
- * some reasonable default configuration appropriate for the current VM. For
- * example, the system packages for the current execution environment should be
- * properly exported. The configuration argument may be <code>null</code>. The
- * framework instance must copy any information needed from the configuration
- * argument since the configuration argument can be changed after the framework
- * instance has been created.
+ * configuration argument. A framework property in the configuration argument
+ * with a <code>null</code> value indicates the framework property is
+ * <i>unset</i> and the system properties must not be examined for a value of
+ * the framework property. This allows the configuration argument to
+ * <i>unset</i> framework properties in the system properties .
+ * <p>
+ * If framework properties are not provided by the configuration argument or the
+ * system properties, this System Bundle must use some reasonable default
+ * configuration appropriate for the current VM. For example, the system
+ * packages for the current execution environment should be properly exported.
+ * The configuration argument may be <code>null</code>. The framework instance
+ * must copy any information needed from the configuration argument since the
+ * configuration argument can be changed after the framework instance has been
+ * created.
  * 
  * <p>
- * A newly constructed System Bundle must be in the {@link Bundle#INSTALLED
- * INSTALLED} state.
+ * A newly constructed System Bundle must be in the {@link #INSTALLED} state.
  * 
  * @ThreadSafe
  * @version $Revision$
@@ -56,28 +62,29 @@ public interface SystemBundle extends Bundle {
 
 	/**
 	 * Initialize this System Bundle. After calling this method, this System
-	 * Bundle must be in the {@link Bundle#STARTING STARTING} state and must
-	 * have a valid Bundle Context.
+	 * Bundle must:
+	 * <ul>
+	 * <li>Be in the {@link #STARTING} state.</li>
+	 * <li>Have a valid Bundle Context.</li>
+	 * <li>Have its framework instance be at start level 0.</li>
+	 * <li>Have event handling enabled in its framework instance.</li>
+	 * <li>Have reified Bundle objects for all bundles installed in its
+	 * framework instance.</li>
+	 * <li>Register framework services. For example, <code>PackageAdmin</code>,
+	 * <code>ConditionalPermissionAdmin</code>, <code>StartLevel</code>.</li>
+	 * </ul>
 	 * 
 	 * <p>
-	 * During initialization, this System Bundle must also register any
-	 * framework provided services such as <code>PackageAdmin</code>,
-	 * <code>ConditionalPermissionAdmin</code>, <code>StartLevel</code>, etc.
-	 * 
-	 * <p>
-	 * This System Bundle will not actually be started until <code>start</code>
-	 * is called. If this System Bundle is not initialized prior to calling
-	 * <code>start</code>, then the <code>start</code> method must initialize
-	 * this System Bundle prior to starting.
+	 * This System Bundle will not actually be started until {@link #start()
+	 * start} is called.
 	 * 
 	 * <p>
 	 * This method does nothing if called when this System Bundle is in the
-	 * {@link Bundle#STARTING STARTING}, {@link Bundle#ACTIVE ACTIVE} or
-	 * {@link Bundle#STOPPING STOPPING} states.
+	 * {@link #STARTING}, {@link #ACTIVE} or {@link #STOPPING} states.
 	 * 
 	 * @throws BundleException If this System Bundle could not be initialized.
 	 */
-	void init() throws BundleException;
+	public void init() throws BundleException;
 
 	/**
 	 * Wait until this System Bundle has completely stopped. The
@@ -85,37 +92,237 @@ public interface SystemBundle extends Bundle {
 	 * performs an asynchronous stop of the System Bundle. This method can be
 	 * used to wait until the asynchronous stop of this System Bundle has
 	 * completed. This method will only wait if called when this System Bundle
-	 * is in the {@link Bundle#STARTING STARTING}, {@link Bundle#ACTIVE ACTIVE},
-	 * or {@link Bundle#STOPPING STOPPING} states. Otherwise it will return
-	 * immediately.
+	 * is in the {@link #STARTING}, {@link #ACTIVE}, or {@link #STOPPING}
+	 * states. Otherwise it will return immediately.
 	 * <p>
-	 * A {@link FrameworkEvent} is returned to indicate why the framework has
-	 * stopped.  The following <code>FrameworkEvent</code> types may be returned
-	 * by this method.
-	 * <ul>
-	 *   <li>{@link FrameworkEvent#STOPPED STOPPED} - The system bundle (bundle with 
-	 *   id 0) has been stopped which shutdown the Framework.</li>
-	 *   <li>{@link FrameworkEvent#UPDATED UPDATED} - The system bundle (bundle with 
-	 *   id 0) has been updated which shutdown and restart the Framework.</li>
-	 *   <li>{@link FrameworkEvent#BOOT_CLASSPATH_MODIFIED BOOT_CLASSPATH_MODIFIED} - The Framework
-	 *   has modified the boot class path and has shutdown.  The Execution 
-	 *   Environment must be restarted in order for the new boot class path
-	 *   to take affect.</li>
-	 *   <li>{@link FrameworkEvent#ERROR ERROR} - The Framework encountered an error
-	 *   while shutting down or an error has occurred which forced the 
-	 *   framework to shutdown.</li>
-	 *   <li>{@link FrameworkEvent#INFO INFO} - The wait for stop call has timed out
-	 *   and the framework has not stopped.</li>
-	 * </ul>
+	 * A Framework Event is returned to indicate why this System Bundle has
+	 * stopped.
+	 * 
 	 * @param timeout Maximum number of milliseconds to wait until this System
 	 *        Bundle has completely stopped. A value of zero will wait
 	 *        indefinitely.
-	 * @return A framework event indicating the reason the framework has stopped.
+	 * @return A Framework Event indicating the reason this method returned. The
+	 *         following <code>FrameworkEvent</code> types may be returned by
+	 *         this method.
+	 *         <ul>
+	 *         <li>{@link FrameworkEvent#STOPPED STOPPED} - This System Bundle
+	 *         has been stopped which has shutdown its framework instance. </li>
+	 *         <li>{@link FrameworkEvent#STOPPED_UPDATE STOPPED_UPDATE} - This
+	 *         System Bundle has been updated which has shutdown and will
+	 *         restart its framework instance.</li> <li>
+	 *         {@link FrameworkEvent#STOPPED_BOOTCLASSPATH_MODIFIED
+	 *         STOPPED_BOOTCLASSPATH_MODIFIED} - This System Bundle has been
+	 *         stopped which has shutdown its framework instance and a
+	 *         bootclasspath extension bundle has been installed or updated. The
+	 *         VM must be restarted in order for the changed boot class path to
+	 *         take affect. </li> <li>{@link FrameworkEvent#ERROR ERROR} - The
+	 *         Framework encountered an error while shutting down or an error
+	 *         has occurred which forced the framework to shutdown. </li> <li>
+	 *         {@link FrameworkEvent#INFO INFO} - This method has timed out and
+	 *         returned before this System Bundle has stopped.</li>
+	 *         </ul>
 	 * @throws InterruptedException If another thread interrupted the current
 	 *         thread before or while the current thread was waiting for this
 	 *         System Bundle to completely stop. The <i>interrupted status</i>
 	 *         of the current thread is cleared when this exception is thrown.
 	 * @throws IllegalArgumentException If the value of timeout is negative.
 	 */
-	FrameworkEvent waitForStop(long timeout) throws InterruptedException;
+	public FrameworkEvent waitForStop(long timeout) throws InterruptedException;
+
+	/**
+	 * Start this System Bundle.
+	 * 
+	 * <p>
+	 * The following steps are taken to start this System Bundle:
+	 * <ol>
+	 * <li>If this System Bundle is not in the {@link #STARTING} state,
+	 * {@link #init() initialize} this System Bundle.</li>
+	 * <li>All installed bundles must be started in accordance with each
+	 * bundle's persistent <i>autostart setting</i>. This means some bundles
+	 * will not be started, some will be started with <i>eager activation</i>
+	 * and some will be started with their <i>declared activation</i> policy.
+	 * <ul>
+	 * <li>If this System Bundle implements the optional <i>Start Level Service
+	 * Specification</i>, then the start level of this System Bundle's framework
+	 * instance is moved to the start level specified by the
+	 * {@link Constants#FRAMEWORK_BEGINNING_STARTLEVEL beginning start level}
+	 * framework property, as described in the <i>Start Level Service
+	 * Specification</i>. If this framework property is not specified, then the
+	 * start level of this System Bundle's framework instance is moved to start
+	 * level one (1).</li>
+	 * </ul>
+	 * Any exceptions that occur during bundle starting must be wrapped in a
+	 * {@link BundleException} and then published as a framework event of type
+	 * {@link FrameworkEvent#ERROR}</li>
+	 * </li>
+	 * <li>This System Bundle's state is set to {@link #ACTIVE}.</li>
+	 * <li>A framework event of type {@link FrameworkEvent#STARTED} is fired</li>
+	 * </ol>
+	 * 
+	 * @throws BundleException If this System Bundle could not be started.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,EXECUTE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 * @see "Start Level Service Specification"
+	 */
+	public void start() throws BundleException;
+
+	/**
+	 * Start this System Bundle.
+	 * 
+	 * <p>
+	 * Calling this method is the same as calling {@link #start()}. There are no
+	 * start options for the System Bundle.
+	 * 
+	 * @param options Ignored. There are no start options for the System Bundle.
+	 * @throws BundleException If this System Bundle could not be started.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,EXECUTE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 * @see #start()
+	 */
+	public void start(int options) throws BundleException;
+
+	/**
+	 * Stop this System Bundle.
+	 * 
+	 * <p>
+	 * The method returns immediately to the caller after initiating the
+	 * following steps to be taken on another thread.
+	 * <ol>
+	 * <li>This System Bundle's state is set to {@link #STOPPING}.</li>
+	 * <li>All installed bundles must be stopped without changing each bundle's
+	 * persistent <i>autostart setting</i>.
+	 * <ul>
+	 * <li>If this System Bundle implements the optional <i>Start Level Service
+	 * Specification</i>, then the start level of this System Bundle's framework
+	 * instance is moved to start level zero (0), as described in the <i>Start
+	 * Level Service Specification</i>.</li>
+	 * </ul>
+	 * Any exceptions that occur during bundle stopping must be wrapped in a
+	 * {@link BundleException} and then published as a framework event of type
+	 * {@link FrameworkEvent#ERROR}</li>
+	 * </li>
+	 * <li>Unregister all services registered by this System Bundle.</li>
+	 * <li>Event handling is disabled in this System Bundle's framework
+	 * instance.</li>
+	 * <li>This System Bundle's state is set to {@link #RESOLVED}.</li>
+	 * <li>All resources held by this System Bundle's framework instance are
+	 * released. This includes threads, bundle class loaders, open files, etc.</li>
+	 * <li>Notify all threads that are waiting at {@link #waitForStop(long)
+	 * waitForStop} that the stop operation has completed.</li>
+	 * </ol>
+	 * <p>
+	 * After being stopped, this System Bundle may be discarded, initialized or
+	 * started.
+	 * 
+	 * @throws BundleException If stopping this System Bundle could not be
+	 *         initiated.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,EXECUTE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 * @see "Start Level Service Specification"
+	 */
+	public void stop() throws BundleException;
+
+	/**
+	 * Stop this System Bundle.
+	 * 
+	 * <p>
+	 * Calling this method is the same as calling {@link #stop()}. There are no
+	 * stop options for the System Bundle.
+	 * 
+	 * @param options Ignored. There are no stop options for the System Bundle.
+	 * @throws BundleException If stopping this System Bundle could not be
+	 *         initiated.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,EXECUTE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 * @see #stop()
+	 */
+	public void stop(int options) throws BundleException;
+
+	/**
+	 * The System Bundle cannot be uninstalled.
+	 * 
+	 * <p>
+	 * This method always throws a BundleException.
+	 * 
+	 * @throws BundleException This System Bundle cannot be uninstalled.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,LIFECYCLE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 */
+	public void uninstall() throws BundleException;
+
+	/**
+	 * Stop and restart this System Bundle.
+	 * 
+	 * <p>
+	 * The method returns immediately to the caller after initiating the
+	 * following steps to be taken on another thread.
+	 * <ol>
+	 * <li>Perform the steps in the {@link #stop()} method to stop this System
+	 * Bundle.</li>
+	 * <li>Perform the steps in the {@link #start()} method to start this System
+	 * Bundle.</li>
+	 * </ol>
+	 * 
+	 * @throws BundleException If stopping and restarting this System Bundle
+	 *         could not be initiated.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,LIFECYCLE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 */
+	public void update() throws BundleException;
+
+	/**
+	 * Stop and restart this System Bundle.
+	 * 
+	 * <p>
+	 * Calling this method is the same as calling {@link #update()} except that
+	 * any provided InputStream is immediately closed.
+	 * 
+	 * @param in Any provided InputStream is immediately closed before returning
+	 *        from this method and otherwise ignored.
+	 * @throws BundleException If stopping and restarting this System Bundle
+	 *         could not be initiated.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,LIFECYCLE]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 */
+	public void update(InputStream in) throws BundleException;
+
+	/**
+	 * Returns the System Bundle unique identifier. This System Bundle is
+	 * assigned the unique identifier zero (0).
+	 * 
+	 * @return 0.
+	 * @see Bundle#getBundleId()
+	 */
+	public long getBundleId();
+
+	/**
+	 * Returns the System Bundle location identifier.
+	 * 
+	 * @return The string &quot;<code>System Bundle</code>&quot;.
+	 * @throws java.lang.SecurityException If the caller does not have the
+	 *         appropriate <code>AdminPermission[this,METADATA]</code>, and the
+	 *         Java Runtime Environment supports permissions.
+	 * @see Bundle#getLocation()
+	 * @see Constants#SYSTEM_BUNDLE_LOCATION
+	 */
+	public String getLocation();
+
+	/**
+	 * Returns the symbolic name of this System Bundle. The symbolic name is
+	 * unique for the implementation of the framework. However, the symbolic
+	 * name &quot;<code>system.bundle</code>&quot; must be recognized as an
+	 * alias to the implementation-defined symbolic name.
+	 * 
+	 * @return The symbolic name of this System Bundle.
+	 * @see Bundle#getSymbolicName()
+	 * @see Constants#SYSTEM_BUNDLE_SYMBOLICNAME
+	 */
+	public String getSymbolicName();
 }
