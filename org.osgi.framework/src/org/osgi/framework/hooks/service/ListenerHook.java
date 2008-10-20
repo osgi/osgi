d5173e8d960b4779b34d962111c1e721885d8a18
BJ Hargrave <hargrave@us.ibm.com>
2008-10-20 00:20:36 +0000

    rfc 126: updated to 1) use interface for listener information object. 2) Pass the objects to the hook in a collection. This is needed for remove would could remove multiple listeners when a bundle is stopped. 3) Remove the initial method. The added method can handle both the initial collection and subsequent adds.

--- org.osgi.framework/src/org/osgi/framework/hooks/service/ListenerHook.java
+++ org.osgi.framework/src/org/osgi/framework/hooks/service/ListenerHook.java
@@ -16,8 +16,9 @@
 
 package org.osgi.framework.hooks.service;
 
+import java.util.Collection;
+
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceListener;
 
 /**
  * OSGi Framework Service Listener Hook Service.
@@ -32,91 +33,55 @@ import org.osgi.framework.ServiceListener;
 
 public interface ListenerHook {
 	/**
-	 * Initial listeners hook method. This method is called when the hook is
-	 * first registered to provide the hook with the set of service listeners
-	 * which were had been added prior to the hook being registered. This method
-	 * is only called once. However, due to the timing of other bundles adding
-	 * or removing service listeners, calls to {@link #added} or {@link #removed
-	 * } may occur before the call to this method.
+	 * Add listener hook method. This method is called to provide the hook
+	 * implementation with information on the current service listeners. This
+	 * method will be called immediately after registration of this hook to
+	 * provide the current collection of service listeners which were had been
+	 * added prior to the hook being registered and also for the service
+	 * listeners added after this hook had been registered.
 	 * 
-	 * @param listeners An array of listeners which are listening to service
-	 * 	events.
+	 * @param listeners A collection of {@link ListenerInfo}s for service
+	 *        listeners which are now listening to service events. Attempting to
+	 *        add to or remove from the collection will result in an
+	 *        <code>UnsupportedOperationException</code>. This argument is not
+	 *        synchronized.
 	 */
-
-	void initial(Listener[] listeners);
-
-	/**
-	 * Add listener hook method. This method is called during service listener
-	 * addition. This method will be called once for each service listener added
-	 * after this hook had been registered.
-	 * 
-	 * @param listener A listener which is now listening to service events.
-	 */
-	void added(Listener listener);
+	void added(Collection/* <? extends ListenerInfo> */listeners);
 
 	/**
 	 * Remove listener hook method. This method is called during service
-	 * listener removal. This method will be called once for each service
-	 * listener removed after this hook had been registered.
+	 * listener removal. This method will be called for the service listeners
+	 * removed after this hook had been registered.
 	 * 
-	 * @param listener A listener which is no longer listening to service
-	 * 	events.
+	 * @param listeners A collection of {@link ListenerInfo}s for service
+	 *        listeners which are no longer listening to service events.
+	 *        Attempting to add to or remove from the collection will result in
+	 *        an <code>UnsupportedOperationException</code>. This argument is
+	 *        not synchronized.
 	 */
-	void removed(Listener listener);
+	void removed(Collection/* <? extends ListenerInfo> */listeners);
 
 	/**
-	 * A Service Listener wrapper. This immutable class encapsulates a {@link
-	 * ServiceListener} and the bundle which added it and the filter with which
-	 * it was added. Objects of this type are created by the framework and
-	 * passed to the {@link ListenerHook}.
+	 * Information about a Service Listener. This interface describes the bundle
+	 * which added the Service Listener and the filter with which it was added.
 	 * 
-	 * @Immutable
+	 * @ThreadSafe
 	 */
-	public static class Listener {
-		private final BundleContext		context;
-		private final ServiceListener	listener;
-		private final String			filter;
-
-		/**
-		 * Create a Service Listener wrapper.
-		 * 
-		 * @param context The context of the bundle which added the listener.
-		 * @param listener The ServiceListener object.
-		 * @param filter The filter with which the listener was added.
-		 */
-		public Listener(BundleContext context, ServiceListener listener,
-				String filter) {
-			this.context = context;
-			this.listener = listener;
-			this.filter = filter;
-		}
-
+	public interface ListenerInfo {
 		/**
 		 * Return the context of the bundle which added the listener.
 		 * 
 		 * @return The context of the bundle which added the listener.
 		 */
-		public BundleContext getBundleContext() {
-			return context;
-		}
+		public BundleContext getBundleContext();
 
 		/**
-		 * Return the service listener.
+		 * Return the filter string with which the listener was added.
 		 * 
-		 * @return The service listener.
+		 * @return The filter string with which the listener was added. This may
+		 *         be <code>null</code> if the listener was added without a
+		 *         filter.
 		 */
-		public ServiceListener getServiceListener() {
-			return listener;
-		}
-
-		/**
-		 * Return the filter with which the listener was added.
-		 * 
-		 * @return The filter with which the listener was added. This may be
-		 * 	<code>null</code> if the listener was added without a filter.
-		 */
-		public String getFilter() {
-			return filter;
-		}
+		public String getFilter();
 	}
 }
