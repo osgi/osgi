1ffcecc05cec2a85e7d1973b245eac96d33e1cec
BJ Hargrave <hargrave@us.ibm.com>
2008-11-10 14:02:46 +0000

    no longer used by rfc 98

---
+++ org.osgi.service.transaction/src/org/osgi/service/transaction/TransactionException.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) OSGi Alliance (2000, 2008). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.service.transaction;
+
+/**
+ * TransactionException exception is thrown when the transaction has been marked
+ * for rollback only or the transaction has been rolled back instead of
+ * committed. This is a local exception thrown by methods in the
+ * {@link Transaction} interface.
+ * 
+ * @version $Revision: 1.0 $
+ */
+public class TransactionException extends Exception {
+	private static final long	serialVersionUID			= -640019191675713475L;
+
+	/**
+	 * Undefined error code
+	 */
+	public static final int		UNDEFINED					= 0;
+
+	/**
+	 * This error code is assigned, when the {@link Transaction#commit()} method
+	 * is invoked and Transaction is assigned to more than one thread.
+	 */
+	public static final int		THREADS_NOT_FINISHED_ERROR	= 1;
+
+	/**
+	 * This error code is assigned, when the {@link Transaction#commit()} method
+	 * is invoked and Transaction is not in active state, or was marked as
+	 * rollback only.
+	 * 
+	 * In this case, the application may check
+	 * {@link TransactionContext#getStatus()} method.
+	 */
+	public static final int		STATE_ERROR					= 2;
+
+	/**
+	 * A transaction resource has failed during the 1st, prepare phase of the
+	 * commit process.
+	 * 
+	 * @see TransactionResource#prepare(TransactionContext)
+	 */
+	public static final int		RESOURCE_ERROR				= 4;
+
+	/**
+	 * A transaction resource has failed during the 2nd, commit phase, of the
+	 * commit process. This is typically a {@link RuntimeException}
+	 * TransactionResource#commit(TransactionContext)
+	 */
+	public static final int		HEURISTICS_COMMIT_ERROR		= 8;
+
+	/**
+	 * A transaction resource has failed during rollback. This is typically a
+	 * {@link RuntimeException}
+	 * 
+	 * @see TransactionResource#rollback(TransactionContext)
+	 */
+	public static final int		HEURISTICS_ROLLBACK_ERROR	= 16;
+
+	/**
+	 * A transaction cannot be committed because it has expired.
+	 */
+	public static final int		TIMEOUT_ERROR				= 32;
+
+	private final int			errorCode;
+
+	/**
+	 * Creates new transaction exception
+	 */
+	public TransactionException() {
+		this(null, UNDEFINED);
+	}
+
+	/**
+	 * Creates a new transaction exception with the specified error code.
+	 * 
+	 * @param errorCode the error code
+	 */
+	public TransactionException(int errorCode) {
+		this(null, errorCode);
+	}
+
+	/**
+	 * Creates new transaction exception, with specified error message
+	 * 
+	 * @param message description of the error
+	 */
+	public TransactionException(String message) {
+		this(message, UNDEFINED);
+	}
+
+	private TransactionException(String message, int errorCode) {
+		super(message != null ? message : getString(errorCode));
+		this.errorCode = errorCode;
+	}
+
+	/**
+	 * Creates new transaction exception, with specified error message and
+	 * nested cause
+	 * 
+	 * @param message description of the error
+	 * @param cause the nested cause
+	 */
+	public TransactionException(String message, Throwable cause) {
+		this(message, cause, UNDEFINED);
+	}
+
+	/**
+	 * Creates new transaction exception, with specified error message and
+	 * nested cause. Also assigns an error code.
+	 * 
+	 * @param message description of the error
+	 * @param cause the nested cause
+	 * @param errorCode the error code
+	 */
+	public TransactionException(String message, Throwable cause, int errorCode) {
+		super(message != null ? message : getString(errorCode), cause);
+		this.errorCode = errorCode;
+	}
+
+	/**
+	 * Used to obtain the error code
+	 * 
+	 * @return the error code
+	 */
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	private static final String getString(int code) {
+		switch (code) {
+			case THREADS_NOT_FINISHED_ERROR :
+				return "Only the owner thread must be assigned to the transaction before committing or rollback";
+			case STATE_ERROR :
+				return "The operation cannot be performed at this state of the transaction";
+			case RESOURCE_ERROR :
+				return "A resource processor failed with error during prepare phase";
+			case HEURISTICS_COMMIT_ERROR :
+				return "A resource processor failed during commit";
+			case HEURISTICS_ROLLBACK_ERROR :
+				return "A resource processor failed during rollback";
+			case TIMEOUT_ERROR :
+				return "The transaction has timed out and is automatically rolled back";
+			default :
+				return "Transaction error!";
+		}
+	}
+}
---
+++ org.osgi.service.transaction/src/org/osgi/service/transaction/TransactionResource.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) OSGi Alliance (2000, 2008). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.service.transaction;
+
+/**
+ * A TransactionResource represents a participant in a transaction.
+ * 
+ * When a subsystem is used and it can participate in a transaction, it should
+ * register itself with the current by calling the join method.
+ * 
+ * The resource manager should carefully lock its objects to provide the
+ * appropriate guarantees.
+ * 
+ * At the end of a transaction, the transaction manager will call-back the
+ * resource manager and ask for the outcome of this transaction with the
+ * {@link #prepare(TransactionContext)} method.
+ * 
+ * If the resource manager has not experienced any problems for this
+ * transaction, it should silently return. If the transaction could not be
+ * persisted, it should throw an appropriate exception.
+ * 
+ * After all the participating resource managers have been consulted, the
+ * transaction manager will call {@link #commit(TransactionContext)} to make the
+ * changes persistent or {@link #rollback(TransactionContext)} to undo all the
+ * changes that were made since the transaction started.
+ * 
+ * @version $Revision: 1.0 $
+ */
+public interface TransactionResource {
+
+  /**
+   * Prepare for the changes to be made persistent, no further changes in this
+   * transaction will be made. This method should verify any constraints that
+   * could not have been checked before and throw an exception if anything is
+   * not correct.
+   * 
+   * This method may be called on another thread then the thread associated with
+   * the transaction to speed up the 2-phase commit. I.e. the getCurrentThread()
+   * may return null.
+   * 
+   * @param transaction The associated transaction.
+   * @throws Exception When there is an inconsistency or other error, the
+   *             exception will reflect this. An exception indicates that the
+   *             transaction will be rolled back. The
+   *             {@link #rollback(TransactionContext)} method will still be
+   *             called.
+   */
+  public void prepare(TransactionContext transaction) throws Exception;
+
+  /**
+   * Undo all the changes made to this subsystem that are associated with the
+   * given transaction. The method may be called on another thread than the one
+   * associated with the transaction object.
+   * 
+   * @param transaction The associated transaction.
+   */
+  public void rollback(TransactionContext transaction);
+
+  /**
+   * Commit all the changes associated with the given transaction.
+   * 
+   * @param transaction
+   */
+  public void commit(TransactionContext transaction);
+
+}
