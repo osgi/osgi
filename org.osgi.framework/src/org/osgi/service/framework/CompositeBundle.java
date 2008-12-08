414923ecf346dc2d06fd9901295b02b09ada70e8
Tom Watson <tjwatson@us.ibm.com>
2008-12-08 20:31:30 +0000

    rename to CompositeBundle.

---
+++ org.osgi.framework/src/org/osgi/service/framework/CompositeBundle.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) OSGi Alliance (2008). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.service.framework;
+
+import java.io.InputStream;
+import java.util.Map;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.launch.Framework;
+
+/**
+ * Composite bundles are composed from other bundles which are installed into a
+ * child framework. The Composite bundle is used to share packages and services
+ * between a parent and child framework. A parent framework has one composite
+ * bundle for each of its child frameworks. A parent framework can have zero or
+ * more child composite bundles installed. A child framework must have one and
+ * only one parent composite bundle. In other words a parent framework can have
+ * many children frameworks but a child framework can only have one parent
+ * framework.
+ * <p>
+ * A child composite bundle does the following as specified by the composite
+ * manifest map:
+ * <ul>
+ * <li>Exports packages to the parent framework from the child framework. These
+ * packages are imported by the companion composite bundle from the child
+ * framework.</li>
+ * <li>Imports packages from the parent framework. These packages are exported
+ * by the companion composite bundle to the child framework.</li>
+ * <li>Registers services to the parent framework from the child framework.</li>
+ * <li>Gets services from the parent framework that are registered to the child
+ * framework by the companion composite bundle.</li>
+ * </ul>
+ * <p>
+ * A parent composite bundle does the following as specified by the link
+ * manifest map:
+ * <ul>
+ * <li>Exports packages to the child framework from the parent framework.</li>
+ * <li>Imports packages from the child framework that are exported to the parent
+ * framework by the companion composite bundle.</li>
+ * <li>Registers services to the child framework from the parent framework.</li>
+ * <li>Gets services from the child framework that are registered to the parent
+ * framework by the companion composite bundle.</li>
+ * </ul>
+ * <p>
+ * 
+ * A newly created child <code>Framework</code> will be in the
+ * {@link Bundle#STARTING STARTING} state. This child <code>Framework</code> can
+ * then be used to manage and control the child framework instance. The child
+ * framework instance is persistent and uses a storage area in the child
+ * composite bundle's data area. The child framework's lifecycle is tied to its
+ * child composite bundle's lifecycle in the following ways:
+ * <p>
+ * <ul>
+ * <li>If the child composite bundle is marked to be persistently started (see
+ * StartLevel.isBundlePersistentlyStarted(Bundle)) then the child framework
+ * instance will automatically be started when the child composite bundle's
+ * start-level is met.</li>
+ * <li>The child framework instance will be stopped if the child composite
+ * bundle is persistently stopped or its start level is no longer met.
+ * Performing operations which transiently stop a child composite bundle do not
+ * cause the child framework to stop (e.g. {@link Bundle#stop(int)
+ * stop(Bundle.STOP_TRANSIENT)}, {@link Bundle#update() update}, refreshPackage
+ * etc.).</li>
+ * </ul>
+ * <p>
+ * The child framework may be persistently started and stopped by persistently
+ * starting and stopping the child composite bundle, but it is still possible to
+ * initialize and start the child framework explicitly while the child link
+ * bundle is not persistently started. This allows for the child framework to be
+ * initialized and populated with a set of bundles before starting the link
+ * bundle. The set of bundles installed into the child framework are considered
+ * the bundles which compose the child composite bundle.
+ * <p>
+ * If the child framework is started while the child composite bundle is not
+ * persistently started then the child framework lifecycle is tied to its parent
+ * framework lifecycle. When the parent <code>Framework</code> enters the
+ * {@link Bundle#STOPPING STOPPING} state then all active child frameworks of
+ * that parent are shutdown using the to the {@link Framework#stop()} method.
+ * The parent <code>Framework</code> must not enter the {@link Bundle#RESOLVED}
+ * state until all the child frameworks have completed their shutdown process.
+ * After the parent framework has completed the shutdown process then all child
+ * framework instances become invalid and must not be allowed to re-initialize
+ * or re-start.
+ * 
+ * @ThreadSafe
+ * @version $Revision: 5897 $
+ */
+public interface CompositeBundle extends Bundle {
+	public static final int TYPE_CHILD = 0x01;
+	public static final int TYPE_PARENT = 0x02;
+
+	/**
+	 * Returns the companion framework associated with this composite bundle. If
+	 * this is a parent composite bundle then the parent framework is returned;
+	 * otherwise the child framework is returned.
+	 * 
+	 * @return the companion framework.
+	 */
+	Framework getCompanionFramework();
+
+	/**
+	 * Returns the companion composite bundle associated with this composite
+	 * bundle. The companion bundle is installed in the companion framework.
+	 * 
+	 * @return the companion link bundle.
+	 */
+	CompositeBundle getCompanionComposite();
+
+	/**
+	 * Updates this child composite bundle and its companion parent composite
+	 * bundle with the specified link manifest.
+	 * 
+	 * @param linkManifest
+	 *            the link manifest to update to.
+	 * @throws BundleException
+	 *             If the update fails or if this is a parent composite bundle.
+	 */
+	void update(Map /* <String, String> */linkManifest) throws BundleException;
+
+	/**
+	 * Returns the type of composite bundle this is. A value of
+	 * {@link #TYPE_CHILD} is returned if this is a child composite bundle. A
+	 * value of {@link #TYPE_PARENT} is returned if this is a parent composite
+	 * bundle.
+	 * 
+	 * @return the type of composite bundle this is.
+	 */
+	int getCompositeType();
+
+	/**
+	 * This operation is not supported for composite bundles. A
+	 * <code>BundleException</code> of type
+	 * {@link BundleException#INVALID_OPERATION invalid operation} must be
+	 * thrown.
+	 */
+	void update() throws BundleException;
+
+	/**
+	 * This operation is not supported for composite bundles. A
+	 * <code>BundleException</code> of type
+	 * {@link BundleException#INVALID_OPERATION invalid operation} must be
+	 * thrown.
+	 */
+	void update(InputStream input) throws BundleException;
+
+	/**
+	 * Uninstalls this composite bundle and its companion bundle.
+	 * <p>
+	 * If this composite bundle is a child composite then the companion child
+	 * framework is shutdown and its persistent storage area is deleted.
+	 */
+	void uninstall() throws BundleException;
+}
---
+++ org.osgi.framework/src/org/osgi/service/framework/CompositeBundleFactory.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) OSGi Alliance (2008). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.service.framework;
+
+import java.util.Map;
+import org.osgi.framework.*;
+import org.osgi.framework.launch.Framework;
+
+/**
+ * Framework service that is used to create composite bundles.
+ * <p>
+ * If present, there will only be a single instance of this service registered
+ * with the Framework.
+ * 
+ * @ThreadSafe
+ * @version $Revision: 5897 $
+ */
+// TODO javadoc needs review
+public interface CompositeBundleFactory {
+	/**
+	 * Manifest header (named &quot;CompositeServiceFilter-Import;) identifying
+	 * the service filters that are used by a child composite bundle to select
+	 * services that will be registered into a child framework from a parent
+	 * composite bundle.
+	 */
+	public static final String COMPOSITE_SERVICE_FILTER_IMPORT = "CompositeServiceFilter-Import";
+
+	/**
+	 * Manifest header (named &quot;CompositeServiceFilter-Export;) identifying
+	 * the service filters that are used by a parent composite bundle to select
+	 * services that will be registered into a parent framework from a child
+	 * composite bundle.
+	 */
+	public static final String COMPOSITE_SERVICE_FILTER_EXPORT = "CompositeServiceFilter-Export";
+
+	/**
+	 * Creates a new child <code>CompositeBundle</code>. The child composite
+	 * bundle has a new child <code>Framework</code> associated with it and a
+	 * companion composite bundle which is installed in the child framework.
+	 * Composite bundles share packages and services between a parent framework
+	 * and a child framework.
+	 * <p>
+	 * The following steps are required to create a child composite bundle:
+	 * <ol>
+	 * <li>If a bundle containing the same location string is already installed,
+	 * then if the Bundle object is a child <code>LinkBundle</code> then that
+	 * composite bundle is returned; otherwise a BundleException is thrown
+	 * indicating that an incompatible bundle is already installed at the
+	 * specified location.</li>
+	 * <li>The child composite bundle's associated resources are allocated. The
+	 * associated resources minimally consist of a unique identifier and a
+	 * persistent storage area. If this step fails, a BundleException is thrown.
+	 * The compositeManifest map is used to provide the headers for the child
+	 * composite bundle and its companion composite bundle.
+	 * <p>
+	 * If composite manifest map does not contain the following header(s) then a
+	 * BundleException is thrown:
+	 * <ul>
+	 * <li> {@link Constants#BUNDLE_SYMBOLICNAME Bundle-SymbolicName} the
+	 * symbolic name used for the child composite bundle and its companion
+	 * composite bundle.
+	 * </ul>
+	 * </p>
+	 * The composite manifest map may optionally contain the following
+	 * header(s):
+	 * <ul>
+	 * <li> {@link Constants#BUNDLE_VERSION Bundle-Version} the bundle version
+	 * used for the child composite bundle and its companion composite bundle.
+	 * <li> {@link Constants#IMPORT_PACKAGE Import-Package} the packages which
+	 * are imported from the parent framework by the child composite bundle and
+	 * are exported to the child framework by the companion composite bundle.</li>
+	 * <li>{@link Constants#EXPORT_PACKAGE Export-Package} the packages which
+	 * are imported from the child framework by the companion composite bundle
+	 * and are exported to the parent framework by the child composite bundle.</li>
+	 * <li>{@link #COMPOSITE_SERVICE_FILTER_IMPORT LinkServiceFilter-Import} the
+	 * service filters which are acquired from the parent framework by the child
+	 * composite bundle and are registered in the child framework by the
+	 * companion composite bundle.</li>
+	 * <li>{@link #COMPOSITE_SERVICE_FILTER_EXPORT LinkServiceFilter-Export} the
+	 * service filters which are acquired from the child framework by the
+	 * companion composite bundle and are registered in the parent framework by
+	 * the child composite bundle.</li>
+	 * </ul>
+	 * <li>A child framework is create which uses a storage area under the child
+	 * composite bundle's associated persistent storage. Note that if the
+	 * framework configuration property {@link Constants#FRAMEWORK_STORAGE
+	 * org.osgi.framework.storage} is specified in the framework config then it
+	 * is ignored and a storage area located under the child composite bundle's
+	 * associated persistent storage area is used.</li>
+	 * <li>The child framework is initialized (see {@link Framework#init()}.
+	 * <li>A parent composite bundle is installed into the child framework</li>
+	 * <li>The child composite bundle's state is set to INSTALLED.</li>
+	 * <li>A bundle event of type {@link BundleEvent#INSTALLED} is fired for the
+	 * child composite bundle.
+	 * <li>The <code>LinkBundle</code> object for the newly child composite
+	 * bundle is returned
+	 * </ol>
+	 * <p>
+	 * 
+	 * @param frameworkConfig
+	 *            the child framework configuration.
+	 * @param location
+	 *            the bundle location used for the child composite bundle and
+	 *            its companion bundle.
+	 * @param compositeManifest
+	 *            the manifest used to create the composite bundle
+	 * @return A new child composite bundle.
+	 * @throws BundleException 
+	 *             If the composite manifest is invalid or there is some other
+	 *             problem with installing the composite bundle.
+	 * @throws SecurityException
+	 *             If the caller does not have <code>AllPermission</code>.
+	 * @see Framework
+	 * @see CompositeBundle
+	 */
+	CompositeBundle newChildCompositeBundle(
+			Map /* <String, String> */frameworkConfig, String location,
+			Map /* <String, String> */compositeManifest) throws BundleException;
+
+}
