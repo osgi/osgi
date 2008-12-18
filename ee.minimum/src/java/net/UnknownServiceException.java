9d82731e5e05c506d50b315235457d9888b4792c
BJ Hargrave <hargrave@us.ibm.com>
2008-12-18 21:47:29 +0000

    update copyright for end of year

--- ee.minimum/src/java/net/UnknownServiceException.java
+++ ee.minimum/src/java/net/UnknownServiceException.java
@@ -2,7 +2,7 @@
  * $Revision$
  *
  * (C) Copyright 2001 Sun Microsystems, Inc.
- * Copyright (c) OSGi Alliance (2001, 2007). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2001, 2008). All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
