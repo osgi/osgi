d0d07ae5952024a1d432c026b09ea1058386d2a7
BJ Hargrave <hargrave@us.ibm.com>
2009-01-07 17:16:26 +0000

    separate tests into its own project fix bnd.bnd to copy javax.transaction packages from geronimo files instead of ee.j2se files.

--- org.osgi.test.cases.transaction/src/org/osgi/test/cases/transaction/TestResource.java
+++ org.osgi.test.cases.transaction/src/org/osgi/test/cases/transaction/TestResource.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package test;
+package org.osgi.test.cases.transaction;
 
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
