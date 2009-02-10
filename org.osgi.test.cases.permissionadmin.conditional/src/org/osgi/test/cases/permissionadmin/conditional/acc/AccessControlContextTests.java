3f4f4347612a06c61f3d993b22c9f6dfe940603e
Tom Watson <tjwatson@us.ibm.com>
2009-02-10 15:49:36 +0000

    Commit ACC tests.

---
+++ org.osgi.test.cases.permissionadmin.conditional/src/org/osgi/test/cases/permissionadmin/conditional/acc/AccessControlContextTests.java
@@ -0,0 +1,290 @@
+/*
+ * Copyright (c) OSGi Alliance (2009). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osgi.test.cases.permissionadmin.conditional.acc;
+
+import java.io.FilePermission;
+import java.security.AccessControlContext;
+import java.security.AccessControlException;
+import java.security.AllPermission;
+import java.util.List;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.condpermadmin.ConditionInfo;
+import org.osgi.service.condpermadmin.ConditionalPermissionAdmin;
+import org.osgi.service.condpermadmin.ConditionalPermissionInfo;
+import org.osgi.service.condpermadmin.ConditionalPermissionUpdate;
+import org.osgi.service.permissionadmin.PermissionAdmin;
+import org.osgi.service.permissionadmin.PermissionInfo;
+import org.osgi.test.support.OSGiTestCase;
+
+public class AccessControlContextTests extends OSGiTestCase {
+	private static final PermissionInfo[] READONLY_INFOS = new PermissionInfo[] {new PermissionInfo("java.io.FilePermission", "<<ALL FILES>>", "read")}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	private static final PermissionInfo[] READWRITE_INFOS = new PermissionInfo[] {
+	// multiple permission infos
+			new PermissionInfo("java.io.FilePermission", "<<ALL FILES>>", "read"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			new PermissionInfo("java.io.FilePermission", "<<ALL FILES>>", "write") //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	};
+	private static final ConditionInfo SIGNER_CONDITION1 = new ConditionInfo("org.osgi.service.condpermadmin.BundleSignerCondition", new String[] {"*;cn=test1,c=US"}); //$NON-NLS-1$//$NON-NLS-2$
+	private static final ConditionInfo SIGNER_CONDITION2 = new ConditionInfo("org.osgi.service.condpermadmin.BundleSignerCondition", new String[] {"*;cn=test2,c=US"}); //$NON-NLS-1$//$NON-NLS-2$
+	private static final ConditionInfo NOT_SIGNER_CONDITION1 = new ConditionInfo("org.osgi.service.condpermadmin.BundleSignerCondition", new String[] {"*;cn=test1,c=US", "!"}); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+
+	private ConditionalPermissionAdmin condPermAdmin;
+	private ServiceReference condPermAdminRef;
+	private PermissionAdmin permAdmin;
+	private ServiceReference permAdminRef;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		// get condpermadmin
+		condPermAdminRef = getContext().getServiceReference(ConditionalPermissionAdmin.class.getName());
+		assertNotNull("No ConditionalPermissionAdmin available", condPermAdminRef);
+		condPermAdmin = (ConditionalPermissionAdmin) getContext().getService(condPermAdminRef);
+		assertNotNull("No ConditionalPermissionAdmin available", condPermAdmin);
+
+		// get permadmin
+		permAdminRef = getContext().getServiceReference(PermissionAdmin.class.getName());
+		assertNotNull("No PermissionAdmin available", permAdminRef);
+		permAdmin = (PermissionAdmin) getContext().getService(permAdminRef);
+		assertNotNull("No PermissionAdmin available", permAdmin);
+		
+	    // make sure this bundle has all permissions
+	    permAdmin.setPermissions(getContext().getBundle().getLocation(), new PermissionInfo[] {new PermissionInfo("(" + AllPermission.class.getName() +")")});
+	}
+
+	protected void tearDown() throws Exception {
+		// clear any permissions set
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+	    update.getConditionalPermissionInfos().clear();
+	    update.commit();
+	    permAdmin.setDefaultPermissions(null);
+	    String[] locations = permAdmin.getLocations();
+	    if (locations != null)
+	    	for (int i = 0; i < locations.length; i++)
+	    		permAdmin.setPermissions(locations[i], null);
+
+		if (condPermAdminRef != null)
+			getContext().ungetService(condPermAdminRef);
+		condPermAdminRef = null;
+		condPermAdmin = null;
+
+		if (permAdminRef != null)
+			getContext().ungetService(permAdminRef);
+		permAdminRef = null;
+		permAdmin = null;
+	}
+
+	public void testSingleSignerRowAllow() {
+		// test single row with signer condition
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+		List rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test1,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+	}
+
+	public void testSingleSignerMultiRowDeny() {
+		// test with DENY row
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+		List rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READWRITE_INFOS, ConditionalPermissionInfo.DENY));
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READWRITE_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test1,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+	}
+
+	public void testMultiSignerMultiRowAllow() {
+		// test multiple signer conditions
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+		List rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+
+		update = condPermAdmin.newConditionalPermissionUpdate();
+		rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION2}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+		acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+	}
+
+	public void testMultiSignerGet() {
+		// test multiple signer conditions
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+		List rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1, SIGNER_CONDITION2}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READWRITE_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+
+		acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test1,c=US", "cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$ //$NON-NLS-2$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+	}
+
+	public void testDefaultPemissions() {
+		// test with empty rows
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+			acc.checkPermission(new AllPermission());
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+		// set the default permissions
+		permAdmin.setDefaultPermissions(READWRITE_INFOS);
+		acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+		try {
+			acc.checkPermission(new AllPermission());
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+	}
+
+	public void testEmptyConditionRows() {
+		// test with empty condition rows
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+		List rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {SIGNER_CONDITION1}, READWRITE_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+	}
+
+	public void testNotSignerRowAllow() {
+		// test ! signer condition
+		ConditionalPermissionUpdate update = condPermAdmin.newConditionalPermissionUpdate();
+		List rows = update.getConditionalPermissionInfos();
+		rows.add(condPermAdmin.newConditionalPermissionInfo(null, new ConditionInfo[] {NOT_SIGNER_CONDITION1}, READONLY_INFOS, ConditionalPermissionInfo.ALLOW));
+		assertTrue("failed to commit", update.commit()); //$NON-NLS-1$
+		AccessControlContext acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test1,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+
+		acc = condPermAdmin.getAccessControlContext(new String[] {"cn=t1,cn=FR;cn=test2,c=US"}); //$NON-NLS-1$
+		try {
+			acc.checkPermission(new FilePermission("test", "write")); //$NON-NLS-1$ //$NON-NLS-2$
+			fail("expecting AccessControlExcetpion"); //$NON-NLS-1$
+		} catch (AccessControlException e) {
+			// expected
+		}
+		try {
+			acc.checkPermission(new FilePermission("test", "read")); //$NON-NLS-1$ //$NON-NLS-2$
+		} catch (AccessControlException e) {
+			fail("Unexpected AccessControlExcetpion", e); //$NON-NLS-1$
+		}
+	}
+}
