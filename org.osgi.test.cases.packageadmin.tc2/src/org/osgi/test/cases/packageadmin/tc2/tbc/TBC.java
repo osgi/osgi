c00a7cd02d5c341d9e796daaf5e231b3dda70826
Tom Watson <tjwatson@us.ibm.com>
2009-02-24 14:57:37 +0000

    Migrate to bnd and junit

--- org.osgi.test.cases.packageadmin.tc2/src/org/osgi/test/cases/packageadmin/tc2/tbc/TBC.java
+++ org.osgi.test.cases.packageadmin.tc2/src/org/osgi/test/cases/packageadmin/tc2/tbc/TBC.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.net.*;
 import org.osgi.framework.*;
 import org.osgi.service.packageadmin.*;
-import org.osgi.test.service.*;
+import org.osgi.test.support.compatibility.DefaultTestBundleControl;
 
 /**
  * This is the bundle initially installed and started by the TestCase when
@@ -18,13 +18,9 @@ import org.osgi.test.service.*;
  * 
  * @author Ericsson
  */
-public class TBC extends Thread {
-	BundleContext			context;
-	//BundleContext otherContext;
+public class TBC extends DefaultTestBundleControl {
 	ServiceRegistration		sr;
-	ServiceReference		linkRef;
-	TestCaseLink			link;
-	String					tcHome;
+
 	boolean					_continue		= true;
 	boolean					called;
 	int						eventQueue[]	= new int[8];
@@ -33,61 +29,11 @@ public class TBC extends Thread {
 	private final String	TP2				= "org.osgi.test.cases.packageadmin.tc2.tb2";
 	private final String	TP3				= "org.osgi.test.cases.packageadmin.tc2.tb3";
 	ServiceReference		serviceRef;
-	static String[]			methods			= new String[] {"tc2"};
-
-	/**
-	 * Constructor. Gets a reference to the TestCaseLink to communicate with the
-	 * TestCase.
-	 */
-	TBC(BundleContext context) {
-		this.context = context;
-		this.linkRef = context
-				.getServiceReference(TestCaseLink.class.getName());
-		this.link = (TestCaseLink) context.getService(linkRef);
-	}
-
-	/**
-	 * This function performs the tests.
-	 */
-	public void run() {
-		sr = context.registerService(TBC.class.getName(), this, null);
-		try {
-			link.log("Test bundle control started Ok.");
-			tcHome = (String) link.receive(10000);
-			for (int i = 0; _continue && i < methods.length; i++) {
-				tc2();
-				link.send("" + 100 * (i + 1) / methods.length);
-			}
-			link.send("ready");
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Releases the reference to the TestCaseLink.
-	 */
-	void quit() {
-		if (_continue) {
-			context.ungetService(linkRef);
-			linkRef = null;
-			_continue = false;
-		}
-	}
-
-	void log(String test, String result) throws IOException {
-		link.log(test + " " + result);
-	}
-
-	void log(String test) throws IOException {
-		link.log(test);
-	}
 
 	/**
 	 *  
 	 */
-	void tc2() throws Exception {
+	public void testTc2() throws Exception {
 		Bundle tb1;
 		Bundle tb2;
 		Bundle tb4;
@@ -95,215 +41,182 @@ public class TBC extends Thread {
 		boolean tp1present = false;
 		boolean tp3present = false;
 		String tb1loc;
-		ServiceReference sr;
-		//2.15.2 Testcase2 (tc2), exporting packages, updating
-		//Tb1 will export package ...testpackage1
-		//Tb2 actively has chosen ...testpackage1, exports TestPackage2
+
+		// 2.15.2 Testcase2 (tc2), exporting packages, updating
+		// Tb1 will export package ...testpackage1
+		// Tb2 actively has chosen ...testpackage1, exports TestPackage2
 		// sr =
-		// context.getServiceReference("org.osgi.service.packageadmin.PackageAdmin");
-		sr = context.getServiceReference(PackageAdmin.class.getName());
-		PackageAdmin pa = (PackageAdmin) context.getService(sr);
-		//Check that the packages doesn?t exist before and do not run if they
+		// getContext().getServiceReference("org.osgi.service.packageadmin.PackageAdmin");
+		ServiceReference sr = getContext().getServiceReference(
+				PackageAdmin.class.getName());
+		assertNotNull("PackageAdmin not available", sr);
+		PackageAdmin pa = (PackageAdmin) getContext().getService(sr);
+		// Check that the packages doesn?t exist before and do not run if they
 		// do
 		if ((pa.getExportedPackage(TP1) != null)
 				|| (pa.getExportedPackage(TP2) != null)
 				|| (pa.getExportedPackage(TP3) != null)) {
-			log("The packages used for the test are already exported, i.e. the test is impossible to make.");
+			fail("The packages used for the test are already exported, i.e. the test is impossible to make.");
 		}
-		else {
-			//Install tb1, tb2 (and start)
-			tb1 = context.installBundle(tcHome + "tb1.jar");
-			tb2 = context.installBundle(tcHome + "tb2.jar");
-			tb1.start();
-			tb2.start();
-			log("Install Testbundle 1 and 2.");
-			tb1loc = tb1.getLocation();
-			//Call PackageAdmin.getExportedPackages(Bundle bundle) for tb1
-			//Is ...testpackage1 exported (yes)
-			ExportedPackage[] tps1 = pa.getExportedPackages(tb1);
-			tp1present = false;
-			for (int i = 0; i < tps1.length; i++) {
-				if (tps1[i].getName().equals(TP1)) {
-					tp1present = true;
+
+		// Install tb1, tb2 (and start)
+		tb1 = installBundle("tc2.tb1.jar");
+		tb2 = installBundle("tc2.tb2.jar");
+		log("Install Testbundle 1 and 2.");
+		tb1loc = tb1.getLocation();
+		// Call PackageAdmin.getExportedPackages(Bundle bundle) for tb1
+		// Is ...testpackage1 exported (yes)
+		ExportedPackage[] tps1 = pa.getExportedPackages(tb1);
+		assertNotNull("No exported packages from tb1:", tps1);
+		assertEquals("Wrong number of exports:", 1, tps1.length);
+		assertEquals("What is the package name:", TP1, tps1[0].getName());
+		assertTrue("Who is exported: " + TP1, tps1[0].getExportingBundle() == tb1);
+		// Call PackageAdmin.getExportedPackage(...testpackage1)
+		// Is ...testpackage1 exported (yes)
+		ExportedPackage[] ep = pa.getExportedPackages(TP1);
+		assertNotNull("No exported packages: " + TP1, ep);
+		assertEquals("Wrong number of exports for: " + TP1, 1, ep.length);
+		assertEquals("What is the package name:", TP1, ep[0].getName());
+		assertTrue("Who is exported: " + TP1, ep[0].getExportingBundle() == tb1);
+
+		// uninstall tb1
+		// install tb3 called the same as tb1 (tb1.jar) but exporting
+		// package ...testpackage3 (simulates updating of the bundle)
+		tb5 = installBundle("tc2.tb5.jar", false);
+		try {
+			assertNotNull("Null resource from tc2.tb5", tb5.getResource("tc2.tb1.jar"));
+		}
+		catch (IllegalStateException e) {
+			fail("Could not getResource on a nonstarted but installed bundle.", e);
+		}
+		tb5.start();
+		// tb5 contains tb3 named as tb1
+		log("Update TestBundle 1. Now TestBundle 1 contains TestPackage 3 instead of TestPackage 1.");
+		try {
+			tb1.update(tb5.getResource("tc2.tb1.jar").openStream());
+		}
+		catch (Exception e) {
+			fail("Update failed", e);
+		}
+		// install tb4, tb4 actively has chosen ...testpackage3
+		tb4 = installBundle("tc2.tb4.jar");
+		// Call PackageAdmin.getExportedPackages(Bundle bundle) for tb3
+		// Should reply ...testpackage3 and ...testpackage1
+		ExportedPackage[] tps13 = pa.getExportedPackages(tb1);
+		tp1present = false;
+		tp3present = false;
+		for (int i = 0; i < tps13.length; i++) {
+			if (tps13[i].getName().equals(TP1)) {
+				tp1present = true;
+			}
+			else
+				if (tps13[i].getName().equals(TP3)) {
+					tp3present = true;
 				}
-			}
-			log("Is Testpackage 1 exported: true : '" + tp1present + "'");
-			//Call PackageAdmin.getExportedPackage(...testpackage1)
-			//Is ...testpackage1 exported (yes)
-			ExportedPackage[] ep = pa.getExportedPackages(tb1);
-			if (ep.length == 1) {
-				log("What is TestBundle 1 exporting. : org.osgi.test.cases.packageadmin.tc2.tb1 : '"
-						+ ep[0].getName() + "'");
-			}
-			ExportedPackage[] tps133 = pa.getExportedPackages(tb1);
-			tp1present = false;
-			tp3present = false;
-			for (int i = 0; i < tps133.length; i++) {
-				if (tps133[i].getName().equals(TP1)) {
-					tp1present = true;
-				}
-				else
-					if (tps133[i].getName().equals(TP3)) {
-						tp3present = true;
-					}
-			}
-			log("Does TestBundle 1 contain Testpackage 1 before update: true : '"
-					+ tp1present + "'");
-			log("Does TestBundle 1 contain Testpackage 3 before update: false : '"
-					+ tp3present + "'");
-			link.send("25");
-			//uninstall tb1
-			//install tb3 called the same as tb1 (tb1.jar) but exporting
-			// package ...testpackage3 (simulates updating of the bundle)
-			tb5 = context.installBundle(tcHome + "tb5.jar");
-			try {
-				tb5.getResource("www/tb1.jar");
-			}
-			catch (IllegalStateException e) {
-				log("Could not getResource on a nonstarted but installed bundle."
-						+ e);
-			}
-			tb5.start();
-			//tb5 contains tb3 named as tb1
-			log("Update TestBundle 1. Now TestBundle 1 contains TestPackage 3 instead of TestPackage 1.");
-			try {
-				tb1.update(tb5.getResource("www/tb1.jar").openStream());
-			}
-			catch (Exception e) {
-				log("Bundle.getResource() failed.");
-			}
-			//install tb4, tb4 actively has chosen ...testpackage3
-			tb4 = context.installBundle(tcHome + "tb4.jar");
-			tb4.start();
-			//Call PackageAdmin.getExportedPackages(Bundle bundle) for tb3
-			//Should reply ...testpackage3 and ...testpackage1
-			ExportedPackage[] tps13 = pa.getExportedPackages(tb1);
-			tp1present = false;
-			tp3present = false;
-			for (int i = 0; i < tps13.length; i++) {
-				if (tps13[i].getName().equals(TP1)) {
-					tp1present = true;
-				}
-				else
-					if (tps13[i].getName().equals(TP3)) {
-						tp3present = true;
-					}
-			}
-			log("Because of lazy update, now TestBundle 1 should contain TestPackage 1 and TestPackage 3.");
-			log("Does TestBundle 1 contain Testpackage 1 after update: true : '"
-					+ tp1present + "'");
-			log("Does TestBundle 1 contain Testpackage 3 after update: true : '"
-					+ tp3present + "'");
-			//Call PackageAdmin.getExportedPackage(...testpackage1) and
-			// getExportedPackage(...testpackage3)
-			//Is ...testpackage1 exported (yes, not null)
-			//Is ...testpackage3 exported (yes, not null)
-			log("Is TestPackage 1 exported: true : '"
-					+ (pa.getExportedPackage(TP1) != null) + "'");
-			log("Is TestPackage 3 exported: true : '"
-					+ (pa.getExportedPackage(TP3) != null) + "'");
-			//is tb4 active (yes)
-			//is tb2 active (yes)
-			log("Is testbundle 2 still active: true: '"
-					+ (tb2.getState() == Bundle.ACTIVE) + "'");
-			log("Is testbundle 4 still active: true: '"
-					+ (tb4.getState() == Bundle.ACTIVE) + "'");
-			link.send("50");
-			//Call PackageAdmin.refreshPackages()
-			Bundle[] b = new Bundle[4];
-			b[0] = tb1;
-			b[1] = tb2;
-			b[2] = tb4;
-			b[3] = tb5;
-			log("Refresh all packages.");
-      
-      PackagesRefreshedListener prl = new PackagesRefreshedListener();
-      try {
-        context.addFrameworkListener(prl);
-        pa.refreshPackages(b);
-        prl.waitForPackagesRefreshedEvent();
-      } finally {
-        context.removeFrameworkListener(prl);
-      }
-      
-			//Call PackageAdmin.getExportedPackages(Bundle bundle) for tb3
-			ExportedPackage[] tps3 = pa.getExportedPackages(tb1);
-			//Should reply only ...testpackage3
-			tp1present = false;
-			tp3present = false;
-			for (int i = 0; i < tps3.length; i++) {
-				if ((tps3[i].getName() != null)
-						&& tps3[i].getName().equals(TP1)) {
-					tp1present = true;
-				}
-				else
-					if ((tps3[i].getName() != null)
-							&& tps3[i].getName().equals(TP3)) {
-						tp3present = true;
-					}
-			}
-			log("Does TestBundle 1 contain Testpackage 1 after refresh: false : '"
-					+ tp1present + "'");
-			log("Does TestBundle 1 contain Testpackage 3 after refresh: true : '"
-					+ tp3present + "'");
-			//Call PackageAdmin.getExportedPackage(...testpackage3)
-			//Is ...testpackage3 exported (yes, not null)
-			log("Is TestPackage 1 exported after refresh: false : '"
-					+ (pa.getExportedPackage(TP1) != null) + "'");
-			log("Is TestPackage 3 exported after refresh: true : '"
-					+ (pa.getExportedPackage(TP3) != null) + "'");
-			//is tb4 active (yes)
-			//is tb2 active (no)
-			log("Is testbundle 2 still active: false: '"
-					+ (tb2.getState() == Bundle.ACTIVE) + "'");
-			log("Is testbundle 4 still active: true: '"
-					+ (tb4.getState() == Bundle.ACTIVE) + "'");
-			link.send("75");
-			URL url = new URL(tb1loc);
-			tb1.update(url.openStream());
-			tb1.stop();
-			tb1.uninstall();
-			tb2.stop();
-			tb2.uninstall();
-			tb4.stop();
-			tb4.uninstall();
-			tb5.stop();
-			tb5.uninstall();
-			b = new Bundle[4];
-			b[0] = tb1;
-			b[1] = tb2;
-			b[2] = tb4;
-			b[3] = tb5;
-			log("Refresh all packages.");
+		}
+		log("Because of lazy update, now TestBundle 1 should contain TestPackage 1 and TestPackage 3.");
+		assertTrue("Does TestBundle 1 contain Testpackage 1 after update:", tp1present);
+		assertTrue("Does TestBundle 1 contain Testpackage 3 after update:", tp3present);
+		// Call PackageAdmin.getExportedPackage(...testpackage1) and
+		// getExportedPackage(...testpackage3)
+		// Is ...testpackage1 exported (yes, not null)
+		// Is ...testpackage3 exported (yes, not null)
+		assertNotNull("Is TestPackage 1 exported", pa.getExportedPackage(TP1));
+		assertNotNull("Is TestPackage 3 exported", pa.getExportedPackage(TP3));
+		// is tb4 active (yes)
+		// is tb2 active (yes)
+		assertTrue("Is testbundle 2 still active", tb2.getState() == Bundle.ACTIVE);
+		assertTrue("Is testbundle 4 still active", tb4.getState() == Bundle.ACTIVE);
+		// Call PackageAdmin.refreshPackages()
+		Bundle[] b = new Bundle[4];
+		b[0] = tb1;
+		b[1] = tb2;
+		b[2] = tb4;
+		b[3] = tb5;
+		log("Refresh all packages.");
+
+		PackagesRefreshedListener prl = new PackagesRefreshedListener();
+		try {
+			getContext().addFrameworkListener(prl);
 			pa.refreshPackages(b);
-			log("PackageAdmin TestCase 2 is completed.");
+			prl.waitForPackagesRefreshedEvent();
+		}
+		finally {
+			getContext().removeFrameworkListener(prl);
+		}
+
+		// Call PackageAdmin.getExportedPackages(Bundle bundle) for tb3
+		ExportedPackage[] tps3 = pa.getExportedPackages(tb1);
+		// Should reply only ...testpackage3
+		tp1present = false;
+		tp3present = false;
+		for (int i = 0; i < tps3.length; i++) {
+			if ((tps3[i].getName() != null) && tps3[i].getName().equals(TP1)) {
+				tp1present = true;
+			}
+			else
+				if ((tps3[i].getName() != null)
+						&& tps3[i].getName().equals(TP3)) {
+					tp3present = true;
+				}
+		}
+		assertFalse("Does TestBundle 1 contain Testpackage 1 after refresh:", tp1present);
+		assertTrue("Does TestBundle 1 contain Testpackage 3 after refresh", tp3present);
+		// Call PackageAdmin.getExportedPackage(...testpackage3)
+		// Is ...testpackage3 exported (yes, not null)
+		assertNull("Is TestPackage 1 exported after refresh:", pa.getExportedPackage(TP1));
+		assertNotNull("Is TestPackage 3 exported after refresh", pa.getExportedPackage(TP3));
+		// is tb4 active (yes)
+		// is tb2 active (no)
+		assertFalse("Is testbundle 2 still active:", tb2.getState() == Bundle.ACTIVE);
+		assertTrue("Is testbundle 4 still active:", tb4.getState() == Bundle.ACTIVE);
+		URL url = new URL(tb1loc);
+		tb1.update(url.openStream());
+		tb1.stop();
+		tb1.uninstall();
+		tb2.stop();
+		tb2.uninstall();
+		tb4.stop();
+		tb4.uninstall();
+		tb5.stop();
+		tb5.uninstall();
+		b = new Bundle[4];
+		b[0] = tb1;
+		b[1] = tb2;
+		b[2] = tb4;
+		b[3] = tb5;
+		log("Refresh all packages.");
+		pa.refreshPackages(b);
+		log("PackageAdmin TestCase 2 is completed.");
+	}
+
+	class PackagesRefreshedListener implements FrameworkListener {
+		int	count	= 0;
+
+		public void frameworkEvent(FrameworkEvent event) {
+			if (event.getType() == FrameworkEvent.PACKAGES_REFRESHED) {
+				count++;
+				synchronized (this) {
+					notify();
+				}
+			}
+		}
+
+		public void waitForPackagesRefreshedEvent() {
+			if (count == 0) {
+				try {
+					synchronized (this) {
+						wait(30000); // wait maximum 30 sec to avoid infinite
+										// wait
+					}
+					if (count == 0) {
+						log("Timeout occured while waiting for PACKAGES_REFRESHED event");
+					}
+				}
+				catch (Exception e) {
+				}
+			}
 		}
 	}
-  
-  class PackagesRefreshedListener implements FrameworkListener {
-    int count = 0;
 
-    public void frameworkEvent(FrameworkEvent event) {
-      if (event.getType() == FrameworkEvent.PACKAGES_REFRESHED) {
-        count++;
-        synchronized (this) {
-          notify();
-        }
-      }
-    }
-    
-    public void waitForPackagesRefreshedEvent() {
-      if (count == 0) {
-        try {
-          synchronized (this) {
-            wait(30000); // wait maximum 30 sec to avoid infinite wait
-          }
-          if (count == 0) {
-            log("Timeout occured while waiting for PACKAGES_REFRESHED event");
-          }
-        } catch (Exception e) {}
-      }
-    }
-  }
-  
 }
