f8debc8729a06f1139290671fa4f0fb8182e9e20
BJ Hargrave <hargrave@us.ibm.com>
2009-02-28 03:04:42 +0000

    add missing close parens in javadoc comment

--- org.osgi.framework/src/org/osgi/framework/Filter.java
+++ org.osgi.framework/src/org/osgi/framework/Filter.java
@@ -85,13 +85,13 @@ public interface Filter {
 	 * Compares this <code>Filter</code> to another <code>Filter</code>.
 	 * 
 	 * <p>
-	 * This implementation returns the result of calling
-	 * <code>this.toString().equals(obj.toString()</code>.
+	 * This method returns the result of calling
+	 * <code>this.toString().equals(obj.toString())</code>.
 	 * 
 	 * @param obj The object to compare against this <code>Filter</code>.
 	 * @return If the other object is a <code>Filter</code> object, then returns
 	 *         the result of calling
-	 *         <code>this.toString().equals(obj.toString()</code>;
+	 *         <code>this.toString().equals(obj.toString())</code>;
 	 *         <code>false</code> otherwise.
 	 */
 	public boolean equals(Object obj);
@@ -100,7 +100,7 @@ public interface Filter {
 	 * Returns the hashCode for this <code>Filter</code>.
 	 * 
 	 * <p>
-	 * This implementation returns the result of calling
+	 * This method returns the result of calling
 	 * <code>this.toString().hashCode()</code>.
 	 * 
 	 * @return The hashCode of this <code>Filter</code>.
