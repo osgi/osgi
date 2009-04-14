47795bc1b4f47a4994bbd2c7e41ded512c77b3b6
Tom Watson <tjwatson@us.ibm.com>
2009-04-14 22:28:25 +0000

    Bug 474 Bundle.findEntries test case needed

--- org.osgi.test.cases.framework/src/org/osgi/test/cases/framework/junit/resources/GetEntryResourceTests.java
+++ org.osgi.test.cases.framework/src/org/osgi/test/cases/framework/junit/resources/GetEntryResourceTests.java
@@ -58,38 +58,73 @@ public class GetEntryResourceTests extends OSGiTestCase {
 		// test directory entries
 		datatxt = newURL(dir1, "../data.txt");
 		assertURL(datatxt);
+		datatxt = newURL(dir1, "../dir1/../data.txt");
+		assertURL(datatxt);
 	}
 
 	public void testFindEntries01() {
-		getEntries(testBundle, "resources", null, false, 4);
-		getEntries(testBundle, "resources", "*.xml", true, 5);
-		getEntries(testBundle, "resources", "data*.xml", true, 5);
-		getEntries(testBundle, "resources", "*.xml", false, 1);
-		getEntries(testBundle, "resources", "data*.xml", false, 1);
-		getEntries(testBundle, "resources", "data.txt", false, 1);
-		getEntries(testBundle, "resources", "data*", true, 10);
-		getEntries(testBundle, "resources", "*d*ta*.*", true, 10);
-		getEntries(testBundle, "resources", "doesNotExist", true, 0);
-		getEntries(testBundle, "resources", "doesNotExist", false, 0);
-		getEntries(testBundle, "resources", "does*Not*Exist", true, 0);
+		assertFindEntries(testBundle, "resources", null, false, 4);
+		assertFindEntries(testBundle, "resources", "*", false, 4);
+		assertFindEntries(testBundle, "resources", null, true, 14);
+		assertFindEntries(testBundle, "resources", "*", true, 14);
+		assertFindEntries(testBundle, "resources", "*.xml", true, 5);
+		assertFindEntries(testBundle, "resources", "data*.xml", true, 5);
+		assertFindEntries(testBundle, "resources", "*.xml", false, 1);
+		assertFindEntries(testBundle, "resources", "data*.xml", false, 1);
+		assertFindEntries(testBundle, "resources", "data.txt", false, 1);
+		assertFindEntries(testBundle, "resources", "data*", true, 10);
+		assertFindEntries(testBundle, "resources", "*d*ta*.*", true, 10);
+		assertFindEntries(testBundle, "resources", "doesNotExist", true, 0);
+		assertFindEntries(testBundle, "resources", "doesNotExist", false, 0);
+		assertFindEntries(testBundle, "resources", "does*Not*Exist", true, 0);
+		assertFindEntries(testBundle, "resources", "dir*", false, 2);
+		assertFindEntries(testBundle, "resources", "dir*", true, 4);
 	}
 
-	private URL[] getEntries(Bundle bundle, String string, String wildCard, boolean recurse, int expectedNum) {
+	public void testGetEntryPaths01() {
+		assertEntryPaths(testBundle, "resources", new String[] {"resources/dir1/", "resources/dir2/", "resources/data.txt", "resources/data.xml"});
+		assertEntryPaths(testBundle, "resources/dir1", new String[] {"resources/dir1/dir1a/", "resources/dir1/data1.txt", "resources/dir1/data1.xml"});
+
+	}
+
+	private void assertEntryPaths(Bundle bundle, String path, String[] expected) {
+		Enumeration paths = bundle.getEntryPaths(path);
+		if (expected != null)
+			assertNotNull(paths);
+		else {
+			assertNull(paths);
+			return;
+		}
+
+		ArrayList expectedList = new ArrayList(expected.length);
+		for (int i = 0; i < expected.length; i++)
+			expectedList.add(expected[i]);
+
+		int numFound = 0;
+		while (paths.hasMoreElements()) {
+			Object element = paths.nextElement();
+			if (!expectedList.contains(element))
+				fail("Unexpected entry found: " + element);
+			numFound++;
+		}
+		assertEquals("Unexpected number of entries", expected.length, numFound);
+	}
+
+	private void assertFindEntries(Bundle bundle, String string, String wildCard, boolean recurse, int expectedNum) {
 		Enumeration entries = bundle.findEntries("resources", wildCard, recurse);
 		if (expectedNum > 0)
 			assertNotNull(entries);
 		else {
 			assertNull(entries);
-			return null;
+			return;
 		}
-		ArrayList urls = new ArrayList();
+
+		int numFound = 0;
 		while (entries.hasMoreElements()) {
-			URL url = (URL) entries.nextElement();
-			assertURL(url);
-			urls.add(url);
+			assertURL((URL) entries.nextElement());
+			numFound++;
 		}
-		assertEquals("Unexpected number of entries", expectedNum, urls.size());
-		return (URL[]) urls.toArray(new URL[urls.size()]);
+		assertEquals("Unexpected number of entries", expectedNum, numFound);
 	}
 
 	protected void tearDown() throws Exception {
@@ -106,6 +141,7 @@ public class GetEntryResourceTests extends OSGiTestCase {
 	}
 
 	private void assertURL(URL url) {
+		assertNotNull("Unexpected null url", url);
 		String expected = url.getPath();
 		int lastSlash = expected.lastIndexOf('/');
 		assertTrue(lastSlash >= 0);
