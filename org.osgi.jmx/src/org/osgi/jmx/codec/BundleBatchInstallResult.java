d6e53aae3be96eccd9727ff6aafc0e4019916a16
BJ Hargrave <hargrave@us.ibm.com>
2009-04-25 02:49:54 +0000

    update copyrights for changes in 2009

--- org.osgi.jmx/src/org/osgi/jmx/codec/BundleBatchInstallResult.java
+++ org.osgi.jmx/src/org/osgi/jmx/codec/BundleBatchInstallResult.java
@@ -1,25 +1,31 @@
 /*
- * Copyright 2008 Oracle Corporation
+ * Copyright (c) OSGi Alliance (2009). All Rights Reserved.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.osgi.jmx.codec;
 
 import static org.osgi.jmx.codec.Util.LONG_ARRAY_TYPE;
-import static org.osgi.jmx.codec.Util.STRING_ARRAY_TYPE;
 import static org.osgi.jmx.codec.Util.LongArrayFrom;
+import static org.osgi.jmx.codec.Util.STRING_ARRAY_TYPE;
 
-import javax.management.openmbean.*;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.CompositeType;
+import javax.management.openmbean.OpenDataException;
+import javax.management.openmbean.OpenType;
+import javax.management.openmbean.SimpleType;
 
 import org.osgi.jmx.framework.FrameworkMBean;
 
--- org.osgi.jmx/src/org/osgi/jmx/codec/OSGiProperties.java
+++ org.osgi.jmx/src/org/osgi/jmx/codec/OSGiProperties.java
@@ -1,25 +1,42 @@
 /*
- * Copyright 2008 Oracle Corporation
+ * Copyright (c) OSGi Alliance (2009). All Rights Reserved.
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.osgi.jmx.codec;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
-import javax.management.openmbean.*;
+import javax.management.openmbean.CompositeData;
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.CompositeType;
+import javax.management.openmbean.OpenDataException;
+import javax.management.openmbean.OpenType;
+import javax.management.openmbean.SimpleType;
+import javax.management.openmbean.TabularData;
+import javax.management.openmbean.TabularDataSupport;
+import javax.management.openmbean.TabularType;
 
 import org.osgi.framework.ServiceReference;
 
