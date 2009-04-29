851f3956dfd2df39d7434c83e91dbdd579d7b0eb
J.J. Snyder <j.j.snyder@oracle.com>
2009-04-29 14:27:32 +0000

    Removed direct references to Derby from tests.

--- org.osgi.test.cases.jdbc/src/org/osgi/test/cases/jdbc/JDBCTestCase.java
+++ org.osgi.test.cases.jdbc/src/org/osgi/test/cases/jdbc/JDBCTestCase.java
@@ -17,34 +17,124 @@
 
 package org.osgi.test.cases.jdbc;
 
+import java.lang.reflect.Method;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import javax.sql.ConnectionPoolDataSource;
 import javax.sql.DataSource;
 import javax.sql.XADataSource;
 
-import org.apache.derby.jdbc.EmbeddedConnectionPoolDataSource;
-import org.apache.derby.jdbc.EmbeddedDataSource;
-import org.apache.derby.jdbc.EmbeddedXADataSource;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.jdbc.DataSourceFactory;
 import org.osgi.test.support.OSGiTestCase;
 
 public class JDBCTestCase extends OSGiTestCase {
+	private String databaseName = "dbName";
+	private String dataSourceName = "dsName";
+	private String description = "desc";
+	private String password = "pswd";
+	private String user = "usr";
+	
+	private ServiceReference	ref;
+	private DataSourceFactory	factory;
 
-	public void testDataSourceFactoryService() throws Exception {
-		ServiceReference[] serviceReferences = getContext()
-				.getServiceReferences(
-				DataSourceFactory.class.getName(), null);
-		assertEquals(1, serviceReferences.length);
-		DataSourceFactory dsf = (DataSourceFactory) getContext().getService(
-				serviceReferences[0]);
-
-		// get the default objects
-		DataSource ds = dsf.createDataSource(null);
-		ConnectionPoolDataSource cpds = dsf.createConnectionPoolDataSource(null);
-		XADataSource xads = dsf.createXADataSource(null);
-		assertTrue( ds instanceof EmbeddedDataSource );
-		assertTrue( cpds instanceof EmbeddedConnectionPoolDataSource );
-		assertTrue( xads instanceof EmbeddedXADataSource );
+	protected void setUp() {
+		ref = getContext()
+				.getServiceReference(DataSourceFactory.class.getName());
+		assertNotNull("No DataSourceFactory service available", ref);
+		factory = (DataSourceFactory) getContext().getService(ref);
+		assertNotNull(factory);
 	}
-}
+	
+	protected void tearDown() {
+		getContext().ungetService(ref);
+	}
 
+	private void assertDSProperty( Object dataSource, String methodName, Object value ) throws Exception {
+		Method method = dataSource.getClass().getMethod( methodName, new Class[] {} );
+		Object res = method.invoke( dataSource, new Object[] {} );
+		assertEquals( res, value );
+	}
+
+	public void testCreateDataSource() throws Exception {
+		Properties props = new Properties();
+		props.put( DataSourceFactory.JDBC_DATABASE_NAME, databaseName );
+		props.put( DataSourceFactory.JDBC_DATASOURCE_NAME, dataSourceName );
+		props.put( DataSourceFactory.JDBC_DESCRIPTION, description );
+		props.put( DataSourceFactory.JDBC_PASSWORD, password );
+		props.put( DataSourceFactory.JDBC_USER, user );
+
+		DataSource ds = factory.createDataSource( props );
+		assertDSProperty( ds, "getDatabaseName", databaseName );
+		assertDSProperty( ds, "getDataSourceName", dataSourceName );
+		assertDSProperty( ds, "getDescription", description );
+		assertDSProperty( ds, "getPassword", password );
+		assertDSProperty( ds, "getUser", user );
+
+		// make sure we get an exception if we use an unknown property
+		props = new Properties();
+		props.put( "junk", "junk" );
+		try {
+			factory.createDataSource( props );
+			fail( "Should have gotten a SQLException." );
+		} catch ( SQLException ignore ) { }
+		
+		// make sure we get an exception if we use an invalid property
+		props = new Properties();
+		props.put( "loginTimeout", "junk" );
+		try {
+			factory.createDataSource( props );
+			fail( "Should have gotten a SQLException." );
+		} catch ( SQLException ignore ) { }
+	}
+
+	public void testCreateConnectionPoolDataSource() throws Exception {
+		Properties props = new Properties();
+		props.put( DataSourceFactory.JDBC_DATABASE_NAME, databaseName );
+		props.put( DataSourceFactory.JDBC_DATASOURCE_NAME, dataSourceName );
+		props.put( DataSourceFactory.JDBC_DESCRIPTION, description );
+		props.put( DataSourceFactory.JDBC_PASSWORD, password );
+		props.put( DataSourceFactory.JDBC_USER, user );
+
+		ConnectionPoolDataSource ds = factory.createConnectionPoolDataSource( props );
+		assertDSProperty( ds, "getDatabaseName", databaseName );
+		assertDSProperty( ds, "getDataSourceName", dataSourceName );
+		assertDSProperty( ds, "getDescription", description );
+		assertDSProperty( ds, "getPassword", password );
+		assertDSProperty( ds, "getUser", user );
+		
+		// make sure we get an exception if we use an unknown property
+		props = new Properties();
+		props.put( "junk", "junk" );
+		try {
+			factory.createConnectionPoolDataSource( props );
+			fail( "Should have gotten a SQLException." );
+		} catch ( SQLException ignore ) { }
+	}
+
+	public void testCreateXADataSource() throws Exception {
+		Properties props = new Properties();
+		props.put( DataSourceFactory.JDBC_DATABASE_NAME, databaseName );
+		props.put( DataSourceFactory.JDBC_DATASOURCE_NAME, dataSourceName );
+		props.put( DataSourceFactory.JDBC_DESCRIPTION, description );
+		props.put( DataSourceFactory.JDBC_PASSWORD, password );
+		props.put( DataSourceFactory.JDBC_USER, user );
+
+		XADataSource ds = factory.createXADataSource( props );
+		assertDSProperty( ds, "getDatabaseName", databaseName );
+		assertDSProperty( ds, "getDataSourceName", dataSourceName );
+		assertDSProperty( ds, "getDescription", description );
+		assertDSProperty( ds, "getPassword", password );
+		assertDSProperty( ds, "getUser", user );
+		
+		// make sure we get an exception if we use an unknown property
+		props = new Properties();
+		props.put( "junk", "junk" );
+		try {
+			factory.createXADataSource( props );
+			fail( "Should have gotten a SQLException." );
+		} catch ( SQLException ignore ) { }
+	}
+
+}
