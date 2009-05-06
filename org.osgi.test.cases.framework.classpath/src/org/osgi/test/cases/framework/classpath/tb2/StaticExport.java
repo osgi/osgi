8794c0fcb4a19209dabc16520d340acce4f70004
BJ Hargrave <hargrave@us.ibm.com>
2009-05-06 20:27:15 +0000

    framework.classpath tests have been moved into framework tests

---
+++ org.osgi.test.cases.framework.classpath/src/org/osgi/test/cases/framework/classpath/tb2/StaticExport.java
@@ -0,0 +1,27 @@
+/** 
+ * OSGi Test Suite Implementation. OSGi Confidential.
+ * (C) Copyright Ericsson Radio Systems AB. 2000.
+ * This source code is owned by Ericsson Radio Systems AB, and is being distributed to OSGi 
+ * MEMBERS as MEMBER LICENSED MATERIALS under the terms of section 3.2 of the OSGi MEMBER AGREEMENT. 
+ */
+package org.osgi.test.cases.framework.classpath.tb2;
+
+import org.osgi.framework.*;
+
+/**
+ */
+public class StaticExport implements BundleActivator {
+	/**
+	 * Starts the bundle.
+	 */
+	public void start(BundleContext bc) {
+		org.osgi.test.cases.framework.classpath.exported.Exported e;
+		e = new org.osgi.test.cases.framework.classpath.exported.Exported();
+	}
+
+	/**
+	 * Stops the bundle.
+	 */
+	public void stop(BundleContext bc) {
+	}
+}
