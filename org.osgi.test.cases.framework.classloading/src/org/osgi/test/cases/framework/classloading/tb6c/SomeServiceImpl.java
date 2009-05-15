e9da591569b947b4fc6d824bfddf7a54f3c591c3
BJ Hargrave <hargrave@us.ibm.com>
2009-05-15 10:35:20 +0000

    framework classloading is moved to framework and framework.secure

---
+++ org.osgi.test.cases.framework.classloading/src/org/osgi/test/cases/framework/classloading/tb6c/SomeServiceImpl.java
@@ -0,0 +1,72 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) OSGi Alliance (2005). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
+
+package org.osgi.test.cases.framework.classloading.tb6c;
+
+import java.security.AccessController;
+
+import org.osgi.framework.Bundle;
+import org.osgi.test.cases.framework.classloading.exports.security.SomePermission;
+import org.osgi.test.cases.framework.classloading.exports.service.SomeService;
+
+/**
+ * A simple service implementation used in tests
+ * 
+ * @author left
+ * @version $Revision$
+ */
+public class SomeServiceImpl implements SomeService {
+
+	private Bundle	registeringBundle;
+
+	/**
+	 * Creates a new instance of SomeServiceImpl
+	 */
+	public SomeServiceImpl(Bundle _registeringBundle) {
+		registeringBundle = _registeringBundle;
+	}
+
+	/**
+	 * Return the registrant bundle
+	 * 
+	 * @return the registrant bundle
+	 */
+	public Bundle getRegistrantBundle() {
+		SomePermission permission;
+
+		permission = new SomePermission("", "");
+		AccessController.checkPermission(permission);
+
+		if (true) {
+			throw new IllegalStateException(
+					"The permission class is loaded from the incorrect bundle");
+		}
+
+		return registeringBundle;
+	}
+
+}
---
+++ org.osgi.test.cases.framework.classloading/src/org/osgi/test/cases/framework/classloading/tb6d/Activator.java
@@ -0,0 +1,87 @@
+/*
+ * $Id$
+ * 
+ * Copyright (c) The OSGi Alliance (2004). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
+
+package org.osgi.test.cases.framework.classloading.tb6d;
+
+import java.security.AccessControlException;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.test.cases.framework.classloading.exports.service.SomeService;
+
+/**
+ * This bundle activator is used to register some services
+ * 
+ * @author left
+ * @version $Revision$
+ */
+public class Activator implements BundleActivator {
+
+	private ServiceReference	serviceReference;
+
+	/**
+	 * Creates a new instance of Activator
+	 */
+	public Activator() {
+
+	}
+
+	/**
+	 * Start the bundle and register the service.
+	 * 
+	 * @param context the bundle context
+	 * @throws Exception if some problem occur
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		SomeService someService;
+
+		serviceReference = context.getServiceReference(SomeService.class
+				.getName());
+
+		someService = (SomeService) context.getService(serviceReference);
+
+		try {
+			someService.getRegistrantBundle();
+		}
+		catch (AccessControlException ex) {
+			// Ignore this exception
+		}
+	}
+
+	/**
+	 * Stop the bundle and unregister the service
+	 * 
+	 * @param context the bundle context
+	 * @throws Exception if some problem occur
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		context.ungetService(serviceReference);
+	}
+}
