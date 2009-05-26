85e6c4e2ba7f597208d955b304b82eb446543c92
BJ Hargrave <hargrave@us.ibm.com>
2009-05-26 19:50:19 +0000

    move framework.div tests into framework tests

---
+++ org.osgi.test.cases.framework/src/org/osgi/test/cases/framework/div/tb5/CheckManifest.java
@@ -0,0 +1,31 @@
+/** 
+ OSGi Test Suite Implementation. OSGi Confidential.
+ (C) Copyright Ericsson Telecom AB. 2001.
+ This source code is owned by Ericsson Telecom AB, and is being distributed to OSGi 
+ MEMBERS as MEMBER LICENSED MATERIALS under the terms of section 3.2 of the OSGi MEMBER AGREEMENT. 
+ */
+package org.osgi.test.cases.framework.div.tb5;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Bundle for the CheckManifest test.
+ * 
+ * @author Ericsson Telecom AB
+ */
+public class CheckManifest implements BundleActivator {
+	/**
+	 * Starts the bundle.
+	 */
+	public void start(BundleContext bc) {
+		// empty
+	}
+
+	/**
+	 * Stops the bundle.
+	 */
+	public void stop(BundleContext bc) {
+		// empty
+	}
+}
---
+++ org.osgi.test.cases.framework/src/org/osgi/test/cases/framework/div/tb6/BundleClass.java
@@ -0,0 +1,16 @@
+/** 
+ OSGi Test Suite Implementation. OSGi Confidential.
+ (C) Copyright Ericsson Radio Systems AB. 2000.
+ This source code is owned by Ericsson Radio Systems AB, and is being distributed to OSGi 
+ MEMBERS as MEMBER LICENSED MATERIALS under the terms of section 3.2 of the OSGi MEMBER AGREEMENT. 
+ */
+package org.osgi.test.cases.framework.div.tb6;
+
+/**
+ * @author Ericsson Radio Systems AB
+ */
+public class BundleClass {
+	public BundleClass() {
+		// empty
+	}
+}
