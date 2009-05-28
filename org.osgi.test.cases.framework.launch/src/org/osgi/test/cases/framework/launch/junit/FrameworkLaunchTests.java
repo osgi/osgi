fd1f9b90d1f8e14576766738dcefb030b5c0e5c7
Tom Watson <tjwatson@us.ibm.com>
2009-05-28 17:06:35 +0000

    refactor junit class to match project name.

--- org.osgi.test.cases.framework.launch/src/org/osgi/test/cases/framework/launch/junit/FrameworkLaunchTests.java
+++ org.osgi.test.cases.framework.launch/src/org/osgi/test/cases/framework/launch/junit/FrameworkLaunchTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.osgi.test.cases.framework.junit.launch;
+package org.osgi.test.cases.framework.launch.junit;
 
 import java.io.BufferedReader;
 import java.io.File;
