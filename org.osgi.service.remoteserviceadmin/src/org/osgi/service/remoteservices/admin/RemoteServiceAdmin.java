5d8aaf1f405e9da60cf61bf2c8116daeb72bc8e3
Peter Kriens <peter.kriens@aqute.biz>
2009-06-11 10:18:59 +0000

    It looks awfully handy to allow the remote controller to provide an endpoint ...

--- org.osgi.service.remoteserviceadmin/src/org/osgi/service/remoteservices/admin/RemoteServiceAdmin.java
+++ org.osgi.service.remoteserviceadmin/src/org/osgi/service/remoteservices/admin/RemoteServiceAdmin.java
@@ -13,16 +13,17 @@ import org.osgi.service.remoteservices.*;
  * 
  * The API allows a remote controller to export a service, to import a service,
  * and find out about the current imports and exports.
+ * 
  * @ThreadSafe
  */
 public interface RemoteServiceAdmin {
-	
+
 	/**
-	 * Export a service to an endpoint. The Remote Service Admin must create an endpoint that
-	 * can be used by other Distrbution Providers to connect to this Remote
-	 * Service Admin and use the exported service. This method can return null
-	 * if the service could not be exported.
-	 * ### do we need exceptions?
+	 * Export a service to an endpoint. The Remote Service Admin must create an
+	 * endpoint that can be used by other Distrbution Providers to connect to
+	 * this Remote Service Admin and use the exported service. This method can
+	 * return null if the service could not be exported. ### do we need
+	 * exceptions?
 	 * 
 	 * @param ref
 	 *            The Service Reference to export
@@ -33,10 +34,31 @@ public interface RemoteServiceAdmin {
 	ExportRegistration exportService(ServiceReference ref);
 
 	/**
-	 * Import a service from an endpoint. The Remote Service Admin must use the given
-	 * endpoint to create a proxy. This method can return null
-	 * if the service could not be imported.
-	 * ### do we need exceptions?
+	 * Export a service to a given endpoint. The Remote Service Admin must
+	 * create an endpoint from the given description that can be used by other
+	 * Distrbution Providers to connect to this Remote Service Admin and use the
+	 * exported service. This method can return null if the service could not be
+	 * exported because the endpoint could not be implemented by this Remote
+	 * Service Admin. ### do we need exceptions?
+	 * 
+	 * @param ref
+	 *            The Service Reference to export
+	 * @param endpoint
+	 *            A description of a local endpoint that can be implemented by
+	 *            this Remote Service Admin. If this is null, the endpoint will
+	 *            be determined by the properties on the service, see
+	 *            {@link #exportService(ServiceReference)}
+	 * @return An Export Registration that combines the Endpoint Description and
+	 *         the Service Reference or
+	 *         <code>null</code if the service could not be exported
+	 */
+	ExportRegistration exportService(ServiceReference ref,
+			EndpointDescription endpoint);
+
+	/**
+	 * Import a service from an endpoint. The Remote Service Admin must use the
+	 * given endpoint to create a proxy. This method can return null if the
+	 * service could not be imported. ### do we need exceptions?
 	 * 
 	 * @param endpoint
 	 *            The Endpoint Description to be used for import
@@ -48,12 +70,14 @@ public interface RemoteServiceAdmin {
 
 	/**
 	 * Answer the currently active Export Registrations.
+	 * 
 	 * @return A collection of Export Registrations that are currently active.
 	 */
 	Collection/* <? extends ExportRegistration> */getExportedServices();
 
 	/**
 	 * Answer the currently active Import Registrations.
+	 * 
 	 * @return A collection of Import Registrations that are currently active.
 	 */
 	Collection/* <? extends ImportRegistration> */getImportedEndpoints();
