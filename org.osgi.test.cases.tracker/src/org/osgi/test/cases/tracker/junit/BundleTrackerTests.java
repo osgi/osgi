30c1d29ca61925ec9d1907b455e427d2e6b4449d
BJ Hargrave <hargrave@us.ibm.com>
2009-07-06 18:53:44 +0000

    spec does not require UNINSTALLED bundles to move through INSTALLED state (from RESOLVED state), so test must allow for UNRESOLVED event to be delivered for bundle in the UNINSTALLED state

--- org.osgi.test.cases.tracker/src/org/osgi/test/cases/tracker/junit/BundleTrackerTests.java
+++ org.osgi.test.cases.tracker/src/org/osgi/test/cases/tracker/junit/BundleTrackerTests.java
@@ -354,14 +354,11 @@ public class BundleTrackerTests extends OSGiTestCase {
 			}
 			synchronized (events) {
 				assertNull("adding called", events[0]);
-				assertNotNull("modified not called", events[1]);
-				assertEquals("event type not unresolved",
-						BundleEvent.UNRESOLVED, events[1].getType());
-				assertEquals("event bundle not tb1", tb1, events[1].getBundle());
+				// ignore any modified events
 				events[1] = null;
 				assertNotNull("removed not called", events[2]);
-				assertEquals("event type not uninstalled",
-						BundleEvent.UNINSTALLED, events[2].getType());
+				boolean actual = (events[2].getType() & (BundleEvent.UNRESOLVED | BundleEvent.UNINSTALLED)) != 0;
+				assertTrue("event type not unresolved or uninstalled", actual);
 				assertEquals("event bundle not tb1", tb1, events[2].getBundle());
 				events[2] = null;
 			}
