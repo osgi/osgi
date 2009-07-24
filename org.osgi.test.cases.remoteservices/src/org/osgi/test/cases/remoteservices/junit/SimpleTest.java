450056a282f97a18d736796b9978cd0f02078a06
Tim Diekman <tdiekman@tibco.com>
2009-07-24 22:24:12 +0000

    updated copyright headers

--- org.osgi.test.cases.remoteservices/src/org/osgi/test/cases/remoteservices/junit/SimpleTest.java
+++ org.osgi.test.cases.remoteservices/src/org/osgi/test/cases/remoteservices/junit/SimpleTest.java
@@ -1,8 +1,18 @@
 /*
- * Copyright (c) 2008 TIBCO Software Inc.
- * All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2008, 2009). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.osgi.test.cases.remoteservices.junit;
 
 import java.io.FileInputStream;
@@ -25,10 +35,8 @@ import org.osgi.framework.launch.Framework;
 import org.osgi.service.packageadmin.ExportedPackage;
 import org.osgi.service.packageadmin.PackageAdmin;
 import org.osgi.service.remoteservices.DistributionProviderConstants;
-import org.osgi.service.remoteservices.ExportedEndpointDescription;
 import org.osgi.service.remoteservices.RemoteServiceConstants;
 import org.osgi.test.cases.remoteservices.common.A;
-import org.osgi.test.cases.remoteservices.common.B;
 import org.osgi.test.cases.remoteservices.impl.TestServiceImpl;
 import org.osgi.util.tracker.ServiceTracker;
 
