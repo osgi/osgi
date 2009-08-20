f67119a21dc95f3bce9ce5fd2a978ef6ee969926
Peter Kriens <peter.kriens@aqute.biz>
2009-08-20 17:49:45 +0000

    Changed prepare for commit with only once guard

--- org.osgi.test.cases.upnp/src/org/osgi/test/cases/upnp/tbc/UPnPControl.java
+++ org.osgi.test.cases.upnp/src/org/osgi/test/cases/upnp/tbc/UPnPControl.java
@@ -12,24 +12,27 @@ import org.osgi.test.support.compatibility.DefaultTestBundleControl;
  * 
  */
 public class UPnPControl extends DefaultTestBundleControl {
-	private HttpService			http;
-	private TestStarter			start;
+	private static HttpService	http;
+	private static TestStarter	start;
+	private static boolean		inited	= false;
 
-	public void prepare() throws Exception {
-		log("Prepare for UPnP Test Case");
-		http = (HttpService) getService(HttpService.class);
-		UPnPConstants.init();
-		//UPnPConstants.HTTP_PORT = ((Integer)
-		// httpRef.getProperty("openPort")).intValue();
-		UPnPConstants.HTTP_PORT = Integer.parseInt(System.getProperty(
-				"org.osgi.service.http.port", "80"));
-		log("Register Service Listener to listen for service changes");
-		ServicesListener listener = new ServicesListener(getContext());
-		listener.open();
-		log("Start the UPnP Test Starter");
-		start = new TestStarter(http, this);
-		
-		listener.waitFor(3);
+	public void setUp() throws Exception {
+		if (!inited) {
+			inited = true;
+			log("Prepare for UPnP Test Case");
+			http = (HttpService) getService(HttpService.class);
+			UPnPConstants.init();
+			// UPnPConstants.HTTP_PORT = ((Integer)
+			// httpRef.getProperty("openPort")).intValue();
+			UPnPConstants.HTTP_PORT = Integer.parseInt(System.getProperty(
+					"org.osgi.service.http.port", "80"));
+			log("Register Service Listener to listen for service changes");
+			ServicesListener listener = new ServicesListener(getContext());
+			listener.open();
+			log("Start the UPnP Test Starter");
+			start = new TestStarter(http, this);
+			listener.waitFor(3);
+		}
 	}
 
 	public void unprepare() throws Exception {
@@ -37,29 +40,29 @@ public class UPnPControl extends DefaultTestBundleControl {
 		ungetService(http);
 	}
 
-	//==========================================TEST
+	// ==========================================TEST
 	// METHODS====================================================================//
 	public void testDiscovery() {
-		new UPnPTester(UPnPTester.DISCOVERY, ServicesListener
-				.getUPnPDevice(), getContext());
+		new UPnPTester(UPnPTester.DISCOVERY, ServicesListener.getUPnPDevice(),
+				getContext());
 	}
 
 	public void testControl() {
-		new UPnPTester(UPnPTester.CONTROL, ServicesListener
-				.getUPnPDevice(), getContext());
+		new UPnPTester(UPnPTester.CONTROL, ServicesListener.getUPnPDevice(),
+				getContext());
 	}
 
 	public void testEvent() {
-		new UPnPTester(UPnPTester.EVENTING, ServicesListener
-				.getUPnPDevice(), getContext());
+		new UPnPTester(UPnPTester.EVENTING, ServicesListener.getUPnPDevice(),
+				getContext());
 	}
 
 	public void testExport() {
 		log("Creating new device and making it UPnP device");
 		UPnPExportedDevice ex_device = UPnPExportedDevice.newUPnPTestervice();
 		ServiceRegistration sr = getContext().registerService(
-				UPnPDevice.class.getName(),
-				ex_device, ex_device.getDescriptions(null));
+				UPnPDevice.class.getName(), ex_device,
+				ex_device.getDescriptions(null));
 		try {
 			Thread.sleep(4000);
 		}
@@ -67,8 +70,8 @@ public class UPnPControl extends DefaultTestBundleControl {
 			// ignored
 		}
 		try {
-			new UPnPTester(UPnPTester.EXPORT, ServicesListener
-					.getUPnPDevice(), getContext());
+			new UPnPTester(UPnPTester.EXPORT, ServicesListener.getUPnPDevice(),
+					getContext());
 		}
 		finally {
 			sr.unregister();
