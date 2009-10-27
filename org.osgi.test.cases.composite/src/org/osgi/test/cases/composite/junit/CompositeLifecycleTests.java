e358bf41c52eaccc015c4e44c02878497320f63e
Tom Watson <tjwatson@us.ibm.com>
2009-10-27 21:24:49 +0000

    update copyright date.

--- org.osgi.test.cases.composite/src/org/osgi/test/cases/composite/junit/CompositeLifecycleTests.java
+++ org.osgi.test.cases.composite/src/org/osgi/test/cases/composite/junit/CompositeLifecycleTests.java
@@ -1,7 +1,7 @@
 /*
  * $Header$
  * 
- * Copyright (c) OSGi Alliance (2008). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2009). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
