0c4b85a7e5fa399a9edd1486e11cb90d072ef03b
Marc Schaaf <mschaaf@progress.com>
2010-01-11 15:19:30 +0000

    new version of the dOSGi RI which fixes some smaller issues and has still no real support for intents

--- org.osgi.test.cases.remoteserviceadmin/src/org/osgi/test/cases/remoteserviceadmin/junit/RemoteServiceAdminExportTest.java
+++ org.osgi.test.cases.remoteserviceadmin/src/org/osgi/test/cases/remoteserviceadmin/junit/RemoteServiceAdminExportTest.java
@@ -397,7 +397,8 @@ public class RemoteServiceAdminExportTest extends DefaultTestBundleControl {
 				assertEquals("org/osgi/service/remoteserviceadmin/EXPORT_UNREGISTRATION", event.getTopic());
 				assertEquals(sref.getBundle(), event.getProperty("bundle"));
 				assertEquals(sref.getBundle().getSymbolicName(), event.getProperty("bundle.symbolicname"));
-				assertEquals(description, event.getProperty("export.unregistration"));
+				// Marc Schaaf: changed from export.unregistration to export.registration
+				assertEquals(description, event.getProperty("export.registration"));
 				rsaevent = (RemoteServiceAdminEvent) event.getProperty("event");
 				assertNotNull(rsaevent);
 				assertEquals(RemoteServiceAdminEvent.EXPORT_UNREGISTRATION, rsaevent.getType());
