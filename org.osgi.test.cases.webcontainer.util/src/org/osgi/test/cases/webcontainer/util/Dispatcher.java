209623c46074442755d2c4eef8dbd33d94423509
Lin Sun <linsun@us.ibm.com>
2010-01-16 02:45:17 +0000

    move webcontainer CT util pkgs to a seperate project to be used by base CT and optional CT

---
+++ org.osgi.test.cases.webcontainer.util/src/org/osgi/test/cases/webcontainer/util/Dispatcher.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) IBM Corporation (2009). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osgi.test.cases.webcontainer.util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class Dispatcher {
+
+    public static String dispatch(HttpURLConnection conn) throws Exception {
+        String inputLine = new String();
+        StringBuffer resbuf = new StringBuffer();
+        BufferedReader in = null;
+
+        in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        while ((inputLine = in.readLine()) != null) {
+            resbuf.append(inputLine);
+        }
+        in.close();
+        return resbuf.toString();
+    }
+
+    public static URL createURL(String request, Server server) throws Exception {
+        final String urlstr = "http://" + server.getHost() + ":"
+                + server.getPort() + request;
+        return new URL(urlstr);
+    }
+
+}
---
+++ org.osgi.test.cases.webcontainer.util/src/org/osgi/test/cases/webcontainer/util/EventUtil.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) IBM Corporation (2009). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osgi.test.cases.webcontainer.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class EventUtil {
+
+    // get all events in the property file
+    public static Properties getAllEventsProperties() {
+        return loadProperties(ConstantsUtil.getLogFile());
+    }
+
+    // load property file
+    protected static Properties loadProperties(File f) {
+        Properties properties = new Properties();
+        try {
+            if (!f.exists()) {
+                throw new FileNotFoundException();// f.createNewFile();
+            }
+            InputStream in = new FileInputStream(f);
+            try {
+                properties.load(in);
+            } finally {
+                in.close();
+            }
+        } catch (IOException e) {
+            // not much to do
+        }
+        return properties;
+    }
+
+    // get event by classname and method name
+    // @return null if no matching event is found
+    public static Event getEvent(String className, String methodName) {
+        Properties properties = getAllEventsProperties();
+        for (Map.Entry entry : properties.entrySet()) {
+            String name = (String) entry.getKey();
+            if (name.equalsIgnoreCase(className + "." + methodName)) {
+                String value = (String) entry.getValue();
+                int i = value.indexOf("@");
+                String desp;
+                String timeString;
+                long time;
+                if (i > 0) {
+                    desp = value.substring(0, i);
+                    timeString = value.substring(i + 2);
+                    try {
+                        time = Long.parseLong(timeString.trim());
+                    } catch (NumberFormatException nfe) {
+                        nfe.printStackTrace();
+                        return null;
+                    }
+                    return new Event(className, methodName, desp.trim(), time);
+                }
+            }
+        }
+        return null;
+    }
+
+}
