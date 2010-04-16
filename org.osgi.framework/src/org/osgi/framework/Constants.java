c678712d621fcf2470782b0af1d2fec4de89f803
BJ Hargrave <hargrave@us.ibm.com>
2010-04-16 12:31:00 +0000

    bug 1396: put in branch

--- org.osgi.framework/src/org/osgi/framework/Constants.java
+++ org.osgi.framework/src/org/osgi/framework/Constants.java
@@ -1333,6 +1333,18 @@ public interface Constants {
 	public static final String	FRAMEWORK_UUID							= "org.osgi.framework.uuid";
 
 	/**
+	 * Specifies that an returned URLs from bundle class loaders must be a jar:
+	 * or file: URL if set to any value. This property must be set in the
+	 * launching parameters of the framework. If a Framework cannot support this
+	 * property it must throw an Illegal Argument Exception during its
+	 * initialization. URLs obtained through the OSGi API do not have this
+	 * guarantee, these URLs must follow the existing rules for resource URLs.
+	 * 
+	 * @since 1.6
+	 */
+	public final static String	FRAMEWORK_JARURLS						= "org.osgi.framework.jarurls";
+
+	/**
 	 * Service property identifying the configuration types supported by a
 	 * distribution provider. Registered by the distribution provider on one of
 	 * its services to indicate the supported configuration types.
