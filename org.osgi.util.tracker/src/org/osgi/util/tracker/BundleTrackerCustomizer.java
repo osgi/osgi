12cfb310b547e63b83bbc8a665b56d01a7f6ae4e
BJ Hargrave <hargrave@us.ibm.com>
2010-07-02 18:46:16 -0400

    Update copyright year to 2010 for files changed in 2010.

--- org.osgi.util.tracker/src/org/osgi/util/tracker/BundleTrackerCustomizer.java
+++ org.osgi.util.tracker/src/org/osgi/util/tracker/BundleTrackerCustomizer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2007, 2008). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2007, 2010). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
