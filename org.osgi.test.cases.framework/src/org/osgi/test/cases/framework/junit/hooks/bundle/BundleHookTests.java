229e501939a0530492ff82df891745c41a16e715
Thomas Watson <tjwatson@us.ibm.com>
2010-08-12 15:40:35 -0500

    update copyright and imports

--- org.osgi.test.cases.framework/src/org/osgi/test/cases/framework/junit/hooks/bundle/BundleHookTests.java
+++ org.osgi.test.cases.framework/src/org/osgi/test/cases/framework/junit/hooks/bundle/BundleHookTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2009). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2010). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.osgi.test.cases.framework.junit.hooks.bundle;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Hashtable;
