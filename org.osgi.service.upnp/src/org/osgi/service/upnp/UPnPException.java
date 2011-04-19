21830a9495e93495e332f0b08c754e7098106b30
Evgeni Grigorov <e.grigorov@prosyst.com>
2011-04-19 16:42:09 +0300

    Fixed Issue Item 1888 UPnPException class needs a revision)

--- org.osgi.service.upnp/src/org/osgi/service/upnp/UPnPException.java
+++ org.osgi.service.upnp/src/org/osgi/service/upnp/UPnPException.java
@@ -15,6 +15,9 @@
  */
 package org.osgi.service.upnp;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 /**
  * There are several defined error situations describing UPnP problems while a
  * control point invokes actions to UPnPDevices.
@@ -23,57 +26,172 @@ package org.osgi.service.upnp;
  * @version $Id$
  */
 public class UPnPException extends Exception {
-	static final long		serialVersionUID		= -262013318122195146L;
+
+	static final long			serialVersionUID		= -262013318122195146L;
 
 	/**
 	 * No Action found by that name at this service.
 	 */
-	public final static int	INVALID_ACTION			= 401;
+	public final static int		INVALID_ACTION			= 401;
 
 	/**
 	 * Not enough arguments, too many arguments with a specific name, or one of
 	 * more of the arguments are of the wrong type.
 	 */
-	public final static int	INVALID_ARGS			= 402;
+	public final static int		INVALID_ARGS			= 402;
 
 	/**
 	 * The different end-points are no longer in synchronization.
 	 */
-	public final static int	INVALID_SEQUENCE_NUMBER	= 403;
+	public final static int		INVALID_SEQUENCE_NUMBER	= 403;
 
 	/**
 	 * Refers to a non existing variable.
 	 */
-	public final static int	INVALID_VARIABLE		= 404;
+	public final static int		INVALID_VARIABLE		= 404;
 
 	/**
 	 * The invoked action failed during execution.
 	 */
-	public final static int	DEVICE_INTERNAL_ERROR	= 501;
+	public final static int		DEVICE_INTERNAL_ERROR	= 501;
+
+	private static final String	CAUSED_BY				= "Caused by: ";
 
 	/**
 	 * Key for an error information that is an int type variable and that is
-	 * used to identify occured errors.
+	 * used to identify occurred errors.
 	 */
-	private final int				errorCode;
+	private final int			errorCode;
 
 	/**
-	 * This constructor creates a UPnPException on the specified error code and
-	 * error description.
-	 * 
-	 * @param errorCode errorCode which defined UPnP Device Architecture V1.0.
-	 * @param errordesc errorDescription which explain the type of propblem.
+	 * The throwable that caused this UPnP exception. <code>null</code>
+	 * indicates that the throwable cause is unknown.
 	 */
-	public UPnPException(int errorCode, String errordesc) {
-		super(errordesc);
-		this.errorCode = errorCode;
+	private Throwable			errorCause;
+
+	/**
+	 * This constructor creates a <code>UPnPException</code> on the specified
+	 * error code and error description.
+	 * 
+	 * @param errorCode error code which defined by UPnP Device Architecture
+	 *        V1.0.
+	 * @param errorDesc error description which explain the type of problem.
+	 */
+	public UPnPException(int errorCode, String errorDesc) {
+		this(errorCode, errorDesc, null);
 	}
 
 	/**
-	 * Returns the UPnPError Code occured by UPnPDevices during invocation.
+	 * This constructor creates a <code>UPnPException</code> on the specified
+	 * error code, error description and error cause.
+	 * 
+	 * @param errorCode error code which defined by UPnP Device Architecture
+	 *        V1.0.
+	 * @param errorDescription error description which explain the type of the
+	 *        problem.
+	 * @param errorCause cause of that <code>UPnPException</code>.
+	 * 
+	 * @since 1.2
+	 */
+	public UPnPException(int errorCode, String errorDescription,
+			Throwable errorCause) {
+		super(errorDescription);
+		this.errorCode = errorCode;
+		this.errorCause = errorCause;
+	}
+
+	/**
+	 * Returns the UPnP Error Code occurred by UPnPDevices during invocation.
 	 * 
 	 * @return The UPnPErrorCode defined by a UPnP Forum working committee or
 	 *         specified by a UPnP vendor.
+	 * 
+	 * @since 1.2
+	 */
+	public int getUPnPErrorCode() {
+		return getUPnPError_Code();
+	}
+
+	/**
+	 * Returns the error cause of that throwable. <code>null</code> value
+	 * indicates that the cause throwable is unknown.
+	 * 
+	 * @return The cause of that exception or <code>null</code> if the throwable
+	 *         cause is unknown.
+	 * 
+	 * @since 1.2
+	 */
+	public Throwable getCause() {
+		synchronized (this) {
+			return errorCause;
+		}
+	}
+
+	/**
+	 * Prints the exception stack trace to the "standard" error output stream.
+	 * 
+	 * @since 1.2
+	 * 
+	 * @see java.lang.Throwable#printStackTrace()
+	 */
+	public void printStackTrace() {
+		this.printStackTrace(System.err);
+	}
+
+	/**
+	 * Prints the exception stack trace to the specified print stream.
+	 * 
+	 * @param s the print stream used for output.
+	 * 
+	 * @since 1.2
+	 * 
+	 * @see java.lang.Throwable#printStackTrace(java.io.PrintStream)
+	 */
+	public void printStackTrace(PrintStream ps) {
+		synchronized (this) {
+			Throwable errorCauseLocal = errorCause;
+			errorCause = null; // prevent duplicated messages
+			synchronized (ps) {
+				super.printStackTrace(ps);
+				errorCause = errorCauseLocal; // restore the error cause
+				if (null != errorCause) {
+					ps.print(CAUSED_BY);
+					errorCause.printStackTrace(ps);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Prints the exception stack trace to the specified print writer.
+	 * 
+	 * @param s the print writer used for output.
+	 * 
+	 * @since 1.2
+	 * 
+	 * @see java.lang.Throwable#printStackTrace(java.io.PrintWriter)
+	 */
+	public void printStackTrace(PrintWriter pw) {
+		synchronized (this) {
+			Throwable errorCauseLocal = errorCause;
+			errorCause = null; // prevent duplicated messages
+			synchronized (pw) {
+				super.printStackTrace(pw);
+				errorCause = errorCauseLocal; // restore the error cause
+				if (null != errorCause) {
+					pw.print(CAUSED_BY);
+					errorCause.printStackTrace(pw);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Returns the UPnPError Code occurred by UPnPDevices during invocation.
+	 * 
+	 * @return The UPnPErrorCode defined by a UPnP Forum working committee or
+	 *         specified by a UPnP vendor.
+	 * @deprecated As of version 1.2, replaced by {@link #getUPnPErrorCode()}
 	 */
 	public int getUPnPError_Code() {
 		return errorCode;
