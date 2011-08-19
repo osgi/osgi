551fff70d62bb0e3a0dfe5b179168637874795ec
BJ Hargrave <hargrave@us.ibm.com>
2011-08-19 11:12:37 -0400

    Remove allowing null for Requirement.getResource()
    Bug 2078 is changing to take Resource objects
    instead of Requirements.

--- org.osgi.framework/src/org/osgi/framework/resource/Requirement.java
+++ org.osgi.framework/src/org/osgi/framework/resource/Requirement.java
@@ -70,10 +70,9 @@ public interface Requirement {
 	Map<String, Object> getAttributes();
 
 	/**
-	 * Returns the resource declaring this requirement. If there is no resource
-	 * declaring this requirement, {@code null} is returned.
+	 * Returns the resource declaring this requirement.
 	 * 
-	 * @return The resource declaring this requirement, if any.
+	 * @return The resource declaring this requirement.
 	 */
 	Resource getResource();
 
