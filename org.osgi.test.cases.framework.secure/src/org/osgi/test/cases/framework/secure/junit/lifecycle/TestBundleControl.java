6b176b771ab3f9bfd12068253b7e585259335c7a
BJ Hargrave <hargrave@us.ibm.com>
2011-09-17 14:55:44 -0400

    Bug 1393: Test Bundle.adapt(AccessControlContext)
    Also added a Bundle.hasPermission test.

--- org.osgi.test.cases.framework.secure/src/org/osgi/test/cases/framework/secure/junit/lifecycle/TestBundleControl.java
+++ org.osgi.test.cases.framework.secure/src/org/osgi/test/cases/framework/secure/junit/lifecycle/TestBundleControl.java
@@ -6,7 +6,10 @@
  */
 package org.osgi.test.cases.framework.secure.junit.lifecycle;
 
+import java.security.AccessControlContext;
+
 import org.osgi.framework.Bundle;
+import org.osgi.framework.ServicePermission;
 import org.osgi.service.permissionadmin.PermissionAdmin;
 import org.osgi.test.cases.framework.secure.lifecycle.servicereferencegetter.ServiceReferenceGetter;
 import org.osgi.test.cases.framework.secure.lifecycle.servicereferencegetter.TestResult;
@@ -15,11 +18,11 @@ import org.osgi.test.support.compatibility.DefaultTestBundleControl;
 /**
  * This is the bundle initially installed and started by the TestCase when
  * started. It performs the various tests and reports back to the TestCase.
- * 
- * 
+ *
+ *
  * The testPermissions test is currently removed because of unknown state. It
  * uses tb10 but this seems to be removed from the upper makefile.
- * 
+ *
  * @author Ericsson Radio Systems AB
  */
 public class TestBundleControl extends DefaultTestBundleControl {
@@ -47,6 +50,53 @@ public class TestBundleControl extends DefaultTestBundleControl {
 			if ((tb.getState() & Bundle.UNINSTALLED) == 0) {
 				tb.uninstall();
 			}
+		}
+	}
+
+	public void testHasPermission() throws Throwable {
+		Bundle tb = installBundle("lifecycle.tb5.jar", false);
+		try {
+			assertTrue(
+					"missing permission",
+					tb.hasPermission(new ServicePermission(
+							"org.osgi.test.cases.framework.secure.lifecycle.tb5.EventTest",
+							ServicePermission.REGISTER)));
+			assertFalse(
+					"has extra permission",
+					tb.hasPermission(new ServicePermission(
+							"org.osgi.test.cases.framework.secure.lifecycle.tb5.EventTest",
+							ServicePermission.GET)));
+		}
+		finally {
+			tb.uninstall();
+		}
+	}
+
+	public void testAccessControlContext() throws Throwable {
+		Bundle tb = installBundle("lifecycle.tb5.jar", false);
+		try {
+			AccessControlContext acc = tb.adapt(AccessControlContext.class);
+			assertNotNull("no adapt to accesscontrolcontext", acc);
+			try {
+				acc.checkPermission(new ServicePermission(
+						"org.osgi.test.cases.framework.secure.lifecycle.tb5.EventTest",
+						ServicePermission.REGISTER));
+			}
+			catch (SecurityException se) {
+				fail("unexpected exception", se);
+			}
+			try {
+				acc.checkPermission(new ServicePermission(
+						"org.osgi.test.cases.framework.secure.lifecycle.tb5.EventTest",
+						ServicePermission.GET));
+				fail("expected security exception");
+			}
+			catch (SecurityException se) {
+				// expected a security exception
+			}
+		}
+		finally {
+			tb.uninstall();
 		}
 	}
 }
