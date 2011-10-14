509dd99efdc8b5ae4cfb9e6a82d27ba77a8ca328
BJ Hargrave <hargrave@us.ibm.com>
2011-10-13 22:40:08 -0400

    RFC 172 Specify ordering of reference elements
    Per a request from Peter Kriens, the references elements must be
    ordered in ascending order of the reference names.

--- org.osgi.service.component.annotations/src/org/osgi/service/component/annotations/Reference.java
+++ org.osgi.service.component.annotations/src/org/osgi/service/component/annotations/Reference.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) OSGi Alliance (2011). All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -33,6 +33,11 @@ import java.lang.annotation.Target;
  * implementation. It must be processed by tools and used to add a Component
  * Description to the bundle.
  * 
+ * <p>
+ * In the generated Component Description for a component, the references must
+ * be ordered in ascending lexicographical order (using {@code String.compareTo}
+ * ) of the reference {@link #name() name}s.
+ * 
  * @see "The reference element of a Component Description."
  * @version $Id$
  */
@@ -41,63 +46,63 @@ import java.lang.annotation.Target;
 public @interface Reference {
 	/**
 	 * The name of this reference.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, the name of this reference is based upon the name of
 	 * the method being annotated. If the method name begins with {@code bind},
 	 * {@code set} or {@code add}, that is removed.
-	 * 
+	 *
 	 * @see "The name attribute of the reference element of a Component Description."
 	 */
 	String name() default "";
 
 	/**
 	 * The type of the service to bind to this reference.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, the type of the service to bind is based upon the type
 	 * of the first argument of the method being annotated.
-	 * 
+	 *
 	 * @see "The interface attribute of the reference element of a Component Description."
 	 */
 	Class< ? > service() default Object.class;
 
 	/**
 	 * The cardinality of the reference.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, the reference has a
 	 * {@link ReferenceCardinality#MANDATORY 1..1} cardinality.
-	 * 
+	 *
 	 * @see "The cardinality attribute of the reference element of a Component Description."
 	 */
 	ReferenceCardinality cardinality() default ReferenceCardinality.MANDATORY;
 
 	/**
 	 * The policy for the reference.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, the {@link ReferencePolicy#STATIC STATIC} reference
 	 * policy is used.
-	 * 
+	 *
 	 * @see "The policy attribute of the reference element of a Component Description."
 	 */
 	ReferencePolicy policy() default ReferencePolicy.STATIC;
 
 	/**
 	 * The policy option for the reference.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, the {@link ReferencePolicyOption#RELUCTANT RELUCTANT}
 	 * reference policy option is used.
-	 * 
+	 *
 	 * @see "The policy-option attribute of the reference element of a Component Description."
 	 */
 	ReferencePolicyOption policyOption() default ReferencePolicyOption.RELUCTANT;
 
 	/**
 	 * The target filter for the reference.
-	 * 
+	 *
 	 * @see "The target attribute of the reference element of a Component Description."
 	 */
 	String target() default "";
@@ -105,10 +110,10 @@ public @interface Reference {
 	/**
 	 * The name of the unbind method which is associated with the annotated bind
 	 * method.
-	 * 
+	 *
 	 * <p>
 	 * To declare no unbind method, the value {@code "-"} must be used.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, the name of the unbind method is derived from the name
 	 * of the annotated bind method. If the annotated method name begins with
@@ -116,7 +121,7 @@ public @interface Reference {
 	 * {@code unbind}, {@code unset} or {@code remove}, respectively, to derive
 	 * the unbind method name. Otherwise, {@code un} is prefixed to the
 	 * annotated method name to derive the unbind method name.
-	 * 
+	 *
 	 * @see "The unbind attribute of the reference element of a Component Description."
 	 */
 	String unbind() default "";
@@ -124,11 +129,11 @@ public @interface Reference {
 	/**
 	 * The name of the updated method which is associated with the annotated
 	 * bind method.
-	 * 
+	 *
 	 * <p>
 	 * If not specified, then no updated method is associated with the annotated
 	 * bind method.
-	 * 
+	 *
 	 * @see "The updated attribute of the reference element of a Component Description."
 	 */
 	String updated() default "";
