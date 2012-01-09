b0290d89071cf85be68d75b8264194173bbcfd4f
John Ross <jwross@us.ibm.com>
2012-01-09 08:54:36 -0600

    Bug 2093: Moved extension bundle tests from framework.secure to framework.launch.secure.
    All of the extension bundle related tests from framework.secure were moved to framework.launch.secure, including supporting files such as test
    bundles, the bnd test bundle descriptors, and the permission files.
    
    testBootClasspathExtensionInvokerPermission()
    testBootClasspathExtensionInvokerPermissionOk()
    testBootClasspathExtensionPermission()
    testFrameworkExtensionInvokerPermission()
    testFrameworkExtensionInvokerPermissionOk()
    testFrameworkExtensionPermission()
    
    The tests were put into a class dedicated to extension bundle tests. Some functionality originally in the FrameworkLaunchTests class was
    refactored into an abstract superclass in order to support the need for the extension bundle tests to have access to a framework.

---
+++ org.osgi.test.cases.framework.launch.secure/src/org/osgi/test/cases/framework/launch/secure/junit/ExtensionBundleTests.java
@@ -0,0 +1,297 @@
+/*
+ * Copyright (c) OSGi Alliance (2004, 2012). All Rights Reserved.
+ * 
+ * Implementation of certain elements of the OSGi Specification may be subject
+ * to third party intellectual property rights, including without limitation,
+ * patent rights (such a third party may or may not be a member of the OSGi
+ * Alliance). The OSGi Alliance is not responsible and shall not be held
+ * responsible in any manner for identifying or failing to identify any or all
+ * such third party intellectual property rights.
+ * 
+ * This document and the information contained herein are provided on an "AS IS"
+ * basis and THE OSGI ALLIANCE DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO ANY WARRANTY THAT THE USE OF THE INFORMATION
+ * HEREIN WILL NOT INFRINGE ANY RIGHTS AND ANY IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL THE
+ * OSGI ALLIANCE BE LIABLE FOR ANY LOSS OF PROFITS, LOSS OF BUSINESS, LOSS OF
+ * USE OF DATA, INTERRUPTION OF BUSINESS, OR FOR DIRECT, INDIRECT, SPECIAL OR
+ * EXEMPLARY, INCIDENTIAL, PUNITIVE OR CONSEQUENTIAL DAMAGES OF ANY KIND IN
+ * CONNECTION WITH THIS DOCUMENT OR THE INFORMATION CONTAINED HEREIN, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS OR DAMAGE.
+ * 
+ * All Company, brand and product names may be trademarks that are the sole
+ * property of their respective owners. All rights reserved.
+ */
+package org.osgi.test.cases.framework.launch.secure.junit;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
+import org.osgi.framework.launch.Framework;
+import org.osgi.test.support.compatibility.DefaultTestBundleControl;
+
+/**
+ * Test cases for testing fragment bundles and extension bundles.
+ * 
+ * @version $Id$
+ */
+public class ExtensionBundleTests extends LaunchTest {
+	private Framework framework;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		Map<String, String> configuration = new HashMap<String, String>();
+		configuration.put(Constants.FRAMEWORK_SECURITY, "osgi");
+		framework = createFramework(configuration);
+		initFramework(framework);
+		assertNotNull("Null SecurityManager", System.getSecurityManager());
+		startFramework(framework);
+	}
+
+	protected void tearDown() throws Exception {
+		stopFramework(framework);
+		super.tearDown();
+	}
+
+	/**
+	 * Tests if a framework extension bundle has to have
+	 * <code>AllPermission</code> permission to be installed. Will only
+	 * perform this test if <code>SUPPORTS_FRAMEWORK_EXTENSION</code> equals
+	 * <code>true</code>.
+	 * 
+	 * @throws Exception if an error occurs or an assertion fails in the test.
+	 * @spec Bundle.installBundle(String)
+	 */
+	public void testFrameworkExtensionPermission() throws Exception {
+		String message = "extension bundle does not have"
+				+ "permission to be installed";
+		Bundle tb11 = null;
+		if ("true".equals(getContext().getProperty(
+				Constants.SUPPORTS_FRAMEWORK_EXTENSION))) {
+			try {
+				tb11 = installBundle(framework, "/fragments.tb11.jar");
+				// should fail, since extension bundles have to have
+				// AllPermission to be installed
+				DefaultTestBundleControl.failException(message, 
+						BundleException.class);
+			}
+			catch (Exception e) {
+				DefaultTestBundleControl.assertException(message, 
+						BundleException.class, e);
+			}
+			finally {
+				if (tb11 != null) {
+					tb11.uninstall();
+				}
+			}
+		}
+		else {
+			DefaultTestBundleControl.trace(
+					"framework extension bundles not supported");
+		}
+	}
+
+	/**
+	 * Tests if a bundle has to have at least
+	 * <code>AdminPermission[<bundle>, EXTENSIONLIFECYCLE]</code> permission
+	 * to install a framework extension bundle. Will only perform this test if
+	 * <code>SUPPORTS_FRAMEWORK_EXTENSION</code> equals <code>true</code>.
+	 * 
+	 * @throws Exception if an error occurs or an assertion fails in the test.
+	 * @spec Bundle.installBundle(String)
+	 */
+	public void testFrameworkExtensionInvokerPermission() throws Exception {
+		Bundle tb16a = null;
+		if ("true".equals(getContext().getProperty(
+				Constants.SUPPORTS_FRAMEWORK_EXTENSION))) {
+			// install regular bundle
+			tb16a = installBundle(framework, "/fragments.tb16a.jar");
+			try {
+				// start regular bundle that tries to install a framework
+				// extension bundle
+				tb16a.start();
+				// installation inside start should fail, since
+				// bundles have to have
+				// AdminPermission[<bundle>, EXTENSIONLIFECYCLE]
+				// to install extension bundles
+				DefaultTestBundleControl.trace(
+						"prevented bundle without permission from installing "
+						+ "a framework extension bundle");
+			}
+			catch (BundleException e) {
+				fail("should not be able to install an extension bundle "
+						+ "without permission");
+			}
+			finally {
+				tb16a.uninstall();
+			}
+		}
+		else {
+			DefaultTestBundleControl.trace(
+					"framework extension bundles not supported");
+		}
+	}
+
+	/**
+	 * Tests if a bundle has to have at least
+	 * <code>AdminPermission[<bundle>, EXTENSIONLIFECYCLE]</code> permission
+	 * to install a framework extension bundle. Will only perform this test if
+	 * <code>SUPPORTS_FRAMEWORK_EXTENSION</code> equals <code>true</code>.
+	 * 
+	 * @throws Exception if an error occurs or an assertion fails in the test.
+	 * @spec Bundle.installBundle(String)
+	 */
+	public void testFrameworkExtensionInvokerPermissionOk() throws Exception {
+		Bundle tb18 = null;
+		if ("true".equals(getContext().getProperty(
+				Constants.SUPPORTS_FRAMEWORK_EXTENSION))) {
+			// install regular bundle
+			tb18 = installBundle(framework, "/fragments.tb18.jar");
+			try {
+				// start regular bundle that tries to install a framework
+				// extension bundle
+				tb18.start();
+				// installation inside start should not fail, since
+				// bundle has AdminPermission[<bundle>, EXTENSIONLIFECYCLE]
+				DefaultTestBundleControl.trace(
+						"bundle with permission installed "
+						+ "a framework extension bundle");
+			}
+			catch (BundleException e) {
+				fail("should be able to install an extension bundle "
+						+ "with permission");
+			}
+			finally {
+				tb18.uninstall();
+			}
+		}
+		else {
+			DefaultTestBundleControl.trace(
+					"framework extension bundles not supported");
+		}
+	}
+
+	/**
+	 * Tests if a boot classpath extension bundle has to have
+	 * <code>AllPermission</code> permission to be installed. Will only
+	 * perform this test if <code>SUPPORTS_BOOTCLASSPATH_EXTENSION</code>
+	 * equals <code>true</code>.
+	 * 
+	 * @throws Exception if an error occurs or an assertion fails in the test.
+	 * @spec Bundle.installBundle(String)
+	 */
+	public void testBootClasspathExtensionPermission() throws Exception {
+		String message = "extension bundle does not have"
+				+ "permission to be installed";
+		Bundle tb14 = null;
+		if ("true".equals(getContext().getProperty(
+				Constants.SUPPORTS_BOOTCLASSPATH_EXTENSION))) {
+			try {
+				tb14 = installBundle(framework, "/fragments.tb14.jar");
+				// should fail, since extension bundles have to have
+				// AllPermission to be installed
+				DefaultTestBundleControl.failException(message, 
+						BundleException.class);
+			}
+			catch (Exception e) {
+				DefaultTestBundleControl.assertException(message, 
+						BundleException.class, e);
+			}
+			finally {
+				if (tb14 != null) {
+					tb14.uninstall();
+				}
+			}
+		}
+		else {
+			DefaultTestBundleControl.trace(
+					"boot classpath extension bundles not supported");
+		}
+	}
+
+	/**
+	 * Tests if a bundle has to have at least
+	 * <code>AdminPermission[<bundle>, EXTENSIONLIFECYCLE]</code> permission
+	 * to install a boot classpath extension bundle. Will only perform this test
+	 * if <code>SUPPORTS_BOOTCLASSPATH_EXTENSION</code> equals
+	 * <code>true</code>.
+	 * 
+	 * @throws Exception if an error occurs or an assertion fails in the test.
+	 * @spec Bundle.installBundle(String)
+	 */
+	public void testBootClasspathExtensionInvokerPermission() throws Exception {
+		Bundle tb17a = null;
+		if ("true".equals(getContext().getProperty(
+				Constants.SUPPORTS_BOOTCLASSPATH_EXTENSION))) {
+			// install regular bundle
+			tb17a = installBundle(framework, "/fragments.tb17a.jar");
+			try {
+				// start regular bundle that tries to install a framework
+				// extension bundle
+				tb17a.start();
+				// installation inside start should fail, since
+				// bundles have to have
+				// AdminPermission[<bundle>, EXTENSIONLIFECYCLE]
+				// to install extension bundles
+				DefaultTestBundleControl.trace(
+						"prevented bundle without permission from installing "
+						+ "a boot classpath extension bundle");
+			}
+			catch (BundleException e) {
+				fail("should not be able to install an extension bundle "
+						+ "without permission");
+			}
+			finally {
+				tb17a.uninstall();
+			}
+		}
+		else {
+			DefaultTestBundleControl.trace(
+					"boot classpath extension bundles not supported");
+		}
+	}
+
+	/**
+	 * Tests if a bundle has to have at least
+	 * <code>AdminPermission[<bundle>, EXTENSIONLIFECYCLE]</code> permission
+	 * to install a boot classpath extension bundle. Will only perform this test
+	 * if <code>SUPPORTS_BOOTCLASSPATH_EXTENSION</code> equals
+	 * <code>true</code>.
+	 * 
+	 * @throws Exception if an error occurs or an assertion fails in the test.
+	 * @spec Bundle.installBundle(String)
+	 */
+	public void testBootClasspathExtensionInvokerPermissionOk()
+			throws Exception {
+		Bundle tb19 = null;
+		if ("true".equals(getContext().getProperty(
+				Constants.SUPPORTS_BOOTCLASSPATH_EXTENSION))) {
+			// install regular bundle
+			tb19 = installBundle(framework, "/fragments.tb19.jar");
+			try {
+				// start regular bundle that tries to install a boot classpath
+				// extension bundle
+				tb19.start();
+				// installation inside start should not fail, since
+				// bundle has AdminPermission[<bundle>, EXTENSIONLIFECYCLE]
+				DefaultTestBundleControl.trace(
+						"bundle with permission installed "
+						+ "a boot classpath extension bundle");
+			}
+			catch (BundleException e) {
+				fail("should be able to install an extension bundle "
+						+ "with permission");
+			}
+			finally {
+				tb19.uninstall();
+			}
+		}
+		else {
+			DefaultTestBundleControl.trace(
+					"boot classpath extension bundles not supported");
+		}
+	}
+}
---
+++ org.osgi.test.cases.framework.launch.secure/src/org/osgi/test/cases/framework/launch/secure/junit/LaunchTest.java
@@ -0,0 +1,231 @@
+/*
+ * Copyright (c) OSGi Alliance (2009, 2012). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.test.cases.framework.launch.secure.junit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
+import org.osgi.framework.FrameworkEvent;
+import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
+import org.osgi.test.support.OSGiTestCase;
+
+public abstract class LaunchTest extends OSGiTestCase {
+	private static final String FRAMEWORK_FACTORY = "/META-INF/services/org.osgi.framework.launch.FrameworkFactory";
+	
+	private FrameworkFactory frameworkFactory;
+	private String frameworkFactoryClassName;
+	private List<String> rootBundles = new LinkedList<String>();
+	
+	protected Framework createFramework(Map<String, String> configuration) {
+		Framework framework = null;
+		try {
+			framework = frameworkFactory.newFramework(configuration);
+		}
+		catch (Exception e) {
+			fail("Failed to construct the framework", e);
+		}
+		assertEquals("Wrong state for newly constructed framework", Bundle.INSTALLED, framework.getState());
+		return framework;
+	}
+	
+	protected BundleContext getBundleContextWithoutFail() {
+		try {
+			if ("true".equals(System.getProperty("noframework")))
+				return null;
+			return getContext();
+		} catch (Throwable t) {
+			return null; // don't fail
+		}
+	}
+	
+	protected URL getBundleInput(String bundle) {
+		BundleContext context = getBundleContextWithoutFail();
+		return context == null ? this.getClass().getResource(bundle) : context.getBundle().getEntry(bundle);
+	}
+	
+	protected void initFramework(Framework framework) {
+		boolean unintialized = (framework.getState() & (Framework.INSTALLED | Framework.RESOLVED)) != 0;
+		try {
+			framework.init();
+			assertNotNull("BundleContext is null after init", framework.getBundleContext());
+		}
+		catch (BundleException e) {
+			fail("Unexpected BundleException initializing", e);
+		}
+		if (unintialized) {
+			installRootBundles(framework);
+		}
+		assertEquals("Wrong framework state after init", Bundle.STARTING, framework.getState());
+	}
+	
+	protected Bundle installBundle(Framework framework, String bundle) throws BundleException, IOException {
+		return installBundle(framework, bundle, bundle);
+	}
+	
+	protected Bundle installBundle(Framework framework, String bundle, String location) throws BundleException, IOException {
+		BundleContext fwkContext = framework.getBundleContext();
+		assertNotNull("Framework context is null", fwkContext);
+		URL input = getBundleInput(bundle);
+		assertNotNull("Cannot find resource: " + bundle, input);
+		return fwkContext.installBundle(location, input.openStream());
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		frameworkFactoryClassName = getFrameworkFactoryClassName();
+		assertNotNull("Could not find framework factory class", frameworkFactoryClassName);
+		frameworkFactory = getFrameworkFactory();
+		StringTokenizer st = new StringTokenizer(System.getProperty(
+				"org.osgi.test.cases.framework.launch.secure.bundles", ""), ",");
+		rootBundles.clear();
+		while (st.hasMoreTokens()) {
+			String bundle = st.nextToken();
+			assertNotNull(bundle);
+			rootBundles.add(bundle);
+		}
+	}
+	
+	protected void startFramework(Framework framework) {
+		boolean unintialized = (framework.getState() & (Framework.INSTALLED | Framework.RESOLVED)) != 0;
+		try {
+			framework.start();
+			assertNotNull("BundleContext is null after start", framework.getBundleContext());
+		}
+		catch (BundleException e) {
+			fail("Unexpected BundleException initializing", e);
+		}
+		if (unintialized) {
+			installRootBundles(framework);
+		}
+		assertEquals("Wrong framework state after init", Bundle.ACTIVE, framework.getState());
+
+	}
+	
+	protected void stopFramework(Framework framework) {
+		int previousState = framework.getState();
+		try {
+			framework.stop();
+			FrameworkEvent event = framework.waitForStop(10000);
+			assertNotNull("FrameworkEvent is null", event);
+			assertEquals("Wrong event type", FrameworkEvent.STOPPED, event.getType());
+			assertNull("BundleContext is not null after stop", framework.getBundleContext());
+		}
+		catch (BundleException e) {
+			fail("Unexpected BundleException stopping", e);
+		}
+		catch (InterruptedException e) {
+			fail("Unexpected InterruptedException waiting for stop", e);
+		}
+		// if the framework was not STARTING STOPPING or ACTIVE then we assume the waitForStop returned immediately with a FrameworkEvent.STOPPED
+		// and does not change the state of the framework
+		int expectedState = (previousState & (Bundle.STARTING | Bundle.ACTIVE | Bundle.STOPPING)) != 0 ? Bundle.RESOLVED : previousState;
+		assertEquals("Wrong framework state after init", expectedState, framework.getState());
+	}
+	
+	private FrameworkFactory getFrameworkFactory() {
+		try {
+			Class<FrameworkFactory> clazz = loadFrameworkFactoryClass(frameworkFactoryClassName);
+			return clazz.newInstance();
+		} catch (Exception e) {
+			fail("Failed to get the framework constructor", e);
+		}
+		return null;
+	}
+	
+	private String getClassName(URL factoryService) throws IOException {
+		InputStream in = factoryService.openStream();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(in));
+			for (String line = br.readLine(); line != null; line=br.readLine()) {
+				int pound = line.indexOf('#');
+				if (pound >= 0)
+					line = line.substring(0, pound);
+				line.trim();
+				if (!"".equals(line))
+					return line;
+			}
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			}
+			catch (IOException e) {
+				// did our best; just ignore
+			}
+		}
+		return null;
+	}
+	
+	private String getFrameworkFactoryClassName() throws IOException {
+		BundleContext context = getBundleContextWithoutFail();
+		URL factoryService = context == null ? this.getClass().getResource(FRAMEWORK_FACTORY) : context.getBundle(0).getEntry(FRAMEWORK_FACTORY);
+		assertNotNull("Could not locate: " + FRAMEWORK_FACTORY, factoryService);
+		return getClassName(factoryService);
+
+	}
+	
+	private void installRootBundles(Framework framework) {
+		List<Bundle> bundles = new LinkedList<Bundle>();
+
+		BundleContext fwkContext = framework.getBundleContext();
+		assertNotNull("Framework context is null", fwkContext);
+		for (String bundle : rootBundles) {
+			try {
+				Bundle b = fwkContext.installBundle("file:" + bundle);
+				assertNotNull("Cannot install bundle: " + bundle, b);
+				System.out.println("installed bundle " + b.getSymbolicName()
+						+ " " + b.getVersion());
+				bundles.add(b);
+			}
+			catch (BundleException e) {
+				fail("Unexpected BundleException installing root ", e);
+			}
+		}
+
+		for (Bundle b : bundles) {
+			if (b.getHeaders().get(Constants.FRAGMENT_HOST) == null) {
+				try {
+					b.start();
+				}
+				catch (BundleException e) {
+					fail("Unexpected BundleException starting root ", e);
+				}
+				System.out.println("started bundle " + b.getSymbolicName());
+			}
+		}
+	}
+	
+	private Class<FrameworkFactory> loadFrameworkFactoryClass(String className)
+			throws ClassNotFoundException {
+		BundleContext context = getBundleContextWithoutFail();
+		return (Class<FrameworkFactory>) (context == null ? Class
+				.forName(className) : getContext().getBundle(0).loadClass(
+				className));
+	}
+}
