94ec3d4463b92fe540677f6ec44edd84f6d05e37
Jan S. Rellermeyer <rellermeyer@us.ibm.com>
2012-03-01 16:09:54 -0600

    added org.osgi.test.cases.resolver skeleton.

---
+++ org.osgi.test.cases.resolver/src/org/osgi/test/cases/resolver/tb1/A.java
@@ -0,0 +1,20 @@
+package org.osgi.test.cases.resolver.tb1;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class A implements BundleActivator {
+	public A() {
+
+	}
+
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+}
