8b7b37e1069e13dc0cab73bc63c482c170405f4e
Thomas Watson <tjwatson@us.ibm.com>
2012-03-09 16:43:29 -0600

    Add test plan item 4A

--- org.osgi.test.cases.subsystem/src/org/osgi/test/cases/subsystem/junit/SubsystemTest.java
+++ org.osgi.test.cases.subsystem/src/org/osgi/test/cases/subsystem/junit/SubsystemTest.java
@@ -161,6 +161,9 @@ public abstract class SubsystemTest extends OSGiTestCase {
 	public static String SUBSYSTEM_EXPORT_PACKAGE_COMPOSITE_B = "export.package.composite.b@1.0.0.esa";
 	public static String SUBSYSTEM_PROVIDE_CAPABILITY_COMPOSITE_A = "provide.capability.composite.a@1.0.0.esa";
 	public static String SUBSYSTEM_PROVIDE_CAPABILITY_COMPOSITE_B = "provide.capability.composite.b@1.0.0.esa";
+	public static String SUBSYSTEM_4A_APPLICATION = "4A.application@1.0.0.esa";
+	public static String SUBSYSTEM_4A_COMPOSITE = "4A.composite@1.0.0.esa";
+	public static String SUBSYSTEM_4A_FEATURE = "4A.feature@1.0.0.esa";
 
 	public static String BUNDLE_NO_DEPS_A_V1 = "no.deps.a@1.0.0.jar";
 	public static String BUNDLE_NO_DEPS_A_V2 = "no.deps.a@2.0.0.jar";
@@ -947,6 +950,18 @@ public abstract class SubsystemTest extends OSGiTestCase {
 		exportPolicy.put(Constants.PROVIDE_CAPABILITY, "y; y=test, does.not.exist; a=b");
 		result.put(SUBSYSTEM_PROVIDE_CAPABILITY_COMPOSITE_A, new SubsystemInfo(new File(testSubsystemRoots, SUBSYSTEM_PROVIDE_CAPABILITY_COMPOSITE_A), true, "1.0.0", SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE, false, contentHeader, content, exportPolicy));
 		result.put(SUBSYSTEM_PROVIDE_CAPABILITY_COMPOSITE_B, new SubsystemInfo(new File(testSubsystemRoots, SUBSYSTEM_PROVIDE_CAPABILITY_COMPOSITE_B), true, "1.0.0", SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE, false, contentHeader, content, exportPolicy));
+
+		
+		contentHeader = 
+				getSymbolicName(BUNDLE_SHARE_A) + "; version=\"[1.0,1.0]\"," +
+				getSymbolicName(BUNDLE_SHARE_B) + "; version=\"[1.0,1.0]\"," +
+				getSymbolicName(BUNDLE_SHARE_C) + "; version=\"[1.0,1.0]\"," +
+				getSymbolicName(BUNDLE_SHARE_D) + "; version=\"[1.0,1.0]\"," +
+				getSymbolicName(BUNDLE_SHARE_E) + "; version=\"[1.0,1.0]\"";
+		content = getBundleContents(null, BUNDLE_SHARE_A, BUNDLE_SHARE_B, BUNDLE_SHARE_C, BUNDLE_SHARE_D, BUNDLE_SHARE_E);
+		result.put(SUBSYSTEM_4A_APPLICATION, new SubsystemInfo(new File(testSubsystemRoots, SUBSYSTEM_4A_APPLICATION), true, "1.0.0", SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION, false, contentHeader, content, null));
+		result.put(SUBSYSTEM_4A_COMPOSITE, new SubsystemInfo(new File(testSubsystemRoots, SUBSYSTEM_4A_COMPOSITE), true, "1.0.0", SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE, false, contentHeader, content, null));
+		result.put(SUBSYSTEM_4A_FEATURE, new SubsystemInfo(new File(testSubsystemRoots, SUBSYSTEM_4A_FEATURE), true, "1.0.0", SubsystemConstants.SUBSYSTEM_TYPE_FEATURE, false, contentHeader, content, null));
 
 		testSubsystems = result;
 	}
