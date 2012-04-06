6eea7f233f575d961a2783d04f67ce2410f6f123
Jan S. Rellermeyer <rellermeyer@us.ibm.com>
2012-04-05 22:50:26 -0500

    now able to load the framework test case bundles.

--- org.osgi.test.cases.resolver/src/org/osgi/test/cases/resolver/junit/ResolverTestCase.java
+++ org.osgi.test.cases.resolver/src/org/osgi/test/cases/resolver/junit/ResolverTestCase.java
@@ -24,15 +24,157 @@
  */
 package org.osgi.test.cases.resolver.junit;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.hooks.resolver.ResolverHook;
+import org.osgi.framework.hooks.resolver.ResolverHookFactory;
+import org.osgi.framework.wiring.BundleCapability;
+import org.osgi.framework.wiring.BundleRequirement;
+import org.osgi.framework.wiring.BundleRevision;
+import org.osgi.resource.Capability;
+import org.osgi.resource.Requirement;
+import org.osgi.resource.Resource;
+import org.osgi.resource.Wiring;
+import org.osgi.service.resolver.HostedCapability;
+import org.osgi.service.resolver.ResolveContext;
+import org.osgi.service.resolver.Resolver;
 import org.osgi.test.support.compatibility.DefaultTestBundleControl;
 
 /**
- * This is the XML Parser Service test suite
+ * This is the Resolver test case
  */
 public class ResolverTestCase extends DefaultTestBundleControl {
 
-	public void testDummy() throws Exception {
-		System.out.println("I CAN RUN!!!");
+	private Bundle fwtestbundle;
+
+	private ServiceRegistration resolverHookRegistration;
+
+	private String webserver;
+
+	protected void setUp() throws Exception {
+		resolverHookRegistration = getContext().registerService(
+				ResolverHookFactory.class, new Factory(), null);
+	}
+
+	protected void tearDown() throws Exception {
+		resolverHookRegistration.unregister();
+		resolverHookRegistration = null;
+	}
+
+	public void testZero() throws Exception {
+		final Resolver resolver = getResolverService();
+
+		resolver.resolve(new ResolveContext() {
+
+			@Override
+			public List<Capability> findProviders(Requirement requirement) {
+				fail();
+				return null;
+			}
+
+			@Override
+			public int insertHostedCapability(List<Capability> capabilities,
+					HostedCapability hostedCapability) {
+				fail();
+				return -1;
+			}
+
+			@Override
+			public boolean isEffective(Requirement requirement) {
+				fail();
+				return false;
+			}
+
+			@Override
+			public Map<Resource, Wiring> getWirings() {
+				fail();
+				return null;
+			}
+
+		});
+	}
+	
+	@Override
+	public String getWebServer() {
+		String w = webserver;
+		if (w == null) {
+			URL base = getFrameworkBundle().getEntry("/");
+			webserver = w = (base == null) ? "/" : base.toExternalForm();
+		}
+		return w;
+	}
+
+	public void testFromFW() throws Exception {
+		final Bundle bundle =installBundle("fragments.tb1a.jar", false);
+
+		final BundleRevision revision = bundle.adapt(BundleRevision.class);
+		final List<Capability> caps = revision.getCapabilities(null);
+		assertNotNull(caps);
+		System.out.println("CAPABILITIES " + caps);
+		System.out.println("REQUIREMENTS " + revision.getRequirements(null));
+
+	}
+
+	private Resolver getResolverService() {
+		final BundleContext context = getContext();
+		final ServiceReference<Resolver> sref = context
+				.getServiceReference(Resolver.class);
+		assertNotNull(sref);
+		return context.getService(sref);
+	}
+
+	private Bundle getFrameworkBundle() {
+		if (fwtestbundle == null) {
+			final BundleContext context = getContext();
+			final Bundle[] bundles = context.getBundles();
+			for (int i = 0; i < bundles.length; i++) {
+				if ("org.osgi.test.cases.framework".equals(bundles[i]
+						.getSymbolicName())) {
+					fwtestbundle = bundles[i];
+					return fwtestbundle;
+				}
+			}
+		}
+
+		fail("cannot find the framework test bundle");
+		return null;
+	}
+
+	private class Factory implements ResolverHookFactory, ResolverHook {
+
+		public ResolverHook begin(Collection<BundleRevision> triggers) {
+			return this;
+		}
+
+		public void filterResolvable(Collection<BundleRevision> candidates) {
+			for (final BundleRevision br : candidates) {
+				candidates.remove(br);
+			}
+		}
+
+		public void filterSingletonCollisions(BundleCapability singleton,
+				Collection<BundleCapability> collisionCandidates) {
+			// nop
+		}
+
+		public void filterMatches(BundleRequirement requirement,
+				Collection<BundleCapability> candidates) {
+			// nop
+		}
+
+		public void end() {
+			// nop
+		}
+
 	}
 
 }
