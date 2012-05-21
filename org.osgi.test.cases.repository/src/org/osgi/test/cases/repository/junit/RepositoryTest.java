fceb8914bdbf54a326f95e05a91f57a1acd18824
David Bosschaert <david@redhat.com>
2012-05-21 16:25:53 +0100

    Working test framework.

--- org.osgi.test.cases.repository/src/org/osgi/test/cases/repository/junit/RepositoryTest.java
+++ org.osgi.test.cases.repository/src/org/osgi/test/cases/repository/junit/RepositoryTest.java
@@ -1,9 +1,9 @@
 package org.osgi.test.cases.repository.junit;
 
 import org.osgi.framework.Bundle;
-import org.osgi.test.support.OSGiTestCase;
+import org.osgi.test.support.compatibility.DefaultTestBundleControl;
 
-public class RepositoryTest extends OSGiTestCase {
+public class RepositoryTest extends DefaultTestBundleControl {
     public void testFoo() {
         for (Bundle b : getContext().getBundles()) {
             System.out.println("**** " + b.getSymbolicName());
