48e07a1e24a2eee880e658387a0c777980406028
David Bosschaert <david@redhat.com>
2012-05-22 08:47:48 +0100

    Added data bundle

--- org.osgi.test.cases.repository/src/org/osgi/test/cases/repository/junit/RepositoryTest.java
+++ org.osgi.test.cases.repository/src/org/osgi/test/cases/repository/junit/RepositoryTest.java
@@ -5,17 +5,28 @@ import java.util.Arrays;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
-import org.osgi.test.support.OSGiTestCase;
+import org.osgi.test.support.compatibility.DefaultTestBundleControl;
 
-public class RepositoryTest extends OSGiTestCase {
+public class RepositoryTest extends DefaultTestBundleControl {
     public void testFooXX() throws Exception {
+        //Bundle tb1 = installBundle(getWebServer() + "xmlresources.tb1.jar", false);
+
         for (Bundle b : getContext().getBundles()) {
-            System.err.println("@@@@ " + b.getSymbolicName());
+            System.err.println("@@@@ " + b.getSymbolicName() + "#" + b.getLocation());
         }
 
         for (ServiceReference<?> sr : getContext().getAllServiceReferences(null, null)) {
             System.err.println("**** " + Arrays.toString((String []) sr.getProperty(Constants.OBJECTCLASS)));
         }
+
+        /*
+        Bundle tb1 = getContext().
+        Enumeration<URL> entries = tb1.findEntries("/", "*.xml", true);
+        while (entries.hasMoreElements()) {
+            System.err.println("$$$ " + entries.nextElement());
+        }
+        uninstallBundle(tb1);
+        */
     }
 
     public void testBar() {
