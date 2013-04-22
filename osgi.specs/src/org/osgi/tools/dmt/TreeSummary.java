40281992aa2a9ba7f0e49222bb059c92d7fcd3e4
BJ Hargrave <hargrave@us.ibm.com>
2013-04-22 14:05:08 -0400

    Residential 2.7: Generate section text from dmforest source code
    The 2.7 section text is the spec source has errors due to changes in the
    deforest project. The text in the spec is removed and is replaced by
    text generated from the dmforest source code.

--- osgi.specs/src/org/osgi/tools/dmt/TreeSummary.java
+++ osgi.specs/src/org/osgi/tools/dmt/TreeSummary.java
@@ -1,32 +1,70 @@
+/*
+ * Copyright (c) OSGi Alliance (2011, 2013). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-package test;
+package org.osgi.tools.dmt;
 
-import java.lang.reflect.*;
-import java.net.*;
-import java.util.*;
-import org.osgi.dmt.ddf.*;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.osgi.dmt.ddf.AddableMAP;
+import org.osgi.dmt.ddf.LIST;
+import org.osgi.dmt.ddf.MAP;
+import org.osgi.dmt.ddf.Mutable;
+import org.osgi.dmt.ddf.MutableLIST;
+import org.osgi.dmt.ddf.MutableMAP;
+import org.osgi.dmt.ddf.NODE;
+import org.osgi.dmt.ddf.NodeType;
+import org.osgi.dmt.ddf.Opt;
+import org.osgi.dmt.ddf.Scope;
 import org.osgi.dmt.ddf.Scope.SCOPE;
-import org.osgi.dmt.residential.*;
+import org.osgi.dmt.ddf.base64;
 
-public class Test {
+/**
+ * 
+ * Generate DMT Tree Summary for {@literal <xi:include>} into spec.
+ * 
+ * @author $Id$
+ */
+public class TreeSummary {
 
 	static class XNode {
-		final XNode	parent;
-		String		name;
-		List<XNode>	children	= new ArrayList<XNode>();
-		boolean		optional	= false;
-		boolean		multiple	= false;
-		boolean		add			= false;
-		boolean		delete		= false;
-		boolean		get			= false;
-		boolean		replace		= false;
-		boolean		recursive	= false;
-		String		dmtType;
-		Type		type;
-		SCOPE		scope		= SCOPE.P;
-		String		cardinality	= "1";
-		String		mime		= "";
-		boolean		primitive	= true;
+		final XNode		parent;
+		final String	name;
+		final Type		type;
+
+		List<XNode>		children	= new ArrayList<XNode>();
+		boolean			optional	= false;
+		boolean			multiple	= false;
+		boolean			add			= false;
+		boolean			delete		= false;
+		boolean			get			= false;
+		boolean			replace		= false;
+		boolean			recursive	= false;
+		String			dmtType;
+		SCOPE			scope		= SCOPE.P;
+		String			cardinality	= "1";
+		String			mime		= "";
+		boolean			primitive	= true;
 
 		XNode(XNode parent, String name, Type type) {
 			this.type = type;
@@ -46,7 +84,9 @@ public class Test {
 				cardinality = "0,1";
 			}
 			if (instanceOf(type, LIST.class)) {
+				@SuppressWarnings("hiding")
 				boolean add = false;
+				@SuppressWarnings("hiding")
 				boolean delete = false;
 				if (instanceOf(type, MutableLIST.class)) {
 					add = true;
@@ -68,7 +108,9 @@ public class Test {
 				return;
 			} else
 				if (instanceOf(type, MAP.class)) {
+					@SuppressWarnings("hiding")
 					boolean add = false;
+					@SuppressWarnings("hiding")
 					boolean delete = false;
 					if (instanceOf(type, MutableMAP.class)) {
 						add = true;
@@ -178,53 +220,70 @@ public class Test {
 											}
 		}
 
-		private boolean isPrimitive() {
-			return primitive;
+		private boolean isRecursive(Type t) {
+			return this.type == t || (parent != null && parent.isRecursive(t));
 		}
 
-		private boolean isRecursive(Type type) {
-			return this.type == type || (parent != null && parent.isRecursive(type));
-		}
-
-		private boolean instanceOf(Type type, Class<?> class1) {
-			if (type instanceof ParameterizedType) {
-				return class1.isAssignableFrom((Class<?>) ((ParameterizedType) type).getRawType());
+		private boolean instanceOf(Type t, Class<?> class1) {
+			if (t instanceof ParameterizedType) {
+				return class1.isAssignableFrom((Class<?>) ((ParameterizedType) t).getRawType());
 			}
-			if (type instanceof TypeVariable) {
-				return instanceOf(((TypeVariable) type).getBounds()[0], class1);
+			if (t instanceof TypeVariable) {
+				return instanceOf(((TypeVariable<?>) t).getBounds()[0], class1);
 			}
-			if (type instanceof GenericArrayType) {
+			if (t instanceof GenericArrayType) {
 				return class1 == byte[].class;
 			}
-			if (type instanceof WildcardType) {
+			if (t instanceof WildcardType) {
 				return false;
 			}
-			return class1.isAssignableFrom((Class<?>) type);
+			return class1.isAssignableFrom((Class<?>) t);
 		}
 
-		String getPath() {
-			String upper = parent == null ? "" : parent.getPath();
-			return upper + "/" + name;
-		}
-
-		public void print(String indent) {
-			String actions = "" + (add ? "A" : "_") + (get ? "G" : "_") + (replace ? "R" : "_") + (delete ? "D" : "_");
+		/**
+		 * Generate the docbook xml for the Tree Summary.
+		 * 
+		 * @param indent Indent prefix to use.
+		 * @param section true is a docbook section should wrap the output.
+		 */
+		public void print(final String indent, final boolean section) {
+			final boolean root = parent == null;
+			if (section) {
+				System.out.printf("<section%s>\n  <title>%s</title>\n  <programlisting>", root ? " xmlns=\"http://docbook.org/ns/docbook\" version=\"5\"" : "", root ? "Tree Summary" : name);
+			}
 
 			if (mime != null && !mime.isEmpty()) {
 				System.out.printf("\n%80s\n", mime);
 			}
-			System.out.printf("%-36s %-5s %-4s  %-20s  %5s   %s\n", indent + name, (recursive ? " ..." : ""), actions, dmtType, cardinality, scope);
+			String actions = (add ? "A" : "_") + (get ? "G" : "_") + (replace ? "R" : "_") + (delete ? "D" : "_");
+			System.out.printf("%-36s %-5s %-4s  %-20s  %5s   %1s\n", indent + name, (recursive ? " ..." : ""), actions, dmtType, cardinality, scope);
 
+			if (section) {
+				if (root) {
+					System.out.printf("</programlisting>\n");
+				}
+			}
 			for (XNode node : children) {
-				node.print(indent + "  ");
+				node.print(indent + "  ", root);
+			}
+			if (section) {
+				if (!root) {
+					System.out.printf("</programlisting>\n");
+				}
+				System.out.printf("</section>\n");
 			}
 		}
 	}
 
-	static $	x;
-
-	public static void main(String args[]) {
-		XNode n = new XNode(null, "$", $.class);
-		n.print("");
+	/**
+	 * Generate the Tree Summary for the DMT.
+	 * 
+	 * @param args Unused.
+	 * @throws Exception
+	 */
+	public static void main(String args[]) throws Exception {
+		Class<?> root = Class.forName(args[0]);
+		XNode n = new XNode(null, root.getSimpleName(), root);
+		n.print("", true);
 	}
 }
