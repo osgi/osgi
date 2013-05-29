d13e7f384a7fb9ac61d63479237e48688b6e8a45
Jean Pierre Poutcheu <jeanpierre.poutcheu@orange.com>
2013-05-29 13:20:01 +0200

    Adding data type standalone classes

---
+++ org.osgi.service.zigbee/src/org/osgi/service/zigbee/datatype/types/ZigBeeTimeOfDay.java
@@ -0,0 +1,41 @@
+package org.osgi.service.zigbee.datatype.types;
+
+import org.osgi.service.zigbee.datatype.ZigBeeDataTypeDescription;
+
+public class ZigBeeTimeOfDay implements
+		ZigBeeDataTypeDescription {
+	public byte[] serialize(Object param) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isAnalog() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Class getJavaDataType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getInvalidNumber() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public short getId() {
+		// TODO Auto-generated method stub
+		return 0x09;
+	}
+
+	public Object deserialize(byte[] data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
