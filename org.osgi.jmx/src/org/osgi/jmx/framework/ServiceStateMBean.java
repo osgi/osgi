776bef537557919de4aecb4bb6dd5ded2e877bff
BJ Hargrave <hargrave@us.ibm.com>
2013-06-06 14:54:47 -0400

    org.osgi.jmx: Replace invalid @version javadoc tags
    The spec process now validates @version javadoc tags have proper OSGi
    version strings. Git ident is now kept on the @author javadoc tag.

--- org.osgi.jmx/src/org/osgi/jmx/framework/ServiceStateMBean.java
+++ org.osgi.jmx/src/org/osgi/jmx/framework/ServiceStateMBean.java
@@ -30,7 +30,7 @@ import org.osgi.jmx.JmxConstants;
  * emits events that clients can use to get notified of the changes in the
  * service state of the framework.
  * 
- * @version $Revision: 1226784 $
+ * @author $Id$
  * @ThreadSafe
  */
 public interface ServiceStateMBean {
