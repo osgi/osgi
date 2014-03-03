1434538e26750fe423781d3c9a83b06803b6eb46
antonin.chazalet@orange.com <antonin.chazalet@orange.com>
2014-03-03 15:05:54 +0100

    RFC192: update javadoc.

--- org.osgi.service.zigbee/src/org/osgi/service/zigbee/ZigBeeCommandHandler.java
+++ org.osgi.service.zigbee/src/org/osgi/service/zigbee/ZigBeeCommandHandler.java
@@ -35,8 +35,8 @@ public interface ZigBeeCommandHandler {
 	 * set the DisableDefaultReponse flag and the status of DefaultResponse
 	 * command is SUCCESS.
 	 * 
-	 * Several responses MAY be sent to an endpoint. A handler could be called
-	 * several times on a command handler.
+	 * Multiple response management: Several responses MAY be sent to an
+	 * endpoint. A handler could be called several times on a command handler.
 	 * 
 	 * @param frame the ZCLFrame
 	 */
