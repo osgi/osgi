bef8ec8c868a527ed5a16243d8e8575aca34b258
Yukio Koike <koike.yukio@lab.ntt.co.jp>
2014-05-01 19:47:44 +0900

    USBCategory: modified based on RFC202 v0.8

--- org.osgi.service.usb/src/org/osgi/service/usb/USBDevice.java
+++ org.osgi.service.usb/src/org/osgi/service/usb/USBDevice.java
@@ -24,56 +24,63 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	static final String DEVICE_CATEGORY = "USB";
 
 	/**
-	 * MANDATORY property key. Value is "USB.device.bcdUSB".
+	 * OPTIONAL property key. Value is "USB.device.bcdUSB".
 	 * 
-	 * The value is Integer, the 4-digit BCD format. Example: 0x0210
+	 * The value is String, the 4-digit BCD format. Example: "0210"
 	 */
 	static final String USB_RELEASE_NUMBER = "USB.device.bcdUSB";
 
 	/**
 	 * MANDATORY property key. Value is "USB.device.bDeviceClass".
 	 * 
-	 * The value is Integer, hexadecimal, 2-digits. Example: 0xff
+	 * The value is String, hexadecimal, 2-digits. Example: "ff"
 	 */
 	static final String DEVICE_CLASS = "USB.device.bDeviceClass";
 
 	/**
 	 * MANDATORY property key. Value is "USB.device.bDeviceSubClass".
 	 * 
-	 * The value is Integer, hexadecimal, 2-digits. Example: 0xff
+	 * The value is String, hexadecimal, 2-digits. Example: "ff"
 	 */
 	static final String DEVICE_SUBCLASS = "USB.device.bDeviceSubClass";
 
 	/**
 	 * MANDATORY property key. Value is "USB.device.bDeviceProtocol".
 	 * 
-	 * The value is Integer, hexadecimal, 2-digits. Example: 0xff
+	 * The value is String, hexadecimal, 2-digits. Example: "ff"
 	 */
 	static final String DEVICE_PROTOCOL = "USB.device.bDeviceProtocol";
 
 	/**
+	 * OPTIONAL property key. Value is "USB.device.bMaxPacketSize0".
+	 * 
+	 * The value is Integer.
+	 */
+	static final String DEVICE_MAXPACETSIZE0 = "USB.device.bMaxPacketSize0";
+	
+	/**
 	 * MANDATORY property key. Value is "USB.device.idVendor".
 	 * 
-	 * The value is Integer, hexadecimal, 4-digits. Example: 0x0403
+	 * The value is String, hexadecimal, 4-digits. Example: "0403"
 	 */
 	static final String VID = "USB.device.idVendor";
 
 	/**
 	 * MANDATORY property key. Value is "USB.device.idProduct".
 	 * 
-	 * The value is Integer, hexadecimal, 4-digits. Example: 0x8372
+	 * The value is String, hexadecimal, 4-digits. Example: "8372"
 	 */
 	static final String PID = "USB.device.idProduct";
 
 	/**
 	 * MANDATORY property key. Value is "USB.device.bcdDevice".
 	 * 
-	 * The value is Integer, the 4-digit BCD format. Example: 0x0200
+	 * The value is String, the 4-digit BCD format. Example: "0200"
 	 */
 	static final String RELEASE_NUMBER = "USB.device.bcdDevice";
 
 	/**
-	 * OPTIONAL Property key. Value is "iManufacturer".
+	 * OPTIONAL property key. Value is "iManufacturer".
 	 * 
 	 * The value is String of indicated in iManufacturer. (The value is not the
 	 * index.) Example: "Buffalo Inc."
@@ -81,7 +88,7 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	static final String MANUFACTURER = "USB.device.iManufacturer";
 
 	/**
-	 * OPTIONAL Property key. Value is "iProduct".
+	 * OPTIONAL property key. Value is "iProduct".
 	 * 
 	 * The value is String of indicated in iProduct. (The value is not the
 	 * index.) Example: "USB2.0 PC Camera"
@@ -89,7 +96,7 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	static final String PRODUCT = "USB.device.iProduct";
 
 	/**
-	 * OPTIONAL Property key. Value is "USB.device.iSerialNumber".
+	 * OPTIONAL property key. Value is "USB.device.iSerialNumber".
 	 * 
 	 * The value is String of indicated in iSerialNumber. (The value is not the
 	 * index.) Example: "57B0002600000001"
@@ -97,23 +104,74 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	static final String SERIALNUMBER = "USB.device.iSerialNumber";
 
 	/**
-	 * MANDATORY property key. Value is "USB.device.interfaceclasses".
+	 * OPTIONAL property key. Value is "USB.device.bNumConfigurations".
 	 * 
-	 * The property value is Integer[], hexadecimal, 6-digits. Each Integer
-	 * responds to each USB interface and is combined the interface's
-	 * bInterfaceClass (2-digits), bInterfaceSubClass (2-digits) and
-	 * bInterfaceProtocol (2-digits). Example: {0x080000, 0x0a00ff}
+	 * The value is Integer.
 	 */
-	static final String INTERFACE_CLASSES = "USB.device.interfaceclasses";
+	static final String NUM_CONFIGURATIONS = "USB.device.bNumConfigurations";
 
+	/**
+	 * MANDATORY property key. Value is "USB.device.bInterfaceNumber".
+	 * 
+	 * The value is Integer.
+	 */
+	static final String INTERFACE_NUMBER = "USB.device.bInterfaceNumber";
+	
+	/**
+	 * OPTIONAL property key. Value is "USB.device.bAlternateSetting".
+	 * 
+	 * The value is Integer.
+	 */
+	static final String ALTERNATE_SETTING = "USB.device.bAlternateSetting";
+	
+	/**
+	 * OPTIONAL property key. Value is "USB.device.bAlternateSetting".
+	 * 
+	 * The value is Integer.
+	 */
+	static final String NUM_ENDPOINTS = "USB.device.bAlternateSetting";
+	
+	
+	/**
+	 * MANDATORY property key. Value is "USB.device.bInterfaceClass".
+	 * 
+	 * The value is String, hexadecimal, 2-digits. Example: "ff"
+	 */
+	static final String INTERFACE_CLASS = "USB.device.bInterfaceClass";
+
+	/**
+	 * MANDATORY property key. Value is "USB.device.bInterfaceSubClass".
+	 * 
+	 * The value is String, hexadecimal, 2-digits. Example: "ff"
+	 */
+	static final String INTERFACE_SUBCLASS = "USB.device.bInterfaceSubClass";
+
+	/**
+	 * MANDATORY property key. Value is "USB.device.bInterfaceProtocol".
+	 * 
+	 * The value is String, hexadecimal, 2-digits. Example: "ff"
+	 */
+	static final String INTERFACE_PROTOCOL = "USB.device.bInterfaceProtocol";
+	
+	
+	/**
+	 * OPTIONAL property key. Value is "USB.device.iInterface".
+	 * 
+	 * The value is String of indicated in iInterface. (The value is not the
+	 * index.)
+	 */
+	static final String INTERFACE_DESCRIPTION = "USB.device.iInterface";
+
+	
+	
 	/**
 	 * MANDATORY property key. Value is "USB.device.bus".
 	 * 
 	 * The value is Integer. Used to identify USB devices with same VID / PID.
 	 * The value is the ID of the USB bus assigned when connecting the USB
-	 * device. USB bus ID is integer (001-127).
+	 * device. USB bus ID is integer.
 	 * 
-	 * The USB bus ID does not change while connecting the USB device.
+	 * The USB bus ID does not change while the USB device remains connected.
 	 * 
 	 * Example: 3
 	 */
@@ -126,7 +184,7 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	 * The value is the ID of the USB address assigned when connecting the USB
 	 * device. USB address is integer (001-127).
 	 * 
-	 * The USB address does not change while connecting the USB device.
+	 * The USB address does not change while the USB device remains connected.
 	 * 
 	 * Example: 2
 	 */
@@ -167,9 +225,9 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	 * 
 	 * Constant for the value of the service property DEVICE_CATEGORY used for a
 	 * USB device which has a serial communication function such as a USB
-	 * dongle. Such a USB base driver bundle must set this property key and
+	 * dongle. The USB base driver bundle must set this property key and
 	 * serial.comport property. This device category's value may be used
-	 * independently of USB. This value is defined because of some USB devices
+	 * independently of USB. This value is defined because some USB devices
 	 * have a serial communication function.
 	 * 
 	 * See Also org.osgi.service.device.Constants.DEVICE_CATEGORY
@@ -181,7 +239,7 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	 * 
 	 * The property value is String. The USB Device has a serial communication
 	 * function, set the value that represents the COM port. If the USB device
-	 * does not have a serial communication function, this key and value is not
+	 * does not have a serial communication function, this key and value are not
 	 * set. The driver can communicate through Java Communications API with this
 	 * value. Set this value "portName" of
 	 * javax.comm.CommPortIdentifier#getPortIdentifier(String portName). Then
@@ -213,7 +271,7 @@ public interface /* org.osgi.service.usb. */USBDevice {
 	 * 
 	 * The property value is String[]. If the USB device is Mass Storage Class,
 	 * set the value that represents the mount point (a path to the USB storage)
-	 * in OS. If the USB device is not Mass Storage Class, this key and value is
+	 * in OS. If the USB device is not Mass Storage Class, this key and value are
 	 * not set. The driver can read and write the USB storage through standard
 	 * File API with this value. Set this value "pathname" of
 	 * java.io.File(String pathname). Then file access is possible.
