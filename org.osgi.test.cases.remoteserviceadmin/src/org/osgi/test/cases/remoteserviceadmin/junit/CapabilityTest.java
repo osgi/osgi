42b9ff51b08ab0e5dd31c2f7a9bf3f95eb96e091
Marc Schaaf <marc.schaaf@fhnw.ch>
2014-07-30 09:24:11 +0200

    RSA: Bug 2712: updated test case

--- org.osgi.test.cases.remoteserviceadmin/src/org/osgi/test/cases/remoteserviceadmin/junit/CapabilityTest.java
+++ org.osgi.test.cases.remoteserviceadmin/src/org/osgi/test/cases/remoteserviceadmin/junit/CapabilityTest.java
@@ -1,18 +1,16 @@
 package org.osgi.test.cases.remoteserviceadmin.junit;
 
-import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import junit.framework.Assert;
 
 import org.osgi.framework.Bundle;
-import org.osgi.framework.ServiceReference;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.Version;
 import org.osgi.framework.wiring.BundleCapability;
 import org.osgi.framework.wiring.BundleWiring;
-import org.osgi.service.remoteserviceadmin.EndpointEventListener;
-import org.osgi.service.remoteserviceadmin.RemoteServiceAdmin;
 import org.osgi.test.support.compatibility.DefaultTestBundleControl;
 
 public class CapabilityTest extends DefaultTestBundleControl {
@@ -23,37 +21,29 @@ public class CapabilityTest extends DefaultTestBundleControl {
 	public void testLocalDiscoveryProviderCapability() throws Exception {
 		boolean foundDiscovery = false;
 
-		Bundle[] bundles = getContext().getBundles();
-		for (int i = 0; i < bundles.length; i++) {
-			Bundle eplBundle = bundles[i];
-			BundleWiring bw = eplBundle.adapt(BundleWiring.class);
+		// find at least one bundle that provides the required capability
+		List<BundleCapability> capabilities = findCapabilities(getContext(),
+				"osgi.extender");
+		if (capabilities.size() >= 1) {
+			for (BundleCapability bc : capabilities) {
+				Map<String, Object> attributes = bc.getAttributes();
+				if ("osgi.remoteserviceadmin".equals(attributes
+						.get("osgi.extender"))) {
+					// this bundle is the discovery provider
 
-			List<BundleCapability> capabilities = bw
-					.getCapabilities("osgi.extender");
+					Version version = (Version) attributes.get("version");
+					Assert.assertEquals(0,
+							version.compareTo(new Version(1, 1, 0)));
 
-			if (capabilities.size() >= 1) {
-				for (BundleCapability bc : capabilities) {
-					Map<String, Object> attributes = bc.getAttributes();
-					if ("osgi.remoteserviceadmin".equals(attributes
-							.get("osgi.extender"))) {
-						// this bundle is the discovery provider
+					String uses = bc.getDirectives().get("uses");
+					Assert.assertNotNull(uses);
+					Assert.assertTrue("org.osgi.service.remoteserviceadmin"
+							.equals(uses));
 
-						Version version = (Version) attributes.get("version");
-						Assert.assertEquals(0,
-								version.compareTo(new Version(1, 1, 0)));
-
-						String uses = bc.getDirectives().get("uses");
-						Assert.assertNotNull(uses);
-						Assert.assertTrue("org.osgi.service.remoteserviceadmin"
-								.equals(uses));
-
-						foundDiscovery = true;
-					}
+					foundDiscovery = true;
 				}
 			}
-
 		}
-
 		Assert.assertTrue(foundDiscovery);
 	}
 
@@ -61,24 +51,15 @@ public class CapabilityTest extends DefaultTestBundleControl {
 	 * 122.9.2 Discovery Provider Capability
 	 */
 	public void testDiscoveryProviderCapability() throws Exception {
-		Collection<ServiceReference<EndpointEventListener>> listenerRefs = getContext()
-				.getServiceReferences(EndpointEventListener.class, null);
-		
 		boolean foundDiscovery = false;
-		
-		for (ServiceReference<EndpointEventListener> eplRef : listenerRefs) {
-			
-			Bundle eplBundle = eplRef.getBundle();
-			BundleWiring bw = eplBundle.adapt(BundleWiring.class);
-			
 
-			List<BundleCapability> capabilities = bw
-					.getCapabilities("osgi.remoteserviceadmin.discovery");
+		List<BundleCapability> capabilities = findCapabilities(getContext(),
+				"osgi.remoteserviceadmin.discovery");
 
-			if (capabilities.size() == 1) {
-				// this bundle is the discovery provider
-				Map<String, Object> attributes = capabilities.get(0)
-						.getAttributes();
+		if (capabilities.size() >= 1) {
+			for (BundleCapability bc : capabilities) {
+				// this bundle is a discovery provider, check capability spec
+				Map<String, Object> attributes = bc.getAttributes();
 				Version version = (Version) attributes.get("version");
 				Assert.assertEquals(0, version.compareTo(new Version(1, 1, 0)));
 				List<String> configs = (List<String>) attributes
@@ -87,9 +68,8 @@ public class CapabilityTest extends DefaultTestBundleControl {
 				Assert.assertTrue(configs.size() > 0);
 				foundDiscovery = true;
 			}
-			
 		}
-		
+
 		Assert.assertTrue(foundDiscovery);
 	}
 
@@ -97,45 +77,34 @@ public class CapabilityTest extends DefaultTestBundleControl {
 	 * 122.9.3 Distribution Provider Capability
 	 */
 	public void testDistributionProviderCapability() throws Exception {
-		ServiceReference<RemoteServiceAdmin> rsaRef = getContext()
-				.getServiceReference(
-				RemoteServiceAdmin.class);
-
-		Bundle rsaBundle = rsaRef.getBundle();
-		BundleWiring bw = rsaBundle.adapt(BundleWiring.class);
-
-		List<BundleCapability> capabilities = bw
-				.getCapabilities("osgi.remoteserviceadmin.distribution");
-
-		Assert.assertEquals(1, capabilities.size());
-		Map<String, Object> attributes = capabilities.get(0).getAttributes();
-		Version version = (Version) attributes.get("version");
-		Assert.assertEquals(0, version.compareTo(new Version(1, 1, 0)));
-		List<String> configs = (List<String>) attributes.get("configs");
-		Assert.assertNotNull(configs);
-		Assert.assertTrue(configs.size() > 0);
+		boolean foundDistributionProvider = false;
+		List<BundleCapability> capabilities = findCapabilities(getContext(),
+				"osgi.remoteserviceadmin.distribution");
+		if (capabilities.size() >= 1) {
+			for (BundleCapability bc : capabilities) {
+				Map<String, Object> attributes = bc.getAttributes();
+				Version version = (Version) attributes.get("version");
+				Assert.assertEquals(0, version.compareTo(new Version(1, 1, 0)));
+				List<String> configs = (List<String>) attributes.get("configs");
+				Assert.assertNotNull(configs);
+				Assert.assertTrue(configs.size() > 0);
+				foundDistributionProvider = true;
+			}
+		}
+		Assert.assertTrue(foundDistributionProvider);
 	}
 
 	/**
 	 * 122.9.4 Topology Manager Capability
 	 */
 	public void testTopologyCapability() throws Exception {
-		Collection<ServiceReference<EndpointEventListener>> listenerRefs = getContext()
-				.getServiceReferences(EndpointEventListener.class, null);
-
 		boolean foundTopologyManager = false;
 
-		for (ServiceReference<EndpointEventListener> eplRef : listenerRefs) {
-
-			Bundle eplBundle = eplRef.getBundle();
-			BundleWiring bw = eplBundle.adapt(BundleWiring.class);
-
-			List<BundleCapability> capabilities = bw
-					.getCapabilities("osgi.remoteserviceadmin.topology");
-			if (capabilities.size() == 1) {
-				// this bundle is the Topology Manager
-				Map<String, Object> attributes = capabilities.get(0)
-						.getAttributes();
+		List<BundleCapability> capabilities = findCapabilities(getContext(),
+				"osgi.remoteserviceadmin.topology");
+		if (capabilities.size() >= 1) {
+			for (BundleCapability bc : capabilities) {
+				Map<String, Object> attributes = bc.getAttributes();
 				Version version = (Version) attributes.get("version");
 				Assert.assertEquals(0, version.compareTo(new Version(1, 1, 0)));
 				List<String> configs = (List<String>) attributes.get("policy");
@@ -143,10 +112,28 @@ public class CapabilityTest extends DefaultTestBundleControl {
 				Assert.assertTrue(configs.size() > 0);
 				foundTopologyManager = true;
 			}
-
 		}
 
 		Assert.assertTrue(foundTopologyManager);
+	}
+
+	/**
+	 * Helper to find all provided capabilities of a certain namespace
+	 * */
+	private static List<BundleCapability> findCapabilities(
+			BundleContext bundleContext, String namespace) {
+		List<BundleCapability> capabilitiesRes = new LinkedList<BundleCapability>();
+
+		Bundle[] bundles = bundleContext.getBundles();
+		for (int i = 0; i < bundles.length; i++) {
+			Bundle bundle = bundles[i];
+			BundleWiring bw = bundle.adapt(BundleWiring.class);
+
+			List<BundleCapability> capabilities = bw.getCapabilities(namespace);
+			capabilitiesRes.addAll(capabilities);
+		}
+
+		return capabilitiesRes;
 	}
 
 }
