4584e1362429fd4c0fbda83e5b421ccd4ac8c8c4
Jan S. Rellermeyer <rellermeyer@us.ibm.com>
2014-08-28 15:25:55 -0500

    added the rest client factory interface.

---
+++ org.osgi.service.rest/src/org/osgi/service/rest/client/RestClientFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) OSGi Alliance (2013). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osgi.service.rest.client;
+import java.net.URI;
+
+
+/**
+ * Factory to construct new REST client instances. Each instance is specific to
+ * a REST service endpoint.
+ * 
+ * Implementations can choose to add additional creation methods, e.g., where
+ * additional arguments are needed for request signing, etc.
+ * 
+ * In OSGi environments this factory can be registered as a service.
+ * 
+ * @author $Id$
+ *
+ */
+public interface RestClientFactory {
+
+	/**
+	 * create a new REST client instance.
+	 * 
+	 * @param uri the URI to the REST service endpoint.
+	 * @return a new REST client instance.
+	 */
+	RestClient createRestClient(URI uri);
+
+}
