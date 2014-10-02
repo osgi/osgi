ebedaa26b0634065c7fff5f68a8a4b1e9ade827d
Evgeni Grigorov <e.grigorov@prosyst.com>
2014-10-02 18:01:06 +0300

    Add TestStep interface to the test support project.

---
+++ org.osgi.test.support/src/org/osgi/test/support/step/TestStep.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) OSGi Alliance (2013). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osgi.test.support.step;
+
+/**
+ * The test step interface is used by the OSGi test cases for the manual test
+ * steps execution. The test case is allowed to execute manual steps with the
+ * step identifier and human readable message. If the step requires some input,
+ * an execution result can be provided. The implementation of this interface
+ * must be registered in the OSGi service registry.
+ */
+public interface TestStep {
+
+	/**
+	 * Executes the command with the given parameters. Executes the test step
+	 * with the given identifier and human readable message.
+	 * <p>
+	 * For example, it can be called in this way:
+	 * {@code testStep.execute("step.1", "Add a new device to the network");}
+	 * 
+	 * @param stepId The step identifier.
+	 * @param userPrompt The step human readable message.
+	 * 
+	 * @return The execution result.
+	 */
+	public String execute(String stepId, String userPrompt);
+
+}
