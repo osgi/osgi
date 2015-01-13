c730d38bd6381eddecda12f0106396b806dc9f5d
Evgeni Grigorov <e.grigorov@prosyst.com>
2015-01-13 10:26:23 +0200

    Signature TC is introduced.

---
+++ org.osgi.test.cases.dal/src/org/osgi/test/cases/dal/SignatureTestCase.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 ProSyst Software GmbH. All Rights Reserved.
+ *
+ * This CODE is owned by ProSyst Software GmbH,
+ * and is being distributed to OSGi PARTICIPANTS as MATERIALS
+ * under the terms of section 1 of the OSGi Alliance Inc. Intellectual Property Rights Policy,
+ * Amended and Restated as of May 23, 2011.
+ */
+
+package org.osgi.test.cases.dal;
+
+/**
+ * Validates the signature of all specified methods.
+ */
+public class SignatureTestCase extends
+		org.osgi.test.support.signature.SignatureTestCase {
+	// all test methods are in the parent class
+}
