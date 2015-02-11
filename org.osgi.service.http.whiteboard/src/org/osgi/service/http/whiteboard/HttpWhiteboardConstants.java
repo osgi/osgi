7b4d0f3863668fbdc36c19eb1149659c390733cb
Carsten Ziegeler <cziegele@adobe.com>
2015-02-11 17:45:44 +0100

    Clarification when failure DTOs are created (bug #2762)

--- org.osgi.service.http.whiteboard/src/org/osgi/service/http/whiteboard/HttpWhiteboardConstants.java
+++ org.osgi.service.http.whiteboard/src/org/osgi/service/http/whiteboard/HttpWhiteboardConstants.java
@@ -350,12 +350,16 @@ public final class HttpWhiteboardConstants {
 
 	/**
 	 * Service property to mark a Listener service as a Whiteboard service.
-	 * Listener services with this property set to {@code Boolean.TRUE} will be
-	 * treated as Whiteboard services opting in to being handled by the Http
-	 * Whiteboard implementation.
+	 * Listener services with this property set to the string value "true" will
+	 * be treated as Whiteboard services opting in to being handled by the Http
+	 * Whiteboard implementation. If the value "false" is specified, the service
+	 * is opting out and this case is treated exactly the same as if this
+	 * property is missing. If an invalid value is specified this is treated as
+	 * a failure.
 	 * 
 	 * <p>
-	 * The value of this service property must be of type {@code Boolean}.
+	 * The value of this service property must be of type {@code String}. Valid
+	 * values are "true" and "false" ignoring case.
 	 */
 	public static final String	HTTP_WHITEBOARD_LISTENER					= "osgi.http.whiteboard.listener";
 
