a5d1e87c725a9e9f27bedb112f77233ed2c8aaf3
Thomas Watson <tjwatson@us.ibm.com>
2015-05-19 08:20:19 -0500

    Fix reference to HttpContext

--- org.osgi.service.http.whiteboard/src/org/osgi/service/http/whiteboard/HttpWhiteboardConstants.java
+++ org.osgi.service.http.whiteboard/src/org/osgi/service/http/whiteboard/HttpWhiteboardConstants.java
@@ -149,10 +149,10 @@ public final class HttpWhiteboardConstants {
 	 * <p>
 	 * Servlet names should be unique among all servlet services associated with
 	 * a single {@link ServletContextHelper}. If multiple servlet services
-	 * associated with the same HttpContext have the same servlet name, the one
-	 * with the highest service ranking is referenced by a servlet filter with
-	 * the name specified through the {@link #HTTP_WHITEBOARD_FILTER_SERVLET}
-	 * service property.
+	 * associated with the same ServletContextHelper have the same servlet name,
+	 * the one with the highest service ranking is referenced by a servlet
+	 * filter with the name specified through the
+	 * {@link #HTTP_WHITEBOARD_FILTER_SERVLET} service property.
 	 * 
 	 * <p>
 	 * The value of this service property must be of type {@code String}.
