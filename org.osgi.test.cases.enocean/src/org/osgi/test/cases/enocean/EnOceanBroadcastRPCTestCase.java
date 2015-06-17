243f95f1d17ce69087a3e84dbf44ac33643d7161
Cyrille Bareau <cyrille.bareau@orange.com>
2015-06-17 09:53:09 +0200

    EnOcean TestCase. Added javadoc.

--- org.osgi.test.cases.enocean/src/org/osgi/test/cases/enocean/EnOceanBroadcastRPCTestCase.java
+++ org.osgi.test.cases.enocean/src/org/osgi/test/cases/enocean/EnOceanBroadcastRPCTestCase.java
@@ -19,61 +19,36 @@ package org.osgi.test.cases.enocean;
 import java.util.Hashtable;
 import java.util.Map;
 
-import org.osgi.framework.ServiceReference;
-import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.enocean.EnOceanEvent;
-import org.osgi.service.enocean.EnOceanHost;
 import org.osgi.service.enocean.EnOceanRPC;
 import org.osgi.service.event.Event;
 import org.osgi.test.cases.enocean.messages.MessageSYS_EX;
 import org.osgi.test.cases.enocean.rpc.QueryFunction;
 import org.osgi.test.cases.enocean.serial.EspPacket;
-import org.osgi.test.cases.enocean.utils.Fixtures;
-import org.osgi.test.cases.enocean.utils.ServiceListener;
 import org.osgi.test.cases.enocean.utils.Utils;
 
 /**
- * EnOceanBasicTestCase contains the following tests:
+ * EnOceanBroadcastRPCTestCase contains the following tests:
  * 
- * - testInterfaceExceptions, tests that common errors cases are properly
- * handled (i.e. that the relevant exceptions are thrown).
- * 
- * - testRPC, tests RPC sending and receiving, i.e. insert an EnOcean device,
- * and test a RPC invocation on this device.
- * 
- * - testUseOfDescriptions, tests that a properly set profile ID in a raw
- * EnOceanMessage is enough to extract all the needed information, provided the
- * necessary descriptions are known.
+ * - testExportBroadcastRPC, tests broadcast RPC exportation: creates a RPC,
+ * sends it as a TOPIC_RPC_BROADCAST event, checks that the message has been
+ * sent, received by the base driver, and checked through the step service, and
+ * that the payload of the received event, processed as a SYS_EX message, is
+ * equal to the payload of the original RPC.
  * 
  * @author $Id$
  */
 public class EnOceanBroadcastRPCTestCase extends AbstractEnOceanTestCase {
 
     /**
-     * Tests device exportation.
+     * Tests broadcast RPC exportation.
      * 
      * @throws Exception
      */
     public void testExportBroadcastRPC() throws Exception {
 
-	ServiceRegistration sReg = Fixtures.registerDevice(getContext());
-
-	/* Wait for the proper and full registration */
-	String lastServiceEvent = devices.waitForService();
-	assertNotNull("Timeout reached.", lastServiceEvent);
-	assertEquals("did not have service addition", ServiceListener.SERVICE_ADDED,
-		lastServiceEvent);
-
-	/* Get CHIP_ID attributed by the driver from the given service PID. */
-	ServiceReference hostRef = getContext().getServiceReference(EnOceanHost.class.getName());
-	EnOceanHost defaultHost = (EnOceanHost) getContext().getService(hostRef);
-	int dynamicChipId = defaultHost.getChipId(Fixtures.DEVICE_PID);
-	assertTrue("The created CHIP_ID for an exported device was not created",
-		dynamicChipId != -1);
-
 	/*
-	 * Now that we have gotten the device registered and all, we are able to
-	 * try and make it send data, via EventAdmin broadcast.
+	 * Create a RPC, and broadcast it via EventAdmin.
 	 */
 	EnOceanRPC rpc = new QueryFunction();
 
@@ -81,6 +56,7 @@ public class EnOceanBroadcastRPCTestCase extends AbstractEnOceanTestCase {
 	properties.put(EnOceanEvent.PROPERTY_EXPORTED, "1");
 	properties.put(EnOceanEvent.PROPERTY_RPC, rpc);
 	Event evt = new Event(EnOceanEvent.TOPIC_RPC_BROADCAST, properties);
+	tlog("Send RPC as an event");
 	eventAdmin.sendEvent(evt);
 
 	tlog("get any new data from testStepService.");
@@ -90,31 +66,21 @@ public class EnOceanBroadcastRPCTestCase extends AbstractEnOceanTestCase {
 			GET_EVENT,
 			"An event has been sent by the test to the base driver. Just hit [enter] to continue, and get this event.");
 	assertNotNull(
-		"The base driver didn't received the expected event (i.e., here, it didn't received any event at all).",
+		"The base driver didn't receive the expected event (i.e., here, it didn't received any event at all).",
 		executionResult);
 
 	byte[] data = Utils.hex2Bytes(executionResult);
 	tlog("executionResult: " + executionResult);
 
-	try {
-	    MessageSYS_EX msg = new MessageSYS_EX(new EspPacket(data).getData().getBytes());
-	    tlog("msg data: " + Utils.bytesToHex(msg.getBytes()));
+	MessageSYS_EX msg = new MessageSYS_EX(new EspPacket(data).getData().getBytes());
+	tlog("msg SYS_EX data: " + Utils.bytesToHex(msg.getBytes()));
 
-	    tlog("Utils.bytesToHex(rpc.getPayload()): " + Utils.bytesToHex(rpc.getPayload())
-		    + ", Utils.bytesToHex(msg.getRPC().getPayload()): "
-		    + Utils.bytesToHex(msg.getRPC().getPayload()));
+	tlog("Utils.bytesToHex(rpc.getPayload()): " + Utils.bytesToHex(rpc.getPayload())
+		+ ", Utils.bytesToHex(msg.getRPC().getPayload()): "
+		+ Utils.bytesToHex(msg.getRPC().getPayload()));
 
-	    assertEquals("EnOcean radio message and forged message mismatch",
-		    Utils.bytesToHex(rpc.getPayload()), Utils.bytesToHex(msg.getRPC().getPayload()));
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    tlog("Error: " + e.getMessage());
-	}
-
-	// Needed not to mess with further tests
-	sReg.unregister();
-	tlog("Unget service with service reference: " + hostRef);
-	getContext().ungetService(hostRef);
+	assertEquals("EnOcean radio message and forged message mismatch",
+		Utils.bytesToHex(rpc.getPayload()), Utils.bytesToHex(msg.getRPC().getPayload()));
     }
 
 }
