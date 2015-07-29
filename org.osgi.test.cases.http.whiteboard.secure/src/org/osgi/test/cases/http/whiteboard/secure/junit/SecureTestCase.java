1f2967a81df0474fa0d19267f8beec7395443c02
Raymond Auge <raymond.auge@liferay.com>
2015-07-28 23:39:36 -0400

    [http whiteboard CT] security tests

--- org.osgi.test.cases.http.whiteboard.secure/src/org/osgi/test/cases/http/whiteboard/secure/junit/SecureTestCase.java
+++ org.osgi.test.cases.http.whiteboard.secure/src/org/osgi/test/cases/http/whiteboard/secure/junit/SecureTestCase.java
@@ -1,5 +1,20 @@
 package org.osgi.test.cases.http.whiteboard.secure.junit;
 
+import java.io.FilePermission;
+import java.security.AccessControlContext;
+import java.security.AccessControlException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import org.osgi.framework.AdminPermission;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServicePermission;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.runtime.HttpServiceRuntime;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
 public class SecureTestCase extends BaseHttpWhiteboardTestCase {
 
 	@Override
@@ -8,7 +23,139 @@ public class SecureTestCase extends BaseHttpWhiteboardTestCase {
 		assertNotNull(System.getSecurityManager());
 	}
 
-	public void testReplaceMe() throws Exception {
-		fail("replace me with real tests; this is just a skeleton project");
+	/*
+	 * The Http Whiteboard implementation must be granted
+	 * ServicePermission[interfaceName, GET] to retrieve the Http Whiteboard
+	 * services from the service registry.
+	 *
+	 * Since a Resource may be associated with any service, the RI needs GET on
+	 * interfaces '*'.
+	 */
+	public void testImplHasGETServicePermission() throws Exception {
+		BundleContext context = getContext();
+
+		ServiceReference<HttpServiceRuntime> serviceReference =
+				context.getServiceReference(HttpServiceRuntime.class);
+
+		Bundle bundle = serviceReference.getBundle();
+
+		AccessControlContext acc = bundle.adapt(AccessControlContext.class);
+
+		try {
+			acc.checkPermission(new ServicePermission("*", ServicePermission.GET));
+		} catch (AccessControlException ace) {
+			fail();
+		}
 	}
+
+	/*
+	 * Bundles that need to introspect the state of the Http runtime will need
+	 * ServicePermission[org.osgi.service.http.runtime.HttpServiceRuntime, GET]
+	 * to obtain the HttpServiceRuntime service and access the DTO types.
+	 */
+	public void testHttpServiceRuntimeClientHasGETServicePermission_tb1() throws Exception {
+		Bundle bundle = install("tb1.jar");
+
+		try {
+			bundle.start();
+
+			AccessControlContext acc = bundle.adapt(AccessControlContext.class);
+
+			acc.checkPermission(new ServicePermission(HttpServiceRuntime.class.getName(), ServicePermission.GET));
+		} catch (AccessControlException ace) {
+			fail();
+		} finally {
+			bundle.uninstall();
+		}
+	}
+
+	/*
+	 * Bundles that need to introspect the state of the Http runtime will need
+	 * ServicePermission[org.osgi.service.http.runtime.HttpServiceRuntime, GET]
+	 * to obtain the HttpServiceRuntime service and access the DTO types.
+	 */
+	public void testHttpServiceRuntimeClientHasNoGETServicePermission_tb2() throws Exception {
+		Bundle bundle = install("tb2.jar");
+
+		try {
+			bundle.start();
+
+			AccessControlContext acc = bundle.adapt(AccessControlContext.class);
+
+			acc.checkPermission(new ServicePermission(HttpServiceRuntime.class.getName(), ServicePermission.GET));
+
+			fail();
+		} catch (AccessControlException ace) {
+		} finally {
+			bundle.uninstall();
+		}
+	}
+
+	/*
+	 * The Http Whiteboard implementation must be granted
+	 * AdminPermission[*,RESOURCE] so that bundles may use the default
+	 * ServletContextHelper implementation.
+	 */
+	public void testImplHasRESOURCEAdminPermission() throws Exception {
+		BundleContext context = getContext();
+
+		ServiceReference<HttpServiceRuntime> serviceReference =
+				context.getServiceReference(HttpServiceRuntime.class);
+
+		Bundle bundle = serviceReference.getBundle();
+
+		AccessControlContext acc = bundle.adapt(AccessControlContext.class);
+
+		try {
+			acc.checkPermission(new AdminPermission("*", AdminPermission.RESOURCE));
+		} catch (AccessControlException ace) {
+			fail();
+		}
+	}
+
+	/*
+	 * The Http Whiteboard implementation must capture the AccessControlContext
+	 * object of the bundle registering a ServletContextHelper service, and then
+	 * use the captured AccessControlContext object when accessing resources
+	 * returned by the ServletContextHelper service.
+	 */
+	public void testImplCapturesAccessControlContext() throws Exception {
+		BundleContext context = getContext();
+
+		ServiceReference<HttpServiceRuntime> serviceReference =
+				context.getServiceReference(HttpServiceRuntime.class);
+
+		Bundle bundle = serviceReference.getBundle();
+
+		AccessControlContext acc = bundle.adapt(AccessControlContext.class);
+
+		try {
+			acc.checkPermission(new FilePermission("/etc/passwd", "read"));
+		} catch (AccessControlException ace) {
+			fail();
+		}
+
+		bundle = install("tb1.jar");
+
+		try {
+			bundle.start();
+
+			Dictionary<String, Object> properties = new Hashtable<String, Object>();
+			properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PATTERN, "/*");
+			properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PREFIX, "/");
+			serviceRegistrations.add(context.registerService(Object.class, new Object(), properties));
+
+			Map<String, List<String>> result = request("/etc/passwd", null);
+
+			assertEquals("500", result.get("responseCode").get(0));
+
+			String responseBody = result.get("responseBody").get(0);
+
+			assertTrue(responseBody.contains("java.security.AccessControlException"));
+			assertTrue(responseBody.contains("access denied (\"java.io.FilePermission\" \"/etc/passwd\" \"read\")"));
+		} finally {
+			bundle.uninstall();
+		}
+	}
+
 }
---
+++ org.osgi.test.cases.http.whiteboard.secure/src/org/osgi/test/cases/http/whiteboard/secure/tb2/HttpWhiteboardSecurityTestBundle2.java
@@ -0,0 +1,14 @@
+package org.osgi.test.cases.http.whiteboard.secure.tb2;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HttpWhiteboardSecurityTestBundle2 implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+	}
+
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
