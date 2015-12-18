de6f5208cda39f6f0a90a134d710e786d523d124
BJ Hargrave <hargrave@us.ibm.com>
2015-12-18 15:58:05 -0500

    Update copyright notice for files modified in 2015

--- org.osgi.test.cases.event.secure/src/org/osgi/test/cases/event/secure/tb1/Activator.java
+++ org.osgi.test.cases.event.secure/src/org/osgi/test/cases/event/secure/tb1/Activator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2004, 2013). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2004, 2015). All Rights Reserved.
  * 
  * Implementation of certain elements of the OSGi Specification may be subject
  * to third party intellectual property rights, including without limitation,
