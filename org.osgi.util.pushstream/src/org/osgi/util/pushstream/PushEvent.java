89adb15419fbcf462a60e46c04fe92902268860b
BJ Hargrave <hargrave@us.ibm.com>
2016-01-29 11:31:02 -0500

    pushstream: Make default constructor package private
    Also make the field final and improve the javadoc.

--- org.osgi.util.pushstream/src/org/osgi/util/pushstream/PushEvent.java
+++ org.osgi.util.pushstream/src/org/osgi/util/pushstream/PushEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2015). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2015, 2016). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,23 +19,25 @@ package org.osgi.util.pushstream;
 import static org.osgi.util.pushstream.PushEvent.EventType.*;
 
 /**
- * A {@link PushEvent} is an immutable object that is transferred through a
+ * A PushEvent is an immutable object that is transferred through a
  * communication channel to push information to a downstream consumer. The event
- * has three different subtypes:
+ * has three different types:
  * <ul>
- * <li>Data – Provides access to a typed data element in the stream
- * <li>Close – The stream is closed. After receiving this event, no more events
- * will follow and and the consumer must assume the stream is dead.
- * <li>Error – The upstream ran into an irrecoverable problem and is sending the
- * reason downstream. No more events will follow after this event
+ * <li>{@link #DATA} – Provides access to a typed data element in the stream.
+ * <li>{@link #CLOSE} – The stream is closed. After receiving this event, no
+ * more events will follow.
+ * <li>{@link #ERROR} – The stream ran into an unrecoverable problem and is
+ * sending the reason downstream. The stream is closed and no more events will
+ * follow after this event.
  * </ul>
  *
- * @param <T> The associated Data type
+ * @param <T> The payload type of the event.
+ * @Immutable
  */
 public abstract class PushEvent<T> {
 
 	/**
-	 * The type of the event
+	 * The type of a {@link PushEvent}.
 	 */
 	public static enum EventType {
 		/**
@@ -54,61 +56,66 @@ public abstract class PushEvent<T> {
 	}
 
 	/**
-	 * Get the type of this event
+	 * Package private default constructor.
+	 */
+	PushEvent() {}
+
+	/**
+	 * Get the type of this event.
 	 * 
-	 * @return the type of the event
+	 * @return The type of this event.
 	 */
 	public abstract EventType getType();
 
 	/**
-	 * Return the data for this event or throw an exception
+	 * Return the data for this event.
 	 * 
-	 * @return the data payload
-	 * @throws IllegalStateException if the event is not an
-	 *         {@link EventType#DATA} event
+	 * @return The data payload.
+	 * @throws IllegalStateException if this event is not a {@link #DATA} event.
 	 */
 	public T getData() throws IllegalStateException {
 		throw new IllegalStateException(
-				"Not a data event, the event type is " + getType());
+				"Not a DATA event, the event type is " + getType());
 	}
 
 	/**
-	 * Return the error that terminated the stream
+	 * Return the error that terminated the stream.
 	 * 
-	 * @return the exception
-	 * 
-	 * @throws IllegalStateException if the event is not an
-	 *         {@link EventType#ERROR} event
+	 * @return The error that terminated the stream.
+	 * @throws IllegalStateException if this event is not an {@link #ERROR}
+	 *             event.
 	 */
 	public Exception getFailure() throws IllegalStateException {
 		throw new IllegalStateException(
-				"Not a failure event, the event type is " + getType());
+				"Not an ERROR event, the event type is " + getType());
 	}
 
 	/**
 	 * Answer if no more events will follow after this event.
 	 * 
-	 * @return false if this is a data event, otherwise true.
+	 * @return {@code false} if this is a data event, otherwise {@code true}.
 	 */
 	public boolean isTerminal() {
 		return true;
 	}
 
 	/**
-	 * Create a new data event
+	 * Create a new data event.
 	 * 
-	 * @param payload The payload
-	 * @return A new data event wrapping the supplied payload
+	 * @param <T> The payload type.
+	 * @param payload The payload.
+	 * @return A new data event wrapping the specified payload.
 	 */
 	public static <T> PushEvent<T> data(T payload) {
 		return new DataEvent<T>(payload);
 	}
 
 	/**
-	 * Create a new error event
+	 * Create a new error event.
 	 * 
-	 * @param e The error
-	 * @return a new error event with the given error
+	 * @param <T> The payload type.
+	 * @param e The error.
+	 * @return A new error event with the specified error.
 	 */
 	public static <T> PushEvent<T> error(Exception e) {
 		return new ErrorEvent<T>(e);
@@ -117,7 +124,8 @@ public abstract class PushEvent<T> {
 	/**
 	 * Create a new close event.
 	 * 
-	 * @return A close event
+	 * @param <T> The payload type.
+	 * @return A new close event.
 	 */
 	public static <T> PushEvent<T> close() {
 		return new CloseEvent<T>();
@@ -129,18 +137,18 @@ public abstract class PushEvent<T> {
 	 * therefore allows you to forward the close/error event downstream without
 	 * creating anew event.
 	 * 
-	 * @return the current error or close event mapped to a new type
-	 * @throws IllegalStateException if the event is an {@link EventType#DATA}
-	 *         event
+	 * @param <X> The new payload type.
+	 * @return The current error or close event mapped to a new payload type.
+	 * @throws IllegalStateException if the event is a {@link #DATA} event.
 	 */
-	@SuppressWarnings("unchecked")
 	public <X> PushEvent<X> nodata() throws IllegalStateException {
-				return (PushEvent<X>) this;
+		@SuppressWarnings("unchecked")
+		PushEvent<X> result = (PushEvent<X>) this;
+		return result;
 	}
 
 	static final class DataEvent<T> extends PushEvent<T> {
-
-		private T data;
+		private final T data;
 
 		DataEvent(T data) {
 			this.data = data;
@@ -163,14 +171,12 @@ public abstract class PushEvent<T> {
 
 		@Override
 		public <X> PushEvent<X> nodata() throws IllegalStateException {
-			throw new IllegalStateException("This event is a Data event");
+			throw new IllegalStateException("This event is a DATA event");
 		}
-
 	}
 
 	static final class ErrorEvent<T> extends PushEvent<T> {
-
-		private Exception error;
+		private final Exception error;
 
 		ErrorEvent(Exception error) {
 			this.error = error;
@@ -185,15 +191,12 @@ public abstract class PushEvent<T> {
 		public EventType getType() {
 			return ERROR;
 		}
-
 	}
 
 	static final class CloseEvent<T> extends PushEvent<T> {
-
 		@Override
 		public EventType getType() {
 			return CLOSE;
 		}
-
 	}
 }
