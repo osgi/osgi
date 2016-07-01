29481f7325128fec674dfd8163c15041cb1c8bd9
BJ Hargrave <hargrave@us.ibm.com>
2016-07-01 10:53:09 +0200

    function: Add Callback functional interface
    This is a "Runnable" whose run method throws Exception
    
    bump package version to 1.1

--- org.osgi.util.function/src/org/osgi/util/function/package-info.java
+++ org.osgi.util.function/src/org/osgi/util/function/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2014). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2014, 2016). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,25 +15,23 @@
  */
 
 /**
- * Function Package Version 1.0.
- * 
+ * Function Package Version 1.1.
  * <p>
  * Bundles wishing to use this package must list the package in the
  * Import-Package header of the bundle's manifest.
- * 
  * <p>
  * Example import for consumers using the API in this package:
  * <p>
- * {@code  Import-Package: org.osgi.util.function; version="[1.0,2.0)"}
+ * {@code  Import-Package: org.osgi.util.function; version="[1.1,2.0)"}
  * <p>
  * Example import for providers implementing the API in this package:
  * <p>
- * {@code  Import-Package: org.osgi.util.function; version="[1.0,1.1)"}
+ * {@code  Import-Package: org.osgi.util.function; version="[1.1,1.2)"}
  * 
  * @author $Id$
  */
 
-@Version("1.0")
+@Version("1.1")
 package org.osgi.util.function;
 
 import org.osgi.annotation.versioning.Version;
