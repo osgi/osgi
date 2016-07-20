836fd833d954d84372db6a08bc1674a1942d948c
Nicola Portinaro <nicola.portinaro@telecomitalia.it>
2016-07-21 00:44:31 +0200

    [RFC192] updated javadocs

--- org.osgi.service.zigbee/src/org/osgi/service/zigbee/ZCLCluster.java
+++ org.osgi.service.zigbee/src/org/osgi/service/zigbee/ZCLCluster.java
@@ -24,6 +24,11 @@ import java.util.Map;
  * commands to the cluster and other methods that wrap most of the ZCL general
  * commands.
  * 
+ * <p>
+ * Any asynchronous method defined in this interface returns back its result by
+ * calling <strong>at most once</strong>, the {@code onSuccess} method OR the
+ * {@code onFailure} method on the passed handler.
+ * 
  * @author $Id$
  */
 public interface ZCLCluster {
@@ -54,28 +59,30 @@ public interface ZCLCluster {
 
 	/**
 	 * Get the cluster {@link ZCLAttribute} identifying corresponding attribute
-	 * that matches the given attributeId.
+	 * that matches the given attributeId. Use, instead the
+	 * {@link ZCLCluster#getAttribute(int, int, ZigBeeHandler)} to retrieve
+	 * manufacturer specific attributes.
 	 * 
-	 * @param attributeId an Attribute identifier
+	 * @param attributeId the attribute identifier
 	 * @param handler the response handler. The
 	 *        {@link ZigBeeHandler#onSuccess(Object)} will be invoked with the
 	 *        proper {@link ZCLAttribute}
 	 * 
-	 * @see ZCLCluster#getAttribute(int, int, ZigBeeHandler) To get Manufacturer
-	 *      specific attribute use ZCLCluster#getAttribute(int, int,
-	 *      ZigBeeHandler)
+	 * 
 	 */
 	void getAttribute(int attributeId, ZigBeeHandler handler);
 
 	/**
-	 * Get a manufacturer specific cluster {@link ZCLAttribute}. If the
-	 * {@code code} parameter is -1 it retrieves a non-manufacturer specific
-	 * attribute.
+	 * Retrieve a {@link ZCLAttribute} object for a manufacturer specific
+	 * attribute. If the {@code code} parameter is -1 it behaves like the
+	 * {@link ZCLCluster#getAttribute(int, ZigBeeHandler)} and retrieves the
+	 * non-manufacturer specific attribute corresponding to the passed
+	 * {@code attributeId}.
 	 * 
-	 * @param attributeId an Attribute identifier
-	 * @param code the int representing the Manufacturer code for getting the
-	 *        vendor specific attribute, use -1 if looking for a standard
-	 *        attribute
+	 * @param attributeId the ZCL attribute identifier
+	 * @param code the manufacturer code of the attribute to be retrieved. If -1
+	 *        is used, the method behaves exactly like
+	 *        {@link ZCLCluster#getAttribute(int, ZigBeeHandler)}
 	 * @param handler the response handler. The
 	 *        {@link ZigBeeHandler#onSuccess(Object)} will be invoked with the
 	 *        requested {@link ZCLAttribute}.
@@ -83,26 +90,33 @@ public interface ZCLCluster {
 	void getAttribute(int attributeId, int code, ZigBeeHandler handler);
 
 	/**
-	 * Get an array of {@link ZCLAttribute} representing all this Cluster's
-	 * Attributes. This method returns only standard attributes
+	 * Get an array of {@link ZCLAttribute} objects representing all this
+	 * cluster's attributes.
+	 * 
+	 * <p>
+	 * This method returns only standard attributes. To retrieve manufacturer
+	 * specific attributes use the method
+	 * {@link ZCLCluster#getAttributes(int, ZigBeeHandler)}
 	 * 
 	 * @param handler the response handler. The
 	 *        {@link ZigBeeHandler#onSuccess(Object)} will be invoked an array
 	 *        of {@link ZCLAttribute}
-	 * 
-	 * @see ZCLCluster#getAttributes(int, ZigBeeHandler) To get Manufacturer
-	 *      specific attribute use ZCLCluster#getAttributes(int, ZigBeeHandler)
 	 */
 	void getAttributes(ZigBeeHandler handler);
 
 	/**
-	 * Get an array of {@link ZCLAttribute} representing all this Cluster's
-	 * Attributes. This method returns only standard attributes when using -1 as
-	 * code or vendor specific attribute when invoked with the proper code
+	 * Get an array of {@link ZCLAttribute} objects representing all the
+	 * specific manufacturer attributes available on the cluster.
+	 * 
+	 * <p>
+	 * This method behaves like the
+	 * {@link ZCLCluster#getAttributes(ZigBeeHandler)} method if the passed
+	 * {@code} value is -1.
 	 * 
 	 * @param code the int representing the Manufacturer code for getting the
 	 *        vendor specific attribute, use -1 if looking for standard
-	 *        attribute
+	 *        attributes.
+	 * 
 	 * @param handler the response handler. The
 	 *        {@link ZigBeeHandler#onSuccess(Object)} will be invoked an array
 	 *        of {@link ZCLAttribute}
@@ -114,23 +128,26 @@ public interface ZCLCluster {
 	 * 
 	 * <p>
 	 * As described in "2.4.1.3 Effect on Receipt" chapter of the ZCL, a
-	 * "read attribute" can have the following status: SUCCESS, or
-	 * UNSUPPORTED_ATTRIBUTE (see {@link ZCLException}).
+	 * "read attribute" can have the following status:
+	 * {@link ZCLException#SUCCESS}, or
+	 * {@link ZCLException#UNSUPPORTED_ATTRIBUTE} (see {@link ZCLException}).
 	 * 
 	 * <p>
-	 * The response object given to the handler is a Map. For each Map entry,
-	 * the key is the attribute identifier of Integer type and the value is the
-	 * associated attribute value in the corresponding Java wrapper type (or
-	 * null if an UNSUPPORTED_ATTRIBUTE occurred or in case of an invalid
-	 * value).
+	 * The response object passed to the handler onSuccess method is a Map. For
+	 * each Map entry, the key is the attribute identifier of Integer type and
+	 * the value is the associated attribute value in the corresponding Java
+	 * wrapper type (or null if an UNSUPPORTED_ATTRIBUTE occurred or in case of
+	 * an invalid value).
 	 * 
 	 * <p>
 	 * <b>NOTE</b> Considering the ZigBee Specification all the attributes must
-	 * be standard attributes or belong to the same Manufacturer otherwise
+	 * be standard attributes or belong to the same manufacturer code, otherwise
 	 * {@link IllegalArgumentException} will be thrown
 	 * 
-	 * @param attributes An array of ZCLAttributeInfo
-	 * @param handler the response handler
+	 * @param attributes An array of {@link ZCLAttributeInfo}
+	 * @param handler the response handler. If the operation is successful the
+	 *        {@code onSuccess} method of the handler is called with a Map
+	 *        object containing the requested attributes.
 	 * @throws NullPointerException the attribute array cannot be null
 	 * 
 	 * @throws IllegalArgumentException if some of {@link ZCLAttributeInfo} are
@@ -189,7 +206,9 @@ public interface ZCLCluster {
 	 * The response object given to the handler is an array containing the
 	 * commandIds. Each commandId is of Integer type.
 	 * 
-	 * @param handler the response handler
+	 * @param handler the response handler. If the operation is successful the
+	 *        handler {@code onSuccess} method is called with an array of
+	 *        {@code int} of the command identifiers supported by the cluster.
 	 */
 	void getCommandIds(ZigBeeHandler handler);
 
@@ -201,8 +220,10 @@ public interface ZCLCluster {
 	 * appropriate message on the network, the base driver must generate a
 	 * source endpoint. The latter must not correspond to any exported endpoint.
 	 * 
-	 * @param frame a command frame sequence.
-	 * @param handler The handler that manages the command response.
+	 * @param frame The frame containing the command to issue.
+	 * @param handler The handler that manages the command response. The
+	 *        response {@link ZCLFrame} is passed to the {@code onSuccess}
+	 *        method of the handler. The handler is called only once.
 	 */
 	void invoke(ZCLFrame frame, ZCLCommandHandler handler);
 
@@ -213,7 +234,7 @@ public interface ZCLCluster {
 	 * differently interpreted if they come from the oven or from the intrusion
 	 * alert system.
 	 * 
-	 * @param frame a command frame sequence.
+	 * @param frame The frame containing the command to issue.
 	 * @param handler The handler that manages the command response.
 	 * @param exportedServicePID : the source endpoint of the command request.
 	 *        In targeted situations, the source endpoint is the valid service
