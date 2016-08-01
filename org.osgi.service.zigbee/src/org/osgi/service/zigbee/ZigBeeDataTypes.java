49987abb1b8a920cd4d1b34bf366b6961fff0081
Nicola Portinaro <nicola.portinaro@telecomitalia.it>
2016-08-01 10:23:33 +0200

    zigbee: added <p> html tag

--- org.osgi.service.zigbee/src/org/osgi/service/zigbee/ZigBeeDataTypes.java
+++ org.osgi.service.zigbee/src/org/osgi/service/zigbee/ZigBeeDataTypes.java
@@ -34,7 +34,6 @@ package org.osgi.service.zigbee;
  * 
  * 
  * <p>
- * 
  * Related documentation:
  * [1] ZigBee Cluster Library specification, Document 075123r04ZB, May 29,
  * 2012.
@@ -47,6 +46,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.1 No Data Type
 	 * 
+	 * <p>
 	 * The no data type is a special type to represent an attribute with no
 	 * associated data.
 	 */
@@ -55,6 +55,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.2 General Data (8, 16, 24, 32, 40, 48, 56 and 64-bit)
 	 * 
+	 * <p>
 	 * This type has no rules about its use, and may be used when a data element
 	 * is needed but its use does not conform to any of the standard types.
 	 */
@@ -77,6 +78,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.3 Boolean
 	 * 
+	 * <p>
 	 * The Boolean type represents a logical value, either FALSE (0x00) or TRUE
 	 * (0x01). The value 0xff represents an invalid value of this type. All
 	 * other values of this type are forbidden.
@@ -86,6 +88,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.4 Bitmap (8, 16, 24, 32, 40, 48, 56 and 64-bit)
 	 * 
+	 * <p>
 	 * The Bitmap type holds 8, 16, 24, 32, 40, 48, 56 or 64 logical values, one
 	 * per bit, depending on its length. There is no value that represents an
 	 * invalid value of this type.
@@ -109,6 +112,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.5 Unsigned Integer (8, 16, 24, 32, 40, 48, 56 and 64-bit)
 	 * 
+	 * <p>
 	 * This type represents an unsigned integer with a decimal range of 0 to
 	 * 2^8-1, 0 to 2^16-1, 0 to 2^24-1, 0 to 2^32-1, 0 to 2^40-1, 0 to 2^48-1, 0
 	 * to 2^56-1, or 0 to 2^64-1, depending on its length. The values that
@@ -135,6 +139,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.6 Signed Integer (8, 16, 24, 32, 40, 48, 56 and 64-bit)
 	 * 
+	 * <p>
 	 * This type represents a signed integer with a decimal range of -(2^7-1) to
 	 * 2^7-1, - (2^15-1) to 2^15-1, -(2^23-1) to 2^23-1, -(2^31-1) to 2^31-1,
 	 * -(2^39-1) to 2^39-1, -(2^47-1) to 2^47-1, -(2^55-1) to 2^55-1, or
@@ -197,6 +202,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.10 Double Precision
 	 * 
+	 * <p>
 	 * The format of the double precision data type is based on the IEEE 754
 	 * standard for binary floating-point arithmetic. This number format should
 	 * be used very sparingly, when absolutely necessary, keeping in mind the
@@ -209,6 +215,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.11 Octet String
 	 * 
+	 * <p>
 	 * The octet string data type contains data in an application-defined
 	 * format, not defined in this specification.
 	 * 
@@ -223,6 +230,7 @@ public interface ZigBeeDataTypes {
 	 * according to the language and character set field of the complex
 	 * descriptor.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	CHARACTER_STRING		= 0x79;
@@ -230,9 +238,11 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.13 Long Octet String
 	 * 
+	 * <p>
 	 * The long octet string data type contains data in an application-defined
 	 * format, not defined in this specification.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	LONG_OCTET_STRING		= 0x7a;
@@ -240,22 +250,27 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.14 Long Character String
 	 * 
+	 * <p>
 	 * The long character string data type contains data octets encoding
 	 * characters according to the language and character set field of the
 	 * complex descriptor.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	LONG_CHARACTER_STRING	= 0x7b;
 
 	/**
+	 *
 	 * 2.5.2.15 Array
-	 * 
+	 *
+	 * <p>
 	 * An array is an ordered sequence of zero or more elements, all of the same
 	 * data type. This data type may be any ZCL defined data type, including
 	 * array, structure, bag or set. The total nesting depth is limited to 15,
 	 * and may be further limited by any relevant profile or application.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	ARRAY					= 0x10;
@@ -263,11 +278,13 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.16 Structure
 	 * 
+	 * <p>
 	 * A structure is an ordered sequence of elements, which may be of different
 	 * data types. Each data type may be any ZCL defined data type, including
 	 * array, structure, bag or set. The total nesting depth is limited to 15,
 	 * and may be further limited by any relevant profile or application.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	STRUCTURE				= 0x11;
@@ -275,11 +292,13 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.17 Set
 	 * 
+	 * <p>
 	 * A set is a collection of elements with no associated order. Each element
 	 * has the same data type, which may be any ZCL defined data type, including
 	 * array, structure, bag or set. The nesting depth is limited to 15, and may
 	 * be further limited by any relevant profile or application.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	SET						= 0x12;
@@ -287,6 +306,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.18 Bag
 	 * 
+	 * <p>
 	 * A bag behaves exactly the same as a set, except that the restriction that
 	 * no two elements may have the same value is removed.
 	 */
@@ -297,6 +317,7 @@ public interface ZigBeeDataTypes {
 	 * 
 	 * The Time of Day data type shall be formatted as illustrated in spec.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	TIME_OF_DAY				= 0x02;
@@ -304,8 +325,10 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.20 Date
 	 * 
+	 * <p>
 	 * The Time of day data type shall be formatted as illustrated in spec.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	DATE					= 0x03;
@@ -327,8 +350,10 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.22 Cluster ID
 	 * 
+	 * <p>
 	 * This type represents a cluster identifier as defined in spec.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	CLUSTER_ID				= 0x05;
@@ -336,8 +361,10 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.23 Attribute ID
 	 * 
+	 * <p>
 	 * This type represents an attribute identifier as defined in spec.
 	 * 
+	 * <p>
 	 * See reference on top of this class.
 	 */
 	public static final short	ATTRIBUTE_ID			= 0x06;
@@ -355,6 +382,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.25 IEEE Address
 	 * 
+	 * <p>
 	 * The IEEE Address data type is a 64-bit IEEE address that is unique to
 	 * every ZigBee device. A value of 0xffffffffffffffff indicates that the
 	 * address is unknown.
@@ -364,6 +392,7 @@ public interface ZigBeeDataTypes {
 	/**
 	 * 2.5.2.26 128-bit Security Key
 	 * 
+	 * <p>
 	 * The 128-bit Security Key data type is for use in ZigBee security, and may
 	 * take any 128-bit value.
 	 */
