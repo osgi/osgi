1e259300b3e4ffa17b40277c4e15044eb1f765ae
BJ Hargrave <hargrave@us.ibm.com>
2016-10-20 17:54:37 -0400

    ds: Add new standard set of component property types for service props

---
+++ org.osgi.service.component/src/org/osgi/service/component/propertytypes/ServiceRanking.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) OSGi Alliance (2016). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osgi.service.component.propertytypes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ComponentPropertyType;
+
+/**
+ * Component Property Type for the {@code service.ranking} service property.
+ * <p>
+ * This annotation can be used on a {@link Component} to declare the value of
+ * the {@link Constants#SERVICE_RANKING} service property.
+ * 
+ * @see "Component Property Types"
+ * @author $Id$
+ * @since 1.4
+ */
+@ComponentPropertyType
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface ServiceRanking {
+	/**
+	 * Service property identifying a service's ranking.
+	 * 
+	 * @return The service ranking.
+	 * @see Constants#SERVICE_RANKING
+	 */
+	int service_ranking();
+}
---
+++ org.osgi.service.component/src/org/osgi/service/component/propertytypes/package-info.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) OSGi Alliance (2016). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Component Property Types Package Version 1.4.
+ * <p>
+ * When used as annotations, component property types are processed by tools to
+ * generate Component Descriptions which are used at runtime.
+ * <p>
+ * Bundles wishing to use this package at runtime must list the package in the
+ * Import-Package header of the bundle's manifest.
+ * <p>
+ * Example import for consumers using the API in this package:
+ * <p>
+ * {@code  Import-Package: org.osgi.service.component.propertytypes; version="[1.4,2.0)"}
+ *
+ * @author $Id$
+ */
+
+@Version(COMPONENT_SPECIFICATION_VERSION)
+package org.osgi.service.component.propertytypes;
+
+import static org.osgi.service.component.ComponentConstants.COMPONENT_SPECIFICATION_VERSION;
+
+import org.osgi.annotation.versioning.Version;
+
