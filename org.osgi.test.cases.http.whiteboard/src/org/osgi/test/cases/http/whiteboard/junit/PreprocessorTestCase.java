78751a3d95cf4ba55571afe6a5b8bd08ee94cd9e
Carsten Ziegeler <cziegele@adobe.com>
2017-04-24 18:23:05 +0200

    [http whiteboard] Added more tests for RFC 223

---
+++ org.osgi.test.cases.http.whiteboard/src/org/osgi/test/cases/http/whiteboard/junit/PreprocessorTestCase.java
@@ -0,0 +1,103 @@
+package org.osgi.test.cases.http.whiteboard.junit;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.Servlet;
+
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.http.runtime.dto.PreprocessorDTO;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.osgi.service.http.whiteboard.Preprocessor;
+import org.osgi.test.cases.http.whiteboard.junit.mock.MockPreprocessor;
+import org.osgi.test.cases.http.whiteboard.junit.mock.MockServlet;
+
+/**
+ * Preprocessors are introduced in 1.1
+ */
+public class PreprocessorTestCase extends BaseHttpWhiteboardTestCase {
+
+	public void testPreprocessorInitParameters() throws Exception {
+		Dictionary<String,Object> properties = new Hashtable<>();
+		properties
+				.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_PREPROCESSOR_INIT_PARAM_PREFIX
+						+ "param1", "value1");
+		properties
+				.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_PREPROCESSOR_INIT_PARAM_PREFIX
+						+ "param2", "value2");
+		properties
+				.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_PREPROCESSOR_INIT_PARAM_PREFIX
+						+ "param3", 345l);
+
+		long before = this.getHttpRuntimeChangeCount();
+		final ServiceRegistration<Preprocessor> reg = getContext()
+				.registerService(Preprocessor.class, new MockPreprocessor(),
+						properties);
+		this.serviceRegistrations.add(reg);
+		this.waitForRegistration(before);
+
+		final PreprocessorDTO[] dtos = this.getHttpServiceRuntime()
+				.getRuntimeDTO().preprocessorDTOs;
+		assertEquals(1, dtos.length);
+
+		assertTrue(dtos[0].initParams.containsKey("param1"));
+		assertTrue(dtos[0].initParams.containsKey("param2"));
+		assertFalse(dtos[0].initParams.containsKey("param3"));
+		assertEquals(getServiceId(reg), dtos[0].serviceId);
+	}
+
+	public void testPreprocessorRanging() throws Exception {
+		// register preprocessor with ranking -5
+		Dictionary<String,Object> properties = new Hashtable<>();
+		properties.put(Constants.SERVICE_RANKING, -5);
+
+		long before = this.getHttpRuntimeChangeCount();
+		this.serviceRegistrations
+				.add(getContext().registerService(Preprocessor.class.getName(),
+						new MockPreprocessor().around("d"), properties));
+		before = this.waitForRegistration(before);
+
+		// register preprocessor with ranking 8
+		properties = new Hashtable<>();
+		properties.put(Constants.SERVICE_RANKING, 8);
+
+		this.serviceRegistrations
+				.add(getContext().registerService(Preprocessor.class.getName(),
+						new MockPreprocessor().around("a"), properties));
+		before = this.waitForRegistration(before);
+
+		// register preprocessor with invalid ranking
+		properties = new Hashtable<>();
+		properties.put(Constants.SERVICE_RANKING, 3L); // this is invalid ->
+														// ranking = 0
+
+		this.serviceRegistrations
+				.add(getContext().registerService(Preprocessor.class.getName(),
+						new MockPreprocessor().around("b"), properties));
+		before = this.waitForRegistration(before);
+
+		// register preprocessor with no ranking
+		properties = new Hashtable<>();
+
+		this.serviceRegistrations
+				.add(getContext().registerService(Preprocessor.class.getName(),
+						new MockPreprocessor().around("c"), properties));
+		before = this.waitForRegistration(before);
+
+		// check that we have four preprocessors
+		final PreprocessorDTO[] dtos = this.getHttpServiceRuntime()
+				.getRuntimeDTO().preprocessorDTOs;
+		assertEquals(4, dtos.length);
+
+		// register endpoint
+		final String path = "/available";
+		properties = new Hashtable<>();
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN,
+				path);
+		this.serviceRegistrations.add(getContext().registerService(
+				Servlet.class, new MockServlet().content("hello"), properties));
+
+		assertEquals("abcdhellodcba", request(path));
+	}
+}
---
+++ org.osgi.test.cases.http.whiteboard/src/org/osgi/test/cases/http/whiteboard/junit/mock/MockPreprocessor.java
@@ -0,0 +1,7 @@
+package org.osgi.test.cases.http.whiteboard.junit.mock;
+
+import org.osgi.service.http.whiteboard.Preprocessor;
+
+public class MockPreprocessor extends MockFilter implements Preprocessor {
+
+}
