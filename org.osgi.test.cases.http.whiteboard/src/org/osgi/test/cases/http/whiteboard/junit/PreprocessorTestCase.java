67fb6ae545f1c6e749df2f8b42b27079e5c72caa
Carsten Ziegeler <cziegele@adobe.com>
2017-04-25 08:22:43 +0200

    [http whiteboard] Finish tests for RFC 223

--- org.osgi.test.cases.http.whiteboard/src/org/osgi/test/cases/http/whiteboard/junit/PreprocessorTestCase.java
+++ org.osgi.test.cases.http.whiteboard/src/org/osgi/test/cases/http/whiteboard/junit/PreprocessorTestCase.java
@@ -1,9 +1,18 @@
 package org.osgi.test.cases.http.whiteboard.junit;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
 
+import javax.servlet.FilterChain;
 import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
@@ -99,5 +108,82 @@ public class PreprocessorTestCase extends BaseHttpWhiteboardTestCase {
 				Servlet.class, new MockServlet().content("hello"), properties));
 
 		assertEquals("abcdhellodcba", request(path));
+	}
+
+	/**
+	 * Test a request with a servlet registered at that url and check if the
+	 * preprocessor is invoked. Do the same with a non existing url.
+	 */
+	public void testPreprocessorInvocation() throws Exception {
+		// register preprocessor
+		final List<String> filterActions = new ArrayList<>();
+		long before = this.getHttpRuntimeChangeCount();
+		this.serviceRegistrations.add(getContext().registerService(
+				Preprocessor.class.getName(), new MockPreprocessor() {
+
+					@Override
+					public void doFilter(ServletRequest request,
+							ServletResponse response, FilterChain chain)
+							throws IOException, ServletException {
+						filterActions.add("a");
+						super.doFilter(request, new HttpServletResponseWrapper(
+								(HttpServletResponse) response) {
+
+							private boolean hasStatus = false;
+
+							private void addStatus(final int sc) {
+								if (!hasStatus) {
+									hasStatus = true;
+									filterActions.add(String.valueOf(sc));
+								}
+							}
+
+							@Override
+							public void setStatus(int sc) {
+								addStatus(sc);
+								super.setStatus(sc);
+							}
+
+							@Override
+							public void sendError(int sc, String msg)
+									throws IOException {
+								addStatus(sc);
+								super.sendError(sc, msg);
+							}
+
+							@Override
+							public void sendError(int sc) throws IOException {
+								addStatus(sc);
+								super.sendError(sc);
+							}
+
+						}, chain);
+						filterActions.add("b");
+					}
+
+				}, null));
+		before = this.waitForRegistration(before);
+
+		// register endpoint
+		final String path = "/available";
+		Dictionary<String,Object> properties = new Hashtable<>();
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN,
+				path);
+		this.serviceRegistrations.add(getContext().registerService(
+				Servlet.class, new MockServlet().content("hello"), properties));
+
+		assertEquals("hello", request(path));
+		assertEquals(2, filterActions.size());
+		assertEquals("a", filterActions.get(0));
+		assertEquals("b", filterActions.get(1));
+
+		// request a non existing pattern - this will somehow set the status
+		// code to 404
+		filterActions.clear();
+		request("/foo");
+		assertEquals(3, filterActions.size());
+		assertEquals("a", filterActions.get(0));
+		assertEquals("404", filterActions.get(1));
+		assertEquals("b", filterActions.get(2));
 	}
 }
