6d7fd9b9da1390c20a20afdbcdd103906614eb32
BJ Hargrave <hargrave@us.ibm.com>
2017-09-06 11:20:46 -0400

    tx control: Remove stray javadoc tag

--- org.osgi.service.transaction.control/src/org/osgi/service/transaction/control/TransactionControl.java
+++ org.osgi.service.transaction.control/src/org/osgi/service/transaction/control/TransactionControl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2016). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2016, 2017). All Rights Reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ public interface TransactionControl extends TransactionStarter {
 	/**
 	 * Build a transaction context to surround a piece of transactional work
 	 * 
-	 * @param propagation The transaction propagation to use
 	 * @return A builder to complete the creation of the transaction
 	 */
 	TransactionBuilder build();
