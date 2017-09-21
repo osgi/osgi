90060fd8ceb23c9833414f3442ad449cec295682
David Bosschaert <bosschae@adobe.com>
2017-09-21 12:35:48 +0200

    Initial configurator test case

--- org.osgi.test.cases.configurator/src/org/osgi/test/cases/configurator/junit/ClusterInfoTestCase.java
+++ org.osgi.test.cases.configurator/src/org/osgi/test/cases/configurator/junit/ClusterInfoTestCase.java
@@ -16,11 +16,54 @@
 
 package org.osgi.test.cases.configurator.junit;
 
+import java.io.IOException;
+import java.util.Dictionary;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.test.support.OSGiTestCase;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class ClusterInfoTestCase extends OSGiTestCase {
-	public void testDummy() {
-		
+	private ConfigurationAdmin configAdmin;
+	private ServiceTracker<ConfigurationAdmin,ConfigurationAdmin> configAdminTracker;
+
+	public void setUp() throws Exception {
+		configAdminTracker = new ServiceTracker<>(
+				getContext(), ConfigurationAdmin.class, null);
+		configAdminTracker.open();
+		configAdmin = configAdminTracker.waitForService(5000);
+	}
+
+	public void tearDown() {
+		configAdminTracker.close();
+	}
+
+	public void testBasicConfigurationLifecycle() throws Exception {
+		assertNull("Precondition, should not yet have the test config",
+				readConfig("org.osgi.test.pid1"));
+
+		Bundle tb1 = install("tb1.jar");
+		System.out.println("*** Bundle state: " + tb1.getState());
+		tb1.start();
+		System.out.println("*** Bundle state: " + tb1.getState());
+		sleep(500); // TODO use notification
+		System.out.println("*** Slept for some time");
+		Configuration cfg = readConfig("org.osgi.test.pid1");
+		Dictionary<String,Object> props = cfg.getProperties();
+		assertEquals("bar", props.get("foo"));
+	}
+
+	private Configuration readConfig(String pid)
+			throws IOException, InvalidSyntaxException {
+		Configuration[] configs = configAdmin.listConfigurations(
+						"(" + Constants.SERVICE_PID + "=" + pid + ")");
+		if (configs == null)
+			return null;
+		return configs[0];
 	}
 }
 
