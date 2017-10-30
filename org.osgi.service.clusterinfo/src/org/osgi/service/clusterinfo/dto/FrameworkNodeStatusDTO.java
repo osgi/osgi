e001a9bb9454d5d566fd01309be31b6f834dfbd5
Tim Verbelen <tim.verbelen@ugent.be>
2017-10-30 13:40:45 +0100

    [clusterinfo] create separate dto package

--- org.osgi.service.clusterinfo/src/org/osgi/service/clusterinfo/dto/FrameworkNodeStatusDTO.java
+++ org.osgi.service.clusterinfo/src/org/osgi/service/clusterinfo/dto/FrameworkNodeStatusDTO.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.osgi.service.clusterinfo;
+package org.osgi.service.clusterinfo.dto;
 
 /**
  * Data Transfer Object for a FrameworkNodeStatus Service.
--- org.osgi.service.clusterinfo/src/org/osgi/service/clusterinfo/dto/NodeStatusDTO.java
+++ org.osgi.service.clusterinfo/src/org/osgi/service/clusterinfo/dto/NodeStatusDTO.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.osgi.service.clusterinfo;
+package org.osgi.service.clusterinfo.dto;
 
 import org.osgi.dto.DTO;
 
