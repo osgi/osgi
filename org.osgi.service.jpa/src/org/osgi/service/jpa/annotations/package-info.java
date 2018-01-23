9b32aa6de24cf95fbb72b9a56e6dc77a6684e637
Tim Ward <tim.ward@paremus.com>
2018-01-23 14:32:39 -0500

    [JPA] Add the missing package-info.java

---
+++ org.osgi.service.jpa/src/org/osgi/service/jpa/annotations/package-info.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) OSGi Alliance (2018). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * JPA Service Annotations Package Version 1.1.
+ * <p>
+ * This package contains annotations that can be used to require the JPA Service
+ * implementation.
+ * <p>
+ * Bundles should not normally need to import this package as the annotations
+ * are only used at build-time.
+ * <p>
+ * 
+ * @author $Id$
+ */
+
+@Version(JPA_SPECIFICATION_VERSION)
+package org.osgi.service.jpa.annotations;
+
+import static org.osgi.service.jpa.EntityManagerFactoryBuilder.JPA_SPECIFICATION_VERSION;
+
+import org.osgi.annotation.versioning.Version;
+
