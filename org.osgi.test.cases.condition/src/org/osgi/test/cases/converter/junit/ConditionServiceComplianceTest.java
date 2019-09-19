be126c5f073fe557b486e828e0604b4a3ae12988
David Bosschaert <bosschae@adobe.com>
2019-09-19 10:16:46 +0300

    Initial test project for the condition spec

---
+++ org.osgi.test.cases.condition/src/org/osgi/test/cases/converter/junit/ConditionServiceComplianceTest.java
@@ -0,0 +1,9 @@
+package org.osgi.test.cases.converter.junit;
+
+import junit.framework.TestCase;
+
+public class ConditionServiceComplianceTest extends TestCase {
+	public void testConversionFromScalar() {
+		fail("boo");
+	}
+}
