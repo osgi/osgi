3f56d4eb92f69ae2308429b06d890d310d1a4df3
Hiroyuki Maeomichi <maeomichi.hiroyuki@lab.ntt.co.jp>
2020-02-03 16:46:17 +0900

    fix broken build
    fix bnd.bnd to follow package name change
    fix bnd.bnd to introduce annotation for use of version
    remove dead implementation code for HTTP and CoAP.

--- org.osgi.impl.service.onem2m/src/org/osgi/impl/service/onem2m/ServiceLayerFactoryImpl.java
+++ org.osgi.impl.service.onem2m/src/org/osgi/impl/service/onem2m/ServiceLayerFactoryImpl.java
@@ -53,105 +53,10 @@ public class ServiceLayerFactoryImpl implements ServiceFactory<ServiceLayer> {
 				|| ServiceLayerUtil.PROTOCOL_SERVICE.equals(property.get(ServiceLayerUtil.PROTOCOL).toLowerCase())){
 
 			ServiceReference<?> cseService = bundle.getBundleContext().getServiceReference(CseService.class.getName());
+			
 			CseService cse = (CseService) bundle.getBundleContext().getService(cseService);
 			sl = new ServiceLayreImplService(property.get(ServiceLayerUtil.ORIGIN), cse);
 			return sl;
-		}
-
-		//Serialize
-		BaseSerialize serialize = null;
-	
-		if(serialize == null){
-			LOGGER.warn(property.get(ServiceLayerUtil.PROTOCOL) + " & " + property.get(ServiceLayerUtil.SERIALIZE)
-			+ " is Unimplemented.");
-			return null;
-		}
-
-		//HTTP
-		if (ServiceLayerUtil.PROTOCOL_HTTP.equals(property.get(ServiceLayerUtil.PROTOCOL).toLowerCase())) {
-
-			// Create ServiceLayer Service
-			sl = new ServiceLayerImplHttp(property.get(ServiceLayerUtil.ORIGIN), serialize);
-
-			// get NotificationListener Service
-			Collection<ServiceReference<NotificationListener>> notificationListenerServices = null;
-			try {
-				notificationListenerServices = bundle.getBundleContext().getServiceReferences(
-						NotificationListener.class, "(" + ServiceLayerUtil.PROPERTY_KEY + "=" + bundleSymbolicName + ")");
-			} catch (InvalidSyntaxException e) {
-				LOGGER.warn("NotificationListener Acquisition failure.", e);
-				return null;
-			}
-			ServiceReference<NotificationListener> notificationListenerService = notificationListenerServices.iterator()
-					.next();
-			NotificationListener listener = bundle.getBundleContext().getService(notificationListenerService);
-
-			// get alias
-			URI uri = null;
-			try {
-				uri = new URI(property.get(ServiceLayerUtil.POA));
-			} catch (URISyntaxException e) {
-				LOGGER.warn("Property PoA is Not URI.", e);
-				return null;
-			}
-			String[] splitUri = (property.get(ServiceLayerUtil.POA)).split(uri.getRawAuthority());
-
-			try {
-				ServiceReference<?> sRef = bundle.getBundleContext().getServiceReference(HttpService.class.getName());
-				if (sRef != null) {
-
-					HttpService service = (HttpService) bundle.getBundleContext().getService(sRef);
-
-					NotificationServlet servlet = new NotificationServlet(listener, serialize);
-
-					service.registerServlet(splitUri[1], servlet, null, service.createDefaultHttpContext());
-				} else {
-					LOGGER.warn("HttpService is None.");
-				}
-			} catch (Exception e) {
-				LOGGER.warn("ServletRegist failed.", e);
-			}
-		}
-
-		//COAP
-		if (ServiceLayerUtil.PROTOCOL_COAP.equals(property.get(ServiceLayerUtil.PROTOCOL).toLowerCase())) {
-
-			// Create ServiceLayer Service
-			sl = new ServiceLayerImplCoap(property.get(ServiceLayerUtil.ORIGIN), serialize);
-
-			// Create CoAPServer
-			URI uri = null;
-			try {
-				uri = new URI(property.get(ServiceLayerUtil.POA));
-			} catch (URISyntaxException e) {
-				LOGGER.warn("Property PoA is Not URI.", e);
-				return null;
-			}
-
-			CoapServer server = new CoapServer();
-			for (InetAddress addr : EndpointManager.getEndpointManager()
-					.getNetworkInterfaces()) {
-				if (addr instanceof Inet4Address || addr.isLoopbackAddress()) {
-					InetSocketAddress bindToAddress = new InetSocketAddress(addr,uri.getPort());
-					server.addEndpoint(new CoapEndpoint(bindToAddress));
-				}
-			}
-
-			// get NotificationListener Service
-			Collection<ServiceReference<NotificationListener>> notificationListenerServices = null;
-			try {
-				notificationListenerServices = bundle.getBundleContext().getServiceReferences(
-						NotificationListener.class, "(" + ServiceLayerUtil.PROPERTY_KEY + "=" + bundleSymbolicName + ")");
-			} catch (InvalidSyntaxException e) {
-				LOGGER.warn("NotificationListener Acquisition failure.", e);
-				return null;
-			}
-			ServiceReference<NotificationListener> notificationListenerService = notificationListenerServices.iterator()
-					.next();
-			NotificationListener listener = bundle.getBundleContext().getService(notificationListenerService);
-
-			// set Deliverer
-			server.setMessageDeliverer(new ServiceLayreDeliver(bundleSymbolicName, listener));
 		}
 
 		if(sl == null){
