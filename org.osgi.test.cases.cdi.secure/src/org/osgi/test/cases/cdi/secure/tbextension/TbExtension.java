944afa40fcb8333ec1238e2e318f55128a14cf5e
Raymond Augé <raymond.auge@liferay.com>
2020-05-16 10:33:08 -0400

    DS is not used in the CDI tests anymore
    Signed-off-by: Raymond Auge <raymond.auge@liferay.com>

--- org.osgi.test.cases.cdi.secure/src/org/osgi/test/cases/cdi/secure/tbextension/TbExtension.java
+++ org.osgi.test.cases.cdi.secure/src/org/osgi/test/cases/cdi/secure/tbextension/TbExtension.java
@@ -8,14 +8,22 @@ import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.util.AnnotationLiteral;
 
 import org.osgi.annotation.bundle.Capability;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.annotation.bundle.Header;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.PrototypeServiceFactory;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.test.support.dictionary.Dictionaries;
+import org.osgi.test.support.map.Maps;
 
-@Component(property = {
-		"osgi.cdi.extension=tb.extension"
-}, scope = ServiceScope.PROTOTYPE)
 @Capability(namespace = "osgi.cdi.extension", name = "tb.extension")
-public class TbExtension implements Extension {
+@Header(name = Constants.BUNDLE_ACTIVATOR, value = "${@class}")
+public class TbExtension implements BundleActivator, Extension,
+		PrototypeServiceFactory<Extension> {
+
+	private ServiceRegistration<Extension> registration;
 
 	@SuppressWarnings("serial")
 	void afterBeanDiscovery(@Observes AfterBeanDiscovery abd) {
@@ -27,4 +35,30 @@ public class TbExtension implements Extension {
 				})
 				.produceWith(i -> whenExtensionCalled);
 	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		registration = context.registerService(Extension.class,
+				(PrototypeServiceFactory<Extension>) this, Dictionaries
+						.asDictionary(
+						Maps.mapOf("osgi.cdi.extension", "tb.extension")));
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		if (registration != null) {
+			registration.unregister();
+		}
+
+	}
+
+	public Extension getService(Bundle bundle,
+			ServiceRegistration<Extension> registration) {
+		return this;
+	}
+
+	public void ungetService(Bundle bundle,
+			ServiceRegistration<Extension> registration, Extension service) {
+	}
+
 }
