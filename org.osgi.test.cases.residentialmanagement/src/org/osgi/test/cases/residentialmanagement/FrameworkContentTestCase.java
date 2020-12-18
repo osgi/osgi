807a297cd529ef2511148e406e71a1bec991359c
BJ Hargrave <hargrave@us.ibm.com>
2020-12-18 09:39:51 -0500

    source: Add newline at end of source file
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- org.osgi.test.cases.residentialmanagement/src/org/osgi/test/cases/residentialmanagement/FrameworkContentTestCase.java
+++ org.osgi.test.cases.residentialmanagement/src/org/osgi/test/cases/residentialmanagement/FrameworkContentTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) OSGi Alliance (2000, 2015). All Rights Reserved.
+ * Copyright (c) OSGi Alliance (2000, 2020). All Rights Reserved.
  *
  * Implementation of certain elements of the OSGi
  * Specification may be subject to third party intellectual property
