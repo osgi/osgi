0effc52d4a4ce09fa8a4fd9ba1e31f47697a4391
BJ Hargrave <hargrave@us.ibm.com>
2021-03-05 18:16:13 -0500

    build: Update/add copyright notices
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- org.osgi.test.cases.framework/resources/div.tb2/solaris_sparc/NativeCodeI.c
+++ org.osgi.test.cases.framework/resources/div.tb2/solaris_sparc/NativeCodeI.c
@@ -1,14 +1,20 @@
-/**
- * Copyright (c) 2000 Sun Microsystems, Inc. All Rights Reserved.
- * 
- * Sun Microsystems, Inc. grants Open Services Gateway Initiative (OSGi)
- * an irrevocable, perpetual, non-exclusive, worldwide, paid-up right and
- * license to reproduce, display, perform, prepare and have prepared
- * derivative works based upon and distribute and sublicense this material
- * and derivative works thereof as set out in the OSGi MEMBER AGREEMENT as
- * of January 24 2000, for use in accordance with Section 2.2 of the BY-LAWS
- * of the OSGi MEMBER AGREEMENT.
- */
+/*******************************************************************************
+ * Copyright (c) Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0 
+ *******************************************************************************/
 
 #include <jni.h>
 #include "../NativeCode.h"
