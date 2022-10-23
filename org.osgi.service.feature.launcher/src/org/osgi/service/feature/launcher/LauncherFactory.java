a65bdff1fa2e67560ab5c77b4d3d5995eda3e833
David Bosschaert <bosschae@adobe.com>
2022-10-23 20:29:20 +0100

    Editorial changes

--- org.osgi.service.feature.launcher/src/org/osgi/service/feature/launcher/LauncherFactory.java
+++ org.osgi.service.feature.launcher/src/org/osgi/service/feature/launcher/LauncherFactory.java
@@ -1,6 +1,7 @@
 package org.osgi.service.feature.launcher;
 
 import java.net.URL;
+import java.util.Map;
 
 import org.osgi.service.feature.Feature;
 
@@ -12,15 +13,17 @@ public interface LauncherFactory {
 	 * Create a new launcher based on the provided URLs.
 	 * 
 	 * @param feature URL to the Feature file.
+	 * @param variables The feature variables to use.
 	 * @return the new launcher;
 	 */
-	Launcher newLauncher(URL feature);
+	Launcher newLauncher(URL feature, Map<String,Object> variables);
 
 	/**
 	 * Create a new launcher based on the provided Feature instances;
 	 * 
 	 * @param feature The feature the launcher should use.
+	 * @param variables The feature variables to use.
 	 * @return the new launcher.
 	 */
-	Launcher newLauncher(Feature... feature);
+	Launcher newLauncher(Feature feature, Map<String,Object> variables);
 }
