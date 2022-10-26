73171fa6c9fa2c8e852e52e8be84604ee57ac34a
BJ Hargrave <hargrave@us.ibm.com>
2022-10-26 11:34:26 +0200

    jdbc: Clean up documentation for the JDBC changes
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- org.osgi.service.jdbc/src/org/osgi/service/jdbc/package-info.java
+++ org.osgi.service.jdbc/src/org/osgi/service/jdbc/package-info.java
@@ -17,22 +17,20 @@
  *******************************************************************************/
 
 /**
- * JDBC Service Package Version 1.0.
- * 
+ * JDBC Service Package Version 1.1.
  * <p>
  * Bundles wishing to use this package must list the package in the
  * Import-Package header of the bundle's manifest. This package has two types of
  * users: the consumers that use the API in this package and the providers that
  * implement the API in this package.
- * 
  * <p>
  * Example import for consumers using the API in this package:
  * <p>
- * {@code  Import-Package: org.osgi.service.jdbc; version="[1.0,2.0)"}
+ * {@code  Import-Package: org.osgi.service.jdbc; version="[1.1,2.0)"}
  * <p>
  * Example import for providers implementing the API in this package:
  * <p>
- * {@code  Import-Package: org.osgi.service.jdbc; version="[1.0,1.1)"}
+ * {@code  Import-Package: org.osgi.service.jdbc; version="[1.1,1.2)"}
  * 
  * @author $Id$
  */
