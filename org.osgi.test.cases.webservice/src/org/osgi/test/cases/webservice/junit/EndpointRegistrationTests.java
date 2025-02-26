d8ae77b70957f9f94d9bab18390b9b1a07d7e0fd
Tim Ward <timothyjward@apache.org>
2025-02-26 15:20:46 +0000

    Update the tests to rely on changecount for validation
    This tests the RuntimeDTO and changecount behaviour, as well as the endpoints and handers
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- org.osgi.test.cases.webservice/src/org/osgi/test/cases/webservice/junit/EndpointRegistrationTests.java
+++ org.osgi.test.cases.webservice/src/org/osgi/test/cases/webservice/junit/EndpointRegistrationTests.java
@@ -18,29 +18,54 @@
 package org.osgi.test.cases.webservice.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.osgi.framework.Constants.SERVICE_ID;
+import static org.osgi.service.webservice.runtime.dto.FailedDTO.FAILURE_REASON_SERVICE_NOT_GETTABLE;
 
 import java.util.Hashtable;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceFactory;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.webservice.runtime.WebserviceServiceRuntime;
 import org.osgi.service.webservice.runtime.dto.EndpointDTO;
-import org.osgi.service.webservice.runtime.dto.RuntimeDTO;
+import org.osgi.service.webservice.runtime.dto.FailedEndpointDTO;
 import org.osgi.service.webservice.whiteboard.WebserviceWhiteboardConstants;
 import org.osgi.test.cases.webservice.webservices.WSEcho;
 import org.osgi.test.common.annotation.InjectBundleContext;
 import org.osgi.test.common.annotation.InjectService;
+import org.osgi.test.common.service.ServiceAware;
 import org.osgi.test.support.map.Maps;
 
 public class EndpointRegistrationTests {
 	
 	private static final String ECHO_URI = "http://127.0.0.1:56780/echo";
 
+	@InjectBundleContext 
+	BundleContext ctx;
+
+	private WebServiceRuntimeTracker webServiceRuntimeTracker;
+	
+	@BeforeEach
+	void setup(@InjectService ServiceAware<WebserviceServiceRuntime> runtime)
+			throws InterruptedException {
+		webServiceRuntimeTracker = new WebServiceRuntimeTracker(ctx, runtime.getServiceReference());
+		webServiceRuntimeTracker.waitForQuiet(1, 5, TimeUnit.SECONDS);
+		assertTrue(webServiceRuntimeTracker.hasChanged(), "No Change Count detected for the WebserviceServiceRuntime");
+	}
+	
+	@AfterEach
+	void shutdown() {
+		webServiceRuntimeTracker.close();
+	}
+	
 	/**
 	 * A basic test that registers an endpoint as described in 161.1.3
 	 * 
@@ -50,42 +75,43 @@ public class EndpointRegistrationTests {
 	 */
 	@ParameterizedTest
 	@ValueSource(classes = {Object.class, WSEcho.class})
-	public void testSimpleWebserviceEndpointRegistration(Class<?> registrationType,
-			@InjectBundleContext BundleContext ctx,
-			@InjectService WebserviceServiceRuntime runtime) throws Exception {
-
-		Hashtable<String, Object> props = new Hashtable<>();
-		props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_IMPLEMENTOR, Boolean.TRUE);
-		props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_ADDRESS, ECHO_URI);
+	public void testSimpleWebserviceEndpointRegistration(
+			Class<?> registrationType) throws Exception {
 		
-		ServiceRegistration<?> reg = ctx.registerService(registrationType.getName(),
-				new WSEcho(), props);
-		
-		// TODO improve this when it is specced
-		boolean found = false;
-		for(int i = 0; i < 30; i++) {
-			RuntimeDTO dto = runtime.getRuntimeDTO();
-			for(EndpointDTO e : dto.endpoints) {
-				if(reg.getReference().getProperty(SERVICE_ID).equals(e.implementor.id)) {
-					found = true;
-					break;
-				}
-			}
-			Thread.sleep(200);
-		}
-		
-		assertTrue(found);
+		webServiceRuntimeTracker.waitForChange(
+				() -> {
+					Hashtable<String, Object> props = new Hashtable<>();
+					props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_IMPLEMENTOR, Boolean.TRUE);
+					props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_ADDRESS, ECHO_URI);
+					return ctx.registerService(registrationType.getName(), new WSEcho(), props);
+				},
+				(reg,dto) -> {
+					for(EndpointDTO e : dto.endpoints) {
+						if(getServiceId(reg).equals(e.implementor.id)) {
+							return true;
+						}
+					}
+					return false;
+				},
+				(reg,dto) -> "The endpoint with id " + getServiceId(reg) + " was not present in the DTO endpoints " + dto,
+				6, TimeUnit.SECONDS);
 		
 		String soapRequest = WebServiceHelper.createSOAPMessage(WSEcho.ECHO_NS, "echoAction", Maps.mapOf("textIn", "BANG"));
 		
 		String soapResponse = WebServiceHelper.getSoapResponse(ECHO_URI, soapRequest);
 		
-		assertEquals("BANG", WebServiceHelper.extractResponse("echoActionResponse", soapResponse));
+		assertEquals("BANG", WebServiceHelper.extractResponse("echoActionResponse", soapResponse),
+				"Received an incorrect response from the Web Service");
+	}
+
+	static Object getServiceId(ServiceRegistration<?> reg) {
+		return reg.getReference().getProperty(SERVICE_ID);
 	}
 
 	/**
 	 * A basic test that fails to register an endpoint due
-	 * to a missing property, as described in 161.1.3
+	 * to an incorrect or missing property, as described in 
+	 * 161.1.3
 	 * 
 	 * It should not matter what type is used to register
 	 * 
@@ -93,30 +119,51 @@ public class EndpointRegistrationTests {
 	 */
 	@ParameterizedTest
 	@ValueSource(strings = {WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_IMPLEMENTOR, "wrongProp"})
-	public void testWrongServiceProp(String propName,
-			@InjectBundleContext BundleContext ctx,
-			@InjectService WebserviceServiceRuntime runtime) throws Exception {
+	public void testWrongServiceProp(String propName) throws Exception {
 		
-		Hashtable<String, Object> props = new Hashtable<>();
-		props.put(propName, Boolean.FALSE);
-		props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_ADDRESS, ECHO_URI);
+		webServiceRuntimeTracker.waitForNoChange(
+				() -> {
+					Hashtable<String, Object> props = new Hashtable<>();
+					props.put(propName, Boolean.FALSE);
+					props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_ADDRESS, ECHO_URI);
+					return ctx.registerService(WSEcho.class, new WSEcho(), props);
+				},
+				6, TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void testNotGettableEndpoint() throws Exception {
 		
-		ServiceRegistration<?> reg = ctx.registerService(WSEcho.class,
-				new WSEcho(), props);
-		
-		// TODO improve this when it is specced
-		boolean found = false;
-		for(int i = 0; i < 30; i++) {
-			RuntimeDTO dto = runtime.getRuntimeDTO();
-			for(EndpointDTO e : dto.endpoints) {
-				if(reg.getReference().getProperty(SERVICE_ID).equals(e.implementor.id)) {
-					found = true;
-					break;
-				}
+		ServiceFactory<Object> failingFactory = new ServiceFactory<Object>() {
+			
+			@Override
+			public void ungetService(Bundle bundle, ServiceRegistration<Object> registration, Object service) {
 			}
-			Thread.sleep(200);
-		}
+			
+			@Override
+			public Object getService(Bundle bundle, ServiceRegistration<Object> registration) {
+				return null;
+			}
+		};
 		
-		assertFalse(found);
+		webServiceRuntimeTracker.waitForChange(
+				() -> {
+					Hashtable<String, Object> props = new Hashtable<>();
+					props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_IMPLEMENTOR, Boolean.TRUE);
+					props.put(WebserviceWhiteboardConstants.WEBSERVICE_ENDPOINT_ADDRESS, ECHO_URI);
+					return ctx.registerService(WSEcho.class.getName(), failingFactory, props);
+				},
+				(reg,dto) -> {
+					for(FailedEndpointDTO e : dto.failedEndpoints) {
+						if(getServiceId(reg).equals(e.implementor.id)) {
+							assertEquals(FAILURE_REASON_SERVICE_NOT_GETTABLE, e.failureCode, "Endpoint service should not be gettable");
+							return true;
+						}
+					}
+					return false;
+				},
+				(reg,dto) -> "The endpoint with id " + getServiceId(reg) + " was not present in the failed DTO endpoints with code " + 
+				FAILURE_REASON_SERVICE_NOT_GETTABLE + ": " + dto,
+				6, TimeUnit.SECONDS);
 	}
 }
