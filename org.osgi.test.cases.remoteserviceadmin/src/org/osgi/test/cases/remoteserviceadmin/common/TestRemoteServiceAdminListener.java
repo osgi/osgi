5faa414672fbbb1c92bd85e83b19479f1b36f016
Amichai Rothman <amichai2@amichais.net>
2026-05-16 23:53:43 +0300

    Fix event listener concurrency

--- org.osgi.test.cases.remoteserviceadmin/src/org/osgi/test/cases/remoteserviceadmin/common/TestRemoteServiceAdminListener.java
+++ org.osgi.test.cases.remoteserviceadmin/src/org/osgi/test/cases/remoteserviceadmin/common/TestRemoteServiceAdminListener.java
@@ -13,14 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * SPDX-License-Identifier: Apache-2.0 
+ * SPDX-License-Identifier: Apache-2.0
  *******************************************************************************/
 package org.osgi.test.cases.remoteserviceadmin.common;
 
 import java.util.LinkedList;
-import java.util.NoSuchElementException;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+import java.util.Queue;
 
 import org.osgi.service.remoteserviceadmin.RemoteServiceAdminEvent;
 import org.osgi.service.remoteserviceadmin.RemoteServiceAdminListener;
@@ -28,15 +26,14 @@ import org.osgi.service.remoteserviceadmin.RemoteServiceAdminListener;
 /**
  * RemoteServiceAdminListener implementation, which collects and returns the
  * received events in order.
- * 
+ *
  * @author <a href="mailto:tdiekman@tibco.com">Tim Diekmann</a>
- * 
+ *
  */
 public class TestRemoteServiceAdminListener implements
 		RemoteServiceAdminListener {
-	private LinkedList<RemoteServiceAdminEvent> eventlist = new LinkedList<RemoteServiceAdminEvent>();
-	private Semaphore sem = new Semaphore(0);
-	private long timeout;
+	private final Queue<RemoteServiceAdminEvent> events = new LinkedList<RemoteServiceAdminEvent>();
+	private final long timeout;
 
 	public TestRemoteServiceAdminListener(long timeout) {
 		this.timeout = timeout;
@@ -47,25 +44,36 @@ public class TestRemoteServiceAdminListener implements
 	 */
 	@Override
 	public void remoteAdminEvent(final RemoteServiceAdminEvent event) {
-		eventlist.add(event);
-		sem.release();
+		synchronized (events) {
+			events.offer(event);
+			events.notify();
+		}
 	}
 
 	public RemoteServiceAdminEvent getNextEvent() {
 		try {
-			sem.tryAcquire(timeout, TimeUnit.MILLISECONDS);
+			long end = System.currentTimeMillis() + timeout;
+			while (true) {
+				synchronized (events) {
+					RemoteServiceAdminEvent event = events.poll();
+					if (event != null) {
+						return event;
+					}
+					long remaining = end - System.currentTimeMillis();
+					if (remaining <= 0) {
+						return null;
+					}
+					events.wait(remaining);
+				}
+			}
 		} catch (InterruptedException e1) {
-			return null;
-		}
-
-		try {
-			return eventlist.removeFirst();
-		} catch (NoSuchElementException e) {
 			return null;
 		}
 	}
 
 	public int getEventCount() {
-		return eventlist.size();
+		synchronized (events) {
+			return events.size();
+		}
 	}
 }
